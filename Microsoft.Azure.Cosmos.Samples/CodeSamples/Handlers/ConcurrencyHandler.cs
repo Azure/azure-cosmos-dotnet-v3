a21b6e934c9abd8693b6e25fb868f98018a6485d
REDMOND\gaausfel <gaausfel@microsoft.com>
2018-12-03 13:30:27 -0800

    Add handler samples

---
+++ Microsoft.Azure.Cosmos.Samples/CodeSamples/Handlers/ConcurrencyHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace Cosmos.Samples.Handlers
+{
+    /// <summary>
+    /// Handler that detects concurrency and etag issues
+    /// </summary>
+    class ConcurrencyHandler: CosmosRequestHandler
+    {
+        public override async Task<CosmosResponseMessage> SendAsync(
+            CosmosRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.PreconditionFailed)
+            {
+                response.Headers.Set("x-ms-substatus", "999");
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Microsoft.Azure.Cosmos.Samples/CodeSamples/Handlers/ThrottlingHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Polly;
+
+namespace Cosmos.Samples.Handlers
+{
+    /// <summary>
+    /// Using Polly to retry on Throttles.
+    /// </summary>
+    class ThrottlingHandler: CosmosRequestHandler
+    {
+        public override Task<CosmosResponseMessage> SendAsync(
+            CosmosRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            return Policy
+                .HandleResult<CosmosResponseMessage>(r => (int)r.StatusCode == 429)
+                .RetryAsync(3)
+                .ExecuteAsync(() => base.SendAsync(request, cancellationToken));
+        }
+    }
+}
