fa1ca6e096eef741cd2f3a9a11bd7223d959663a
Kiran Kumar Kolli <kirankk@microsoft.com>
2019-04-24 23:35:58 +0530

    Excluding fluent builder for now

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/CosmosContainerSettingsTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/CosmosContainerSettingsTests.cs
@@ -17,68 +17,6 @@ namespace Microsoft.Azure.Cosmos
     public class CosmosContainerSettingsTests
     {
         [TestMethod]
-        public void DefaultSerialization()
-        {
-            CosmosContainerSettings containerSettings =
-                new CosmosContainerSettings("TestContainer", "/partitionKey")
-                .WithIncludeIndexPath("/includepath1")
-                .WithIncludeIndexPath("/includepath2")
-                .WithExcludeIndexPath("/excludepath1")
-                .WithExcludeIndexPath("/excludepath2")
-                .WithCompositeIndex("/compPath1", "/compPath2")
-                .WithCompositeIndex(CompositePathDefinition.Create("/property1", CompositePathSortOrder.Descending),
-                    CompositePathDefinition.Create("/property2", CompositePathSortOrder.Descending))
-                .WithUniqueKey("/uniqueueKey1", "/uniqueueKey2")
-                .WithSpatialIndex("/spatialPath", SpatialType.Point);
-        }
-
-        [TestMethod]
-        public void TestDefaultTtl()
-        {
-            CosmosContainerSettings containerSettings =
-                new CosmosContainerSettings("TestContainer", "/partitionKey")
-                .WithDefaultTimeToLive(TimeSpan.FromHours(1));
-
-            Assert.AreEqual(containerSettings.DefaultTimeToLive, TimeSpan.FromHours(1).TotalSeconds);
-
-            containerSettings =
-                new CosmosContainerSettings("TestContainer", "/partitionKey")
-                .WithDefaultTimeToLive(defaulTtlInSeconds: -1);
-
-            Assert.AreEqual(-1, TimeSpan.FromHours(1).TotalSeconds);
-        }
-
-        [TestMethod]
-        public void V2Way()
-        {
-            IndexingPolicy ip = new IndexingPolicy();
-            ip.IncludedPaths.Add(new IncludedPath() { Path = "/includepath1", Indexes = CosmosContainerSettings.DefaultIndexes});
-            ip.ExcludedPaths.Add(new ExcludedPath() { Path = "/excludepath1" });
-
-            Collection<CompositePathDefinition> compositePath = new Collection<CompositePathDefinition>();
-            compositePath.Add(new CompositePathDefinition() { Path = "/compositepath1", Order = CompositePathSortOrder.Ascending });
-            compositePath.Add(new CompositePathDefinition() { Path = "/compositepath2", Order = CompositePathSortOrder.Ascending });
-            ip.CompositeIndexes.Add(compositePath);
-
-            SpatialIndexDefinition spatialSpec = new SpatialIndexDefinition();
-            spatialSpec.Path = "/spatialpath1";
-            spatialSpec.SpatialTypes = new Collection<SpatialType>();
-            spatialSpec.SpatialTypes.Add(SpatialType.Point);
-            ip.SpatialIndexes.Add(spatialSpec);
-
-            UniqueKeyPolicy uniqueKeyPolicy = new UniqueKeyPolicy();
-            UniqueKey uniqueKey = new UniqueKey();
-            uniqueKey.Paths = new Collection<string>();
-            uniqueKey.Paths.Add("/uniqueuekey1");
-            uniqueKey.Paths.Add("/uniqueuekey2");
-            uniqueKeyPolicy.UniqueKeys.Add(uniqueKey);
-
-            CosmosContainerSettings testContainerSettings = new CosmosContainerSettings("TestContainer", "/partitionKey");
-            testContainerSettings.IndexingPolicy = ip;
-            testContainerSettings.UniqueKeyPolicy = uniqueKeyPolicy;
-        }
-
-        [TestMethod]
         public void DefaultIncludesPopulated()
         {
             CosmosContainerSettings containerSettings = new CosmosContainerSettings("TestContainer", "/partitionKey");
@@ -158,37 +96,6 @@ namespace Microsoft.Azure.Cosmos
             Documents.IndexingPolicy ip = dc.IndexingPolicy;
 
             CosmosContainerSettingsTests.AssertSerializedPayloads(containerSettings, dc);
-        }
-
-        [TestMethod]
-        public void ValidationTests()
-        {
-            CosmosContainerSettings containerSettings =
-                new CosmosContainerSettings("TestContainer", "/partitionKey");
-
-            CosmosContainerSettingsTests.AssertException<ArgumentNullException>(() => containerSettings.WithIncludeIndexPath(null));
-            CosmosContainerSettingsTests.AssertException<ArgumentNullException>(() => containerSettings.WithIncludeIndexPath(string.Empty));
-
-            CosmosContainerSettingsTests.AssertException<ArgumentNullException>(() => containerSettings.WithExcludeIndexPath(null));
-            CosmosContainerSettingsTests.AssertException<ArgumentNullException>(() => containerSettings.WithExcludeIndexPath(string.Empty));
-
-            CosmosContainerSettingsTests.AssertException<ArgumentNullException>(() => containerSettings.WithSpatialIndex(null));
-            CosmosContainerSettingsTests.AssertException<ArgumentNullException>(() => CompositePathDefinition.Create("ABC", CompositePathSortOrder.Descending));
-
-            CosmosContainerSettingsTests.AssertException<ArgumentOutOfRangeException>(() => containerSettings.WithCompositeIndex(string.Empty));
-            CosmosContainerSettingsTests.AssertException<ArgumentOutOfRangeException>(() => containerSettings.WithCompositeIndex("abc", null));
-            CosmosContainerSettingsTests.AssertException<ArgumentOutOfRangeException>(() => containerSettings.WithCompositeIndex(null, CompositePathDefinition.Create("ABC", CompositePathSortOrder.Descending)));
-        }
-
-        public static void AssertException<T>(Action action) where T : Exception
-        {
-            try
-            {
-                action();
-            }
-            catch(T)
-            {
-            }
         }
 
         private static string SerializeDocumentCollection(DocumentCollection collection)
