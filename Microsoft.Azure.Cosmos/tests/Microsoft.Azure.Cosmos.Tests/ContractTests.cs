2587dc13277bb587c53b855035754badb7c63c6f
Matias Quaranta <ealsur@hotmail.com>
2019-05-22 11:23:29 -0700

    Adjusting namespace test

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/ContractTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/ContractTests.cs
@@ -31,7 +31,7 @@ namespace Microsoft.Azure.Cosmos
         [TestMethod]
         public void ClientDllNamespaceTest()
         {
-            ContractTests.NamespaceCountTest(typeof(CosmosClient), 3);
+            ContractTests.NamespaceCountTest(typeof(CosmosClient), 4);
         }
 
         private static void NamespaceCountTest(Type input, int expected)
