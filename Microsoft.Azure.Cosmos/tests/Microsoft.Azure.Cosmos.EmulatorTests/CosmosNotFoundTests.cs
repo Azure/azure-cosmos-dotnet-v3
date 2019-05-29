ecec9441c28821d99a992e3cb18e6e7c2398b0d1
Jake Willey <jawilley@microsoft.com>
2019-05-29 09:53:58 -0700

    Fix test

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/CosmosNotFoundTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/CosmosNotFoundTests.cs
@@ -57,7 +57,7 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
             var crossPartitionQueryIterator = container.Items.CreateItemQueryAsStream("select * from t where true", maxConcurrency: 2);
             var queryResponse = await crossPartitionQueryIterator.FetchNextSetAsync();
             Assert.IsNotNull(queryResponse);
-            Assert.AreEqual(HttpStatusCode.Gone, queryResponse.StatusCode);
+            Assert.AreEqual(HttpStatusCode.NotFound, queryResponse.StatusCode);
 
             var queryIterator = container.Items.CreateItemQueryAsStream("select * from t where true", maxConcurrency: 1, partitionKey: "testpk");
             this.VerifyQueryNotFoundResponse(await queryIterator.FetchNextSetAsync());
