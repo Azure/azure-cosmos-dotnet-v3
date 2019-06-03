243291f6bb9821a24dd4051e59d338c8313481d8
Matias Quaranta <ealsur@hotmail.com>
2019-06-03 12:21:04 -0700

    Making ChangeFeedProcessorOptions  public

--- Microsoft.Azure.Cosmos/src/ChangeFeedProcessor/Configuration/ChangeFeedProcessorOptions.cs
+++ Microsoft.Azure.Cosmos/src/ChangeFeedProcessor/Configuration/ChangeFeedProcessorOptions.cs
@@ -2,15 +2,16 @@
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 //------------------------------------------------------------
 
-namespace Microsoft.Azure.Cosmos.ChangeFeed.Configuration
+namespace Microsoft.Azure.Cosmos
 {
     using System;
-    using Microsoft.Azure.Cosmos;
+    using Microsoft.Azure.Cosmos.ChangeFeed;
+    using Microsoft.Azure.Cosmos.ChangeFeed.Configuration;
 
     /// <summary>
     /// Options to control various aspects of partition distribution happening within <see cref="ChangeFeedProcessorCore{T}"/> instance.
     /// </summary>
-    internal class ChangeFeedProcessorOptions
+    public class ChangeFeedProcessorOptions
     {
         private const int DefaultQueryPartitionsMaxBatchSize = 100;
         private static readonly TimeSpan DefaultFeedPollDelay = TimeSpan.FromSeconds(5);
@@ -30,27 +31,12 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.Configuration
         /// Applies only after a read on the change feed yielded no results.
         /// </remarks>
         /// </summary>
-        public TimeSpan FeedPollDelay { get; set; }
-
-        /// <summary>
-        /// Gets or sets the frequency how often to checkpoint leases.
-        /// </summary>
-        public CheckpointFrequency CheckpointFrequency { get; set; }
+        public virtual TimeSpan FeedPollDelay { get; set; }
 
         /// <summary>
         /// Gets or sets the maximum number of items to be returned in the enumeration operation in the Azure Cosmos DB service.
         /// </summary>
-        public int? MaxItemCount { get; set; }
-
-        /// <summary>
-        /// Gets or sets the start request continuation token to start looking for changes after.
-        /// </summary>
-        /// <remarks>
-        /// This is only used when lease store is not initialized and is ignored if a lease exists and has continuation token.
-        /// If this is specified, both StartTime and StartFromBeginning are ignored.
-        /// </remarks>
-        /// <seealso cref="ChangeFeedOptions.RequestContinuation"/>
-        public string StartContinuation { get; set; }
+        public virtual int? MaxItemCount { get; set; }
 
         /// <summary>
         /// Gets or sets the time (exclusive) to start looking for changes after.
@@ -62,7 +48,7 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.Configuration
         /// If this is specified, StartFromBeginning is ignored.
         /// </remarks>
         /// <seealso cref="ChangeFeedOptions.StartTime"/>
-        public DateTime? StartTime
+        public virtual DateTime? StartTime
         {
             get
             {
@@ -72,7 +58,10 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.Configuration
             set
             {
                 if (value.HasValue && value.Value.Kind == DateTimeKind.Unspecified)
+                {
                     throw new ArgumentException("StartTime cannot have DateTimeKind.Unspecified", nameof(value));
+                }
+
                 this.startTime = value;
             }
         }
@@ -88,7 +77,12 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.Configuration
         /// (3) StartTime is not specified.
         /// </remarks>
         /// <seealso cref="ChangeFeedOptions.StartFromBeginning"/>
-        public bool StartFromBeginning { get; set; }
+        public virtual bool StartFromBeginning { get; set; }
+
+        /// <summary>
+        /// Gets or sets the frequency how often to checkpoint leases.
+        /// </summary>
+        internal CheckpointFrequency CheckpointFrequency { get; set; }
 
         /// <summary>
         /// Gets or sets the session token for use with session consistency in the Azure Cosmos DB service.
@@ -99,5 +93,15 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.Configuration
         /// Gets or sets the Batch size of query API.
         /// </summary>
         internal int QueryFeedMaxBatchSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the start request continuation token to start looking for changes after.
+        /// </summary>
+        /// <remarks>
+        /// This is only used when lease store is not initialized and is ignored if a lease exists and has continuation token.
+        /// If this is specified, both StartTime and StartFromBeginning are ignored.
+        /// </remarks>
+        /// <seealso cref="ChangeFeedOptions.RequestContinuation"/>
+        internal string StartContinuation { get; set; }
     }
 }
