ddd9abfa34f3fb4f3bf7fc34a196265a9d875752
Matias Quaranta <ealsur@hotmail.com>
2019-06-07 14:38:02 -0700

    functions

--- Microsoft.Azure.Cosmos.Samples/CodeSamples/AzureFunctions/Startup.cs
+++ Microsoft.Azure.Cosmos.Samples/CodeSamples/AzureFunctions/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
