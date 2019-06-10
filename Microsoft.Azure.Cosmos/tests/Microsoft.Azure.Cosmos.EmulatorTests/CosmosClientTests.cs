510933d84864b29bab1b6a1395a8f2d4dda7b4c4
j82w <j82w@users.noreply.github.com>
2019-06-10 12:53:55 -0700

    Add support for account key as secure string (#401)
    * Add support for secure string
    
    * Fixed unit tests
    
    * Fixed changefeed tests

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/CosmosClientTests.cs
@@ -0,0 +1,33 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CosmosClientTests
+    {
+        [TestMethod]
+        public async Task ClientTest()
+        {
+            var clientBuilder = TestCommon.GetDefaultConfiguration();
+            using (CosmosClient client = clientBuilder.Build())
+            {
+                await client.GetAccountSettingsAsync();
+            }
+        }
+
+        [TestMethod]
+        public async Task ClientSecureStringTest()
+        {
+            var clientBuilder = TestCommon.GetDefaultSecureStringConfiguration();
+            using (CosmosClient client = clientBuilder.Build())
+            {
+                await client.GetAccountSettingsAsync();
+            }
+        }
+    }
+}
