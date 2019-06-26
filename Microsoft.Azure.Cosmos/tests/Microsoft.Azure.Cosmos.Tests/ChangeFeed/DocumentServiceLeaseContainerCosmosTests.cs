2125247a2c7c5f933d48f2be572eb4b01921200c
Jake Willey <jawilley@microsoft.com>
2019-06-26 12:14:27 -0700

    Fixed tests

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/ChangeFeed/DocumentServiceLeaseContainerCosmosTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/ChangeFeed/DocumentServiceLeaseContainerCosmosTests.cs
@@ -80,7 +80,7 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.Tests
             Mock<Container> mockedItems = new Mock<Container>();
             mockedItems.Setup(i => i.GetItemQueryIterator<DocumentServiceLeaseCore>(
                 // To make sure the SQL Query gets correctly created
-                It.Is<QueryDefinition>(value => ("SELECT * FROM c WHERE STARTSWITH(c.id, '" + DocumentServiceLeaseContainerCosmosTests.leaseStoreManagerSettings.GetPartitionLeasePrefix() + "')").Equals(value.ToSqlQuerySpec().QueryText)),
+                It.Is<string>(value => string.Equals("SELECT * FROM c WHERE STARTSWITH(c.id, '" + DocumentServiceLeaseContainerCosmosTests.leaseStoreManagerSettings.GetPartitionLeasePrefix() + "')", value)),
                 It.IsAny<string>(), 
                 It.IsAny<QueryRequestOptions>()))
                 .Returns(()=>
