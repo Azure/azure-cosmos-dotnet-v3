33ee8c54fbbd1abf245e1c700bc90cfad18c5ad2
Jahmai Lay <jahmai@criticalarc.com>
2019-07-10 20:28:27 +1000

    Can't do implicit conversion from XTags

--- Microsoft.Azure.Cosmos/src/Linq/Tags.cs
+++ Microsoft.Azure.Cosmos/src/Linq/Tags.cs
@@ -18,7 +18,7 @@ namespace Microsoft.Azure.Cosmos.Linq
         /// <param name="dataTags"></param>
         /// <param name="queryTags"></param>
         /// <returns>throws Exception</returns>
-        public static bool Match(IEnumerable<string> dataTags, IEnumerable<string> queryTags) => throw new Exception("Tags.Match is only for linq expressions");
+        public static bool Match(object dataTags, IEnumerable<string> queryTags) => throw new Exception("Tags.Match is only for linq expressions");
 
         /// <summary>
         /// Tag matching for LINQ
@@ -27,6 +27,6 @@ namespace Microsoft.Azure.Cosmos.Linq
         /// <param name="queryTags"></param>
         /// <param name="supportDocumentRequiredTags"></param>
         /// <returns>throws Exception</returns>
-        public static bool Match(IEnumerable<string> dataTags, IEnumerable<string> queryTags, bool supportDocumentRequiredTags) => throw new Exception("Tags.Match is only for linq expressions");
+        public static bool Match(object dataTags, IEnumerable<string> queryTags, bool supportDocumentRequiredTags) => throw new Exception("Tags.Match is only for linq expressions");
     }
 }
