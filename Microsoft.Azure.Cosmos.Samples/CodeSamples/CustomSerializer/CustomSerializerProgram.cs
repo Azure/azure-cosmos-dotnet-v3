6af9b81e2e093d214c4e874bdfeac6bd47ff1d8e
Jake Willey <jawilley@microsoft.com>
2019-07-16 12:48:37 -0700

    Adding a sample on how to use the custom serializer.

---
+++ Microsoft.Azure.Cosmos.Samples/CodeSamples/CustomSerializer/CustomSerializerProgram.cs
@@ -0,0 +1,91 @@
+﻿namespace Cosmos.Samples.Shared
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos;
+    using Microsoft.Extensions.Configuration;
+    using Newtonsoft.Json.Linq;
+
+    class CustomSerializerProgram
+    {
+        // Async main requires c# 7.1 which is set in the csproj with the LangVersion attribute
+        // <Main>
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appSettings.json")
+                    .Build();
+
+                string endpoint = configuration["EndPointUrl"];
+                if (string.IsNullOrEmpty(endpoint))
+                {
+                    throw new ArgumentNullException("Please specify a valid endpoint in the appSettings.json");
+                }
+
+                string authKey = configuration["AuthorizationKey"];
+                if (string.IsNullOrEmpty(authKey) || string.Equals(authKey, "Super secret key"))
+                {
+                    throw new ArgumentException("Please specify a valid AuthorizationKey in the appSettings.json");
+                }
+
+                //Read the Cosmos endpointUrl and authorisationKeys from configuration
+                //These values are available from the Azure Management Portal on the Cosmos Account Blade under "Keys"
+                //NB > Keep these values in a safe & secure location. Together they provide Administrative access to your Cosmos account
+                CustomSerializerProgram program = new CustomSerializerProgram();
+                await program.RunDemo(endpoint, authKey);
+
+            }
+            catch (CosmosException cre)
+            {
+                Console.WriteLine(cre.ToString());
+            }
+            catch (Exception e)
+            {
+                Exception baseException = e.GetBaseException();
+                Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        public async Task RunDemo(string endpoint, string key)
+        {
+            CosmosClientOptions options = new CosmosClientOptions()
+            {
+                Serializer = new JsonSerializerIgnoreNull(),
+            };
+
+            using (CosmosClient client = new CosmosClient(endpoint, key, options))
+            {
+                Database db = null;
+                try
+                {
+                    await client.CreateDatabaseIfNotExistsAsync("CustomSerializerDemo");
+                    Container container = await db.CreateContainerIfNotExistsAsync(
+                        id: "ContainerDemo",
+                        partitionKeyPath: "/pk");
+
+                    dynamic testItem = new { id = "MyTestItemWithIgnoreNull" + Guid.NewGuid(), pk = "ItExists", description = (string)null };
+                    ItemResponse<dynamic> itemResponse = await container.CreateItemAsync<dynamic>(testItem, new PartitionKey(testItem.pk));
+                    dynamic responseObject = itemResponse.Resource;
+                    if (responseObject["description"] != null)
+                    {
+                        throw new InvalidOperationException("Description was not ignored");
+                    }
+                }
+                finally
+                {
+                    if(db != null)
+                    {
+                        await db.DeleteAsync();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Microsoft.Azure.Cosmos.Samples/CodeSamples/CustomSerializer/JsonSerializerIgnoreNull.cs
@@ -0,0 +1,58 @@
+﻿namespace Cosmos.Samples.Shared
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using Microsoft.Azure.Cosmos;
+    using Newtonsoft.Json;
+
+    class JsonSerializerIgnoreNull : CosmosSerializer
+    {
+        private static readonly Encoding DefaultEncoding = new UTF8Encoding(false, true);
+        private static readonly JsonSerializer Serializer = new JsonSerializer()
+        {
+            NullValueHandling = NullValueHandling.Ignore
+        };
+
+        public override T FromStream<T>(Stream stream)
+        {
+            using (stream)
+            {
+                if (typeof(Stream).IsAssignableFrom(typeof(T)))
+                {
+                    return (T)(object)(stream);
+                }
+
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    using (JsonTextReader jsonTextReader = new JsonTextReader(sr))
+                    {
+                        return JsonSerializerIgnoreNull.Serializer.Deserialize<T>(jsonTextReader);
+                    }
+                }
+            }
+        }
+
+        public override Stream ToStream<T>(T input)
+        {
+            MemoryStream streamPayload = new MemoryStream();
+            using (StreamWriter streamWriter = new StreamWriter(
+                streamPayload,
+                encoding: JsonSerializerIgnoreNull.DefaultEncoding,
+                bufferSize: 1024,
+                leaveOpen: true))
+            {
+                using (JsonWriter writer = new JsonTextWriter(streamWriter))
+                {
+                    writer.Formatting = Newtonsoft.Json.Formatting.None;
+                    JsonSerializerIgnoreNull.Serializer.Serialize(writer, input);
+                    writer.Flush();
+                    streamWriter.Flush();
+                }
+            }
+
+            streamPayload.Position = 0;
+            return streamPayload;
+        }
+    }
+}
