44c5feec09477b48b274872144a78ca969ffaad8
kirankumarkolli <kirankk@microsoft.com>
2019-08-03 21:16:05 +0530

    Some databae and container UT

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.UT.FakeItEasy/Microsoft.Azure.Cosmos.UT.FakeItEasy/ContainerUT.cs
@@ -0,0 +1,139 @@
+﻿namespace Microsoft.Azure.Cosmos.UT.FakeItEasy
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using FakeItEasy;
+    using global::FakeItEasy;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ContainerUT
+    {
+        const string ContainerName = "Some Container";
+        const string PartitionKeyPath = "/somepk";
+        const string Query = "seelct * from c";
+        const string QueryCT = "CONTINUATION_TOKEN";
+
+        [TestMethod]
+        public async Task ReadContainerTest()
+        {
+            Container container = A.Fake<Container>();
+            ContainerResponse fakeResponse = A.Fake<ContainerResponse>();
+
+            A.CallTo(() => container.ReadContainerAsync(A<ContainerRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(fakeResponse);
+
+            ContainerResponse response = await container.ReadContainerAsync();
+
+            Assert.ReferenceEquals(fakeResponse, response);
+            A.CallTo(() => container.ReadContainerAsync(A<ContainerRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [TestMethod]
+        public async Task DeleteContainerTest()
+        {
+            Container container = A.Fake<Container>();
+            ContainerResponse fakeResponse = A.Fake<ContainerResponse>();
+
+            A.CallTo(() => container.DeleteContainerAsync(A<ContainerRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(fakeResponse);
+
+            ContainerResponse response = await container.DeleteContainerAsync();
+
+            Assert.ReferenceEquals(fakeResponse, response);
+            A.CallTo(() => container.DeleteContainerAsync(A<ContainerRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [TestMethod]
+        public async Task CreateItemTest()
+        {
+            Container container = A.Fake<Container>();
+            ItemResponse<ItemPayload> fakeResponse = A.Fake<ItemResponse<ItemPayload>>();
+
+            A.CallTo(() => container.CreateItemAsync<ItemPayload>(A<ItemPayload>.Ignored, A<PartitionKey?>.Ignored, A<ItemRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(fakeResponse);
+
+            ItemResponse<ItemPayload> response = await container.CreateItemAsync<ItemPayload>(null, null);
+
+            Assert.ReferenceEquals(fakeResponse, response);
+            A.CallTo(() => container.CreateItemAsync<ItemPayload>(null, null, A<ItemRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [TestMethod]
+        public async Task ReadItemTest()
+        {
+            Container container = A.Fake<Container>();
+            ItemResponse<ItemPayload> fakeResponse = A.Fake<ItemResponse<ItemPayload>>();
+
+            A.CallTo(() => container.ReadItemAsync<ItemPayload>(A<string>.Ignored, A<PartitionKey>.Ignored, A<ItemRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(fakeResponse);
+
+            ItemResponse<ItemPayload> response = await container.ReadItemAsync<ItemPayload>(null, PartitionKey.None);
+
+            Assert.ReferenceEquals(fakeResponse, response);
+            A.CallTo(() => container.ReadItemAsync<ItemPayload>(null, PartitionKey.None, A<ItemRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [TestMethod]
+        public async Task ReplaceItemTest()
+        {
+            Container container = A.Fake<Container>();
+            ItemResponse<ItemPayload> fakeResponse = A.Fake<ItemResponse<ItemPayload>>();
+
+            A.CallTo(() => container.ReplaceItemAsync<ItemPayload>(A<ItemPayload>.Ignored, A<string>.Ignored, A<PartitionKey?>.Ignored, A<ItemRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(fakeResponse);
+
+            ItemResponse<ItemPayload> response = await container.ReplaceItemAsync<ItemPayload>(null, null, null);
+
+            Assert.ReferenceEquals(fakeResponse, response);
+            A.CallTo(() => container.ReplaceItemAsync<ItemPayload>(null, null, null, A<ItemRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [TestMethod]
+        public async Task DeleteItemTest()
+        {
+            Container container = A.Fake<Container>();
+            ItemResponse<ItemPayload> fakeResponse = A.Fake<ItemResponse<ItemPayload>>();
+
+            A.CallTo(() => container.DeleteItemAsync<ItemPayload>(A<string>.Ignored, A<PartitionKey>.Ignored, A<ItemRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(fakeResponse);
+
+            ItemResponse<ItemPayload> response = await container.DeleteItemAsync<ItemPayload>(null, PartitionKey.None);
+
+            Assert.ReferenceEquals(fakeResponse, response);
+            A.CallTo(() => container.DeleteItemAsync<ItemPayload>(null, PartitionKey.None, A<ItemRequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [TestMethod]
+        public void IteratorTest()
+        {
+            Container container = A.Fake<Container>();
+            FeedIterator<ItemPayload> fakeResponse = A.Fake<FeedIterator<ItemPayload>>();
+
+            A.CallTo(() => container.GetItemQueryIterator<ItemPayload>(A<string>.Ignored, A<string>.Ignored, A<QueryRequestOptions>.Ignored))
+                .Returns(fakeResponse);
+
+            FeedIterator<ItemPayload> response = container.GetItemQueryIterator<ItemPayload>(ContainerUT.Query, ContainerUT.QueryCT);
+
+            Assert.ReferenceEquals(fakeResponse, response);
+            A.CallTo(() => container.GetItemQueryIterator<ItemPayload>(ContainerUT.Query, ContainerUT.QueryCT, A<QueryRequestOptions>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        public class ItemPayload
+        {
+            string Prop1 { get; set; }
+            string Prop2 { get; set; }
+            string Prop3 { get; set; }
+        }
+    }
+}
---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.UT.FakeItEasy/Microsoft.Azure.Cosmos.UT.FakeItEasy/DatabaseUT.cs
@@ -0,0 +1,140 @@
+﻿namespace Microsoft.Azure.Cosmos.UT.FakeItEasy
+{
+    using FakeItEasy;
+    using global::FakeItEasy;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Net.Cache;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class DatabaseUT
+    {
+        const string ContainerName = "Some Container";
+        const string PartitionKey = "/somepk";
+        const string Query = "seelct * from c";
+
+        [TestMethod]
+        public void GetId()
+        {
+            Database db = A.Fake<Database>();
+
+            A.CallTo(() => db.Id).Returns(DatabaseUT.ContainerName);
+
+            Assert.ReferenceEquals(DatabaseUT.ContainerName, db.Id);
+            A.CallTo(() => db.Id).MustHaveHappenedOnceExactly();
+        }
+
+        [TestMethod]
+        public async Task CreateTests()
+        {
+            Database db = A.Fake<Database>();
+            ContainerResponse fakeContainerResponse = A.Fake<ContainerResponse>();
+
+            A.CallTo(() => db.CreateContainerAsync(A<ContainerProperties>.Ignored, A<int?>.Ignored, A<RequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(fakeContainerResponse);
+
+            ContainerProperties cp = new ContainerProperties(DatabaseUT.ContainerName, DatabaseUT.PartitionKey);
+            ContainerResponse response = await db.CreateContainerAsync(cp);
+
+            Assert.ReferenceEquals(fakeContainerResponse, response);
+            A.CallTo(() => db.CreateContainerAsync(
+                    A<ContainerProperties>.That.Matches(e => e.Id == DatabaseUT.ContainerName && e.PartitionKeyPath == DatabaseUT.PartitionKey),
+                    A<int?>.Ignored,
+                    A<RequestOptions>.Ignored,
+                    A<CancellationToken>.Ignored))
+                .MustHaveHappenedOnceExactly();
+
+        }
+
+        [TestMethod]
+        public async Task CreateStreamTests()
+        {
+            Database db = A.Fake<Database>();
+            ResponseMessage fakeContainerResponse = A.Fake<ResponseMessage>();
+
+            A.CallTo(() => db.CreateContainerStreamAsync(A<ContainerProperties>.Ignored, A<int?>.Ignored, A<RequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(fakeContainerResponse);
+
+            ContainerProperties cp = new ContainerProperties(DatabaseUT.ContainerName, DatabaseUT.PartitionKey);
+            ResponseMessage response = await db.CreateContainerStreamAsync(cp);
+
+            Assert.ReferenceEquals(fakeContainerResponse, response);
+            A.CallTo(() => db.CreateContainerStreamAsync(
+                    A<ContainerProperties>.That.Matches(e => e.Id == DatabaseUT.ContainerName && e.PartitionKeyPath == DatabaseUT.PartitionKey),
+                    A<int?>.Ignored,
+                    A<RequestOptions>.Ignored,
+                    A<CancellationToken>.Ignored))
+                .MustHaveHappenedOnceExactly();
+
+        }
+
+        [TestMethod]
+        public async Task DeleteDbTests()
+        {
+            Database db = A.Fake<Database>();
+            DatabaseResponse fakeDbResponse = A.Fake<DatabaseResponse>();
+
+            A.CallTo(() => db.DeleteAsync(A<RequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(fakeDbResponse);
+
+            DatabaseResponse response = await db.DeleteAsync();
+
+            Assert.ReferenceEquals(fakeDbResponse, response);
+            A.CallTo(() => db.DeleteAsync(A<RequestOptions>.Ignored, A<CancellationToken>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [TestMethod]
+        public async Task DbThroughputTests()
+        {
+            Database db = A.Fake<Database>();
+            int? testThroughput = 100000;
+
+            A.CallTo(() => db.ReadThroughputAsync(A<CancellationToken>.Ignored))
+                .Returns(testThroughput);
+
+            int? response = await db.ReadThroughputAsync();
+
+            Assert.ReferenceEquals(testThroughput, response);
+            A.CallTo(() => db.ReadThroughputAsync(A<CancellationToken>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [TestMethod]
+        public void GetContainerTest()
+        {
+            Database db = A.Fake<Database>();
+            Container fakeContainer = A.Fake<Container>();
+
+            A.CallTo(() => db.GetContainer(A<string>.Ignored))
+                .Returns(fakeContainer);
+
+            Container response = db.GetContainer(DatabaseUT.ContainerName);
+
+            Assert.ReferenceEquals(fakeContainer, response);
+            A.CallTo(() => db.GetContainer(A<string>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+
+        [TestMethod]
+        public void ContainerIteratorTests()
+        {
+            Database db = A.Fake<Database>();
+            FeedIterator<ContainerProperties> fakeContainerIter = A.Fake<FeedIterator<ContainerProperties>>();
+
+            A.CallTo(() => db.GetContainerQueryIterator<ContainerProperties>(A<string>.Ignored, A<string>.Ignored, A<QueryRequestOptions>.Ignored))
+                .Returns(fakeContainerIter);
+
+            FeedIterator<ContainerProperties> response = db.GetContainerQueryIterator<ContainerProperties>(DatabaseUT.Query);
+
+            Assert.ReferenceEquals(fakeContainerIter, response);
+            A.CallTo(() => db.GetContainerQueryIterator<ContainerProperties>(DatabaseUT.Query, A<string>.Ignored, A<QueryRequestOptions>.Ignored))
+                .MustHaveHappenedOnceExactly();
+        }
+    }
+}
