5de82a6135af2bbea964a313ad8eb410f4910331
Jake Willey <jawilley@microsoft.com>
2019-08-08 13:42:20 -0700

    Added more comments

--- Microsoft.Azure.Cosmos/src/Serializer/CosmosSerializationOptions.cs
+++ Microsoft.Azure.Cosmos/src/Serializer/CosmosSerializationOptions.cs
@@ -13,17 +13,26 @@ namespace Microsoft.Azure.Cosmos
         /// <summary>
         /// Gets or sets if the serializer should ignore null properties
         /// </summary>
+        /// <remarks>
+        /// The default value is false
+        /// </remarks>
         public bool IgnoreNullValues { get; set; }
 
         /// <summary>
         /// Gets or sets if the serializer should use indentation
         /// </summary>
+        /// <remarks>
+        /// The default value is false
+        /// </remarks>
         public bool Indented { get; set; }
 
         /// <summary>
         /// Gets or sets whether the naming policy used to convert a string-based name to another format,
         /// such as a camel-casing format.
         /// </summary>
+        /// <remarks>
+        /// The default value is CosmosPropertyNamingPolicy.Default
+        /// </remarks>
         public CosmosPropertyNamingPolicy PropertyNamingPolicy { get; set; }
     }
 }
