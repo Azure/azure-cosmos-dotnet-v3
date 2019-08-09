2441f382f3f5ed93b2f20149d57a2280ecfe27d3
REDMOND\gaausfel <gaausfel@microsoft.com>
2019-08-09 16:09:34 -0700

    add ResourceTokenExpirySecondsHeaderIsAdded test

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/CosmosPermissionUnitTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/CosmosPermissionUnitTests.cs
@@ -4,7 +4,9 @@
 
 namespace Microsoft.Azure.Cosmos.Tests
 {
+    using Microsoft.Azure.Cosmos.Client.Core.Tests;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Net;
     using System.Threading.Tasks;
 
     [TestClass]
@@ -13,8 +15,36 @@ namespace Microsoft.Azure.Cosmos.Tests
         [TestMethod]
         public async Task ResourceTokenExpirySecondsHeaderIsAdded()
         {
-            //TODO
-            await Task.Run(null);
+            int testHandlerHitCount = 0;
+            const int ResourceTokenExpirySeconds = 9000;
+
+            TestHandler testHandler = new TestHandler((request, cancellationToken) =>
+            {
+                Assert.AreEqual(ResourceTokenExpirySeconds, int.Parse(request.Headers[Documents.HttpConstants.HttpHeaders.ResourceTokenExpiry]));
+                testHandlerHitCount++;
+                ResponseMessage response = new ResponseMessage(HttpStatusCode.OK, request, errorMessage: null);
+                response.Content = request.Content;
+                return Task.FromResult(response);
+            });
+
+            CosmosClient client = MockCosmosUtil.CreateMockCosmosClient(
+                (builder) => builder.AddCustomHandlers(testHandler));
+
+            Database database = client.GetDatabase("testdb");
+            await database.GetUser("testUser").CreatePermissionAsync(
+                new PermissionProperties("permissionId", PermissionMode.All, database.GetContainer("containerId")), 
+                resourceTokenExpirySeconds: ResourceTokenExpirySeconds
+            );
+
+            await database.GetUser("testUser").GetPermission("permissionId").ReplaceAsync(
+                new PermissionProperties("permissionId", PermissionMode.All, database.GetContainer("containerId")),
+                resourceTokenExpirySeconds: ResourceTokenExpirySeconds
+            );
+
+            await database.GetUser("testUser").GetPermission("permissionId").ReadAsync(resourceTokenExpirySeconds: ResourceTokenExpirySeconds);
+
+            //create,read, and replace
+            Assert.AreEqual(3, testHandlerHitCount);
         }
     }
 }
