e7a595644bdb7c8e2acd7da5d1b2c3448f23be21
kirankumarkolli <kirankk@microsoft.com>
2019-08-16 07:31:03 +0530

    Configurable Tcp settings to CosmosClientOptions (#708)
    * pull master, add new direct/tcp settings
    
    * validate in setters instead of GetConnectionPolicy
    
    * fix bug
    
    * improve error message
    
    * refactor
    
    * rename test
    
    * Fixing build break
    
    * Removing unnecessary virtuals
    
    * Contrct and change log refresh
    
    * Excluding builder
    
    * Excluding unintended changes

--- Microsoft.Azure.Cosmos/src/Fluent/CosmosClientBuilder.cs
+++ Microsoft.Azure.Cosmos/src/Fluent/CosmosClientBuilder.cs
@@ -170,6 +170,54 @@ namespace Microsoft.Azure.Cosmos.Fluent
         {
             this.clientOptions.ConnectionMode = ConnectionMode.Direct;
             this.clientOptions.ConnectionProtocol = Protocol.Tcp;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the connection mode to Direct. This is used by the client when connecting to the Azure Cosmos DB service.
+        /// </summary>
+        /// <param name="idleTcpConnectionTimeout">
+        /// Controls the amount of idle time after which unused connections are closed.
+        /// By default, idle connections are kept open indefinitely. Value must be greater than or equal to 10 minutes. Recommended values are between 20 minutes and 24 hours.
+        /// Mainly useful for sparse infrequent access to a large database account.
+        /// </param>
+        /// <param name="openTcpConnectionTimeout">
+        /// Controls the amount of time allowed for trying to establish a connection.
+        /// The default timeout is 5 seconds. Recommended values are greater than or equal to 5 seconds.
+        /// When the time elapses, the attempt is cancelled and an error is returned. Longer timeouts will delay retries and failures.
+        /// </param>
+        /// <param name="maxRequestsPerTcpConnection">
+        /// Controls the number of requests allowed simultaneously over a single TCP connection. When more requests are in flight simultaneously, the direct/TCP client will open additional connections.
+        /// The default settings allow 30 simultaneous requests per connection.
+        /// Do not set this value lower than 4 requests per connection or higher than 50-100 requests per connection.       
+        /// The former can lead to a large number of connections to be created. 
+        /// The latter can lead to head of line blocking, high latency and timeouts.
+        /// Applications with a very high degree of parallelism per connection, with large requests or responses, or with very tight latency requirements might get better performance with 8-16 requests per connection.
+        /// </param>
+        /// <param name="maxTcpConnectionsPerEndpoint">
+        /// Controls the maximum number of TCP connections that may be opened to each Cosmos DB back-end.
+        /// Together with MaxRequestsPerTcpConnection, this setting limits the number of requests that are simultaneously sent to a single Cosmos DB back-end(MaxRequestsPerTcpConnection x MaxTcpConnectionPerEndpoint).
+        /// The default value is 65,535. Value must be greater than or equal to 16.
+        /// </param>
+        /// <remarks>
+        /// For more information, see <see href="https://docs.microsoft.com/azure/documentdb/documentdb-performance-tips#direct-connection">Connection policy: Use direct connection mode</see>.
+        /// </remarks>
+        /// <returns>The current <see cref="CosmosClientBuilder"/>.</returns>
+        /// <seealso cref="CosmosClientOptions.ConnectionMode"/>
+        internal CosmosClientBuilder WithConnectionModeDirect(TimeSpan? idleTcpConnectionTimeout = null,
+            TimeSpan? openTcpConnectionTimeout = null,
+            int? maxRequestsPerTcpConnection = null,
+            int? maxTcpConnectionsPerEndpoint = null)
+        {
+            this.clientOptions.IdleTcpConnectionTimeout = idleTcpConnectionTimeout;
+            this.clientOptions.OpenTcpConnectionTimeout = openTcpConnectionTimeout;
+            this.clientOptions.MaxRequestsPerTcpConnection = maxRequestsPerTcpConnection;
+            this.clientOptions.MaxTcpConnectionsPerEndpoint = maxTcpConnectionsPerEndpoint;
+
+            this.clientOptions.ConnectionMode = ConnectionMode.Direct;
+            this.clientOptions.ConnectionProtocol = Protocol.Tcp;
+
             return this;
         }
 
