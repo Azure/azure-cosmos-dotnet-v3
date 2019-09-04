36ea84ad6e06fef907a0ee9f14af946df787d02b
Matias Quaranta <ealsur@hotmail.com>
2019-09-04 10:03:10 -0700

    Introducing itembatchoperationstatistics

---
+++ Microsoft.Azure.Cosmos/src/Resource/Settings/ItemBatchOperationStatistics.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal class ItemBatchOperationStatistics : CosmosDiagnostics
+    {
+        private readonly List<PointOperationStatistics> pointOperationStatistics = new List<PointOperationStatistics>();
+
+        public void AppendPointOperation(PointOperationStatistics pointOperationStatistic)
+        {
+            this.pointOperationStatistics.Add(pointOperationStatistic);
+        }
+
+        public override string ToString()
+        {
+            if (this.pointOperationStatistics.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            StringBuilder statistics = new StringBuilder();
+            foreach (PointOperationStatistics pointOperationStatistic in this.pointOperationStatistics)
+            {
+                statistics.AppendLine(pointOperationStatistic.ToString());
+            }
+
+            return statistics.ToString();
+        }
+    }
+}
---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Batch/ItemBatchOperationStatisticsTests.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ItemBatchOperationStatisticsTests
+    {
+        [TestMethod]
+        public void ToString_WhenEmptyReturnEmptyString()
+        {
+            ItemBatchOperationStatistics itemBatchOperationStatistics = new ItemBatchOperationStatistics();
+            Assert.IsTrue(string.IsNullOrEmpty(itemBatchOperationStatistics.ToString()));
+        }
+
+        [TestMethod]
+        public void ToString_ReturnItemsToString()
+        {
+            ItemBatchOperationStatistics itemBatchOperationStatistics = new ItemBatchOperationStatistics();
+
+            CosmosClientSideRequestStatistics cosmosClientSideRequestStatistics1 = new CosmosClientSideRequestStatistics();
+            cosmosClientSideRequestStatistics1.ContactedReplicas.Add(new Uri("https://one.com"));
+            PointOperationStatistics pointOperation1 = new PointOperationStatistics(cosmosClientSideRequestStatistics1);
+
+            CosmosClientSideRequestStatistics cosmosClientSideRequestStatistics2 = new CosmosClientSideRequestStatistics();
+            cosmosClientSideRequestStatistics2.ContactedReplicas.Add(new Uri("https://two.com"));
+            PointOperationStatistics pointOperation2 = new PointOperationStatistics(cosmosClientSideRequestStatistics2);
+
+            itemBatchOperationStatistics.AppendPointOperation(pointOperation1);
+            itemBatchOperationStatistics.AppendPointOperation(pointOperation2);
+
+            string toString = itemBatchOperationStatistics.ToString();
+
+            Assert.IsTrue(toString.Contains(pointOperation1.ToString()));
+            Assert.IsTrue(toString.Contains(pointOperation2.ToString()));
+        }
+    }
+}
