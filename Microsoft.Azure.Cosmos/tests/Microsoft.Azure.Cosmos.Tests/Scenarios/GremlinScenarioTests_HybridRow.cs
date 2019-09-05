3a85bd0a98d998413073e9359483860d4cca55bb
john-pao <53020793+john-pao@users.noreply.github.com>
2019-09-05 00:53:22 -0700

    Added Gremlin scenario test for document modification using CosmosElement (#740)
    * Added Gremlin scenario test for document modification using CosmosElement
    - Refactored Gremlin scenario tests for maintainability.
    - Added Gremlin scenario test that exercises document modification using CosmosElement classes.
    
    * Added comments to test methods to clarify scenarios.

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Scenarios/GremlinScenarioTests_HybridRow.cs
@@ -0,0 +1,42 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GremlinScenarioTests.cs" company="Microsoft Corporation">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Scenarios
+{
+    using System;
+    using Microsoft.Azure.Cosmos.Json;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Tests for CosmosDB Gremlin use case scenarios of CosmosElement and JsonNavigator interfaces using Text serialization.
+    /// </summary>
+    [TestClass]
+    public sealed class GremlinScenarioTests_HybridRow : GremlinScenarioTests
+    {
+        internal override JsonSerializationFormat SerializationFormat => JsonSerializationFormat.HybridRow;
+
+        [TestMethod]
+        public override void SerializeAndDeserializeGremlinEdgeDocument()
+        {
+            Assert.ThrowsException<ArgumentException>(
+                () => this.SerializeAndDeserializeEdgeDocumentTest(this.SerializationFormat));
+        }
+
+        [TestMethod]
+        public override void SerializeAndDeserializeGremlinVertexDocument()
+        {
+            Assert.ThrowsException<ArgumentException>(
+                () => this.SerializeAndDeserializeVertexDocumentTest(this.SerializationFormat));
+        }
+
+        [TestMethod]
+        public override void DeserializeModifyAndSerializeVertexDocument()
+        {
+            Assert.ThrowsException<ArgumentException>(
+                () => this.DeserializeModifyAndSerializeVertexDocumentTest(this.SerializationFormat));
+        }
+    }
+}
