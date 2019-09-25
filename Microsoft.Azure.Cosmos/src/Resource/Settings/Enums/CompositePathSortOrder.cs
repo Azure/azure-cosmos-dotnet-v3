eca0e94283e85c626ff9c33f5c5ac648559cabae
Matias Quaranta <ealsur@hotmail.com>
2019-09-25 15:52:55 -0700

    Reordering enums

---
+++ Microsoft.Azure.Cosmos/src/Resource/Settings/Enums/CompositePathSortOrder.cs
@@ -0,0 +1,25 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+namespace Microsoft.Azure.Cosmos
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Defines the target data type of an index path specification in the Azure Cosmos DB service.
+    /// </summary>
+    public enum CompositePathSortOrder
+    {
+        /// <summary>
+        /// Ascending sort order for composite paths.
+        /// </summary>
+        [EnumMember(Value = "ascending")]
+        Ascending,
+
+        /// <summary>
+        /// Descending sort order for composite paths.
+        /// </summary>
+        [EnumMember(Value = "descending")]
+        Descending
+    }
+}
