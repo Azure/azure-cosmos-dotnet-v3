4414700d59f82dcfd019a949be8904035626c62b
Jake Willey <jawilley@microsoft.com>
2019-10-02 08:02:23 -0700

    Adding documentation that replace does not support changing the id or partition key value.

--- Microsoft.Azure.Cosmos/src/Resource/Container/Container.cs
+++ Microsoft.Azure.Cosmos/src/Resource/Container/Container.cs
@@ -580,6 +580,10 @@ namespace Microsoft.Azure.Cosmos
         /// <summary>
         /// Replaces a item in the Azure Cosmos service as an asynchronous operation.
         /// </summary>
+        /// <remarks>
+        /// The item id and partition key value are immutable. 
+        /// To change an item id or partition key value you must delete the original item and insert a new item.
+        /// </remarks>
         /// <param name="streamPayload">A <see cref="Stream"/> containing the payload.</param>
         /// <param name="id">The cosmos item id</param>
         /// <param name="partitionKey">The partition key for the item. <see cref="PartitionKey"/></param>
@@ -626,7 +630,11 @@ namespace Microsoft.Azure.Cosmos
 
         /// <summary>
         /// Replaces a item in the Azure Cosmos service as an asynchronous operation.
-        /// </summary>        
+        /// </summary>
+        /// <remarks>
+        /// The item id and partition key value are immutable. 
+        /// To change an item id or partition key value you must delete the original item and insert a new item.
+        /// </remarks>
         /// <param name="item">A JSON serializable object that must contain an id property. <see cref="CosmosSerializer"/> to implement a custom serializer.</param>
         /// <param name="id">The cosmos item id, which is expected to match the value within T.</param>
         /// <param name="partitionKey">Partition key for the item. If not specified will be populated by extracting from {T}</param>
