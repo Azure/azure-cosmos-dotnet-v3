d01f194c2831a17aefc4cb51dd5947047653ac88
Matias Quaranta <ealsur@hotmail.com>
2019-10-16 12:02:04 -0700

    CF iterators

---
+++ Microsoft.Azure.Cosmos/azuredata/Resource/QueryResponses/ChangeFeedResultSetIteratorCore.cs
@@ -0,0 +1,168 @@
+//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Azure.Cosmos
+{
+    using System;
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos.Query;
+    using Microsoft.Azure.Cosmos.Routing;
+
+    /// <summary>
+    /// Cosmos Stand-By Feed iterator implementing Composite Continuation Token
+    /// </summary>
+    internal class ChangeFeedResultSetIteratorCore : FeedIterator
+    {
+        private const int DefaultMaxItemCount = 100;
+        private const string PageSizeErrorOnChangeFeedText = "Reduce page size and try again.";
+
+        internal StandByFeedContinuationToken compositeContinuationToken;
+
+        private readonly CosmosClientContext clientContext;
+        private readonly ContainerCore container;
+        private readonly int? originalMaxItemCount;
+        private string containerRid;
+        private string continuationToken;
+        private int? maxItemCount;
+
+        internal ChangeFeedResultSetIteratorCore(
+            CosmosClientContext clientContext,
+            ContainerCore container,
+            string continuationToken,
+            int? maxItemCount,
+            ChangeFeedRequestOptions options)
+        {
+            if (container == null) throw new ArgumentNullException(nameof(container));
+
+            this.clientContext = clientContext;
+            this.container = container;
+            this.changeFeedOptions = options;
+            this.maxItemCount = maxItemCount;
+            this.originalMaxItemCount = maxItemCount;
+            this.continuationToken = continuationToken;
+        }
+
+        /// <summary>
+        /// The query options for the result set
+        /// </summary>
+        protected readonly ChangeFeedRequestOptions changeFeedOptions;
+
+        public override bool HasMoreResults => true;
+
+        /// <summary>
+        /// Get the next set of results from the cosmos service
+        /// </summary>
+        /// <param name="cancellationToken">(Optional) <see cref="CancellationToken"/> representing request cancellation.</param>
+        /// <returns>A query response from cosmos service</returns>
+        public override async Task<Response> ReadNextAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            string firstNotModifiedKeyRangeId = null;
+            string currentKeyRangeId;
+            string nextKeyRangeId;
+            Response response;
+            do
+            {
+                (currentKeyRangeId, response) = await this.ReadNextInternalAsync(cancellationToken);
+                if (response.StatusCode != HttpStatusCode.NotModified)
+                {
+                    break;
+                }
+
+                // HttpStatusCode.NotModified
+                if (string.IsNullOrEmpty(firstNotModifiedKeyRangeId))
+                {
+                    // First NotModified Response
+                    firstNotModifiedKeyRangeId = currentKeyRangeId;
+                }
+
+                // Current Range is done, push it to the end
+                this.compositeContinuationToken.MoveToNextToken();
+                (_, nextKeyRangeId) = await this.compositeContinuationToken.GetCurrentTokenAsync();
+            }
+            // We need to keep checking across all ranges until one of them returns OK or we circle back to the start
+            while (!firstNotModifiedKeyRangeId.Equals(nextKeyRangeId, StringComparison.InvariantCultureIgnoreCase));
+
+            // Send to the user the composite state for all ranges
+            response.Headers.ContinuationToken = this.compositeContinuationToken.ToString();
+            return response;
+        }
+
+        internal async Task<Tuple<string, Response>> ReadNextInternalAsync(CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (this.compositeContinuationToken == null)
+            {
+                PartitionKeyRangeCache pkRangeCache = await this.clientContext.DocumentClient.GetPartitionKeyRangeCacheAsync();
+                this.containerRid = await this.container.GetRIDAsync(cancellationToken);
+                this.compositeContinuationToken = await StandByFeedContinuationToken.CreateAsync(this.containerRid, this.continuationToken, pkRangeCache.TryGetOverlappingRangesAsync);
+            }
+
+            (CompositeContinuationToken currentRangeToken, string rangeId) = await this.compositeContinuationToken.GetCurrentTokenAsync();
+            string partitionKeyRangeId = rangeId;
+            this.continuationToken = currentRangeToken.Token;
+            Response response = await this.NextResultSetDelegateAsync(this.continuationToken, partitionKeyRangeId, this.maxItemCount, this.changeFeedOptions, cancellationToken);
+            if (await this.ShouldRetryFailureAsync(response, cancellationToken))
+            {
+                return await this.ReadNextInternalAsync(cancellationToken);
+            }
+
+            if (response.IsSuccessStatusCode
+                || response.StatusCode == HttpStatusCode.NotModified)
+            {
+                // Change Feed read uses Etag for continuation
+                currentRangeToken.Token = response.Headers.ETag;
+            }
+
+            return new Tuple<string, Response>(partitionKeyRangeId, response);
+        }
+
+        /// <summary>
+        /// During Feed read, split can happen or Max Item count can go beyond the max response size
+        /// </summary>
+        internal async Task<bool> ShouldRetryFailureAsync(
+            Response response,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            bool partitionSplit = response.StatusCode == HttpStatusCode.Gone
+                && (response.Headers.SubStatusCode == Documents.SubStatusCodes.PartitionKeyRangeGone || response.Headers.SubStatusCode == Documents.SubStatusCodes.CompletingSplit);
+            if (partitionSplit)
+            {
+                // Forcing stale refresh of Partition Key Ranges Cache
+                await this.compositeContinuationToken.GetCurrentTokenAsync(forceRefresh: true);
+                return true;
+            }
+
+            return false;
+        }
+
+        internal virtual Task<Response> NextResultSetDelegateAsync(
+            string continuationToken,
+            string partitionKeyRangeId,
+            int? maxItemCount,
+            ChangeFeedRequestOptions options,
+            CancellationToken cancellationToken)
+        {
+            Uri resourceUri = this.container.LinkUri;
+            return this.clientContext.ProcessResourceOperationAsync<Response>(
+                resourceUri: resourceUri,
+                resourceType: Documents.ResourceType.Document,
+                operationType: Documents.OperationType.ReadFeed,
+                requestOptions: options,
+                cosmosContainerCore: this.container,
+                requestEnricher: request =>
+                {
+                    ChangeFeedRequestOptions.FillContinuationToken(request, continuationToken);
+                    ChangeFeedRequestOptions.FillMaxItemCount(request, maxItemCount);
+                    ChangeFeedRequestOptions.FillPartitionKeyRangeId(request, partitionKeyRangeId);
+                },
+                responseCreator: response => response,
+                partitionKey: null,
+                streamPayload: null,
+                cancellationToken: cancellationToken);
+        }
+    }
+}
