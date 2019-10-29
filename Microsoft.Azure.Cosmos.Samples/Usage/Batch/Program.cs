cfd992557c16f238d1420c5d4fe871b015d0615f
Abhijit P. Pai <abpai@microsoft.com>
2019-10-29 11:07:21 +0530

    More polish to sample

--- Microsoft.Azure.Cosmos.Samples/Usage/Batch/Program.cs
+++ Microsoft.Azure.Cosmos.Samples/Usage/Batch/Program.cs
@@ -103,29 +103,27 @@
             Container gamesContainer = Program.database.GetContainer(containerId);
 
             // This code demonstrates interactions by a multi-player game service that hosts games with the database to save game state.
-            // The objective of this game is to collect 2 balls of the same color, or a golden ball.
-            // Players move about the 10x10 map and try to find balls. 
-            // When held by players, balls may disappear after a while.
+            // In this fictional game, players move about the 10x10 map and try to find balls. 
+            // The objective is to collect 2 balls of the same color, or a golden ball if one appears.
             // After 5 minutes, if the game is not complete, the player with highest number of balls wins.
 
-            // =========== At the start of the game, the balls are added on the map, and the players are added ======================================
+            Console.WriteLine("At the start of the game, the balls are added on the map, and the players are added ...");
             int gameId = 420;
             int playerCount = 3;
-            int ballCount = 5;
+            int ballCount = 3;
+            List<GameBall> balls = new List<GameBall>();
+            List<GameParticipant> players = new List<GameParticipant>();
 
             // The below batch request is used to create the game balls and participants in an atomic fashion
             BatchResponse gameStartResponse = await gamesContainer.CreateBatch(new PartitionKey(gameId))
                 .CreateItem<GameBall>(GameBall.Create(gameId, Color.Red, 4, 2))
-                .CreateItem<GameBall>(GameBall.Create(gameId, Color.Green, 5, 3))
                 .CreateItem<GameBall>(GameBall.Create(gameId, Color.Blue, 6, 4))
-                .CreateItem<GameBall>(GameBall.Create(gameId, Color.Green, 8, 7))
                 .CreateItem<GameBall>(GameBall.Create(gameId, Color.Red, 8, 8))
                 .CreateItem<GameParticipant>(GameParticipant.Create(gameId, "alice"))
                 .CreateItem<GameParticipant>(GameParticipant.Create(gameId, "bob"))
                 .CreateItem<GameParticipant>(GameParticipant.Create(gameId, "carla"))
                 .ExecuteAsync();
 
-            GameBall[] balls = new GameBall[ballCount];
             GameParticipant alice, bob, carla;
             GameBall blueBall, secondRedBall;
 
@@ -141,20 +139,18 @@
                     // log exception and handle failure
                 }
 
-
                 // Refresh in-memory state from response
                 // The BatchResponse has a list of BatchOperationResult, one for each operation within the batch request in the order of operations.
                 for (int index = 0; index < ballCount; index++)
                 {
                     // The GetOperationResultAtIndex method returns the result of the batch operation with a Resource deserialized to the provided type.
                     BatchOperationResult<GameBall> gameBallResult = gameStartResponse.GetOperationResultAtIndex<GameBall>(index);
-                    balls[index] = gameBallResult.Resource;
+                    balls.Add(gameBallResult.Resource);
                 }
 
-                blueBall = balls[3];
-                secondRedBall = balls[4];
+                blueBall = balls[1];
+                secondRedBall = balls[2];
 
-                List<GameParticipant> players = new List<GameParticipant>();
                 for (int index = ballCount; index < gameStartResponse.Count; index++)
                 {
                     players.Add(gameStartResponse.GetOperationResultAtIndex<GameParticipant>(index).Resource);
@@ -165,17 +161,20 @@
                 carla = players.Single(p => p.Nickname == "carla");
             }
 
-            //  =========== Alice goes to 6, 4 and finds a blue ball ================================================================================
-            blueBall.AssignedToNickName = "alice";
+            PrintState(players, balls);
+
+            Console.WriteLine("Alice goes to 6, 4 and finds a blue ball ...");
+            balls.Remove(blueBall);
             alice.BlueCount++;
 
-            // Upserts maybe used to replace items, or create them if they are not already present.
+            // Upserts maybe used to replace items or create them if they are not already present.
             // Existing entities maybe replaced along with concurrency checks using ETags returned in the responses of earlier requests on the item
-            // or without these checks when they are not required.
+            // or without these checks if they are not required.
+            // Delets may also be a part of
             BatchResponse aliceFoundBallResponse = await gamesContainer.CreateBatch(new PartitionKey(gameId))
                 .UpsertItem<ParticipantLastActive>(ParticipantLastActive.Create(gameId, "alice"))
-                .ReplaceItem<GameBall>(blueBall.Id, blueBall, new BatchItemRequestOptions { IfMatchEtag = blueBall.ETag })
-                .ReplaceItem<GameParticipant>(alice.Nickname, alice)
+                .ReplaceItem<GameParticipant>(alice.Nickname, alice, new BatchItemRequestOptions { IfMatchEtag = alice.ETag })
+                .DeleteItem(blueBall.Id)
                 .ExecuteAsync();
 
             using (aliceFoundBallResponse)
@@ -185,14 +184,15 @@
                     // log exception and handle failure
                 }
 
-                // Refresh in-memory state
+                // Refresh in-memory state from response
                 // We only update the etag as we have the rest of the state we care about here already as needed.
-                blueBall.ETag = aliceFoundBallResponse[1].ETag;
-                alice.ETag = aliceFoundBallResponse[2].ETag;
+                alice.ETag = aliceFoundBallResponse[1].ETag;
             }
 
-            //  =========== Bob goes to 8, 8 and finds a red ball ===================================================================================
-            secondRedBall.AssignedToNickName = "bob";
+            PrintState(players, balls);
+
+            Console.WriteLine("Bob goes to 8, 8 and finds a red ball ...");
+            balls.Remove(secondRedBall);
             bob.RedCount++;
 
             // Stream variants for all batch operations that accept an item are also available for use when the item is available as a Stream.
@@ -200,8 +200,8 @@
 
             BatchResponse bobFoundBallResponse = await gamesContainer.CreateBatch(new PartitionKey(gameId))
                 .UpsertItemStream(bobIsActiveStream)
-                .ReplaceItemStream(secondRedBall.Id, Program.AsStream(secondRedBall), new BatchItemRequestOptions { IfMatchEtag = secondRedBall.ETag })
-                .ReplaceItemStream(bob.Nickname, Program.AsStream(bob))
+                .ReplaceItemStream(bob.Nickname, Program.AsStream(bob), new BatchItemRequestOptions { IfMatchEtag = bob.ETag })
+                .DeleteItem(secondRedBall.Id)
                 .ExecuteAsync();
 
             using (bobFoundBallResponse)
@@ -211,32 +211,16 @@
                     // log exception and handle failure
                 }
 
-                // Refresh in-memory state
-                // The resultant item for the operations is also available as a Stream that can be used for example if the response is just
+                // Refresh in-memory state from response
+                // The resultant item for each operation is also available as a Stream that can be used for example if the response is just
                 // going to be transferred to some other system.
-                Stream updatedBallAsStream = bobFoundBallResponse[1].ResourceStream;
-                blueBall = Program.FromStream<GameBall>(updatedBallAsStream);
-
-                bob.ETag = bobFoundBallResponse[2].ETag;
+                Stream updatedPlayerAsStream = bobFoundBallResponse[1].ResourceStream;
+                bob = Program.FromStream<GameParticipant>(updatedPlayerAsStream);
             }
 
-            // ============ Alice has held the blue ball for too long and the ball disappears =======================================================
-            alice.BlueCount--;
+            PrintState(players, balls);
 
-            BatchResponse deleteBallResponse = await gamesContainer.CreateBatch(new PartitionKey(gameId))
-               .ReplaceItem<GameParticipant>(alice.Nickname, alice)
-               .DeleteItem(blueBall.Id)
-               .ExecuteAsync();
-
-            using (deleteBallResponse)
-            {
-                if (deleteBallResponse.StatusCode != HttpStatusCode.OK)
-                {
-                    // log exception and handle failure
-                }
-            }
-
-            // =========== Add a golden ball near each of the players to see if any of them pick it up ==============================================
+            Console.WriteLine("A golden ball appears near each of the players to select an instant winner ...");
             BatchResponse goldenBallResponse = await gamesContainer.CreateBatch(new PartitionKey(gameId))
                .CreateItem<GameBall>(GameBall.Create(gameId, Color.Gold, 2, 2))
                .CreateItem<GameBall>(GameBall.Create(gameId, Color.Gold, 6, 3))
@@ -258,8 +242,9 @@
                 // Given a batch request is atomic, in case any operation within a batch fails, no changes from the batch will be committed.
                 if (goldenBallResponse.StatusCode != HttpStatusCode.OK)
                 {
-                    foreach (BatchOperationResult operationResult in goldenBallResponse)
+                    for (int index = 0; index < goldenBallResponse.Count; index++)
                     {
+                        BatchOperationResult operationResult = goldenBallResponse[index];
                         if ((int)operationResult.StatusCode == 424)
                         {
                             // This operation failed because it was batched along with another operation where the latter
@@ -267,12 +252,21 @@
                             continue;
                         }
 
-                        // Log and handle failure
+                        if (operationResult.StatusCode == HttpStatusCode.Conflict)
+                        {
+                            Console.WriteLine("Creation of the {0}rd golden ball failed because there was already an existing ball at that position.", index + 1);
+                        }
+
+                        // Log and handle other failures
                     }
                 }
             }
 
-            // ========== 5 minutes have passed, but we don't have a winner yet. Determine winner as player with highest balls ======================
+            PrintState(players, balls);
+
+            Console.WriteLine("We need to end the game now; determining the winner as the player with highest balls ...");
+
+            // Batch requests may also be used to atomically read the state of multiple items within a partition key.
             BatchResponse playersResponse = await gamesContainer.CreateBatch(new PartitionKey(gameId))
                 .ReadItem(alice.Nickname)
                 .ReadItem(bob.Nickname)
@@ -280,6 +274,7 @@
                 .ExecuteAsync();
 
             GameParticipant winner = null;
+            bool isTied = false;
 
             using (playersResponse)
             {
@@ -290,15 +285,58 @@
 
                 for (int index = 0; index < playerCount; index++)
                 {
-                    GameParticipant current = playersResponse.GetOperationResultAtIndex<GameParticipant>(index).Resource;
+                    GameParticipant current;
 
-                    // Not handling ties
+                    if (index == 0)
+                    {
+                        // The item returned can be made available as the required POCO type using GetOperationResultAtIndex.
+                        current = playersResponse.GetOperationResultAtIndex<GameParticipant>(index).Resource;
+                    }
+                    else
+                    {
+                        // The item returned can also instead be accessed directly as a Stream for example to pass as-is to another component.
+                        Stream aliceInfo = playersResponse[index].ResourceStream;
+
+                        current = Program.FromStream<GameParticipant>(aliceInfo);
+                    }
+
                     if (winner == null || current.TotalCount > winner.TotalCount)
                     {
                         winner = current;
+                        isTied = false;
+                    }
+                    else if(current.TotalCount == winner.TotalCount)
+                    {
+                        isTied = true;
                     }
                 }
             }
+
+            if (!isTied)
+            {
+                Console.WriteLine($"{winner.Nickname} has won the game!\n");
+            }
+            else
+            {
+                Console.WriteLine("The game is a tie; there is no clear winner.\n");
+            }
+        }
+
+        private static void PrintState(List<GameParticipant> players, List<GameBall> balls)
+        {
+            Console.WriteLine("{0,-8}{1,6}{2,6}{3,6}", "Nick", "Red", "Blue", "Total");
+            foreach(GameParticipant player in players)
+            {
+                Console.WriteLine("{0,-8}{1,6}{2,6}{3,6}", player.Nickname, player.RedCount, player.BlueCount, player.TotalCount);
+            }
+
+            Console.Write("Ball positions: ");
+            foreach(GameBall ball in balls)
+            {
+                Console.Write("[{0},{1}] ", ball.XCoord, ball.YCoord);
+            }
+
+            Console.WriteLine("\n===================================================================================\n");
         }
 
         private static async Task CleanupAsync()
@@ -323,7 +361,6 @@
         private enum Color
         {
             Red,
-            Green,
             Blue,
             Gold
         }
@@ -337,8 +374,6 @@
             public int XCoord { get; set; }
 
             public int YCoord { get; set; }
-
-            public string AssignedToNickName { get; set; }
 
             [JsonProperty("id")]
             internal string Id { get { return $"{XCoord}:{YCoord}"; } }
@@ -355,6 +390,22 @@
                     XCoord = xCoord,
                     YCoord = yCoord
                 };
+            }
+
+            public override bool Equals(object obj)
+            {
+                var ball = obj as GameBall;
+                return ball != null &&
+                       GameId == ball.GameId &&
+                       Color == ball.Color &&
+                       XCoord == ball.XCoord &&
+                       YCoord == ball.YCoord &&
+                       Id == ball.Id;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(GameId, Color, XCoord, YCoord, Id);
             }
         }
 
@@ -394,11 +445,9 @@
 
             public int RedCount { get; set; }
 
-            public int GreenCount { get; set; }
-
             public int BlueCount { get; set; }
 
-            public int TotalCount { get { return this.RedCount + this.GreenCount + this.BlueCount; } }
+            public int TotalCount { get { return this.RedCount + this.BlueCount; } }
 
             [JsonProperty("_etag")]
             internal string ETag { get; set; }
@@ -410,6 +459,21 @@
                     GameId = gameId,
                     Nickname = nickname,
                 };
+            }
+
+            public override bool Equals(object obj)
+            {
+                var participant = obj as GameParticipant;
+                return participant != null &&
+                       GameId == participant.GameId &&
+                       Nickname == participant.Nickname &&
+                       RedCount == participant.RedCount &&
+                       BlueCount == participant.BlueCount;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(GameId, Nickname, RedCount, BlueCount);
             }
         }
     }
