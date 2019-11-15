2ce87a008170f1abcb71243b61403b01c54a439c
Brandon Chong <bchong95@users.noreply.github.com>
2019-11-14 18:39:00 -0800

    added bad request exception

---
+++ Microsoft.Azure.Cosmos/src/Resource/CosmosExceptions/BadRequestException.cs
@@ -0,0 +1,26 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Resource.CosmosExceptions
+{
+    using System;
+
+    internal sealed class BadRequestException : CosmosException
+    {
+        public BadRequestException()
+            : base(statusCode: System.Net.HttpStatusCode.BadRequest, message: null)
+        {
+        }
+
+        public BadRequestException(string message)
+            : base(statusCode: System.Net.HttpStatusCode.BadRequest, message: message)
+        {
+        }
+
+        public BadRequestException(string message, Exception inner)
+            : base(statusCode: System.Net.HttpStatusCode.BadRequest, message: message, inner: inner)
+        {
+        }
+    }
+}
