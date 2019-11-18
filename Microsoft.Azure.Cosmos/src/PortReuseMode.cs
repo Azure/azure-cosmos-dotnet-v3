75b906149f3a6cbec85d04627d249d920f237a83
REDMOND\gaausfel <gaausfel@microsoft.com>
2019-11-18 13:28:48 -0800

    add see also links to ReuseUnicastPort

--- Microsoft.Azure.Cosmos/src/PortReuseMode.cs
+++ Microsoft.Azure.Cosmos/src/PortReuseMode.cs
@@ -12,6 +12,12 @@ namespace Microsoft.Azure.Cosmos
         /// Windows Server 2016 and newer: Uses the SO_REUSE_UNICASTPORT option if the operating system has automatic client port reuse enabled.
         /// Older versions of Windows, Linux, other: Uses default socket options.
         /// </summary>
+        /// <remarks>
+        /// see also
+        /// https://docs.microsoft.com/en-us/windows/win32/winsock/sol-socket-socket-options
+        /// https://docs.microsoft.com/en-us/powershell/module/nettcpip/set-nettcpsetting?view=win10-ps
+        /// https://support.microsoft.com/en-us/help/3149157/reliability-and-scalability-improvements-in-tcp-ip-for-windows-8-1-and
+        /// </remarks>
         ReuseUnicastPort = 0,
         /// <summary>
         /// Windows: Tracks client ports used by the Cosmos DB client and reuses them. Ports are reused at DocumentClient scope.
