d480e471bbb171be83442917bac80edb6ceb75a7
Brandon Chong <bchong95@users.noreply.github.com>
2019-11-22 09:08:12 -0800

    added support for stack trace and inner exception

--- Microsoft.Azure.Cosmos/src/Resource/CosmosException.cs
+++ Microsoft.Azure.Cosmos/src/Resource/CosmosException.cs
@@ -7,6 +7,7 @@ namespace Microsoft.Azure.Cosmos
     using System;
     using System.IO;
     using System.Net;
+    using System.Text;
     using Microsoft.Azure.Documents;
 
     /// <summary>
@@ -14,6 +15,7 @@ namespace Microsoft.Azure.Cosmos
     /// </summary>
     public class CosmosException : Exception
     {
+        private static readonly string FullName = typeof(CosmosException).FullName;
         internal CosmosException(
             HttpStatusCode statusCode,
             string message,
@@ -157,8 +159,49 @@ namespace Microsoft.Azure.Cosmos
         /// <returns>A string representation of the exception.</returns>
         public override string ToString()
         {
-            string diagnostics = this.Diagnostics != null ? this.Diagnostics.ToString() : string.Empty;
-            return $"{nameof(CosmosException)};StatusCode={this.StatusCode};SubStatusCode={this.SubStatusCode};ActivityId={this.ActivityId ?? string.Empty};RequestCharge={this.RequestCharge};Message={this.Message};Diagnostics{diagnostics}";
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(CosmosException.FullName);
+            if (this.Message != null)
+            {
+                stringBuilder.Append(" : ");
+                stringBuilder.Append(this.Message);
+                stringBuilder.AppendLine();
+            }
+
+            stringBuilder.AppendFormat("StatusCode = {0}", this.StatusCode);
+            stringBuilder.AppendLine();
+
+            stringBuilder.AppendFormat("SubStatusCode = {0}", this.SubStatusCode);
+            stringBuilder.AppendLine();
+
+            stringBuilder.AppendFormat("ActivityId = {0}", this.ActivityId ?? Guid.Empty.ToString());
+            stringBuilder.AppendLine();
+
+            stringBuilder.AppendFormat("RequestCharge = {0}", this.RequestCharge);
+            stringBuilder.AppendLine();
+
+            stringBuilder.AppendFormat("RequestCharge = {0}", this.RequestCharge);
+            stringBuilder.AppendLine();
+
+            if (this.Diagnostics != null)
+            {
+                stringBuilder.Append(this.Diagnostics);
+                stringBuilder.AppendLine();
+            }
+
+            if (this.InnerException != null)
+            {
+                stringBuilder.Append(" ---> ");
+                stringBuilder.Append(this.InnerException);
+                stringBuilder.AppendLine();
+                stringBuilder.Append("   ");
+                stringBuilder.Append("--- End of inner exception stack trace ---");
+            }
+
+            stringBuilder.AppendLine();
+            stringBuilder.Append(this.StackTrace);
+
+            return stringBuilder.ToString();
         }
 
         internal ResponseMessage ToCosmosResponseMessage(RequestMessage request)
