85d44593c5d872fc764000730d9092b2c725632b
Abhijit P. Pai <abpai@microsoft.com>
2019-12-15 12:32:03 +0530

    Add 'new' files

---
+++ Microsoft.Azure.Cosmos/src/Encryption/KeyUnwrapResponse.cs
@@ -0,0 +1,39 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos
+{
+    using System;
+
+    /// <summary>
+    /// Response from a <see cref="KeyWrapProvider"/> on unwrapping a wrapped data encryption key.
+    /// </summary>
+    public class KeyUnwrapResponse
+    {
+        /// <summary>
+        /// Initializes a new instance of the response of unwrapping a wrapped data encryption key.
+        /// </summary>
+        /// <param name="dataEncryptionKey">Raw form of data encryption key.</param>
+        /// <param name="clientCacheTimeToLive">
+        /// Amount of time after which the raw data encryption key must not be used
+        /// without invoking the <see cref="KeyWrapProvider.UnwrapKeyAsync"/> again.
+        /// </param>
+        public KeyUnwrapResponse(byte[] dataEncryptionKey, TimeSpan clientCacheTimeToLive)
+        {
+            this.DataEncryptionKey = dataEncryptionKey;
+            this.ClientCacheTimeToLive = clientCacheTimeToLive;
+        }
+
+        /// <summary>
+        /// Raw form of the data encryption key.
+        /// </summary>
+        public byte[] DataEncryptionKey { get; }
+
+        /// <summary>
+        /// Amount of time after which the raw data encryption key must not be used
+        /// without invoking the <see cref="KeyWrapProvider.UnwrapKeyAsync"/> again.
+        /// </summary>
+        public TimeSpan ClientCacheTimeToLive { get; }
+    }
+}
---
+++ Microsoft.Azure.Cosmos/src/Encryption/KeyWrapProvider.cs
@@ -0,0 +1,32 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interface for interacting with a provider that can be used to wrap (encrypt) and unwrap (decrypt) data encryption keys for envelope based encryption.
+    /// See <see href="tbd"/> for more information on client-side encryption support in Azure Cosmos DB.
+    /// </summary>
+    public abstract class KeyWrapProvider
+    {
+        /// <summary>
+        /// Wraps (i.e. encrypts) the provided data encryption key.
+        /// </summary>
+        /// <param name="key">Data encryption key that needs to be wrapped.</param>
+        /// <param name="metadata">Metadata for the wrap provider that should be used to wrap / unwrap the key.</param>
+        /// <returns>Awaitable wrapped (i.e. encrypted) version of data encryption key passed in possibly with updated metadata.</returns>
+        public abstract Task<KeyWrapResponse> WrapKeyAsync(byte[] key, KeyWrapMetadata metadata);
+
+        /// <summary>
+        /// Unwraps (i.e. decrypts) the provided wrapped data encryption key.
+        /// </summary>
+        /// <param name="wrappedKey">Wrapped form of data encryption key that needs to be unwrapped.</param>
+        /// <param name="metadata">Metadata for the wrap provider that should be used to wrap / unwrap the key.</param>
+        /// <returns>Awaitable unwrapped (i.e. unencrypted) version of data encryption key passed in and how long the raw data encryption key can be cached on the client.</returns>
+        public abstract Task<KeyUnwrapResponse> UnwrapKeyAsync(byte[] wrappedKey, KeyWrapMetadata metadata);
+    }
+}
