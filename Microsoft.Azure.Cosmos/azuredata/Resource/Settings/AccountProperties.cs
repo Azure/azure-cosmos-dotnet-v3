6bc3c25ed77e818a287252ce76af9c975d708a49
Matias Quaranta <ealsur@hotmail.com>
2019-12-18 10:05:52 -0800

    Normalizing NullValueHandling

--- Microsoft.Azure.Cosmos/azuredata/Resource/Settings/AccountProperties.cs
+++ Microsoft.Azure.Cosmos/azuredata/Resource/Settings/AccountProperties.cs
@@ -75,7 +75,7 @@ namespace Azure.Cosmos
         /// <remarks>
         /// ETags are used for concurrency checking when updating resources. 
         /// </remarks>
-        [JsonProperty(PropertyName = Constants.Properties.ETag)]
+        [JsonProperty(PropertyName = Constants.Properties.ETag, NullValueHandling = NullValueHandling.Ignore)]
         [JsonConverter(typeof(ETagConverter))]
         public ETag? ETag { get; internal set; }
 
