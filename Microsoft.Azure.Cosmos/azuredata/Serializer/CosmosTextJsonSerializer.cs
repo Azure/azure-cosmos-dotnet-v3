416e37d3b4d8e5ddb378b30741128e511ace3fd9
Matias Quaranta <ealsur@hotmail.com>
2019-12-20 10:48:13 -0800

    Serializer sync

--- Microsoft.Azure.Cosmos/azuredata/Serializer/CosmosTextJsonSerializer.cs
+++ Microsoft.Azure.Cosmos/azuredata/Serializer/CosmosTextJsonSerializer.cs
@@ -4,10 +4,9 @@
 
 namespace Azure.Cosmos
 {
+    using System;
     using System.IO;
     using System.Text.Json;
-    using System.Threading;
-    using System.Threading.Tasks;
     using Azure.Cosmos.Serialization;
 
     /// <summary>
@@ -65,20 +64,39 @@ namespace Azure.Cosmos
         /// </summary>
         /// <typeparam name="T">The type of object that should be deserialized</typeparam>
         /// <param name="stream">An open stream that is readable that contains JSON</param>
-        /// <param name="cancellationToken">(Optional) <see cref="CancellationToken"/> representing request cancellation.</param>
         /// <returns>The object representing the deserialized stream</returns>
-        public override ValueTask<T> FromStreamAsync<T>(
-            Stream stream,
-            CancellationToken cancellationToken)
+        public override T FromStream<T>(Stream stream)
         {
             using (stream)
             {
                 if (typeof(Stream).IsAssignableFrom(typeof(T)))
                 {
-                    return new ValueTask<T>((T)(object)stream);
+                    return (T)(object)stream;
                 }
 
-                return JsonSerializer.DeserializeAsync<T>(stream, this.jsonSerializerSettings, cancellationToken);
+                ReadOnlySpan<byte> span;
+                MemoryStream memoryStream = stream as MemoryStream;
+                if (memoryStream != null)
+                {
+                    span = new ReadOnlySpan<byte>(memoryStream.ToArray());
+                }
+                else
+                {
+                    byte[] buffer = new byte[16 * 1024];
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        int read;
+                        while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            ms.Write(buffer, 0, read);
+                        }
+
+                        span = new ReadOnlySpan<byte>(ms.ToArray());
+                    }
+                    
+                }
+                
+                return JsonSerializer.Deserialize<T>(span, this.jsonSerializerSettings);
             }
         }
 
@@ -87,14 +105,12 @@ namespace Azure.Cosmos
         /// </summary>
         /// <typeparam name="T">The type of object being serialized</typeparam>
         /// <param name="input">The object to be serialized</param>
-        /// <param name="cancellationToken">(Optional) <see cref="CancellationToken"/> representing request cancellation.</param>
         /// <returns>An open readable stream containing the JSON of the serialized object</returns>
-        public override async Task<Stream> ToStreamAsync<T>(
-            T input,
-            CancellationToken cancellationToken)
+        public override Stream ToStream<T>(T input)
         {
             MemoryStream streamPayload = new MemoryStream();
-            await JsonSerializer.SerializeAsync<T>(streamPayload, input, CosmosTextJsonSerializer.DefaultSerializationOptions, cancellationToken);
+            using Utf8JsonWriter utf8JsonWriter = new Utf8JsonWriter(streamPayload);
+            JsonSerializer.Serialize<T>(utf8JsonWriter, input, CosmosTextJsonSerializer.DefaultSerializationOptions);
             streamPayload.Position = 0;
             return streamPayload;
         }
