27d59760922412482c6ebc84c2bdd8440bc72e7b
Matias Quaranta <ealsur@hotmail.com>
2019-12-26 15:47:42 -0800

    Cleanup

--- Microsoft.Azure.Cosmos/tests/Azure.Cosmos/Azure.Cosmos.EmulatorTests/Utils/CosmosSerializerHelper.cs
+++ Microsoft.Azure.Cosmos/tests/Azure.Cosmos/Azure.Cosmos.EmulatorTests/Utils/CosmosSerializerHelper.cs
@@ -63,7 +63,6 @@ namespace Azure.Cosmos.EmulatorTests
             public override void Write(Utf8JsonWriter writer, int value, JsonSerializerOptions options)
             {
                 writer.WriteStringValue(value.ToString(CultureInfo.InvariantCulture));
-
             }
 
             public override int Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
