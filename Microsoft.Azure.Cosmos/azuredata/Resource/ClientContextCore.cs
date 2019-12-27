5969036d8ddb6e234d6e4d1c12ac6055e1b6166d
Matias Quaranta <ealsur@hotmail.com>
2019-12-26 17:07:33 -0800

    Conditional options and headers

--- Microsoft.Azure.Cosmos/azuredata/Resource/ClientContextCore.cs
+++ Microsoft.Azure.Cosmos/azuredata/Resource/ClientContextCore.cs
@@ -43,7 +43,7 @@ namespace Azure.Cosmos
             this.DocumentClient = documentClient;
 
             this.ClientOptions.Transport = new ClientPipelineTransport(requestHandler);
-            // Disable Azure Core pipeline retry policies, the SDK has its own retries
+            // Disable the default Azure Core pipeline retry policies, the SDK has its own retries
             this.ClientOptions.Retry.MaxRetries = 0;
             this.pipeline = HttpPipelineBuilder.Build(this.ClientOptions);
             this.diagnostics = new ClientDiagnostics(this.ClientOptions);
