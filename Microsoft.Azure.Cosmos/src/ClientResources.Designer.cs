e1275a8d9c020238572b5aca4ac5e03e7ff5a45e
Matias Quaranta <ealsur@hotmail.com>
2020-02-10 11:22:31 -0800

    Client resources

--- Microsoft.Azure.Cosmos/src/ClientResources.Designer.cs
+++ Microsoft.Azure.Cosmos/src/ClientResources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Azure.Cosmos {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class ClientResources {
@@ -300,6 +300,15 @@ namespace Microsoft.Azure.Cosmos {
         internal static string FailedToEvaluateSpatialExpression {
             get {
                 return ResourceManager.GetString("FailedToEvaluateSpatialExpression", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The provided string does not represent any known FeedToken format..
+        /// </summary>
+        internal static string FeedToken_UnknownFormat {
+            get {
+                return ResourceManager.GetString("FeedToken_UnknownFormat", resourceCulture);
             }
         }
         
