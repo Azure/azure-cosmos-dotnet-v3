d1650c8aeaff00e7f6e1d4fc69dbcb25ef8d1b0f
Matias Quaranta <ealsur@hotmail.com>
2020-02-10 11:22:24 -0800

    FeedTokenInternal

---
+++ Microsoft.Azure.Cosmos/src/FeedTokenInternal.cs
@@ -0,0 +1,25 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos
+{
+    using System.Threading.Tasks;
+
+    internal abstract class FeedTokenInternal : FeedToken
+    {
+        public abstract void FillHeaders(RequestMessage request);
+
+        public abstract string GetContinuation();
+
+        public static bool TryParse(
+            string toStringValue,
+            out FeedToken parsedToken)
+        {
+            parsedToken = null;
+            return false;
+        }
+
+        public virtual Task SplitAsync() => Task.CompletedTask;
+    }
+}
