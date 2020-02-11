7225248b11fd8e829f9e0beb4dc05cc92cbf4df4
Jake Willey <jawilley@microsoft.com>
2020-02-11 13:12:04 -0800

    Added initial TSG pages

--- Microsoft.Azure.Cosmos/src/Resource/CosmosExceptions/CosmosTroubleShootingLink.cs
+++ Microsoft.Azure.Cosmos/src/Resource/CosmosExceptions/CosmosTroubleShootingLink.cs
@@ -8,13 +8,13 @@ namespace Microsoft.Azure.Cosmos.Resource.CosmosExceptions
     using System.Net;
     using System.Text;
 
-    internal class CosmosTroubleShootingLinks
+    internal class CosmosTroubleShootingLink
     {
         internal string Link { get; }
         internal int StatusCode { get; }
         internal int? SubStatusCode { get; }
 
-        private CosmosTroubleShootingLinks(
+        private CosmosTroubleShootingLink(
             int statusCode,
             int? subStatusCode,
             string link)
@@ -24,52 +24,57 @@ namespace Microsoft.Azure.Cosmos.Resource.CosmosExceptions
             this.Link = link ?? throw new ArgumentNullException(nameof(link));
         }
 
-        internal static readonly CosmosTroubleShootingLinks NotFound = new CosmosTroubleShootingLinks(
+        internal static readonly CosmosTroubleShootingLink TransportException = new CosmosTroubleShootingLink(
+            statusCode: (int)HttpStatusCode.ServiceUnavailable,
+            subStatusCode: null,
+            link: "http");
+
+        internal static readonly CosmosTroubleShootingLink NotFound = new CosmosTroubleShootingLink(
             statusCode: (int)HttpStatusCode.NotFound,
             subStatusCode: null,
             link: "http");
 
-        internal static readonly CosmosTroubleShootingLinks ToManyRequestRUBudgetExceeded = new CosmosTroubleShootingLinks(
+        internal static readonly CosmosTroubleShootingLink ToManyRequestRUBudgetExceeded = new CosmosTroubleShootingLink(
             statusCode: 429,
             subStatusCode: 3200,
             link: "http");
 
-        internal static readonly CosmosTroubleShootingLinks ToManyRequestStoredProcedureConcurrency = new CosmosTroubleShootingLinks(
+        internal static readonly CosmosTroubleShootingLink ToManyRequestStoredProcedureConcurrency = new CosmosTroubleShootingLink(
             statusCode: 429,
             subStatusCode: 3084,
             link: "http");
 
-        internal static readonly CosmosTroubleShootingLinks NotModified = new CosmosTroubleShootingLinks(
+        internal static readonly CosmosTroubleShootingLink NotModified = new CosmosTroubleShootingLink(
             statusCode: 304,
             subStatusCode: null,
             link: "http");
 
-        internal static readonly CosmosTroubleShootingLinks Conflict = new CosmosTroubleShootingLinks(
+        internal static readonly CosmosTroubleShootingLink Conflict = new CosmosTroubleShootingLink(
             statusCode: 409,
             subStatusCode: null,
             link: "http");
 
-        internal static readonly CosmosTroubleShootingLinks BadRequestConfigurationNameNotFound = new CosmosTroubleShootingLinks(
+        internal static readonly CosmosTroubleShootingLink BadRequestConfigurationNameNotFound = new CosmosTroubleShootingLink(
             statusCode: 400,
             subStatusCode: 1004,
             link: "http");
 
-        internal static readonly CosmosTroubleShootingLinks PreconditionFailed = new CosmosTroubleShootingLinks(
+        internal static readonly CosmosTroubleShootingLink PreconditionFailed = new CosmosTroubleShootingLink(
             statusCode: 412,
             subStatusCode: null,
             link: "http");
 
-        internal static readonly CosmosTroubleShootingLinks RetryWith = new CosmosTroubleShootingLinks(
+        internal static readonly CosmosTroubleShootingLink RetryWith = new CosmosTroubleShootingLink(
             statusCode: 449,
             subStatusCode: null,
             link: "http");
 
-        internal static readonly CosmosTroubleShootingLinks GonePartitionKeyRangeGone = new CosmosTroubleShootingLinks(
+        internal static readonly CosmosTroubleShootingLink GonePartitionKeyRangeGone = new CosmosTroubleShootingLink(
             statusCode: 410,
             subStatusCode: 1002,
             link: "http");
 
-        internal static readonly CosmosTroubleShootingLinks Forbidden = new CosmosTroubleShootingLinks(
+        internal static readonly CosmosTroubleShootingLink Forbidden = new CosmosTroubleShootingLink(
             statusCode: 403,
             subStatusCode: null,
             link: "http");
---
+++ TroubleshootingGuides/template.md
@@ -0,0 +1,31 @@
+## SA1000
+
+<table>
+<tr>
+  <td>TypeName</td>
+  <td>Cosmos{CheckId}{Description}</td>
+</tr>
+<tr>
+  <td>CheckId</td>
+  <td>Cosmos{4 digit unique id}</td>
+</tr>
+<tr>
+  <td>Category</td>
+  <td></td>
+</tr>
+</table>
+
+## Cause
+
+
+
+## Rule description
+
+
+
+## How to fix violations
+
+
+```csharp
+
+```
