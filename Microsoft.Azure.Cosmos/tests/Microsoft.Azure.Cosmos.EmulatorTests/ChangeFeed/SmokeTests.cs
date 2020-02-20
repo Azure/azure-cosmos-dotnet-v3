61905b19bd6216a0fa80481765c863359da85893
Jake Willey <jawilley@microsoft.com>
2020-02-20 07:08:57 -0800

    Fixed tests

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/ChangeFeed/SmokeTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/ChangeFeed/SmokeTests.cs
@@ -11,6 +11,7 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests.ChangeFeed
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos.Query.Core;
+    using Microsoft.Azure.Cosmos.Resource.CosmosExceptions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
@@ -75,7 +76,7 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests.ChangeFeed
                 .WithInstanceName("random")
                 .WithLeaseContainer(notFoundContainer).Build();
 
-            CosmosException exception = await Assert.ThrowsExceptionAsync<CosmosException>(() => processor.StartAsync());
+            CosmosException exception = await Assert.ThrowsExceptionAsync<CosmosNotFoundException>(() => processor.StartAsync());
             Assert.AreEqual(HttpStatusCode.NotFound, exception.StatusCode);
         }
 
