a504c33907460e34dd59331ca939b725877499d3
Matias Quaranta <ealsur@users.noreply.github.com>
2020-02-25 11:35:02 -0800

    Preview - Change Feed Pull model (#1210)
    Change Feed pull model and FeedToken introduction on preview

--- Microsoft.Azure.Cosmos/src/Query/v3Query/QueryIterator.cs
+++ Microsoft.Azure.Cosmos/src/Query/v3Query/QueryIterator.cs
@@ -81,6 +81,13 @@ namespace Microsoft.Azure.Cosmos.Query
 
         public override bool HasMoreResults => !this.cosmosQueryExecutionContext.IsDone;
 
+#if PREVIEW
+        public override
+#else
+        internal
+#endif
+        FeedToken FeedToken => throw new NotImplementedException();
+
         public override async Task<ResponseMessage> ReadNextAsync(CancellationToken cancellationToken = default)
         {
             CosmosDiagnosticsContext diagnostics = CosmosDiagnosticsContext.Create(this.requestOptions);
