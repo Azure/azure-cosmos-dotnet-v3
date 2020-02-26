7b327f5d999e863c70b69e019437016170495175
Matias Quaranta <ealsur@hotmail.com>
2020-02-25 16:42:52 -0800

    Tests

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/FeedToken/QueryFeedTokenTests.cs
@@ -0,0 +1,84 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Globalization;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos.Query;
+    using Microsoft.Azure.Cosmos.Query.Core;
+    using Microsoft.Azure.Cosmos.Query.Core.ContinuationTokens;
+    using Microsoft.Azure.Cosmos.Query.Core.ExecutionContext;
+    using Microsoft.Azure.Cosmos.Query.Core.QueryClient;
+    using Microsoft.Azure.Cosmos.Routing;
+    using Microsoft.Azure.Documents;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
+
+    [TestClass]
+    public class QueryFeedTokenTests : BaseCosmosClientHelper
+    {
+        [TestInitialize]
+        public async Task TestInitialize()
+        {
+            await base.TestInit();
+        }
+
+        [TestCleanup]
+        public async Task Cleanup()
+        {
+            await base.TestCleanup();
+        }
+
+        [TestMethod]
+        public async Task GetTargetPartitionKeyRangesAsyncWithFeedToken()
+        {
+            ContainerCore container = null;
+            try
+            {
+                // Create a container large enough to have at least 2 partitions
+                ContainerResponse containerResponse = await this.database.CreateContainerAsync(
+                    id: Guid.NewGuid().ToString(),
+                    partitionKeyPath: "/pk",
+                    throughput: 15000);
+                container = (ContainerInlineCore)containerResponse;
+
+                // Get all the partition key ranges to verify there is more than one partition
+                IRoutingMapProvider routingMapProvider = await this.cosmosClient.DocumentClient.GetPartitionKeyRangeCacheAsync();
+
+                ContainerQueryProperties containerQueryProperties = new ContainerQueryProperties(
+                    containerResponse.Resource.ResourceId,
+                    null,
+                    containerResponse.Resource.PartitionKey);
+
+                IReadOnlyList<FeedToken> feedTokens = await container.GetFeedTokensAsync();
+
+                Assert.IsTrue(feedTokens.Count > 1, " RUs of the container needs to be increased to ensure at least 2 partitions.");
+
+                // There should only be one range since we get 1 FeedToken per range
+                foreach (FeedToken feedToken in feedTokens)
+                {
+                    List<PartitionKeyRange> partitionKeyRanges = await CosmosQueryExecutionContextFactory.GetTargetPartitionKeyRangesAsync(
+                        queryClient: new CosmosQueryClientCore(container.ClientContext, container),
+                        resourceLink: container.LinkUri.OriginalString,
+                        partitionedQueryExecutionInfo: null,
+                        containerQueryProperties: containerQueryProperties,
+                        properties: null,
+                        feedTokenInternal: feedToken as FeedTokenInternal);
+
+                    Assert.IsTrue(partitionKeyRanges.Count == 1, "Only 1 partition key range should be selected since the FeedToken represents a single range.");
+                }
+            }
+            finally
+            {
+                await container?.DeleteContainerAsync();
+            }
+        }
+    }
+}
