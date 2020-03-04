4275177d1ff2611c9d6f39b9e96843f55a89333a
Brandon Chong <bchong95@users.noreply.github.com>
2020-03-04 08:56:06 -0800

    updated test

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/CosmosClientTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/CosmosClientTests.cs
@@ -5,15 +5,7 @@
 namespace Microsoft.Azure.Cosmos.Tests
 {
     using System;
-    using System.IO;
-    using System.Linq;
-    using System.Net;
-    using System.Net.Http;
-    using Microsoft.Azure.Cosmos.Fluent;
-    using Microsoft.Azure.Documents;
-    using Microsoft.Azure.Documents.Client;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using Moq;
 
     [TestClass]
     public class CosmosClientTests
@@ -22,7 +14,7 @@ namespace Microsoft.Azure.Cosmos.Tests
         public const string ConnectionString = "AccountEndpoint=https://localtestcosmos.documents.azure.com:443/;AccountKey=425Mcv8CXQqzRNCgFNjIhT424GK99CKJvASowTnq15Vt8LeahXTcN5wt3342vQ==;";
 
         [TestMethod]
-        [ExpectedException(typeof(InvalidOperationException))]
+        [ExpectedException(typeof(ObjectDisposedException))]
         public void TestDispose()
         {
             CosmosClient cosmosClient = new CosmosClient(ConnectionString);
