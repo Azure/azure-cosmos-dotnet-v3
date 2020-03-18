b13353610d0bbb701f12ff55d1a190d6cb174740
Brandon Chong <bchong95@users.noreply.github.com>
2020-03-17 17:03:54 -0700

    added request rate too large

---
+++ Microsoft.Azure.Cosmos/src/Resource/CosmosExceptions/Http/RequestRateTooLarge/RequestRateTooLargeException.cs
@@ -0,0 +1,29 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+// This is generated code:
+
+namespace Microsoft.Azure.Cosmos.Resource.CosmosExceptions.Http.RequestRateTooLarge
+{
+    using System;
+    using System.Net;
+
+    internal abstract class RequestRateTooLargeException : CosmosHttpWithSubstatusCodeException
+    {
+        protected RequestRateTooLargeException(TimeSpan retryAfter, int subStatusCode)
+            : this(retryAfter: retryAfter, subStatusCode, message: null)
+        {
+        }
+
+        protected RequestRateTooLargeException(TimeSpan retryAfter, int subStatusCode, string message)
+            : this(retryAfter: retryAfter, subStatusCode, message: message, innerException: null)
+        {
+        }
+
+        protected RequestRateTooLargeException(TimeSpan retryAfter, int subStatusCode, string message, Exception innerException)
+            : base(statusCode: (HttpStatusCode)429, retryAfter: retryAfter, subStatusCode: subStatusCode, message: message, innerException: innerException)
+        {
+        }
+    }
+}
