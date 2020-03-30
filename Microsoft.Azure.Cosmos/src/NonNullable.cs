e3112d0e7f74076fa98060ba4a97c5c07020151a
Brandon Chong <bchong95@users.noreply.github.com>
2020-03-30 09:49:31 -0700

    added non nullable struct

---
+++ Microsoft.Azure.Cosmos/src/NonNullable.cs
@@ -0,0 +1,57 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos
+{
+    using System;
+
+    internal readonly struct NonNullable<T> : IEquatable<NonNullable<T>>
+        where T : class
+    {
+        public NonNullable(T reference)
+        {
+            if (reference == null)
+            {
+                throw new ArgumentNullException(nameof(reference));
+            }
+
+            this.Reference = reference;
+        }
+
+        public T Reference { get; }
+
+        public bool Equals(NonNullable<T> other)
+        {
+            return this.Reference.Equals(other.Reference);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is T reference)
+            {
+                return this.Equals(reference);
+            }
+
+            if (obj is NonNullable<T> nonNullable)
+            {
+                return this.Equals(nonNullable);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Reference.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.Reference.ToString();
+        }
+
+        public static implicit operator T(NonNullable<T> nonNullable) => nonNullable.Reference;
+        public static implicit operator NonNullable<T>(T reference) => new NonNullable<T>(reference);
+    }
+}
---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/NonNullableTests.cs
@@ -0,0 +1,72 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NonNullableTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Constructor_Null()
+        {
+            NonNullable<object> nonNullable = new NonNullable<object>(null);
+        }
+
+        [TestMethod]
+        public void Constructor_NonNull()
+        {
+            object nonNull = new object();
+            NonNullable<object> nonNullable = new NonNullable<object>(nonNull);
+            Assert.AreEqual(nonNull, nonNullable.Reference);
+        }
+
+        [TestMethod]
+        public void ImplicitConversion_ValueToNonNullable()
+        {
+            void Blah(NonNullable<object> foo)
+            {
+            }
+
+            Blah(new object());
+        }
+
+        [TestMethod]
+        public void ImplicitConversion_NonNullableToValue()
+        {
+            void Blah(object foo)
+            {
+            }
+
+            Blah(new NonNullable<object>(new object()));
+        }
+
+        [TestMethod]
+        public void TestEquals()
+        {
+            object reference = new object();
+            NonNullable<object> nonNullable = new NonNullable<object>(reference);
+            Assert.AreEqual(nonNullable, reference);
+        }
+
+        [TestMethod]
+        public void TestGetHashCode()
+        {
+            object reference = new object();
+            NonNullable<object> nonNullable = new NonNullable<object>(reference);
+            Assert.AreEqual(nonNullable.GetHashCode(), reference.GetHashCode());
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            object reference = new object();
+            NonNullable<object> nonNullable = new NonNullable<object>(reference);
+            Assert.AreEqual(nonNullable.ToString(), reference.ToString());
+        }
+    }
+}
