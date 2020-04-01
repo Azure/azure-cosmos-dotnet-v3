95cde5e97bced84eea2204aca1b96387484c15d1
Brandon Chong <bchong95@users.noreply.github.com>
2020-04-01 11:14:04 -0700

    added start from types

--- Microsoft.Azure.Cosmos/src/RequestOptions/ChangeFeedRequestOptions.cs
+++ Microsoft.Azure.Cosmos/src/RequestOptions/ChangeFeedRequestOptions.cs
@@ -20,7 +20,6 @@ namespace Microsoft.Azure.Cosmos
     class ChangeFeedRequestOptions : RequestOptions
     {
         public const string IfNoneMatchAllHeaderValue = "*";
-        public static readonly DateTime DateTimeStartFromBeginning = DateTime.MinValue.ToUniversalTime();
 
         /// <summary>
         /// Gets or sets the maximum number of items to be returned in the enumeration operation in the Azure Cosmos DB service.
@@ -31,13 +30,13 @@ namespace Microsoft.Azure.Cosmos
         public int? MaxItemCount { get; set; }
 
         /// <summary>
-        /// Gets or sets a particular point in time to start to read the change feed.
+        /// Gets or sets where the ChangeFeed operation should start from. If not set then the ChangeFeed operation will start from the begining.
         /// </summary>
         /// <remarks>
         /// Only applies in the case where no FeedToken is provided or the FeedToken was never used in a previous iterator.
         /// In order to read the Change Feed from the beginning, set this to DateTime.MinValue.ToUniversalTime().
         /// </remarks>
-        public DateTime? StartTime { get; set; }
+        public StartFrom From { get; set; }
 
         /// <summary>
         /// Fill the CosmosRequestMessage headers with the set properties
@@ -79,7 +78,7 @@ namespace Microsoft.Azure.Cosmos
             set => throw new NotSupportedException($"{nameof(ChangeFeedRequestOptions)} does not use the {nameof(this.IfNoneMatchEtag)} property.");
         }
 
-        public static void FillPartitionKeyRangeId(RequestMessage request, string partitionKeyRangeId)
+        internal static void FillPartitionKeyRangeId(RequestMessage request, string partitionKeyRangeId)
         {
             Debug.Assert(request != null);
 
@@ -89,14 +88,14 @@ namespace Microsoft.Azure.Cosmos
             }
         }
 
-        public static void FillPartitionKey(RequestMessage request, PartitionKey partitionKey)
+        internal static void FillPartitionKey(RequestMessage request, PartitionKey partitionKey)
         {
             Debug.Assert(request != null);
 
             request.Headers.PartitionKey = partitionKey.ToJsonString();
         }
 
-        public static void FillContinuationToken(RequestMessage request, string continuationToken)
+        internal static void FillContinuationToken(RequestMessage request, string continuationToken)
         {
             Debug.Assert(request != null);
 
@@ -107,7 +106,7 @@ namespace Microsoft.Azure.Cosmos
             }
         }
 
-        public static void FillMaxItemCount(RequestMessage request, int? maxItemCount)
+        internal static void FillMaxItemCount(RequestMessage request, int? maxItemCount)
         {
             Debug.Assert(request != null);
 
@@ -115,6 +114,117 @@ namespace Microsoft.Azure.Cosmos
             {
                 request.Headers.Add(HttpConstants.HttpHeaders.PageSize, maxItemCount.Value.ToString(CultureInfo.InvariantCulture));
             }
+        }
+
+        /// <summary>
+        /// Base class for where to start a ChangeFeed operation in <see cref="ChangeFeedRequestOptions"/>.
+        /// </summary>
+        /// <remarks>Use one of the static constructors to generate a StartFrom option.</remarks>
+        public abstract class StartFrom
+        {
+            /// <summary>
+            /// Initializes an instance of the <see cref="StartFrom"/> class.
+            /// </summary>
+            protected StartFrom()
+            {
+            }
+
+            /// <summary>
+            /// Creates a <see cref="StartFrom"/> that tells the ChangeFeed operation to start reading changes from this moment onward.
+            /// </summary>
+            /// <returns>A <see cref="StartFrom"/> that tells the ChangeFeed operation to start reading changes from this moment onward.</returns>
+            public static StartFrom CreateFromNow()
+            {
+                return StartFromNow.Singleton;
+            }
+
+            /// <summary>
+            /// Creates a <see cref="StartFrom"/> that tells the ChangeFeed operation to start reading changes from some point in time onward.
+            /// </summary>
+            /// <param name="dateTime">The time to start reading from.</param>
+            /// <returns>A <see cref="StartFrom"/> that tells the ChangeFeed operation to start reading changes from some point in time onward.</returns>
+            public static StartFrom CreateFromTime(DateTime dateTime)
+            {
+                return new StartFromTime(dateTime);
+            }
+
+            /// <summary>
+            /// Creates a <see cref="StartFrom"/> that tells the ChangeFeed operation to start reading changes from a save point.
+            /// </summary>
+            /// <param name="continuation">The continuation to resume from.</param>
+            /// <returns>A <see cref="StartFrom"/> that tells the ChangeFeed operation to start reading changes from a save point.</returns>
+            public static StartFrom CreateFromContinuation(string continuation)
+            {
+                return new StartFromContinuation(continuation);
+            }
+        }
+
+        /// <summary>
+        /// Derived instance of <see cref="StartFrom"/> that tells the ChangeFeed operation to start reading changes from this moment onward.
+        /// </summary>
+        internal sealed class StartFromNow : StartFrom
+        {
+            public static readonly StartFromNow Singleton = new StartFromNow();
+
+            /// <summary>
+            /// Intializes an instance of the <see cref="StartFromNow"/> class.
+            /// </summary>
+            public StartFromNow()
+                : base()
+            {
+            }
+        }
+
+        /// <summary>
+        /// Derived instance of <see cref="StartFrom"/> that tells the ChangeFeed operation to start reading changes from some point in time onward.
+        /// </summary>
+        internal sealed class StartFromTime : StartFrom
+        {
+            /// <summary>
+            /// Initializes an instance of the <see cref="StartFromTime"/> class.
+            /// </summary>
+            /// <param name="time">The time to start reading from.</param>
+            public StartFromTime(DateTime time)
+                : base()
+            {
+                if (time.Kind != DateTimeKind.Utc)
+                {
+                    throw new ArgumentOutOfRangeException($"{nameof(time)}.{nameof(DateTime.Kind)} must be {nameof(DateTimeKind)}.{nameof(DateTimeKind.Utc)}");
+                }
+
+                this.Time = time;
+            }
+
+            /// <summary>
+            /// Gets the time the ChangeFeed operation should start reading from.
+            /// </summary>
+            public DateTime Time { get; }
+        }
+
+        /// <summary>
+        /// Derived instance of <see cref="StartFrom"/> that tells the ChangeFeed operation to start reading changes from a save point.
+        /// </summary>
+        internal sealed class StartFromContinuation : StartFrom
+        {
+            /// <summary>
+            /// Initializes an instance of the <see cref="StartFromContinuation"/> class.
+            /// </summary>
+            /// <param name="continuation">The continuation to resume from.</param>
+            public StartFromContinuation(string continuation)
+                : base()
+            {
+                if (string.IsNullOrWhiteSpace(continuation))
+                {
+                    throw new ArgumentOutOfRangeException($"{nameof(continuation)} must not be null, empty, or whitespace.");
+                }
+
+                this.Continuation = continuation;
+            }
+
+            /// <summary>
+            /// Gets the continuation to resume from.
+            /// </summary>
+            public string Continuation { get; }
         }
     }
 }
