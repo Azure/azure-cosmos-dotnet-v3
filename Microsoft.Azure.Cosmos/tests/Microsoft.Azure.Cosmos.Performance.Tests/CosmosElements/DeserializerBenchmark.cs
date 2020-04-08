05127c450752a4b927cb0f3ebd7eade63253ca53
Brandon Chong <bchong95@users.noreply.github.com>
2020-04-08 11:19:13 -0700

    added benchmark against newtonsoft

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/CosmosElements/DeserializerBenchmark.cs
@@ -0,0 +1,44 @@
+﻿namespace Microsoft.Azure.Cosmos.Performance.Tests.CosmosElements
+{
+    using System;
+    using System.Collections.Generic;
+    using BenchmarkDotNet.Attributes;
+    using Microsoft.Azure.Cosmos.CosmosElements;
+    using Microsoft.Azure.Cosmos.Json;
+    using Microsoft.Azure.Cosmos.Performance.Tests.Models;
+    using Microsoft.Azure.Cosmos.Query.Core.Monads;
+    using Newtonsoft.Json;
+
+    [MemoryDiagnoser]
+    public class DeserializerBenchmark
+    {
+        private readonly string json;
+        private readonly ReadOnlyMemory<byte> textBuffer;
+
+        public DeserializerBenchmark()
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < 1000; i++)
+            {
+                people.Add(Person.GetRandomPerson());
+            }
+
+            this.json = JsonConvert.SerializeObject(people);
+            IJsonWriter jsonTextWriter = Json.JsonWriter.Create(JsonSerializationFormat.Text);
+            CosmosElement.Parse(this.json).WriteTo(jsonTextWriter);
+            this.textBuffer = jsonTextWriter.GetResult();
+        }
+
+        [Benchmark]
+        public void CosmosElementDeserializer_TryDeserialize()
+        {
+            TryCatch<IReadOnlyList<Person>> tryCatch = CosmosElementDeserializer.TryDeserialize<IReadOnlyList<Person>>(this.textBuffer);
+        }
+
+        [Benchmark]
+        public void JsonConvert_DeserializeObject()
+        {
+            IReadOnlyList<Person> deserializedPeople = JsonConvert.DeserializeObject<IReadOnlyList<Person>>(this.json);
+        }
+    }
+}
