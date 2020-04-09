6978fe963ae4bf815508b0fc1654c06e0c8deb39
Brandon Chong <bchong95@users.noreply.github.com>
2020-04-09 09:53:44 -0700

    added benchmark code

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/CosmosElements/SerializerBenchmark.cs
@@ -0,0 +1,52 @@
+﻿namespace Microsoft.Azure.Cosmos.Performance.Tests.CosmosElements
+{
+    using System.Collections.Generic;
+    using BenchmarkDotNet.Attributes;
+    using Microsoft.Azure.Cosmos.Json;
+    using Microsoft.Azure.Cosmos.Json.Interop;
+    using Microsoft.Azure.Cosmos.Performance.Tests.Models;
+    using Newtonsoft.Json;
+
+    [MemoryDiagnoser]
+    public class ToStringBenchmark
+    {
+        private readonly IReadOnlyList<Person> people;
+
+        public ToStringBenchmark()
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < 1000; i++)
+            {
+                people.Add(Person.GetRandomPerson());
+            }
+
+            this.people = people;
+        }
+
+        [Benchmark]
+        public void CosmosElement_Serializer_Text()
+        {
+            Serializer.Serialize(this.people, JsonSerializationFormat.Text);
+        }
+
+        [Benchmark]
+        public void CosmosElement_Serializer_Binary()
+        {
+            Serializer.Serialize(this.people, JsonSerializationFormat.Text);
+        }
+
+        [Benchmark]
+        public void Newtonsoft_Serializer_Text()
+        {
+            JsonConvert.SerializeObject(this.people);
+        }
+
+        [Benchmark]
+        public void Newtonsoft_Serializer_Binary()
+        {
+            JsonSerializer jsonSerializer = JsonSerializer.CreateDefault();
+            CosmosDBToNewtonsoftWriter writer = new CosmosDBToNewtonsoftWriter(JsonSerializationFormat.Binary);
+            jsonSerializer.Serialize(writer, this.people);
+        }
+    }
+}
