d4498a7dcf7e0819141a57829bedc6cf9fa5ed97
j82w <j82w@users.noreply.github.com>
2020-04-13 13:57:47 -0700

    ReplaceItemAsync documentation: Fix id comment
    ReplaceItemAsync allows updating the id value. Only the PartitionKeyValue is required to be the same.

--- Microsoft.Azure.Cosmos/src/Resource/Container/Container.cs
+++ Microsoft.Azure.Cosmos/src/Resource/Container/Container.cs
@@ -652,7 +652,7 @@ namespace Microsoft.Azure.Cosmos
         /// To change an item's partition key value you must delete the original item and insert a new item.
         /// </remarks>
         /// <param name="item">A JSON serializable object that must contain an id property. <see cref="CosmosSerializer"/> to implement a custom serializer.</param>
-        /// <param name="id">The Cosmos item id, which is expected to match the value within T.</param>
+        /// <param name="id">The Cosmos item id of the existing item.</param>
         /// <param name="partitionKey"><see cref="PartitionKey"/> for the item. If not specified will be populated by extracting from {T}</param>
         /// <param name="requestOptions">(Optional) The options for the item request.</param>
         /// <param name="cancellationToken">(Optional) <see cref="CancellationToken"/> representing request cancellation.</param>
