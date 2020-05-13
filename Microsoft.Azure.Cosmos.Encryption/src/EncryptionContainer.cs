1b8aa08a996d7e83ec938adb9f1cc6fee8bd5602
anujtoshniwal <62551957+anujtoshniwal@users.noreply.github.com>
2020-05-13 20:30:05 +0530

    Do not attempt to encrypt if EncryptionOptions is null (#1510)

--- Microsoft.Azure.Cosmos.Encryption/src/EncryptionContainer.cs
+++ Microsoft.Azure.Cosmos.Encryption/src/EncryptionContainer.cs
@@ -54,7 +54,8 @@ namespace Microsoft.Azure.Cosmos.Encryption
                 throw new ArgumentNullException(nameof(item));
             }
 
-            if (requestOptions is EncryptionItemRequestOptions encryptionItemRequestOptions)
+            if (requestOptions is EncryptionItemRequestOptions encryptionItemRequestOptions &&
+                encryptionItemRequestOptions.EncryptionOptions != null)
             {
                 if (partitionKey == null)
                 {
@@ -97,7 +98,8 @@ namespace Microsoft.Azure.Cosmos.Encryption
             CosmosDiagnosticsContext diagnosticsContext = CosmosDiagnosticsContext.Create(requestOptions);
             using (diagnosticsContext.CreateScope("CreateItemStream"))
             {
-                if (requestOptions is EncryptionItemRequestOptions encryptionItemRequestOptions)
+                if (requestOptions is EncryptionItemRequestOptions encryptionItemRequestOptions &&
+                    encryptionItemRequestOptions.EncryptionOptions != null)
                 {
                     streamPayload = await EncryptionProcessor.EncryptAsync(
                         streamPayload,
@@ -221,7 +223,8 @@ namespace Microsoft.Azure.Cosmos.Encryption
                 throw new ArgumentNullException(nameof(item));
             }
 
-            if (requestOptions is EncryptionItemRequestOptions encryptionItemRequestOptions)
+            if (requestOptions is EncryptionItemRequestOptions encryptionItemRequestOptions &&
+                encryptionItemRequestOptions.EncryptionOptions != null)
             {
                 if (partitionKey == null)
                 {
@@ -272,7 +275,8 @@ namespace Microsoft.Azure.Cosmos.Encryption
             CosmosDiagnosticsContext diagnosticsContext = CosmosDiagnosticsContext.Create(requestOptions);
             using (diagnosticsContext.CreateScope("ReplaceItemStream"))
             {
-                if (requestOptions is EncryptionItemRequestOptions encryptionItemRequestOptions)
+                if (requestOptions is EncryptionItemRequestOptions encryptionItemRequestOptions &&
+                    encryptionItemRequestOptions.EncryptionOptions != null)
                 {
                     if (partitionKey == null)
                     {
@@ -363,7 +367,8 @@ namespace Microsoft.Azure.Cosmos.Encryption
             CosmosDiagnosticsContext diagnosticsContext = CosmosDiagnosticsContext.Create(requestOptions);
             using (diagnosticsContext.CreateScope("UpsertItemStream"))
             {
-                if (requestOptions is EncryptionItemRequestOptions encryptionItemRequestOptions)
+                if (requestOptions is EncryptionItemRequestOptions encryptionItemRequestOptions &&
+                    encryptionItemRequestOptions.EncryptionOptions != null)
                 {
                     if (partitionKey == null)
                     {
