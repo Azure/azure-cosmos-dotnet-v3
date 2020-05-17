8307377ad5257547647286f1276d42f51cee848a
Jake Willey <jawilley@microsoft.com>
2020-05-17 10:36:33 -0700

    Add feed benchmark

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Benchmarks/Book.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BenchmarkSDK
+{
+    public class Book
+    {
+        public string id { get; set; }
+        public string pk { get; set; }
+        public int taskNum { get; set; }
+        public double cost { get; set; }
+        public string description { get; set; }
+        public string status { get; set; }
+        public string CamelCase { get; set; }
+
+        public bool valid { get; set; }
+
+        public Book[] children { get; set; }
+
+        public override bool Equals(Object obj)
+        {
+            Book input = obj as Book;
+            if (input == null)
+            {
+                return false;
+            }
+
+            return string.Equals(this.id, input.id)
+                && string.Equals(this.pk, input.pk)
+                && this.taskNum == input.taskNum
+                && this.cost == input.cost
+                && string.Equals(this.description, input.description)
+                && string.Equals(this.status, input.status);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public static Book CreateRandomBook(
+            string pk = null, 
+            string id = null)
+        {
+            if (string.IsNullOrEmpty(pk))
+            {
+                pk = "TBD" + Guid.NewGuid().ToString();
+            }
+            if (id == null)
+            {
+                id = Guid.NewGuid().ToString();
+            }
+            return new Book()
+            {
+                id = id,
+                pk = pk,
+                description = "CreateRandomToDoActivity",
+                status = "something" + Guid.NewGuid(),
+                taskNum = 42,
+                cost = double.MaxValue,
+                CamelCase = "camelCase",
+                children = new Book[]
+                { 
+                    new Book { id = "child1", taskNum = 30 },
+                    new Book { id = "child2", taskNum = 40}
+                },
+                valid = true
+            };
+        }
+    }
+}
---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Benchmarks/FeedBenchmark.cs
@@ -0,0 +1,133 @@
+﻿// ----------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ----------------------------------------------------------------
+
+namespace BenchmarkSDK
+{
+    using BenchmarkDotNet.Attributes;
+    using Microsoft.Azure.Cosmos;
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    [MemoryDiagnoser]
+    public class FeedBenchmark
+    {
+        private CosmosClient client;
+        private Container containerOneHundred;
+        private Container containerOneThousand;
+        private Container containerTenThousand;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ItemBenchmark"/> class.
+        /// </summary>
+        public FeedBenchmark()
+        {
+        }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            this.client = new CosmosClient(
+                accountEndpoint: null,
+                authKeyOrResourceToken: null);
+
+
+            Database db = this.client.CreateDatabaseIfNotExistsAsync("BenchMarkDb").GetAwaiter().GetResult();
+            this.containerOneHundred = this.CreateAndPopulateContainer(db, "OneHundredItems", 100);
+            this.containerOneThousand = this.CreateAndPopulateContainer(db, "OneThousandItems", 1000);
+            this.containerTenThousand = this.CreateAndPopulateContainer(db, "TenThousandItems", 10000);
+        }
+
+        [Benchmark]
+        public async Task ReadFeedOfT100()
+        {
+            await this.ReadFeedOfT(this.containerOneHundred, 100);
+        }
+
+        [Benchmark]
+        public async Task ReadFeedOfT1000()
+        {
+            await this.ReadFeedOfT(this.containerOneThousand, 1000);
+        }
+
+        [Benchmark]
+        public async Task ReadFeedOfT10000()
+        {
+            await this.ReadFeedOfT(this.containerTenThousand, 10000);
+        }
+
+        private async Task ReadFeedOfT(Container container, int count)
+        {
+            FeedIterator<Book> resultIterator = container.GetItemQueryIterator<Book>();
+            List<Book> allBooks = new List<Book>();
+            while (resultIterator.HasMoreResults)
+            {
+                FeedResponse<Book> response = await resultIterator.ReadNextAsync();
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception();
+                }
+
+                allBooks.AddRange(response);
+            }
+
+            if (allBooks.Count != count)
+            {
+                throw new Exception(allBooks.Count.ToString());
+            }
+        }
+
+        [Benchmark]
+        public async Task ReadFeedStream100()
+        {
+            await this.ReadFeedStream(this.containerOneHundred);
+        }
+
+        [Benchmark]
+        public async Task ReadFeedStream1000()
+        {
+            await this.ReadFeedStream(this.containerOneThousand);
+        }
+
+        [Benchmark]
+        public async Task ReadFeedStream10000()
+        {
+            await this.ReadFeedStream(this.containerTenThousand);
+        }
+
+        private async Task ReadFeedStream(Container container)
+        {
+            FeedIterator resultIterator = container.GetItemQueryStreamIterator();
+            while (resultIterator.HasMoreResults)
+            {
+                using(ResponseMessage response = await resultIterator.ReadNextAsync())
+                {
+                    response.EnsureSuccessStatusCode();
+                }
+            }
+        }
+
+        private Container CreateAndPopulateContainer(Database db, string id, int count)
+        {
+            ContainerResponse containerResponse = db.CreateContainerIfNotExistsAsync(
+               id,
+               "/pk",
+               10000).GetAwaiter().GetResult();
+
+            Container container = containerResponse;
+            if (containerResponse.StatusCode == HttpStatusCode.Created)
+            {
+                Book book = Book.CreateRandomBook(pk: "Book");
+                for (int i = 0; i < count; i++)
+                {
+                    book.id = Guid.NewGuid().ToString();
+                    container.CreateItemAsync<Book>(book, new PartitionKey(book.pk)).GetAwaiter().GetResult();
+                }
+            }
+
+            return container;
+        }
+    }
+}
