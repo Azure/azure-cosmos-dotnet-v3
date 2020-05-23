162d4f6fc98cef743a75268acd5ec84dc4c18050
kirankumarkolli <kirankk@microsoft.com>
2020-05-23 14:13:53 +0530

    usings refresh

--- Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/Program.cs
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/Program.cs
@@ -1,7 +1,6 @@
 ﻿namespace CosmosBenchmark
 {
     using System;
-    using System.Collections;
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.Diagnostics.Tracing;
