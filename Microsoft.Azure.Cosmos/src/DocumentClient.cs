e609c4b3900ea832f7ca1bf6a51011cee329774b
Matias Quaranta <ealsur@hotmail.com>
2020-05-26 09:39:09 -0700

    Gateway has a different default

--- Microsoft.Azure.Cosmos/src/DocumentClient.cs
+++ Microsoft.Azure.Cosmos/src/DocumentClient.cs
@@ -110,6 +110,8 @@ namespace Microsoft.Azure.Cosmos
         private const bool DefaultEnableCpuMonitor = true;
         private const bool EnableAuthFailureTraces = false;
 
+        private static readonly TimeSpan GatewayRequestTimeout = TimeSpan.FromSeconds(65);
+        // Gateway has backoff/retry logic to hide transient errors.
         private readonly IDictionary<string, List<PartitionKeyAndResourceTokenPair>> resourceTokens;
         private RetryPolicy retryPolicy;
         private bool allowOverrideStrongerConsistency = false;
@@ -581,7 +583,7 @@ namespace Microsoft.Azure.Cosmos
                 httpClient = messageHandler == null ? new HttpClient() : new HttpClient(messageHandler);
             }
 
-            httpClient.Timeout = connectionPolicy.RequestTimeout;
+            httpClient.Timeout = (connectionPolicy.RequestTimeout > DocumentClient.GatewayRequestTimeout) ? connectionPolicy.RequestTimeout : DocumentClient.GatewayRequestTimeout;
             httpClient.DefaultRequestHeaders.CacheControl = new CacheControlHeaderValue { NoCache = true };
 
             httpClient.AddUserAgentHeader(connectionPolicy.UserAgentContainer);
