59a32e083288f47f5a363fcb42a765f14447d3e0
Jake Willey <jawilley@microsoft.com>
2020-05-29 14:52:17 -0700

    Fixed merge issue

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/TransportWrapperTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/TransportWrapperTests.cs
@@ -194,7 +194,6 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
                     itemLSN: 5,
                     sessionToken: null,
                     usingLocalLSN: true,
-                    ,
                     activityId: Guid.NewGuid().ToString()));
 
                 throw Documents.Rntbd.TransportExceptions.GetServiceUnavailableException(physicalAddress, Guid.NewGuid(),
