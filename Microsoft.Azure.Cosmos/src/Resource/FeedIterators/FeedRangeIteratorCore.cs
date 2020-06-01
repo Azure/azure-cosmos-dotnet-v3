cd89034845c910e3298506f86fb0d0e984723bd0
Matias Quaranta <ealsur@hotmail.com>
2020-06-01 08:45:48 -0700

    Wire conflicts

--- Microsoft.Azure.Cosmos/src/Resource/FeedIterators/FeedRangeIteratorCore.cs
+++ Microsoft.Azure.Cosmos/src/Resource/FeedIterators/FeedRangeIteratorCore.cs
@@ -7,17 +7,15 @@ namespace Microsoft.Azure.Cosmos
     using System;
     using System.Collections.Generic;
     using System.IO;
-    using System.Runtime.InteropServices;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos.CosmosElements;
-    using Microsoft.Azure.Cosmos.Json;
-    using Microsoft.Azure.Cosmos.Json.Interop;
     using Microsoft.Azure.Cosmos.Query.Core;
     using Microsoft.Azure.Cosmos.Query.Core.Monads;
     using Microsoft.Azure.Cosmos.Resource.CosmosExceptions;
     using Microsoft.Azure.Cosmos.Serializer;
     using Microsoft.Azure.Documents;
+    using static Microsoft.Azure.Documents.RuntimeConstants;
 
     /// <summary>
     /// Cosmos feed stream iterator. This is used to get the query responses with a Stream content
@@ -30,19 +28,23 @@ namespace Microsoft.Azure.Cosmos
         private readonly CosmosClientContext clientContext;
         private readonly QueryRequestOptions queryRequestOptions;
         private readonly AsyncLazy<TryCatch<string>> lazyContainerRid;
+        private readonly ResourceType resourceType;
+        private readonly SqlQuerySpec querySpec;
         private bool hasMoreResultsInternal;
 
         public static FeedRangeIteratorCore Create(
             ContainerInternal containerCore,
             FeedRangeInternal feedRangeInternal,
             string continuation,
-            QueryRequestOptions options)
+            QueryRequestOptions options,
+            ResourceType resourceType = ResourceType.Document,
+            QueryDefinition queryDefinition = null)
         {
             if (!string.IsNullOrEmpty(continuation))
             {
                 if (FeedRangeContinuation.TryParse(continuation, out FeedRangeContinuation feedRangeContinuation))
                 {
-                    return new FeedRangeIteratorCore(containerCore, feedRangeContinuation, options);
+                    return new FeedRangeIteratorCore(containerCore, feedRangeContinuation, options, resourceType, queryDefinition);
                 }
 
                 // Backward compatible with old format
@@ -59,11 +61,11 @@ namespace Microsoft.Azure.Cosmos
                                 isMaxInclusive: false)
                     },
                     continuation);
-                return new FeedRangeIteratorCore(containerCore, feedRangeContinuation, options);
+                return new FeedRangeIteratorCore(containerCore, feedRangeContinuation, options, resourceType, queryDefinition);
             }
 
             feedRangeInternal = feedRangeInternal ?? FeedRangeEPK.ForCompleteRange();
-            return new FeedRangeIteratorCore(containerCore, feedRangeInternal, options);
+            return new FeedRangeIteratorCore(containerCore, feedRangeInternal, options, resourceType, queryDefinition);
         }
 
         /// <summary>
@@ -72,8 +74,10 @@ namespace Microsoft.Azure.Cosmos
         internal FeedRangeIteratorCore(
             ContainerInternal containerCore,
             FeedRangeContinuation feedRangeContinuation,
-            QueryRequestOptions options)
-            : this(containerCore, feedRangeContinuation.FeedRange, options)
+            QueryRequestOptions options,
+            ResourceType resourceType,
+            QueryDefinition queryDefinition)
+            : this(containerCore, feedRangeContinuation.FeedRange, options, resourceType, queryDefinition)
         {
             this.FeedRangeContinuation = feedRangeContinuation;
         }
@@ -81,20 +85,26 @@ namespace Microsoft.Azure.Cosmos
         private FeedRangeIteratorCore(
             ContainerInternal containerCore,
             FeedRangeInternal feedRangeInternal,
-            QueryRequestOptions options)
-            : this(containerCore, options)
+            QueryRequestOptions options,
+            ResourceType resourceType,
+            QueryDefinition queryDefinition)
+            : this(containerCore, options, resourceType, queryDefinition)
         {
             this.FeedRangeInternal = feedRangeInternal ?? throw new ArgumentNullException(nameof(feedRangeInternal));
         }
 
         private FeedRangeIteratorCore(
             ContainerInternal containerCore,
-            QueryRequestOptions options)
+            QueryRequestOptions options,
+            ResourceType resourceType,
+            QueryDefinition queryDefinition)
         {
             this.containerCore = containerCore ?? throw new ArgumentNullException(nameof(containerCore));
             this.clientContext = containerCore.ClientContext;
             this.queryRequestOptions = options;
             this.hasMoreResultsInternal = true;
+            this.resourceType = resourceType;
+            this.querySpec = queryDefinition?.ToSqlQuerySpec();
             this.lazyContainerRid = new AsyncLazy<TryCatch<string>>(valueFactory: (innerCancellationToken) =>
             {
                 return this.TryInitializeContainerRIdAsync(innerCancellationToken);
@@ -154,20 +164,32 @@ namespace Microsoft.Azure.Cosmos
             CancellationToken cancellationToken = default)
         {
             cancellationToken.ThrowIfCancellationRequested();
+            Stream stream = null;
+            OperationType operation = OperationType.ReadFeed;
+            if (this.querySpec != null)
+            {
+                stream = this.clientContext.SerializerCore.ToStreamSqlQuerySpec(this.querySpec, this.resourceType);
+                operation = OperationType.Query;
+            }
 
             ResponseMessage responseMessage = await this.clientContext.ProcessResourceOperationStreamAsync(
                resourceUri: this.containerCore.LinkUri,
-               resourceType: ResourceType.Document,
-               operationType: OperationType.ReadFeed,
+               resourceType: this.resourceType,
+               operationType: operation,
                requestOptions: this.queryRequestOptions,
                cosmosContainerCore: this.containerCore,
                partitionKey: this.queryRequestOptions?.PartitionKey,
-               streamPayload: null,
+               streamPayload: stream,
                requestEnricher: request =>
                {
                    FeedRangeVisitor feedRangeVisitor = new FeedRangeVisitor(request);
                    this.FeedRangeInternal.Accept(feedRangeVisitor);
                    this.FeedRangeContinuation.Accept(feedRangeVisitor, QueryRequestOptions.FillContinuationToken);
+                   if (this.querySpec != null)
+                   {
+                       request.Headers.Add(HttpConstants.HttpHeaders.ContentType, MediaTypes.QueryJson);
+                       request.Headers.Add(HttpConstants.HttpHeaders.IsQuery, bool.TrueString);
+                   }
                },
                diagnosticsContext: diagnostics,
                cancellationToken: cancellationToken);
