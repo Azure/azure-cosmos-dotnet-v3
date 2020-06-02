80ee6db0c7910fa7f4b060939d703ea2025b34c9
Brandon Chong <bchong95@users.noreply.github.com>
2020-06-01 18:31:57 -0700

    initial draft

---
+++ Microsoft.Azure.Cosmos/src/Query/Core/QueryClient/CosmosQueryExecutionInfo.cs
@@ -0,0 +1,24 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Query.Core.QueryClient
+{
+    using Newtonsoft.Json;
+
+    internal sealed class CosmosQueryExecutionInfo
+    {
+        [JsonConstructor]
+        public CosmosQueryExecutionInfo(bool reverseRidEnabled, bool reverseIndexScan)
+        {
+            this.ReverseRidEnabled = reverseRidEnabled;
+            this.ReverseIndexScan = reverseIndexScan;
+        }
+
+        [JsonProperty("reverseRidEnabled")]
+        public bool ReverseRidEnabled { get; }
+
+        [JsonProperty("reverseIndexScan")]
+        public bool ReverseIndexScan { get; }
+    }
+}
