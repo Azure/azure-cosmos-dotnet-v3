6e0ab98c3a4fd87bae3205da9bea37d6cb9de9cd
Matias Quaranta <ealsur@hotmail.com>
2020-06-04 10:11:04 -0700

    test

--- Microsoft.Azure.Cosmos/tests/Azure.Cosmos/Azure.Cosmos.Tests/Diagnostics/DiagnosticsScopeTests.cs
+++ Microsoft.Azure.Cosmos/tests/Azure.Cosmos/Azure.Cosmos.Tests/Diagnostics/DiagnosticsScopeTests.cs
@@ -1,7 +1,9 @@
 ﻿namespace Azure.Cosmos.Tests
 {
     using System.Collections.Generic;
+    using System.Diagnostics.Tracing;
     using System.Threading.Tasks;
+    using Azure.Core.Diagnostics;
     using Azure.Cosmos.Diagnostics;
     using Microsoft.Azure.Cosmos;
     using Microsoft.Azure.Documents;
@@ -30,6 +32,22 @@
                 new KeyValuePair<string, string>(DiagnosticProperty.Container, "dbs/test/colls/test"),
                 new KeyValuePair<string, string>(DiagnosticProperty.ResourceType, ResourceType.Document.ToString()),
                 new KeyValuePair<string, string>(DiagnosticProperty.OperationType, OperationType.Create.ToString()));
+        }
+
+        [TestMethod]
+        public async Task VerifyConsoleLogger()
+        {
+            AzureEventSourceListener.CreateConsoleLogger(EventLevel.Informational);
+
+            CosmosClient client = MockCosmosUtil.CreateMockCosmosClient();
+
+            TestHandler testHandler = new TestHandler((request, cancellationToken) =>
+            {
+                return TestHandler.ReturnSuccess(new MockDiagnostics());
+            });
+
+            client.RequestHandler.InnerHandler = testHandler;
+            await client.GetContainer("test", "test").CreateItemAsync(new Dictionary<string, object> { { "id", "test" } });
         }
 
         private class MockDiagnostics : CosmosDiagnostics
