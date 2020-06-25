4ce4a13c890d449ae9559701e1ae74493fb29e3e
Brandon Chong <bchong95@users.noreply.github.com>
2020-06-25 13:32:44 -0700

    made the single partition range  enumerator more plug and play

--- Microsoft.Azure.Cosmos/src/Pagination/PartitionRangePageEnumerator.cs
+++ Microsoft.Azure.Cosmos/src/Pagination/PartitionRangePageEnumerator.cs
@@ -5,6 +5,7 @@
 namespace Microsoft.Azure.Cosmos.Pagination
 {
     using System.Collections.Generic;
+    using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos.Query.Core.Monads;
     using Microsoft.Azure.Cosmos.Routing;
@@ -14,14 +15,30 @@ namespace Microsoft.Azure.Cosmos.Pagination
     /// </summary>
     internal abstract class PartitionRangePageEnumerator : IAsyncEnumerator<TryCatch<Page>>
     {
+        private bool hasStarted;
+
         public FeedRange Range { get; }
 
-        public TryCatch<Page> Current { get; protected set; }
+        public TryCatch<Page> Current { get; private set; }
+
+        public State State { get; private set; }
+
+        public async ValueTask<bool> MoveNextAsync()
+        {
+            if (this.hasStarted && (this.State == default))
+            {
+                return false;
+            }
+
+            (TryCatch<Page> page, State state) = await this.GetNextPageAsync(this.State);
+            this.State = state;
+            this.Current = page;
+
+            return true;
+        }
+
+        public abstract Task<(TryCatch<Page>, State)> GetNextPageAsync(State state, CancellationToken cancellationToken = default);
 
         public abstract ValueTask DisposeAsync();
-
-        public abstract State GetState();
-
-        public abstract ValueTask<bool> MoveNextAsync();
     }
 }
