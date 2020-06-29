01e51673a5b7ab5bd90f8aacd7f8d365c5201a7e
Jake Willey <jawilley@microsoft.com>
2020-06-29 13:34:53 -0700

    Fix updatecontracts script messages. Add bad contract to verify tests CI..

--- UpdateContracts.ps1
+++ UpdateContracts.ps1
@@ -5,24 +5,20 @@
 #Run the Cosmos DB SDK GA contract tests
 $projResult = dotnet test '.\Microsoft.Azure.Cosmos\tests\Microsoft.Azure.Cosmos.Tests\Microsoft.Azure.Cosmos.Tests.csproj' --filter "TestCategory=UpdateContract" --configuration Release
 
-$noContractsUpdated = $true
-
 $updatedContractFile = ".\Microsoft.Azure.Cosmos\tests\Microsoft.Azure.Cosmos.Tests\bin\Release\netcoreapp2.0\Contracts\DotNetSDKAPIChanges.json"
 if(!(Test-Path -Path $updatedContractFile)){
-    Write-warning ("Please run ContractEnforcement.ContractChanges tests to generate an updated contract at " + $updatedContractFile)
+    Write-Error ("The contract file did not get updated with the build. Please fix the test to output the contract file: " + $updatedContractFile)
 }else{
     Copy-Item -Path $updatedContractFile -Destination ".\Microsoft.Azure.Cosmos\tests\Microsoft.Azure.Cosmos.Tests\Contracts\DotNetSDKAPI.json"
     Write-Output ("Updated contract " + $updatedContractFile)
-    $noContractsUpdated = $false
 }
 
 $updatedContractFolder = ".\Microsoft.Azure.Cosmos\tests\Microsoft.Azure.Cosmos.Tests\bin\Release\netcoreapp2.0\BaselineTest\TestOutput\*"
 if(!(Test-Path -Path $updatedContractFolder)){
-    Write-warning ("Please run tests to generate an updated contract at " + $updatedContractFolder)
+    Write-Error ("The contract file did not get updated with the build. Please fix the test to output the contract file: " + $updatedContractFile)
 }else{
     Copy-Item -Path $updatedContractFolder -Destination ".\Microsoft.Azure.Cosmos\tests\Microsoft.Azure.Cosmos.Tests\BaselineTest\TestBaseline" -Recurse
     Write-Output ("Updated contract " + $updatedContractFolder)
-    $noContractsUpdated = $false
 }
 
 #Run the Cosmos DB SDK Preview contract tests
@@ -30,11 +26,10 @@ $projResult = dotnet test '.\Microsoft.Azure.Cosmos\tests\Microsoft.Azure.Cosmos
 
 $updatedContractFile = ".\Microsoft.Azure.Cosmos\tests\Microsoft.Azure.Cosmos.Tests\bin\Release\netcoreapp2.0\Contracts\DotNetPreviewSDKAPIChanges.json"
 if(!(Test-Path -Path $updatedContractFile)){
-    Write-warning ("Please run ContractEnforcement.ContractChanges tests to generate an updated contract at " + $updatedContractFile)
+    Write-Error ("The contract file did not get updated with the preview build. Please fix the test to output the contract file: " + $updatedContractFile)
 }else{
     Copy-Item -Path $updatedContractFile -Destination ".\Microsoft.Azure.Cosmos\tests\Microsoft.Azure.Cosmos.Tests\Contracts\DotNetPreviewSDKAPI.json"
     Write-Output ("Updated contract " + $updatedContractFile)
-    $noContractsUpdated = $false
 }
 
 #Run the Encryption SDK contract tests
@@ -42,13 +37,8 @@ $projResult = dotnet test '.\Microsoft.Azure.Cosmos.Encryption\tests\Microsoft.A
 
 $updatedContractFile = ".\Microsoft.Azure.Cosmos.Encryption\tests\Microsoft.Azure.Cosmos.Encryption.Tests\bin\Release\netcoreapp2.0\Contracts\DotNetSDKEncryptionAPIChanges.json"
 if(!(Test-Path -Path $updatedContractFile)){
-    Write-warning ("Please run ContractEnforcement.ContractChanges tests to generate an updated contract at " + $updatedContractFile)
+    Write-Error ("The contract file did not get updated with the build. Please fix the test to output the contract file: " + $updatedContractFile)
 }else{
     Copy-Item -Path $updatedContractFile -Destination ".\Microsoft.Azure.Cosmos.Encryption\tests\Microsoft.Azure.Cosmos.Encryption.Tests\Contracts\DotNetSDKEncryptionAPI.json"
     Write-Output ("Updated contract " + $updatedContractFile)
-    $noContractsUpdated = $false
-}
-
-if($noContractsUpdated){
-    Write-Error "No contracts have been updated. Please run the tests with category 'UpdateContract' for the projects requiring an update"
 }
