5bbe6f108fcfbb231d21c8aa2300d39d93cb91ca
Abhijit P. Pai <abpai@microsoft.com>
2020-07-09 20:25:51 +0530

    More legible; progress persistence

--- Microsoft.Azure.Cosmos.Samples/Usage/ChangeFeedPull/Program.cs
+++ Microsoft.Azure.Cosmos.Samples/Usage/ChangeFeedPull/Program.cs
@@ -2,7 +2,7 @@
 {
     using System;
     using System.Collections.Concurrent;
-    using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos;
     using Microsoft.Azure.Cosmos.Fluent;
@@ -10,6 +10,112 @@
 
     class Program
     {
+        ConcurrentQueue<string> continuationTokenQueue = new ConcurrentQueue<string>();
+
+        ConcurrentDictionary<string, int> pendingCountByContinuationToken = new ConcurrentDictionary<string, int>();
+
+        PartitionKey emptyPartitionKey = new PartitionKey(string.Empty);
+
+        ItemRequestOptions targetItemRequestOptions = new ItemRequestOptions()
+        {
+            EnableContentResponseOnWrite = false
+        };
+
+        private async Task RunAsync(Container source, Container target)
+        {
+            FeedIterator<MyDocument> iterator = source
+                .GetChangeFeedIterator<MyDocument>(
+                changeFeedRequestOptions: new ChangeFeedRequestOptions()
+                {
+                    StartTime = DateTime.MinValue.ToUniversalTime(),
+                    MaxItemCount = 1000
+                });
+
+            while (true)
+            {
+                FeedResponse<MyDocument> response;
+                try
+                {
+                    response = await iterator.ReadNextAsync();
+                    if ((int)response.StatusCode == 304)
+                    {
+                        Console.WriteLine("304");
+                        await Task.Delay(50);
+                    }
+                    else if ((int)response.StatusCode >= 300)
+                    {
+                        Console.WriteLine(response.StatusCode);
+                    }
+                    else
+                    {
+                        // shouldn't be, but anyway
+                        if (response.Count == 0)
+                        {
+                            continue;
+                        }
+
+                        string continuationToken = response.ContinuationToken;
+                        this.continuationTokenQueue.Enqueue(continuationToken);
+                        this.pendingCountByContinuationToken.TryAdd(continuationToken, response.Count);
+
+                        foreach (MyDocument doc in response.Resource)
+                        {
+                            MyDocument transformedDoc = Transform(doc);
+
+                            Task _ = target.CreateItemAsync(transformedDoc, new PartitionKey(transformedDoc.pk), this.targetItemRequestOptions)
+                                .ContinueWith(_ => this.UpdateProgressAsync(target, continuationToken));
+                        }
+                    }
+                }
+                catch (CosmosException ex)
+                {
+                    Console.WriteLine(ex.StatusCode + " " + ex.Message);
+                }
+            }
+
+        }
+
+        private static MyDocument Transform(MyDocument doc)
+        {
+            string sourceId = doc.id;
+            doc.id = doc.pk;
+            doc.pk = sourceId;
+            return doc;
+        }
+
+        private async Task UpdateProgressAsync(Container target, string continuationToken)
+        {
+            int updatedPendingCountForContinuationToken = this.pendingCountByContinuationToken.AddOrUpdate(
+                continuationToken,
+                -1,
+                (key, old) => old - 1);
+
+            if (updatedPendingCountForContinuationToken == 0
+                && this.continuationTokenQueue.TryPeek(out string firstPendingContinuationToken)
+                && firstPendingContinuationToken == continuationToken)
+            {
+                await target.UpsertItemAsync(new
+                {
+                    id = "Progress",
+                    pk = string.Empty,
+                    cont = continuationToken
+                }, this.emptyPartitionKey);
+
+                bool wasDequeued = this.continuationTokenQueue.TryDequeue(out string dequeuedToken);
+                Debug.Assert(wasDequeued);
+                Debug.Assert(dequeuedToken == continuationToken);
+            }
+        }
+
+        private class MyDocument
+        {
+            public string id { get; set; }
+
+            public string pk { get; set; }
+
+            public string other { get; set; }
+        }
+
         static async Task Main(string[] args)
         {
             try
@@ -33,13 +139,6 @@
                 using (CosmosClient client = new CosmosClientBuilder(endpoint, authKey)
                     .WithThrottlingRetryOptions(TimeSpan.FromSeconds(1), maxRetryAttemptsOnThrottledRequests: 0).Build())
                 {
-                    FeedIterator<MyDocument> iterator = client.GetContainer("samples", "source")
-                        .GetChangeFeedIterator<MyDocument>(
-                        changeFeedRequestOptions: new ChangeFeedRequestOptions()
-                        {
-                            StartTime = DateTime.MinValue.ToUniversalTime()
-                        });
-
                     Database samples = client.GetDatabase("samples");
                     Container target = await samples.CreateContainerIfNotExistsAsync("target", "/pk", 24000);
                     await Task.Delay(5000);
@@ -48,48 +147,7 @@
                     await Task.Delay(5000);
 
                     Console.WriteLine("Starting...");
-
-                    ConcurrentDictionary<Task, bool> allTasks = new ConcurrentDictionary<Task, bool>();
-                    while (true)
-                    {
-                        FeedResponse<MyDocument> response;
-                        try
-                        {
-                            response = await iterator.ReadNextAsync();
-                            if ((int)response.StatusCode == 304)
-                            {
-                                Console.WriteLine("304");
-                                await Task.Delay(50);
-                            }
-                            else if ((int)response.StatusCode >= 300)
-                            {
-                                Console.WriteLine(response.StatusCode);
-                            }
-                            else
-                            {
-                                List<Task> currentTasks = new List<Task>();
-                                foreach (MyDocument doc in response.Resource)
-                                {
-                                    Task t = target.CreateItemAsync(doc, new PartitionKey(doc.pk));
-                                    currentTasks.Add(t);
-                                    allTasks.TryAdd(t, true);
-                                }
-
-                                Task _ = Task.WhenAll(currentTasks).ContinueWith(_ =>
-                                 {
-                                     foreach (Task task in currentTasks)
-                                     {
-                                         allTasks.TryRemove(task, out bool _);
-                                     }
-                                 });
-                            }
-                        }
-                        catch (CosmosException ex)
-                        {
-                            Console.WriteLine(ex.StatusCode + " " + ex.Message);
-                        }
-                    }
-
+                    await new Program().RunAsync(client.GetContainer("samples", "temp"), target);
                 }
             }
             finally
@@ -98,15 +156,5 @@
                 Console.ReadKey();
             }
         }
-
-        private class MyDocument
-        {
-            public string id { get; set; }
-
-            public string pk { get; set; }
-
-            public string other { get; set; }
-        }
-
     }
 }
