da3fb94af0b7093c5a4651d6daca892c7d600997
Brandon Chong <bchong95@users.noreply.github.com>
2020-07-14 17:12:18 -0700

    added end to end tests

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Query/Pipeline/FullPipelineTests.cs
@@ -0,0 +1,190 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Tests.Query.Pipeline
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos.CosmosElements;
+    using Microsoft.Azure.Cosmos.Pagination;
+    using Microsoft.Azure.Cosmos.Query.Core;
+    using Microsoft.Azure.Cosmos.Query.Core.ExecutionContext;
+    using Microsoft.Azure.Cosmos.Query.Core.Monads;
+    using Microsoft.Azure.Cosmos.Query.Core.Pipeline;
+    using Microsoft.Azure.Cosmos.Query.Core.QueryPlan;
+    using Microsoft.Azure.Cosmos.Tests.Pagination;
+    using Microsoft.Azure.Documents;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FullPipelineTests
+    {
+        private static readonly Dictionary<string, object> DefaultQueryEngineConfiguration = new Dictionary<string, object>()
+        {
+            {"maxSqlQueryInputLength", 30720},
+            {"maxJoinsPerSqlQuery", 5},
+            {"maxLogicalAndPerSqlQuery", 200},
+            {"maxLogicalOrPerSqlQuery", 200},
+            {"maxUdfRefPerSqlQuery", 2},
+            {"maxInExpressionItemsCount", 8000},
+            {"queryMaxInMemorySortDocumentCount", 500},
+            {"maxQueryRequestTimeoutFraction", 0.90},
+            {"sqlAllowNonFiniteNumbers", false},
+            {"sqlAllowAggregateFunctions", true},
+            {"sqlAllowSubQuery", true},
+            {"sqlAllowScalarSubQuery", false},
+            {"allowNewKeywords", true},
+            {"sqlAllowLike", false},
+            {"sqlAllowGroupByClause", false},
+            {"maxSpatialQueryCells", 12},
+            {"spatialMaxGeometryPointCount", 256},
+            {"sqlDisableQueryILOptimization", false},
+            {"sqlDisableFilterPlanOptimization", false}
+        };
+
+        private static readonly QueryPartitionProvider queryPartitionProvider = new QueryPartitionProvider(DefaultQueryEngineConfiguration);
+        private static readonly PartitionKeyDefinition partitionKeyDefinition = new PartitionKeyDefinition()
+        {
+            Paths = new Collection<string>()
+            {
+                "/pk"
+            },
+            Kind = PartitionKind.Hash,
+            Version = PartitionKeyDefinitionVersion.V2,
+        };
+
+        [TestMethod]
+        public async Task SelectStar()
+        {
+            List<CosmosObject> documents = new List<CosmosObject>();
+            for (int i = 0; i < 250; i++)
+            {
+                documents.Add(CosmosObject.Parse($"{{\"pk\" : {i} }}"));
+            }
+
+            List<CosmosElement> documentsQueried = await ExecuteQueryAsync(
+                query: "SELECT * FROM c",
+                documents: documents);
+
+            Assert.AreEqual(expected: documents.Count, actual: documentsQueried.Count);
+        }
+
+        private static async Task<List<CosmosElement>> ExecuteQueryAsync(
+            string query,
+            IReadOnlyList<CosmosObject> documents)
+        {
+            IDocumentContainer documentContainer = await CreateDocumentContainerAsync(documents);
+
+            List<CosmosElement> resultsFromDrainWithoutState = await DrainWithoutStateAsync(query, documentContainer);
+            List<CosmosElement> resultsFromDrainWithState = await DrainWithStateAsync(query, documentContainer);
+
+            Assert.IsTrue(resultsFromDrainWithoutState.SequenceEqual(resultsFromDrainWithState));
+
+            return resultsFromDrainWithoutState;
+        }
+
+        private static async Task<List<CosmosElement>> DrainWithoutStateAsync(string query, IDocumentContainer documentContainer)
+        {
+            IQueryPipelineStage pipelineStage = CreatePipeline(documentContainer, query);
+
+            List<CosmosElement> elements = new List<CosmosElement>();
+            while (await pipelineStage.MoveNextAsync())
+            {
+                TryCatch<QueryPage> tryGetQueryPage = pipelineStage.Current;
+                tryGetQueryPage.ThrowIfFailed();
+
+                elements.AddRange(tryGetQueryPage.Result.Documents);
+            }
+
+            return elements;
+        }
+
+        private static async Task<List<CosmosElement>> DrainWithStateAsync(string query, IDocumentContainer documentContainer)
+        {
+            IQueryPipelineStage pipelineStage;
+            CosmosElement state = null;
+
+            List<CosmosElement> elements = new List<CosmosElement>();
+            do
+            {
+                pipelineStage = CreatePipeline(documentContainer, query, state);
+
+                await pipelineStage.MoveNextAsync();
+                TryCatch<QueryPage> tryGetQueryPage = pipelineStage.Current;
+
+                tryGetQueryPage.ThrowIfFailed();
+
+                elements.AddRange(tryGetQueryPage.Result.Documents);
+                state = tryGetQueryPage.Result.State?.Value;
+            }
+            while (state != null);
+
+            return elements;
+        }
+
+        private static async Task<IDocumentContainer> CreateDocumentContainerAsync(
+            IReadOnlyList<CosmosObject> documents,
+            DocumentContainer.FailureConfigs failureConfigs = null)
+        {
+            InMemoryCollection inMemoryCollection = new InMemoryCollection(partitionKeyDefinition, failureConfigs);
+
+            await inMemoryCollection.SplitAsync(partitionKeyRangeId: 0, cancellationToken: default);
+
+            await inMemoryCollection.SplitAsync(partitionKeyRangeId: 1, cancellationToken: default);
+            await inMemoryCollection.SplitAsync(partitionKeyRangeId: 2, cancellationToken: default);
+
+            await inMemoryCollection.SplitAsync(partitionKeyRangeId: 3, cancellationToken: default);
+            await inMemoryCollection.SplitAsync(partitionKeyRangeId: 4, cancellationToken: default);
+            await inMemoryCollection.SplitAsync(partitionKeyRangeId: 5, cancellationToken: default);
+            await inMemoryCollection.SplitAsync(partitionKeyRangeId: 6, cancellationToken: default);
+
+            foreach (CosmosObject document in documents)
+            {
+                while (true)
+                {
+                    TryCatch<Record> monadicCreateRecord = await inMemoryCollection.MonadicCreateItemAsync(
+                        document,
+                        cancellationToken: default);
+                    if (monadicCreateRecord.Succeeded)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return inMemoryCollection;
+        }
+
+        private static IQueryPipelineStage CreatePipeline(IDocumentContainer documentContainer, string query, CosmosElement state = null)
+        {
+            TryCatch<IQueryPipelineStage> tryCreatePipeline = PipelineFactory.MonadicCreate(
+                ExecutionEnvironment.Compute,
+                documentContainer,
+                new SqlQuerySpec(query),
+                GetQueryPlan(query),
+                pageSize: 10,
+                requestContinuationToken: state);
+
+            tryCreatePipeline.ThrowIfFailed();
+
+            return tryCreatePipeline.Result;
+        }
+
+        private static QueryInfo GetQueryPlan(string query)
+        {
+            TryCatch<PartitionedQueryExecutionInfoInternal> info = queryPartitionProvider.TryGetPartitionedQueryExecutionInfoInternal(
+                new SqlQuerySpec(query),
+                partitionKeyDefinition,
+                requireFormattableOrderByQuery: true,
+                isContinuationExpected: false,
+                allowNonValueAggregateQuery: true,
+                hasLogicalPartitionKey: false);
+
+            info.ThrowIfFailed();
+            return info.Result.QueryInfo;
+        }
+    }
+}
