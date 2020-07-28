c566d5b9298583edcc07d7f0d26d396339b85018
Matias Quaranta <ealsur@hotmail.com>
2020-07-28 09:56:26 -0700

    adding migration tests

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/ChangeFeed/SmokeTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/ChangeFeed/SmokeTests.cs
@@ -13,6 +13,7 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests.ChangeFeed
     using Microsoft.Azure.Cosmos.Query.Core;
     using Microsoft.Azure.Cosmos.Resource.CosmosExceptions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json.Linq;
 
     [TestClass]
     [TestCategory("ChangeFeed")]
@@ -65,10 +66,8 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests.ChangeFeed
         }
 
         [TestMethod]
-        public async Task DefaultsToNoPartitionIdInSchema()
+        public async Task Schema_DefaultsToNoPartitionId()
         {
-            IEnumerable<int> expectedIds = Enumerable.Range(0, 100);
-            List<int> receivedIds = new List<int>();
             ChangeFeedProcessor processor = this.Container
                 .GetChangeFeedProcessorBuilder("test", (IReadOnlyCollection<TestClass> docs, CancellationToken token) =>
                 {
@@ -101,6 +100,103 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests.ChangeFeed
             }
 
             await processor.StopAsync();
+        }
+
+        /// <summary>
+        /// When the user migrates from V2 CFP, the leases contain PartitionId.
+        /// To allow for backward compatibility (V2 -> V3 -> V2) we need to honor the existence of PartitionId and maintain its value.
+        /// </summary>
+        [TestMethod]
+        public async Task Schema_OnV2MigrationMaintainPartitionId()
+        {
+            IEnumerable<int> expectedIds = Enumerable.Range(0, 20);
+            List<int> receivedIds = new List<int>();
+            ChangeFeedProcessor processor = this.Container
+                .GetChangeFeedProcessorBuilder("test", (IReadOnlyCollection<TestClass> docs, CancellationToken token) =>
+                {
+                    foreach (TestClass doc in docs)
+                    {
+                        receivedIds.Add(int.Parse(doc.id));
+                    }
+
+                    return Task.CompletedTask;
+                })
+                .WithInstanceName("random")
+                .WithLeaseContainer(this.LeaseContainer).Build();
+
+            await processor.StartAsync();
+            // Letting processor initialize
+            await Task.Delay(BaseChangeFeedClientHelper.ChangeFeedSetupTime);
+
+            // Inserting some documents
+            foreach (int id in expectedIds.Take(10))
+            {
+                await this.Container.CreateItemAsync<dynamic>(new { id = id.ToString() });
+            }
+
+            // Waiting on all notifications to finish
+            await Task.Delay(BaseChangeFeedClientHelper.ChangeFeedCleanupTime);
+            await processor.StopAsync();
+
+            // At this point we have leases for V3, so we will simulate V2 by manually adding PartitionId and removing LeaseToken
+            using FeedIterator<JObject> iterator = this.LeaseContainer.GetItemQueryIterator<JObject>();
+            while (iterator.HasMoreResults)
+            {
+                FeedResponse<JObject> page = await iterator.ReadNextAsync();
+                foreach (JObject lease in page)
+                {
+                    string leaseId = lease.Value<string>("id");
+                    if (leaseId.Contains(".info") || leaseId.Contains(".lock"))
+                    {
+                        // These are the store initialization marks
+                        continue;
+                    }
+
+                    // create the PartitionId property
+                    lease.Add("PartitionId", lease.Value<string>("LeaseToken"));
+
+                    lease.Remove("LeaseToken");
+
+                    await this.LeaseContainer.UpsertItemAsync<JObject>(lease);
+                }
+            }
+
+            // Now all leases are V2 leases, create the rest of the documents
+            foreach (int id in expectedIds.TakeLast(10))
+            {
+                await this.Container.CreateItemAsync<dynamic>(new { id = id.ToString() });
+            }
+
+            await processor.StartAsync();
+            // Letting processor initialize
+            await Task.Delay(BaseChangeFeedClientHelper.ChangeFeedSetupTime);
+
+            // Waiting on all notifications to finish, should be using PartitionId from the V2 lease
+            await Task.Delay(BaseChangeFeedClientHelper.ChangeFeedCleanupTime);
+            await processor.StopAsync();
+
+            // Verify we processed all items (including when using the V2 leases)
+            CollectionAssert.AreEqual(expectedIds.ToList(), receivedIds);
+
+            // Verify the after-migration leases have both PartitionId and LeaseToken with the same value
+            using FeedIterator<dynamic> iteratorAfter = this.LeaseContainer.GetItemQueryIterator<dynamic>();
+            while (iteratorAfter.HasMoreResults)
+            {
+                FeedResponse<dynamic> page = await iteratorAfter.ReadNextAsync();
+                foreach (dynamic lease in page)
+                {
+                    string leaseId = lease.id;
+                    if (leaseId.Contains(".info") || leaseId.Contains(".lock"))
+                    {
+                        // These are the store initialization marks
+                        continue;
+                    }
+
+                    Assert.IsNotNull(lease.LeaseToken, "LeaseToken is missing after migration of lease schema");
+                    Assert.IsNotNull(lease.PartitionId, "PartitionId is missing after migration of lease schema");
+                    Assert.AreEqual(lease.LeaseToken, lease.PartitionId, "LeaseToken and PartitionId should be equal after migration");
+                }
+            }
         }
 
         [TestMethod]
