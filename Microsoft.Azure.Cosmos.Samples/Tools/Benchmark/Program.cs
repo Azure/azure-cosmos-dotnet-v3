b913671efeac1be399062311e716c0ba851fd2af
kirankumarkolli <kirankk@microsoft.com>
2020-07-30 02:54:18 +0530

    Experimental

--- Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/Program.cs
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/Program.cs
@@ -30,6 +30,14 @@ namespace CosmosBenchmark
         {
             try
             {
+                System.Diagnostics.Trace.TraceInformation(nameof(System.Diagnostics.Trace.TraceInformation));
+                DefaultTrace.TraceInformation(nameof(DefaultTrace.TraceInformation));
+                System.Diagnostics.Trace.Flush();
+                DefaultTrace.Flush();
+
+                DefaultTrace.PrintListeners();
+                DefaultTrace.ClearListeners();
+
                 BenchmarkConfig config = BenchmarkConfig.From(args);
                 ThreadPool.SetMinThreads(config.MinThreadPoolSize, config.MinThreadPoolSize);
                 TelemetrySpan.IncludePercentile = config.EnableLatencyPercentiles;
@@ -234,6 +242,114 @@ namespace CosmosBenchmark
 
                 string partitionKeyPath = options.PartitionKeyPath;
                 return await database.CreateContainerAsync(options.Container, partitionKeyPath, options.Throughput);
+            }
+        }
+
+        internal static class DefaultTrace
+        {
+            private static readonly TraceSource TraceSourceInternal = new TraceSource("DocDBTrace");
+
+            static DefaultTrace()
+            {
+                // From MSDN: http://msdn.microsoft.com/en-us/library/system.diagnostics.trace.usegloballock%28v=vs.110%29.aspx
+                // The global lock is always used if the trace listener is not thread safe,
+                // regardless of the value of UseGlobalLock. The IsThreadSafe property is used to determine
+                // if the listener is thread safe. The global lock is not used only if the value of
+                // UseGlobalLock is false and the value of IsThreadSafe is true.
+                // The default behavior is to use the global lock.
+                System.Diagnostics.Trace.UseGlobalLock = false;
+
+                SourceSwitch sourceSwitch = new SourceSwitch("ClientSwitch", "Information");
+                DefaultTrace.TraceSourceInternal.Switch = sourceSwitch;
+            }
+
+            public static void PrintListeners()
+            {
+                foreach(TraceListener listener in DefaultTrace.TraceSourceInternal.Listeners)
+                {
+                    Console.WriteLine($"Attached listener {listener.Name} -> {listener.GetType().FullName}");
+                }
+            }
+
+            public static void ClearListeners()
+            {
+                DefaultTrace.TraceSourceInternal.Listeners.Clear();
+            }
+
+            public static TraceSource TraceSource
+            {
+                get { return DefaultTrace.TraceSourceInternal; }
+            }
+
+            public static void Flush()
+            {
+                DefaultTrace.TraceSource.Flush();
+            }
+
+            public static void TraceVerbose(string message)
+            {
+                DefaultTrace.TraceSource.TraceEvent(TraceEventType.Verbose, 0, message);
+            }
+
+            public static void TraceVerbose(string format, params object[] args)
+            {
+                DefaultTrace.TraceSource.TraceEvent(TraceEventType.Verbose, 0, format, args);
+            }
+
+            public static void TraceInformation(string message)
+            {
+                DefaultTrace.TraceSource.TraceInformation(message);
+            }
+
+            public static void TraceInformation(string format, params object[] args)
+            {
+                DefaultTrace.TraceSource.TraceInformation(format, args);
+            }
+
+            public static void TraceWarning(string message)
+            {
+                DefaultTrace.TraceSource.TraceEvent(TraceEventType.Warning, 0, message);
+            }
+
+            public static void TraceWarning(string format, params object[] args)
+            {
+                DefaultTrace.TraceSource.TraceEvent(TraceEventType.Warning, 0, format, args);
+            }
+
+            public static void TraceError(string message)
+            {
+                DefaultTrace.TraceSource.TraceEvent(TraceEventType.Error, 0, message);
+            }
+
+            public static void TraceError(string format, params object[] args)
+            {
+                DefaultTrace.TraceSource.TraceEvent(TraceEventType.Error, 0, format, args);
+            }
+
+            public static void TraceCritical(string message)
+            {
+                DefaultTrace.TraceSource.TraceEvent(TraceEventType.Critical, 0, message);
+            }
+
+            public static void TraceCritical(string format, params object[] args)
+            {
+                DefaultTrace.TraceSource.TraceEvent(TraceEventType.Critical, 0, format, args);
+            }
+
+            /// <summary>
+            /// Emit a trace for a set of metric values.
+            /// This is intended to be used next to MDM metrics
+            /// Details:
+            /// Produce a semi-typed trace format as a pipe delimited list of metrics values.
+            /// 'TraceMetrics' prefix provides a search term for indexing.
+            /// 'name' is an identifier to correlate to call site
+            /// Example: TraceMetric|LogServicePoolInfo|0|123|1.
+            /// </summary>
+            /// <param name="name">metric name.</param>
+            /// <param name="values">sequence of values to be emitted in the trace.</param>
+            internal static void TraceMetrics(string name, params object[] values)
+            {
+                DefaultTrace.TraceInformation(string.Join("|", new object[] { "TraceMetrics", name }.Concat(values)));
             }
         }
     }
