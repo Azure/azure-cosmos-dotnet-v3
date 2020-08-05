6f543fe995f304a8a62f0b31d4c21f129c40af7e
Matias Quaranta <ealsur@hotmail.com>
2020-08-05 15:30:54 -0700

    contract

--- Microsoft.Azure.Cosmos/src/ChangeFeedProcessor/FeedManagement/EqualPartitionsBalancingStrategy.cs
+++ Microsoft.Azure.Cosmos/src/ChangeFeedProcessor/FeedManagement/EqualPartitionsBalancingStrategy.cs
@@ -130,7 +130,7 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.FeedManagement
             {
                 Debug.Assert(lease.CurrentLeaseToken != null, "TakeLeasesAsync: lease.CurrentLeaseToken cannot be null.");
 
-                allPartitions.Add(lease.CurrentLeaseToken?.ToString(), lease);
+                allPartitions.Add(lease.CurrentLeaseToken, lease);
                 if (string.IsNullOrWhiteSpace(lease.Owner) || this.IsExpired(lease))
                 {
                     DefaultTrace.TraceVerbose("Found unused or expired lease: {0}", lease);
