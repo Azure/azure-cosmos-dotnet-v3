c91aa02c64bce57f8bba77b7ee1b8cd91ac55b8a
Brandon Chong <bchong95@users.noreply.github.com>
2020-08-21 22:08:31 -0700

    bcl hash code

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Contracts/DirectContractTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Contracts/DirectContractTests.cs
@@ -106,6 +106,7 @@ namespace Microsoft.Azure.Cosmos.Contracts
                 { "System.Threading.Tasks.Extensions", new Version(4, 5, 2) },
                 { "System.ValueTuple", new Version(4, 5, 0) },
                 { "Antlr4.Runtime.Standard", new Version(4, 8, 0) },
+                { "Microsoft.Bcl.HashCode", new Version(1, 1, 0) },
             };
 
             Assert.AreEqual(projectDependencies.Count, baselineDependencies.Count);
