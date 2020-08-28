158add926ac793b44b6a9ff9014aa93024cd88dd
Jake Willey <jawilley@microsoft.com>
2020-08-28 06:58:46 -0700

    Fix RM resource file header name space

--- Microsoft.Azure.Cosmos/src/direct/RMResources.Designer.cs
+++ Microsoft.Azure.Cosmos/src/direct/RMResources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Microsoft.Azure.Documents {
+namespace Microsoft.Azure.Documents{
     using System;
     
     
