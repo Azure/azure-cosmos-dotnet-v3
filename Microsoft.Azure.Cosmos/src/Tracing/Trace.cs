69b799b961009994c1b9056d169fa660700022d1
Brandon Chong <bchong95@users.noreply.github.com>
2020-09-04 13:53:46 -0700

    drafted out trace library

---
+++ Microsoft.Azure.Cosmos/src/Tracing/Trace.cs
@@ -0,0 +1,86 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Tracing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+
+    internal sealed class Trace : IDisposable
+    {
+        private static readonly DateTime globalStartTime = DateTime.UtcNow;
+        private static readonly Stopwatch globalStopwatch = Stopwatch.StartNew();
+
+        private readonly List<Trace> children;
+        private TimeSpan? duration;
+
+        private Trace(
+            string name,
+            StackFrame stackFrame,
+            TraceLevel level,
+            TraceComponent component,
+            Trace parent)
+        {
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+            this.Id = Guid.NewGuid();
+            this.StackFrame = stackFrame ?? throw new ArgumentNullException(nameof(name));
+            this.StartTime = globalStartTime.AddTicks(globalStopwatch.ElapsedTicks);
+            this.Level = level;
+            this.Component = component;
+            this.Parent = parent;
+            this.children = new List<Trace>();
+        }
+
+        public string Name { get; }
+
+        public Guid Id { get; }
+
+        public StackFrame StackFrame { get; }
+
+        public DateTime StartTime { get; }
+
+        public TimeSpan Duration => this.duration.Value;
+
+        public TraceLevel Level { get; }
+
+        public TraceComponent Component { get; }
+
+        public Trace Parent { get; }
+
+        public IReadOnlyList<Trace> Children => this.children;
+
+        public ITraceInfo Info { get; set; }
+
+        public void Dispose()
+        {
+            this.duration = globalStartTime.AddTicks(globalStopwatch.ElapsedTicks) - this.StartTime;
+        }
+
+        public Trace StartChild(
+            string name,
+            TraceLevel level = TraceLevel.Verbose,
+            TraceComponent component = TraceComponent.Unknown)
+        {
+            Trace child = new Trace(
+                name: name,
+                stackFrame: new StackFrame(fNeedFileInfo: true),
+                level: level,
+                component: component,
+                parent: this);
+            this.children.Add(child);
+            return child;
+        }
+
+        public static Trace GetRootTrace(
+            string name,
+            TraceLevel level = TraceLevel.Verbose,
+            TraceComponent component = TraceComponent.Unknown) => new Trace(
+                name: name,
+                stackFrame: new StackFrame(skipFrames: 1, fNeedFileInfo: true),
+                level: level,
+                component: component,
+                parent: null);
+    }
+}
---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Tracing/TraceTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Microsoft.Azure.Cosmos.Tests.Tracing
+{
+    using System;
+    using Microsoft.Azure.Cosmos.Tracing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TraceTests
+    {
+        [TestMethod]
+        public void TestRootTrace()
+        {
+            Trace rootTrace;
+            using (rootTrace = Trace.GetRootTrace(name: "RootTrace"))
+            {
+                Assert.IsNotNull(rootTrace);
+                Assert.IsNotNull(rootTrace.Children);
+                Assert.AreEqual(rootTrace.Children.Count, 0);
+                Assert.AreEqual(rootTrace.Component, TraceComponent.Unknown);
+                Assert.AreNotEqual(rootTrace.Id, Guid.Empty);
+                Assert.IsNull(rootTrace.Info);
+                Assert.AreEqual(rootTrace.Level, System.Diagnostics.TraceLevel.Verbose);
+                Assert.AreEqual(rootTrace.Name, "RootTrace");
+                Assert.IsNull(rootTrace.Parent);
+                Assert.IsNotNull(rootTrace.StackFrame);
+            }
+
+            Assert.IsTrue(rootTrace.Duration > TimeSpan.Zero);
+        }
+
+        [TestMethod]
+        public void TestTraceChildren()
+        {
+            using (Trace rootTrace = Trace.GetRootTrace(name: "RootTrace"))
+            {
+                using (Trace childTrace1 = rootTrace.StartChild("Child1"))
+                {
+                }
+
+                using (Trace childTrace2 = rootTrace.StartChild("Child2"))
+                {
+                }
+
+                Assert.AreEqual(rootTrace.Children.Count, 2);
+            }
+        }
+    }
+}
