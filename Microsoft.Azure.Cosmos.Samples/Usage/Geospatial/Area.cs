89fb2f9871d15e6237350c3ed272183bde4d776f
Estefani Arroyo <29219340+StefArroyo@users.noreply.github.com>
2020-09-22 17:55:03 -0400

    Added geospatial samples for V3

---
+++ Microsoft.Azure.Cosmos.Samples/Usage/Geospatial/Area.cs
@@ -0,0 +1,34 @@
+﻿namespace Cosmos.Samples.Geospatial
+{
+    using Newtonsoft.Json;
+    using System;
+    using Microsoft.Azure.Cosmos.Spatial;
+
+    class Area
+    {
+
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the creature.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location of the creature.
+        /// </summary>
+        [JsonProperty("boundary")]
+        public Polygon Boundary { get; set; }
+
+        /// <summary>
+        /// Returns the JSON string representation.
+        /// </summary>
+        /// <returns>The string representation.</returns>
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.None);
+        }
+    }
+}
