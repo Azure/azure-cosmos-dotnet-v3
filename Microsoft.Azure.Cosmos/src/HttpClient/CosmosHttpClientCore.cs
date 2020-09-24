d918f4cc4aaab680af00affda458e465e287d35f
Matias Quaranta <ealsur@hotmail.com>
2020-09-23 19:29:52 -0700

    Potential fix

--- Microsoft.Azure.Cosmos/src/HttpClient/CosmosHttpClientCore.cs
+++ Microsoft.Azure.Cosmos/src/HttpClient/CosmosHttpClientCore.cs
@@ -228,6 +228,11 @@ namespace Microsoft.Azure.Cosmos
                                 HttpCompletionOption.ResponseHeadersRead,
                                 cancellationToken);
 
+                        if (responseMessage.RequestMessage == null)
+                        {
+                            responseMessage.RequestMessage = requestMessage;
+                        }
+
                         DateTime receivedTimeUtc = DateTime.UtcNow;
                         TimeSpan durationTimeSpan = receivedTimeUtc - sendTimeUtc;
 
