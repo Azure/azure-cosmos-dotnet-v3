3513e900925ecaa80dca43a69dd4f12964edfa87
j82w <j82w@users.noreply.github.com>
2020-09-24 12:45:07 -0700

    Private Preview AAD: Adds AAD support to the SDK (#1798)
    Adds a constructor to take a TokenCredential
    
    Adds a new TokenCredentialCache which store the AAD token while it is valid, and has a background task to refresh the token roughly 25% of the token lifespan.
    
    AuthorizationTokenProvider is created as a base contract for all the authorization interfaces. Each authorization type now has it's own implementation. This avoids runtime exceptions and all the if else logic that is copy pasted everywhere.

---
+++ Microsoft.Azure.Cosmos/src/Authorization/AuthorizationTokenProviderResourceToken.cs
@@ -0,0 +1,75 @@
+//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos.Core.Trace;
+    using Microsoft.Azure.Documents;
+    using Microsoft.Azure.Documents.Collections;
+    
+    internal sealed class AuthorizationTokenProviderResourceToken : AuthorizationTokenProvider
+    {
+        private readonly string urlEncodedAuthKeyResourceToken;
+        private readonly ValueTask<string> urlEncodedAuthKeyResourceTokenValueTask;
+        private readonly ValueTask<(string, string)> urlEncodedAuthKeyResourceTokenValueTaskWithPayload;
+        private readonly ValueTask defaultValueTask;
+
+        public AuthorizationTokenProviderResourceToken(
+            string authKeyResourceToken)
+        {
+            this.urlEncodedAuthKeyResourceToken = HttpUtility.UrlEncode(authKeyResourceToken);
+            this.urlEncodedAuthKeyResourceTokenValueTask = new ValueTask<string>(this.urlEncodedAuthKeyResourceToken);
+            this.urlEncodedAuthKeyResourceTokenValueTaskWithPayload = new ValueTask<(string, string)>((this.urlEncodedAuthKeyResourceToken, default));
+            this.defaultValueTask = new ValueTask();
+        }
+
+        public override ValueTask<(string token, string payload)> GetUserAuthorizationAsync(
+            string resourceAddress,
+            string resourceType,
+            string requestVerb,
+            INameValueCollection headers,
+            AuthorizationTokenType tokenType)
+        {
+            // If the input auth token is a resource token, then use it as a bearer-token.
+            return this.urlEncodedAuthKeyResourceTokenValueTaskWithPayload;
+        }
+
+        public override ValueTask<string> GetUserAuthorizationTokenAsync(
+            string resourceAddress,
+            string resourceType,
+            string requestVerb,
+            INameValueCollection headers,
+            AuthorizationTokenType tokenType,
+            CosmosDiagnosticsContext diagnosticsContext)
+        {
+            // If the input auth token is a resource token, then use it as a bearer-token.
+            return this.urlEncodedAuthKeyResourceTokenValueTask;
+        }
+
+        public override ValueTask AddAuthorizationHeaderAsync(
+            INameValueCollection headersCollection,
+            Uri requestAddress,
+            string verb,
+            AuthorizationTokenType tokenType)
+        {
+            headersCollection.Add(HttpConstants.HttpHeaders.Authorization, this.urlEncodedAuthKeyResourceToken);
+            return this.defaultValueTask;
+        }
+
+        public override void TraceUnauthorized(
+            DocumentClientException dce,
+            string authorizationToken,
+            string payload)
+        {
+            DefaultTrace.TraceError($"Un-expected authorization for resource token. {dce.Message}");
+        }
+
+        public override void Dispose()
+        {
+        }
+    }
+}
