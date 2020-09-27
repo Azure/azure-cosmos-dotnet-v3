bfbeb464a22d9b6fff414fbb43a62c2bc3fe085e
Jake Willey <jawilley@microsoft.com>
2020-09-27 09:41:30 -0700

    Fixed namespace

--- Microsoft.Azure.Cosmos/src/SessionContainer.cs
+++ Microsoft.Azure.Cosmos/src/SessionContainer.cs
@@ -1,7 +1,7 @@
 ﻿//------------------------------------------------------------
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 //------------------------------------------------------------
-namespace Microsoft.Azure.Cosmos
+namespace Microsoft.Azure.Cosmos.Common
 {
     using System;
     using System.Collections.Concurrent;
