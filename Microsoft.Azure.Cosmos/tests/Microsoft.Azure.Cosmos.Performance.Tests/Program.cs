39e3e2d6b503556ad0db7c1eb6099ca35b4cd6fe
Jake Willey <jawilley@microsoft.com>
2020-09-28 09:54:28 -0700

    Test failure scenario

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Program.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Program.cs
@@ -5,19 +5,37 @@
 namespace Microsoft.Azure.Cosmos.Performance.Tests
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Reports;
     using BenchmarkDotNet.Running;
 
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             //CosmosDBConfiguration environmentConfiguration = ConfigurationService.Configuration;
             //Console.WriteLine($"Starting benchmark and dropping results on {environmentConfiguration.ReportsPath}.");
             //BenchmarkRunner.Run<ItemBenchmark>(new CustomBenchmarkConfiguration(environmentConfiguration));
 
-            BenchmarkSwitcher
+            IEnumerable<Summary> summaries = BenchmarkSwitcher
                 .FromAssembly(typeof(Program).Assembly)
                 .Run(args);
+
+            foreach (Summary summary in summaries)
+            {
+                string[] content = summary.Table.Columns.First(x => string.Equals(@"Op/s", x.Header)).Content;
+                foreach (string ops in content)
+                {
+                    if (string.Equals(@"NA", ops))
+                    {
+                        return -1;
+                    }
+                }
+            }
+
+            return 0;
         }
     }
 }
