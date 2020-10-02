601d3255bb9616ed2ec31603eab3e01f34ab3ee1
Jake Willey <jawilley@microsoft.com>
2020-10-02 13:38:08 -0700

    Fixed white space

--- Microsoft.Azure.Cosmos/src/Headers/Headers.cs
+++ Microsoft.Azure.Cosmos/src/Headers/Headers.cs
@@ -37,7 +37,6 @@ namespace Microsoft.Azure.Cosmos
         public virtual string ContinuationToken
         {
             get => this.CosmosMessageHeaders.Continuation;
-
             internal set => this.CosmosMessageHeaders.Continuation = value;
         }
 
