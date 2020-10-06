826f48ca17bbb9ed6db2023b4168a6a8e3efd385
anujtoshniwal <62551957+anujtoshniwal@users.noreply.github.com>
2020-10-07 00:16:46 +0530

    Client Encryption : Adds DecryptableItem, a type which allows for lazy decryption & deserialization (#1832)
    * Define DecryptableItem, a type which allows for lazy decryption & deserialiazation.
    - exposes decryption operation related information
    - provides user a way to handle possible exceptions encountered as part of feed / query processing
    - allows us to remove delegate method handler for callback in case of exception (not required in lieu of above)
    
    Co-authored-by: j82w <j82w@users.noreply.github.com>

---
+++ Microsoft.Azure.Cosmos.Encryption/src/DecryptableItemCore.cs
@@ -0,0 +1,61 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Encryption
+{
+    using System;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json.Linq;
+
+    internal sealed class DecryptableItemCore : DecryptableItem
+    {
+        /// <summary>
+        /// The encrypted content which is yet to be decrypted.
+        /// </summary>
+        private readonly JToken decryptableContent;
+        private readonly Encryptor encryptor;
+        private readonly CosmosSerializer cosmosSerializer;
+
+        public DecryptableItemCore(
+            JToken decryptableContent,
+            Encryptor encryptor,
+            CosmosSerializer cosmosSerializer)
+        {
+            this.decryptableContent = decryptableContent ?? throw new ArgumentNullException(nameof(decryptableContent));
+            this.encryptor = encryptor ?? throw new ArgumentNullException(nameof(encryptor));
+            this.cosmosSerializer = cosmosSerializer ?? throw new ArgumentNullException(nameof(cosmosSerializer));
+        }
+
+        public override async Task<(T, DecryptionContext)> GetItemAsync<T>()
+        {
+            if (!(this.decryptableContent is JObject document))
+            {
+                return (this.cosmosSerializer.FromStream<T>(EncryptionProcessor.BaseSerializer.ToStream(this.decryptableContent)), null);
+            }
+
+            try
+            {
+                (JObject decryptedItem, DecryptionContext decryptionContext) = await EncryptionProcessor.DecryptAsync(
+                    document,
+                    this.encryptor,
+                    new CosmosDiagnosticsContext(),
+                    cancellationToken: default);
+
+                return (this.cosmosSerializer.FromStream<T>(EncryptionProcessor.BaseSerializer.ToStream(decryptedItem)), decryptionContext);
+            }
+            catch (Exception exception)
+            {
+                string dataEncryptionKeyId = !(document.TryGetValue(Constants.EncryptedInfo, out JToken encryptedInfo) &&
+                    (encryptedInfo is JObject encryptedInfoObject))
+                    ? null
+                    : (string)encryptedInfoObject.GetValue(Constants.EncryptionDekId);
+
+                throw new EncryptionException(
+                    dataEncryptionKeyId,
+                    this.decryptableContent.ToString(),
+                    exception);
+            }
+        }
+    }
+}
