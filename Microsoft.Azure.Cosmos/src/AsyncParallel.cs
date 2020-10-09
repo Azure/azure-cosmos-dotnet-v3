e96ace4e85156c8b7c15ec1824c313340da11484
Brandon Chong <bchong95@users.noreply.github.com>
2020-10-09 13:41:59 -0700

    drafted out solution

---
+++ Microsoft.Azure.Cosmos/src/AsyncParallel.cs
@@ -0,0 +1,83 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal static class AsyncParallel
+    {
+        public static Task ForEachAllOrNothingAsync<TSource>(
+            IEnumerable<TSource> source,
+            Func<TSource, CancellationToken, Task> taskFactory,
+            int maxDegreeOfParallelism,
+            CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (taskFactory == null)
+            {
+                throw new ArgumentNullException(nameof(taskFactory));
+            }
+
+            if (maxDegreeOfParallelism < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(maxDegreeOfParallelism));
+            }
+
+            // Semaphore to uphold maxDegreeOfParallelism
+            SemaphoreSlim semaphore = new SemaphoreSlim(
+                initialCount: maxDegreeOfParallelism,
+                maxCount: maxDegreeOfParallelism);
+
+            // Create a cancellation token that be in the cancelled state if 
+            // the user supplied one is cancelled or 
+            // any of the child tasks end up in a faulted state
+            CancellationTokenSource linkedCancellationTokensSource = CancellationTokenSource
+                .CreateLinkedTokenSource(cancellationToken);
+
+            IEnumerable<Task> tasks = source.Select(item => ProccessAsync<TSource>(
+                item,
+                taskFactory,
+                semaphore,
+                linkedCancellationTokensSource));
+            return Task.WhenAll(tasks);
+        }
+
+        private static async Task ProccessAsync<TSource>(
+            TSource source,
+            Func<TSource, CancellationToken, Task> taskFactory,
+            SemaphoreSlim semaphore,
+            CancellationTokenSource cancellationTokenSource)
+        {
+            await semaphore.WaitAsync();
+
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+            Task task = taskFactory(source, cancellationToken);
+
+            try 
+            {
+                await task;
+            }
+            catch
+            {
+                // Let your siblings know to stop what they are doing.
+                cancellationTokenSource.Cancel();
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+    }
+}
---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/ParallelTests.cs
@@ -0,0 +1,92 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Tests
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ParallelTests
+    {
+        [TestMethod]
+        public async Task SimpleTest()
+        {
+            ThreadSafeCounter counter = new ThreadSafeCounter();
+            int count = 100;
+            await AsyncParallel.ForEachAllOrNothingAsync(
+                Enumerable.Repeat<int>(element: 42, count: count),
+                (source, cancellationToken) => counter.AddOneAsync(cancellationToken),
+                maxDegreeOfParallelism: 1,
+                cancellationToken: default);
+            Assert.AreEqual(count, counter.Counter);
+        }
+
+        [TestMethod]
+        public async Task SimpleTestWithParallelism()
+        {
+            ThreadSafeCounter counter = new ThreadSafeCounter();
+            int count = 100;
+            await AsyncParallel.ForEachAllOrNothingAsync(
+                Enumerable.Repeat<int>(element: 42, count: count),
+                (source, cancellationToken) => counter.AddOneAsync(cancellationToken),
+                maxDegreeOfParallelism: 10,
+                cancellationToken: default);
+            Assert.AreEqual(count, counter.Counter);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public async Task OneChildFailure()
+        {
+            ThreadSafeCounter counter = new ThreadSafeCounter();
+            int problemChild = 42;
+            await AsyncParallel.ForEachAllOrNothingAsync(
+                Enumerable.Range(start: 0, count: 100),
+                (source, cancellationToken) =>
+                {
+                    if (source == problemChild)
+                    {
+                        throw new Exception();
+                    }
+
+                    return counter.AddOneAsync(cancellationToken);
+                },
+                maxDegreeOfParallelism: 1,
+                cancellationToken: default);
+            Assert.AreEqual(problemChild, counter.Counter);
+        }
+
+        private sealed class ThreadSafeCounter
+        {
+            private readonly SemaphoreSlim mutex;
+            private readonly TimeSpan? delay;
+
+            public ThreadSafeCounter(TimeSpan? delay = null)
+            {
+                this.mutex = new SemaphoreSlim(initialCount: 1, maxCount: 1);
+                this.delay = delay;
+            }
+
+            public int Counter { get; private set; }
+
+            public async Task AddOneAsync(CancellationToken cancellationToken)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                if (this.delay.HasValue)
+                {
+                    await Task.Delay(this.delay.Value);
+                }
+
+                await this.mutex.WaitAsync();
+                this.Counter++;
+                this.mutex.Release();
+            }
+        }
+    }
+}
