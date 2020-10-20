3aec4c65e7cf74e70faa690466dbab1755fc204d
Jake Willey <jawilley@microsoft.com>
2020-10-20 13:42:48 -0700

    Added HeaderBenchmark fixed MockItemBenchmark

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Benchmarks/HeaderBenchmark.cs
@@ -0,0 +1,35 @@
+﻿// ----------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ----------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Performance.Tests.Benchmarks
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using BenchmarkDotNet.Attributes;
+    using Microsoft.Azure.Documents;
+    using Microsoft.Azure.Documents.Collections;
+
+    [MemoryDiagnoser]
+    public class HeaderBenchmark
+    {
+        public HeaderBenchmark()
+        {
+            
+        }
+
+        [Benchmark]
+        public void StoreRequestHeaders()
+        {
+            new StoreRequestHeaders();
+        }
+
+        [Benchmark]
+        public void DictionaryNameValueCollection()
+        {
+            new DictionaryNameValueCollection();
+        }
+    }
+}
