f6fbc15c930f490573f94f596c369f04ef1d95f1
Matias Quaranta <ealsur@hotmail.com>
2020-10-30 12:34:33 -0700

    an array

--- Microsoft.Azure.Cosmos/src/TimerWheel/TimerWheelCore.cs
+++ Microsoft.Azure.Cosmos/src/TimerWheel/TimerWheelCore.cs
@@ -13,7 +13,7 @@ namespace Microsoft.Azure.Cosmos.Timers
 #nullable enable
     internal sealed class TimerWheelCore : TimerWheel, IDisposable
     {
-        private readonly List<ConcurrentQueue<TimerWheelTimer>> timers;
+        private readonly ConcurrentQueue<TimerWheelTimer>[] timers;
         private readonly int resolutionInTicks;
         private readonly int resolutionInMs;
         private readonly int buckets;
@@ -42,10 +42,10 @@ namespace Microsoft.Azure.Cosmos.Timers
             this.resolutionInMs = (int)resolution;
             this.resolutionInTicks = (int)TimeSpan.FromMilliseconds(this.resolutionInMs).Ticks;
             this.buckets = buckets;
-            this.timers = new List<ConcurrentQueue<TimerWheelTimer>>(buckets);
+            this.timers = new ConcurrentQueue<TimerWheelTimer>[buckets];
             for (int i = 0; i < buckets; i++)
             {
-                this.timers.Add(new ConcurrentQueue<TimerWheelTimer>());
+                this.timers[i] = new ConcurrentQueue<TimerWheelTimer>();
             }
 
             this.timerConcurrencyLock = new object();
