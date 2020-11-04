803e2a634441ae16fd720f68e439ea9e1a5c17e4
Jake Willey <jawilley@microsoft.com>
2020-11-03 16:39:18 -0800

    Fix endpoint host

--- Microsoft.Azure.Cosmos/src/CosmosClient.cs
+++ Microsoft.Azure.Cosmos/src/CosmosClient.cs
@@ -247,7 +247,7 @@ namespace Microsoft.Azure.Cosmos
             this.Endpoint = new Uri(accountEndpoint);
             this.AuthorizationTokenProvider = new AuthorizationTokenProviderTokenCredential(
                 tokenCredential,
-                accountEndpoint,
+                this.Endpoint.Host,
                 clientOptions.RequestTimeout,
                 clientOptions.TokenCredentialBackgroundRefreshInterval);
 
