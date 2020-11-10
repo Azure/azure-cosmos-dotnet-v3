f23f16e776b1ea36e2cd365842c648642a81ba76
Brandon Chong <bchong95@users.noreply.github.com>
2020-11-10 11:55:20 -0800

    more macros

--- Microsoft.Azure.Cosmos/src/Tracing/ITraceDatum.cs
+++ Microsoft.Azure.Cosmos/src/Tracing/ITraceDatum.cs
@@ -4,8 +4,20 @@
 
 namespace Microsoft.Azure.Cosmos.Tracing
 {
-    internal interface ITraceDatum
+    /// <summary>
+    /// The interface for a single entry in the <see cref="ITrace.Data"/> dictionary.
+    /// </summary>
+#if INTERNAL
+    public
+#else
+    internal
+#endif
+        interface ITraceDatum
     {
+        /// <summary>
+        /// Accept the visitor.
+        /// </summary>
+        /// <param name="traceDatumVisitor">The visitor to accept.</param>
         void Accept(ITraceDatumVisitor traceDatumVisitor);
     }
 }
--- Microsoft.Azure.Cosmos/src/Tracing/ITraceDatumVisitor.cs
+++ Microsoft.Azure.Cosmos/src/Tracing/ITraceDatumVisitor.cs
@@ -6,10 +6,26 @@ namespace Microsoft.Azure.Cosmos.Tracing
 {
     using Microsoft.Azure.Cosmos.Tracing.TraceData;
 
-    internal interface ITraceDatumVisitor
+    /// <summary>
+    /// Visitor for <see cref="ITraceDatum"/>
+    /// </summary>
+#if INTERNAL
+    public
+#else
+    internal
+#endif
+        interface ITraceDatumVisitor
     {
+        /// <summary>
+        /// Visits a <see cref="CosmosDiagnosticsTraceDatum"/> instance.
+        /// </summary>
+        /// <param name="cosmosDiagnosticsTraceDatum">The datum to visit.</param>
         void Visit(CosmosDiagnosticsTraceDatum cosmosDiagnosticsTraceDatum);
 
+        /// <summary>
+        /// Visits a <see cref="QueryMetricsTraceDatum"/> instance.
+        /// </summary>
+        /// <param name="queryMetricsTraceDatum">The datum to visit.</param>
         void Visit(QueryMetricsTraceDatum queryMetricsTraceDatum);
     }
 }
