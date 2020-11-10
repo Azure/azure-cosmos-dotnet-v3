3ad08318ab8267ee03981b1c468df50b251ba0d8
Brandon Chong <bchong95@users.noreply.github.com>
2020-11-10 10:50:53 -0800

    added macro

--- Microsoft.Azure.Cosmos/src/Tracing/ITrace.cs
+++ Microsoft.Azure.Cosmos/src/Tracing/ITrace.cs
@@ -8,34 +8,90 @@ namespace Microsoft.Azure.Cosmos.Tracing
     using System.Collections.Generic;
     using System.Runtime.CompilerServices;
 
-    internal interface ITrace : IDisposable
+    /// <summary>
+    /// Interface to represent a single node in a trace tree.
+    /// </summary>
+#if INTERNAL
+    public
+#else
+    internal
+#endif 
+        interface ITrace : IDisposable
     {
+        /// <summary>
+        /// Gets the name of the node.
+        /// </summary>
         string Name { get; }
 
+        /// <summary>
+        /// Gets the ID of the node.
+        /// </summary>
         Guid Id { get; }
 
+        /// <summary>
+        /// Gets the information for what line of source code this trace was called on.
+        /// </summary>
         CallerInfo CallerInfo { get; }
 
+        /// <summary>
+        /// Gets the time when the trace was started.
+        /// </summary>
         DateTime StartTime { get; }
 
+        /// <summary>
+        /// Gets the duration of the trace.
+        /// </summary>
         TimeSpan Duration { get; }
 
+        /// <summary>
+        /// Gets the level (of information) of the trace.
+        /// </summary>
         TraceLevel Level { get; }
 
+        /// <summary>
+        /// Gets the component that governs this trace.
+        /// </summary>
         TraceComponent Component { get; }
 
+        /// <summary>
+        /// Gets the parent node of this trace.
+        /// </summary>
         ITrace Parent { get; }
 
+        /// <summary>
+        /// Gets the children of this trace.
+        /// </summary>
         IReadOnlyList<ITrace> Children { get; }
 
+        /// <summary>
+        /// Gets additional datum associated with this trace.
+        /// </summary>
         IReadOnlyDictionary<string, object> Data { get; }
 
+        /// <summary>
+        /// Starts a Trace and adds it as a child to this instance.
+        /// </summary>
+        /// <param name="name">The name of the child.</param>
+        /// <param name="memberName">The member name of the child.</param>
+        /// <param name="sourceFilePath">The path to the source file of the child.</param>
+        /// <param name="sourceLineNumber">The line number of the child.</param>
+        /// <returns>A reference to the initialized child (that needs to be disposed to stop the timing).</returns>
         ITrace StartChild(
             string name,
             [CallerMemberName] string memberName = "",
             [CallerFilePath] string sourceFilePath = "",
             [CallerLineNumber] int sourceLineNumber = 0);
 
+        /// <summary>
+        /// Starts a trace and adds it as a child to this instance.
+        /// </summary>
+        /// <param name="name">The name of the child.</param>
+        /// <param name="component">The component that governs the child.</param>
+        /// <param name="level">The level (of information) of the child.</param>
+        /// <param name="memberName">The member name of the child.</param>
+        /// <param name="sourceFilePath">The path to the source file of the child.</param>
+        /// <param name="sourceLineNumber">The line number of the child.</param>
+        /// <returns>A reference to the initialized child (that needs to be disposed to stop the timing).</returns>
         ITrace StartChild(
             string name,
             TraceComponent component,
@@ -44,8 +100,18 @@ namespace Microsoft.Azure.Cosmos.Tracing
             [CallerFilePath] string sourceFilePath = "",
             [CallerLineNumber] int sourceLineNumber = 0);
 
+        /// <summary>
+        /// Adds a datum to the this trace instance.
+        /// </summary>
+        /// <param name="key">The key to associate the datum.</param>
+        /// <param name="traceDatum">The datum itself.</param>
         void AddDatum(string key, ITraceDatum traceDatum);
 
+        /// <summary>
+        /// Adds a datum to the this trace instance.
+        /// </summary>
+        /// <param name="key">The key to associate the datum.</param>
+        /// <param name="value">The datum itself.</param>
         void AddDatum(string key, object value);
     }
 }
--- Microsoft.Azure.Cosmos/src/Tracing/TraceComponent.cs
+++ Microsoft.Azure.Cosmos/src/Tracing/TraceComponent.cs
@@ -4,14 +4,49 @@
 
 namespace Microsoft.Azure.Cosmos.Tracing
 {
-    internal enum TraceComponent
+    /// <summary>
+    /// The component that governs an <see cref="ITrace"/>.
+    /// </summary>
+#if INTERNAL
+    public
+#else
+    internal
+#endif 
+        enum TraceComponent
     {
-        Json,
-        Routing,
-        Pagination,
-        ReadFeed,
-        Transport,
-        Query,
-        Unknown,
+        /// <summary>
+        /// Component that handles JSON reading, writing, navigating, serialization, and deserialization.
+        /// </summary>
+        Json, 
+
+        /// <summary>
+        /// Component that handles paging results within and across partitions from the service.
+        /// </summary>
+        Pagination, 
+
+        /// <summary>
+        /// Component that handles client side aggregation of distributed query results.
+        /// </summary>
+        Query, 
+
+        /// <summary>
+        /// Component that handles aggregating ReadFeed results across multiple pages and partitons.
+        /// </summary>
+        ReadFeed, 
+
+        /// <summary>
+        /// Component that handles routing requests to physical partitons and maintaining physical partition topology.
+        /// </summary>
+        Routing, 
+
+        /// <summary>
+        /// Component that handles sending requests over the wire (along with selecting the correct replica set for consistency).
+        /// </summary>
+        Transport, 
+
+        /// <summary>
+        /// Component is yet to be categorized.
+        /// </summary>
+        Unknown
     }
 }
