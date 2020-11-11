2a61097e895ab9ef13db68f957a92408bf9359b5
Brandon Chong <bchong95@users.noreply.github.com>
2020-11-11 11:53:19 -0800

    fixed diagnostics (#1992)

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Pagination/FlakyDocumentContainer.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Pagination/FlakyDocumentContainer.cs
@@ -127,6 +127,7 @@ namespace Microsoft.Azure.Cosmos.Tests.Pagination
                             new MemoryStream(Encoding.UTF8.GetBytes("{\"Documents\": [], \"_count\": 0, \"_rid\": \"asdf\"}")),
                             requestCharge: 42,
                             activityId: Guid.NewGuid().ToString(),
+                            diagnostics: CosmosDiagnosticsContext.Create(default),
                             nonNullState)));
             }
 
