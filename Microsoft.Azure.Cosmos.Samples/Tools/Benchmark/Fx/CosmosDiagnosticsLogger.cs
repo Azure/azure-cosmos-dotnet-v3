eb6546c72dd5205c6ace918728565f035f009a44
Jake Willey <jawilley@microsoft.com>
2020-11-20 11:21:18 -0800

    Added a minimum time between diagnostics

--- Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/Fx/CosmosDiagnosticsLogger.cs
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/Fx/CosmosDiagnosticsLogger.cs
@@ -7,6 +7,7 @@ namespace CosmosBenchmark
     using System;
     using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Linq;
     using System.Text;
     using Microsoft.Azure.Cosmos;
@@ -17,12 +18,18 @@ namespace CosmosBenchmark
         private readonly static ConcurrentQueue<CosmosDiagnostics> CosmosDiagnosticsToLog = new ConcurrentQueue<CosmosDiagnostics>();
         private static TimeSpan maxTimeSpan = TimeSpan.Zero;
         private static readonly int MaxSize = 2;
+        private static readonly TimeSpan minimumDelayBetweenDiagnostics = TimeSpan.FromSeconds(10);
+        private static readonly Stopwatch stopwatch = new Stopwatch();
 
         public static void Log(CosmosDiagnostics cosmosDiagnostics)
         {
             TimeSpan elapsedTime = cosmosDiagnostics.GetClientElapsedTime();
-            if (elapsedTime > CosmosDiagnosticsLogger.maxTimeSpan)
+            // Require the diagnostics to be at least 10 seconds apart to avoid getting the
+            // diagnostics from the exact same time frame to avoid the same issue multiple times
+            if (stopwatch.Elapsed > CosmosDiagnosticsLogger.minimumDelayBetweenDiagnostics &&
+                elapsedTime > CosmosDiagnosticsLogger.maxTimeSpan)
             {
+                stopwatch.Restart();
                 maxTimeSpan = elapsedTime;
                 CosmosDiagnosticsLogger.CosmosDiagnosticsToLog.Enqueue(cosmosDiagnostics);
                 if(CosmosDiagnosticsToLog.Count > MaxSize)
@@ -40,7 +47,7 @@ namespace CosmosBenchmark
             }
 
             JArray jArray = new JArray();
-            foreach(CosmosDiagnostics cosmosDiagnostics in CosmosDiagnosticsToLog)
+            foreach(CosmosDiagnostics cosmosDiagnostics in CosmosDiagnosticsLogger.CosmosDiagnosticsToLog)
             {
                 try
                 {
