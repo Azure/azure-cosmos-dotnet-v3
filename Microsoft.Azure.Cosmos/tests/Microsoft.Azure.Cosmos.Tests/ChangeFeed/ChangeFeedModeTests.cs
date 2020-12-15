2551070abd930a02f4b98835317561c172909ea3
Matias Quaranta <ealsur@hotmail.com>
2020-12-14 16:05:54 -0800

    initial tests

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/ChangeFeed/ChangeFeedModeTests.cs
@@ -0,0 +1,34 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.ChangeFeed.Tests
+{
+    using Microsoft.Azure.Documents;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    [TestCategory("ChangeFeed")]
+    public class ChangeFeedModeTests
+    {
+        [TestMethod]
+        public void ChangeFeedMode_Incremental_SetsCorrectHeader()
+        {
+            ChangeFeedMode changeFeedMode = ChangeFeedMode.Incremental();
+            RequestMessage requestMessage = new RequestMessage();
+            changeFeedMode.Accept(requestMessage);
+
+            Assert.AreEqual(HttpConstants.A_IMHeaderValues.IncrementalFeed, requestMessage.Headers[HttpConstants.HttpHeaders.A_IM]);
+        }
+
+        [TestMethod]
+        public void ChangeFeedMode_FullFidelity_SetsCorrectHeader()
+        {
+            ChangeFeedMode changeFeedMode = ChangeFeedMode.FullFidelity();
+            RequestMessage requestMessage = new RequestMessage();
+            changeFeedMode.Accept(requestMessage);
+
+            Assert.AreEqual(ChangeFeedModeFullFidelity.FullFidelityHeader, requestMessage.Headers[HttpConstants.HttpHeaders.A_IM]);
+        }
+    }
+}
