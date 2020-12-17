33c0e9a1a548b7fcef0a3607e4c206e8220e2169
j82w <j82w@users.noreply.github.com>
2020-12-17 14:31:15 -0800

    Control Plane Hot Path: Adds more aggressive timeout and retry logic (#1954)
    * Adding a new flag for http request to allow more aggressive timeouts for control plane hot paths.
    
    * Fixes and new tests
    
    * Fix operation cancelled logic

---
+++ Microsoft.Azure.Cosmos/src/HttpClient/HttpTimeoutPolicyDefault.cs
@@ -0,0 +1,34 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+namespace Microsoft.Azure.Cosmos
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal sealed class HttpTimeoutPolicyDefault : HttpTimeoutPolicy
+    {
+        public static readonly HttpTimeoutPolicy Instance = new HttpTimeoutPolicyDefault();
+        private static readonly string Name = nameof(HttpTimeoutPolicyDefault);
+
+        private HttpTimeoutPolicyDefault()
+        {
+        }
+
+        private readonly IReadOnlyList<(TimeSpan requestTimeout, TimeSpan delayForNextRequest)> TimeoutsAndDelays = new List<(TimeSpan requestTimeout, TimeSpan delayForNextRequest)>()
+        {
+            (TimeSpan.FromSeconds(65), TimeSpan.Zero),
+            (TimeSpan.FromSeconds(65), TimeSpan.FromSeconds(1)),
+            (TimeSpan.FromSeconds(65), TimeSpan.Zero),
+        };
+
+        public override string TimeoutPolicyName => HttpTimeoutPolicyDefault.Name;
+
+        public override TimeSpan MaximumRetryTimeLimit => CosmosHttpClient.GatewayRequestTimeout;
+
+        public override int TotalRetryCount => this.TimeoutsAndDelays.Count;
+
+        public override IEnumerator<(TimeSpan requestTimeout, TimeSpan delayForNextRequest)> TimeoutEnumerator => this.TimeoutsAndDelays.GetEnumerator();
+    }
+}
