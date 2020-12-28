5009e822b76c54ee13df66073909f4e42963a319
j82w <j82w@users.noreply.github.com>
2020-12-28 10:19:16 -0800

    Query: Adds fast timeouts and retries to query plan (#2094)
    PR #1954 only did retries on get operations. Get query plan is a post causing it to not do the faster retries. This adds explicit test to verify the behavior and to prevent future regression.
    
    The diagnostics was updated to always include the retry policy to help trouble shoot issues in the future.

--- Microsoft.Azure.Cosmos/src/HttpClient/HttpTimeoutPolicyControlPlaneRetriableHotPath.cs
+++ Microsoft.Azure.Cosmos/src/HttpClient/HttpTimeoutPolicyControlPlaneRetriableHotPath.cs
@@ -6,24 +6,25 @@ namespace Microsoft.Azure.Cosmos
     using System;
     using System.Collections.Generic;
     using System.Net.Http;
+    using System.Text;
 
-    internal sealed class HttpTimeoutPolicyControlPlaneRead : HttpTimeoutPolicy
+    internal sealed class HttpTimeoutPolicyControlPlaneRetriableHotPath : HttpTimeoutPolicy
     {
-        public static readonly HttpTimeoutPolicy Instance = new HttpTimeoutPolicyControlPlaneRead();
-        private static readonly string Name = nameof(HttpTimeoutPolicyControlPlaneRead);
+        public static readonly HttpTimeoutPolicy Instance = new HttpTimeoutPolicyControlPlaneRetriableHotPath();
+        private static readonly string Name = nameof(HttpTimeoutPolicyControlPlaneRetriableHotPath);
 
-        private HttpTimeoutPolicyControlPlaneRead()
+        private HttpTimeoutPolicyControlPlaneRetriableHotPath()
         {
         }
 
         private readonly IReadOnlyList<(TimeSpan requestTimeout, TimeSpan delayForNextRequest)> TimeoutsAndDelays = new List<(TimeSpan requestTimeout, TimeSpan delayForNextRequest)>()
         {
-            (TimeSpan.FromSeconds(5), TimeSpan.Zero),
-            (TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(1)),
-            (TimeSpan.FromSeconds(20), TimeSpan.Zero), 
+            (TimeSpan.FromSeconds(.5), TimeSpan.Zero),
+            (TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(1)),
+            (TimeSpan.FromSeconds(10), TimeSpan.Zero),
         };
 
-        public override string TimeoutPolicyName => HttpTimeoutPolicyControlPlaneRead.Name;
+        public override string TimeoutPolicyName => HttpTimeoutPolicyControlPlaneRetriableHotPath.Name;
 
         public override TimeSpan MaximumRetryTimeLimit => CosmosHttpClient.GatewayRequestTimeout;
 
@@ -34,7 +35,8 @@ namespace Microsoft.Azure.Cosmos
             return this.TimeoutsAndDelays.GetEnumerator();
         }
 
-        // This is for control plane reads which should always be safe to retry on.
+        // The hot path should always be safe to retires since it should be retrieving meta data 
+        // information that is not idempotent.
         public override bool IsSafeToRetry(HttpMethod httpMethod)
         {
             return true;
