54532bbe8b234f236fd5f412a194d4381e4169e7
Matias Quaranta <ealsur@hotmail.com>
2021-01-04 16:56:10 -0800

    project setup

---
+++ Microsoft.Azure.Cosmos.Samples/Tools/CTL/CTLConfig.cs
@@ -0,0 +1,92 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace CosmosCTL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using CommandLine;
+    using Microsoft.Azure.Cosmos;
+    using Newtonsoft.Json;
+
+    public class CTLConfig
+    {
+        private static readonly string UserAgentSuffix = "cosmosdbdotnetctl";
+
+        [Option("ctl_endpoint", Required = true, HelpText = "Cosmos account end point")]
+        public string EndPoint { get; set; }
+
+        [Option("ctl_key", Required = true, HelpText = "Cosmos account master key")]
+        [JsonIgnore]
+        public string Key { get; set; }
+
+        [Option("ctl_operation", Required = false, HelpText = "Workload type")]
+        public string WorkloadType { get; set; } = "ReadWriteQuery";
+
+        [Option("ctl_consistency_level", Required = false, HelpText = "Client consistency level to override")]
+        public string ConsistencyLevel { get; set; }
+
+        [Option("ctl_concurrency", Required = false, HelpText = "Client concurrency")]
+        public int Concurrency { get; set; } = 50;
+
+        [Option("ctl_throughput", Required = false, HelpText = "Provisioned throughput to use")]
+        public int Throughput { get; set; } = 100000;
+
+        [Option("ctl_read_write_query_pct", Required = false, HelpText = "Distribution of read, writes, and queries")]
+        public string ReadWriteQueryPercentage { get; set; } = "90,9,1";
+
+        [Option("ctl_number_of_operations", Required = false, HelpText = "Number of documents to insert")]
+        public int Operations { get; set; } = -1;
+
+        [Option("ctl_max_running_time_duration", Required = false, HelpText = "Running time.")]
+        public string RunningTime { get; set; } = "PT10H";
+
+        [Option("ctl_number_Of_collection", Required = false, HelpText = "Number of collections to use")]
+        public int CollectionCount { get; set; } = 4;
+
+        [Option("ctl_diagnostics_threshold_duration", Required = false, HelpText = "Threshold to log diagnostics")]
+        public string DiagnosticsThreshold { get; set; } = "PT60S";
+
+        internal static CTLConfig From(string[] args)
+        {
+            CTLConfig options = null;
+            Parser parser = new Parser((settings) => settings.CaseSensitive = false);
+            parser.ParseArguments<CTLConfig>(args)
+                .WithParsed<CTLConfig>(e => options = e)
+                .WithNotParsed<CTLConfig>(e => CTLConfig.HandleParseError(e));
+
+            return options;
+        }
+
+        internal CosmosClient CreateCosmosClient(string accountKey)
+        {
+            CosmosClientOptions clientOptions = new CosmosClientOptions()
+            {
+                ApplicationName = CTLConfig.UserAgentSuffix,
+                MaxRetryAttemptsOnRateLimitedRequests = 0
+            };
+
+            if (!string.IsNullOrWhiteSpace(this.ConsistencyLevel))
+            {
+                clientOptions.ConsistencyLevel = (Microsoft.Azure.Cosmos.ConsistencyLevel)Enum.Parse(typeof(Microsoft.Azure.Cosmos.ConsistencyLevel), this.ConsistencyLevel, ignoreCase: true);
+            }
+
+            return new CosmosClient(
+                        this.EndPoint,
+                        accountKey,
+                        clientOptions);
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errors)
+        {
+            foreach (Error e in errors)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Environment.Exit(errors.Count());
+        }
+    }
+}
---
+++ Microsoft.Azure.Cosmos.Samples/Tools/CTL/Program.cs
@@ -0,0 +1,52 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace CosmosCTL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Reflection;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos;
+    using Newtonsoft.Json.Linq;
+
+    public sealed class Program
+    {
+        /// <summary>
+        /// Main method for the sample.
+        /// </summary>
+        /// <param name="args">command line arguments.</param>
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                CTLConfig config = CTLConfig.From(args);
+                Program program = new Program();
+            }
+            finally
+            {
+                Console.WriteLine($"{nameof(CosmosCTL)} completed successfully.");
+                if (Debugger.IsAttached)
+                {
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void ClearCoreSdkListeners()
+        {
+            Type defaultTrace = Type.GetType("Microsoft.Azure.Cosmos.Core.Trace.DefaultTrace,Microsoft.Azure.Cosmos.Direct");
+            TraceSource traceSource = (TraceSource)defaultTrace.GetProperty("TraceSource").GetValue(null);
+            traceSource.Switch.Level = SourceLevels.All;
+            traceSource.Listeners.Clear();
+        }
+    }
+}
