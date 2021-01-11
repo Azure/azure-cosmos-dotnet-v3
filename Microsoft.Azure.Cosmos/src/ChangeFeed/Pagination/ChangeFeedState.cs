e4d8d93edd1d2c1a9085365b888a506bd20f9e3d
Brandon Chong <bchong95@users.noreply.github.com>
2021-01-11 13:44:59 -0800

    fixed build issues

--- Microsoft.Azure.Cosmos/src/ChangeFeed/Pagination/ChangeFeedState.cs
+++ Microsoft.Azure.Cosmos/src/ChangeFeed/Pagination/ChangeFeedState.cs
@@ -11,18 +11,17 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.Pagination
 #if INTERNAL
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 #pragma warning disable SA1600 // Elements should be documented
-#pragma warning disable SA1601 // Partial elements should be documented
     public
 #else
     internal
 #endif 
         abstract class ChangeFeedState : State
     {
-        internal abstract void Accept<TInput>(IChangeFeedStateVisitor<TInput> visitor, TInput input);
+        public abstract void Accept<TInput>(IChangeFeedStateVisitor<TInput> visitor, TInput input);
 
-        internal abstract TOutput Accept<TInput, TOutput>(IChangeFeedStateVisitor<TInput, TOutput> visitor, TInput input);
+        public abstract TOutput Accept<TInput, TOutput>(IChangeFeedStateVisitor<TInput, TOutput> visitor, TInput input);
 
-        internal abstract TResult Accept<TResult>(IChangeFeedStateTransformer<TResult> visitor);
+        public abstract TResult Accept<TResult>(IChangeFeedStateTransformer<TResult> visitor);
 
         public static ChangeFeedState Now() => ChangeFeedStateNow.Singleton;
 
--- Microsoft.Azure.Cosmos/src/FeedRange/FeedRanges/FeedRangeEpk.cs
+++ Microsoft.Azure.Cosmos/src/FeedRange/FeedRanges/FeedRangeEpk.cs
@@ -29,7 +29,7 @@ namespace Microsoft.Azure.Cosmos
 
         public Documents.Routing.Range<string> Range { get; }
 
-        public override Task<List<Documents.Routing.Range<string>>> GetEffectiveRangesAsync(
+        internal override Task<List<Documents.Routing.Range<string>>> GetEffectiveRangesAsync(
             IRoutingMapProvider routingMapProvider,
             string containerRid,
             Documents.PartitionKeyDefinition partitionKeyDefinition)
@@ -37,7 +37,7 @@ namespace Microsoft.Azure.Cosmos
             return Task.FromResult(new List<Documents.Routing.Range<string>>() { this.Range });
         }
 
-        public override async Task<IEnumerable<string>> GetPartitionKeyRangesAsync(
+        internal override async Task<IEnumerable<string>> GetPartitionKeyRangesAsync(
             IRoutingMapProvider routingMapProvider,
             string containerRid,
             Documents.PartitionKeyDefinition partitionKeyDefinition,
@@ -47,36 +47,36 @@ namespace Microsoft.Azure.Cosmos
             return partitionKeyRanges.Select(partitionKeyRange => partitionKeyRange.Id);
         }
 
-        public override void Accept(IFeedRangeVisitor visitor)
+        internal override void Accept(IFeedRangeVisitor visitor)
         {
             visitor.Visit(this);
         }
 
-        public override void Accept<TInput>(IFeedRangeVisitor<TInput> visitor, TInput input)
+        internal override void Accept<TInput>(IFeedRangeVisitor<TInput> visitor, TInput input)
         {
             visitor.Visit(this, input);
         }
 
-        public override TOutput Accept<TInput, TOutput>(IFeedRangeVisitor<TInput, TOutput> visitor, TInput input)
+        internal override TOutput Accept<TInput, TOutput>(IFeedRangeVisitor<TInput, TOutput> visitor, TInput input)
         {
             return visitor.Visit(this, input);
         }
 
-        public override Task<TResult> AcceptAsync<TResult>(
+        internal override Task<TResult> AcceptAsync<TResult>(
             IFeedRangeAsyncVisitor<TResult> visitor,
             CancellationToken cancellationToken = default)
         {
             return visitor.VisitAsync(this, cancellationToken);
         }
 
-        public override Task<TResult> AcceptAsync<TResult, TArg>(
+        internal override Task<TResult> AcceptAsync<TResult, TArg>(
             IFeedRangeAsyncVisitor<TResult, TArg> visitor,
             TArg argument,
             CancellationToken cancellationToken) => visitor.VisitAsync(this, argument, cancellationToken);
 
         public override string ToString() => this.Range.ToString();
 
-        public override TResult Accept<TResult>(IFeedRangeTransformer<TResult> transformer)
+        internal override TResult Accept<TResult>(IFeedRangeTransformer<TResult> transformer)
         {
             return transformer.Visit(this);
         }
