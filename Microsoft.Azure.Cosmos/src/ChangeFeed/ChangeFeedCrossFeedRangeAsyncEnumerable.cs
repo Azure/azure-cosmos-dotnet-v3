7eaf9283edd8bec502eecb4851c8ffe5180696ee
Matias Quaranta <ealsur@users.noreply.github.com>
2021-01-13 14:14:18 -0800

    [Internal] Change Feed: Adds Full Fidelity support (#2109)
    * header
    
    * Initial APIs
    
    * tests
    
    * comments
    
    * Unused
    
    * initial tests
    
    * File rename
    
    * Test refactoring
    
    * Wiring down ChangeFeedMode
    
    * more unit tests
    
    * promoting Mode
    
    * Adjusting preview contract
    
    * adjusting test
    
    * Updating tests
    
    * Public
    
    * Contract
    
    * undo file
    
    * simplify tests
    
    * adding emulator test
    
    * Polishing test
    
    * Enabling test
    
    * exception
    
    * comments
    
    * Making contract internal
    
    * Wiring internal contract correctly

--- Microsoft.Azure.Cosmos/src/ChangeFeed/ChangeFeedCrossFeedRangeAsyncEnumerable.cs
+++ Microsoft.Azure.Cosmos/src/ChangeFeed/ChangeFeedCrossFeedRangeAsyncEnumerable.cs
@@ -14,15 +14,18 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed
     internal sealed class ChangeFeedCrossFeedRangeAsyncEnumerable : IAsyncEnumerable<TryCatch<ChangeFeedPage>>
     {
         private readonly IDocumentContainer documentContainer;
+        private readonly ChangeFeedMode changeFeedMode;
         private readonly ChangeFeedRequestOptions changeFeedRequestOptions;
         private readonly ChangeFeedCrossFeedRangeState state;
 
         public ChangeFeedCrossFeedRangeAsyncEnumerable(
             IDocumentContainer documentContainer,
+            ChangeFeedMode changeFeedMode,
             ChangeFeedRequestOptions changeFeedRequestOptions,
             ChangeFeedCrossFeedRangeState state)
         {
             this.documentContainer = documentContainer ?? throw new ArgumentNullException(nameof(documentContainer));
+            this.changeFeedMode = changeFeedMode ?? throw new ArgumentNullException(nameof(changeFeedMode));
             this.changeFeedRequestOptions = changeFeedRequestOptions;
             this.state = state;
         }
@@ -32,6 +35,7 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed
             CrossFeedRangeState<ChangeFeedState> innerState = new CrossFeedRangeState<ChangeFeedState>(this.state.FeedRangeStates);
             CrossPartitionChangeFeedAsyncEnumerator innerEnumerator = CrossPartitionChangeFeedAsyncEnumerator.Create(
                 this.documentContainer,
+                this.changeFeedMode,
                 this.changeFeedRequestOptions,
                 innerState,
                 cancellationToken);
