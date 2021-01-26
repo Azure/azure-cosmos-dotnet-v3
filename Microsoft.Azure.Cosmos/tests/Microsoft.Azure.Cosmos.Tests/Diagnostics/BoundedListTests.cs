07e70fd169a12809f770854eb34777907de0ec38
neildsh <35383880+neildsh@users.noreply.github.com>
2021-01-25 18:18:43 -0800

    CosmosDiagnostics: Fixes memory leak due to unbounded growth of CosmosDiagnosticsContextCore and removes reference to response stream. (#2129)
    * Do not hold on to the entire StoreResult object in StoreResponseStatistics
    
    * Quick fix for unbounded memory consumption in cosmos diagnostics due to NetworkAttachedDocumentContainer hanging on to a single instance of diagnostics context and adding to it during long running operations
    
    * fix up bad merge
    
    * fix up broken UT
    
    * Incorporate code review feedback
    
    * Allocates the diagnostics string for StoreResult lazily rather than eagerly
    
    * Add a test to check that the underlying response stream is freed
    
    * more calls to force GC
    
    * Plumb Status and SubStatus correctly
    
    * Add region to keep in sync with LocationNames
    
    * No need for  CosmosDiagnosticsContextCore.Capacity to be a lambda
    
    * update API to reflect the new region that was added
    
    Co-authored-by: Brandon Chong <bchong95@users.noreply.github.com>
    Co-authored-by: Samer Boshra <sboshra@microsoft.com>

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Diagnostics/BoundedListTests.cs
@@ -0,0 +1,50 @@
+//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+namespace Microsoft.Azure.Cosmos.Diagnostics
+{
+    using System;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BoundedListTests
+    {
+        [TestMethod]
+        public void CapacityValidationTests()
+        {
+            foreach (int x in new[] {-512, -256, -1, 0 })
+            {
+                Assert.ThrowsException<ArgumentOutOfRangeException>(() => new BoundedList<int>(x));
+            }
+
+            foreach (int x in new[] { 1, 2, 8, 256 })
+            {
+                Assert.IsNotNull(new BoundedList<int>(x));
+            }
+        }
+
+        [TestMethod]
+        [DataRow(    3,    21, DisplayName = "Extra small")]
+        [DataRow(    5,    25, DisplayName = "Small")]
+        [DataRow(  256,  1024, DisplayName = "Medium")]
+        [DataRow( 5120, 10240, DisplayName = "Large")]
+        [DataRow(10240, 20480, DisplayName = "Large")]
+        public void BasicTests(int capacity, int numElements)
+        {
+            BoundedList<int> boundedList = new BoundedList<int>(capacity);
+
+            for (int i = 0; i < numElements; ++i)
+            {
+                boundedList.Add(i);
+
+                int expected = (i >= capacity) ? (i - capacity + 1) : 0; 
+                foreach(int actual in boundedList)
+                {
+                    Assert.AreEqual(expected, actual);
+                    ++expected;
+                }
+            }
+        }
+    }
+}
