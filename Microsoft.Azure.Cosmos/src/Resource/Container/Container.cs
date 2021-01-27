1e00093a14d5af0954f3d739558042ba483653d8
Devansh Parashar <deparash@microsoft.com>
2021-01-27 12:08:53 +0530

    minor update.

--- Microsoft.Azure.Cosmos/src/Resource/Container/Container.cs
+++ Microsoft.Azure.Cosmos/src/Resource/Container/Container.cs
@@ -690,7 +690,7 @@ namespace Microsoft.Azure.Cosmos
             string id,
             PartitionKey partitionKey,
             IReadOnlyList<PatchOperation> patchOperations,
-            ItemRequestOptions requestOptions = null,
+            PatchRequestOptions requestOptions = null,
             CancellationToken cancellationToken = default);
 
         /// <summary>
