677b1b4cc3ca6e7d3f1117713a1ab160032f501c
Brandon Chong <bchong95@users.noreply.github.com>
2021-02-16 10:11:34 -0800

    build

--- Microsoft.Azure.Cosmos/src/PartitionKey.cs
+++ Microsoft.Azure.Cosmos/src/PartitionKey.cs
@@ -220,17 +220,9 @@ namespace Microsoft.Azure.Cosmos
             }
         }
 
-#if INTERNAL
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-#pragma warning disable SA1600 // Elements should be documented
-#pragma warning disable SA1601 // Partial elements should be documented
-        public
-#else
-        internal
-#endif
-            static PartitionKey CreateFromCosmosElementAndDefinition(
-                CosmosObject cosmosObject,
-                PartitionKeyDefinition partitionKeyDefinition)
+        internal static PartitionKey CreateFromCosmosElementAndDefinition(
+            CosmosObject cosmosObject,
+            PartitionKeyDefinition partitionKeyDefinition)
         {
             if (cosmosObject == null)
             {
