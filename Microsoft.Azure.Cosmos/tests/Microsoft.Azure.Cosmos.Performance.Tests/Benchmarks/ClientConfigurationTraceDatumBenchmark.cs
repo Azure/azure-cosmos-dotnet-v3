ff65801ba7fd8322a4b1012cfaf2442de26fb027
Asket Agarwal <asket.agarwal96@gmail.com>
2021-03-02 15:26:25 +0530

    Baseline test for Diagnostics

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Benchmarks/ClientConfigurationTraceDatumBenchmark.cs
@@ -0,0 +1,43 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+namespace Microsoft.Azure.Cosmos.Performance.Tests
+{
+    using System.Threading.Tasks;
+    using BenchmarkDotNet.Attributes;
+    using Microsoft.Azure.Cosmos.Performance.Tests.Benchmarks;
+
+    [Config(typeof(SdkBenchmarkConfiguration))]
+    public class ClientConfigurationTraceDatumBenchmark
+    {
+        private readonly MockedItemBenchmarkHelper benchmarkHelper;
+
+        public ClientConfigurationTraceDatumBenchmark()
+        {
+            this.benchmarkHelper = new MockedItemBenchmarkHelper();
+        }
+
+        [Benchmark]
+        [BenchmarkCategory("GateBenchmark")]
+        public async Task ReadItemTDiagnostics()
+        {
+            ItemResponse<ToDoActivity> response = await this.benchmarkHelper.TestContainer.ReadItemAsync<ToDoActivity>(
+                            MockedItemBenchmarkHelper.ExistingItemId,
+                            MockedItemBenchmarkHelper.ExistingPartitionId);
+
+            response.Diagnostics.ToString();
+        }
+
+        [Benchmark]
+        [BenchmarkCategory("GateBenchmark")]
+        public async Task ReadItemStreamDiagnostics()
+        {
+            using (ResponseMessage response = await this.benchmarkHelper.TestContainer.ReadItemStreamAsync(
+                MockedItemBenchmarkHelper.ExistingItemId,
+                new Cosmos.PartitionKey(MockedItemBenchmarkHelper.ExistingItemId)))
+            {
+                response.Diagnostics.ToString();
+            }
+        }
+    }
+}
