cf72e3c3c45fa18b6c61e90d32a65dc10bafd91d
Jake Willey <jawilley@microsoft.com>
2021-03-26 07:40:17 -0700

    3 Ms

--- Microsoft.Azure.Cosmos/src/Handler/TransportHandler.cs
+++ Microsoft.Azure.Cosmos/src/Handler/TransportHandler.cs
@@ -60,7 +60,7 @@ namespace Microsoft.Azure.Cosmos.Handlers
             }
         }
 
-        private readonly static TimeSpan RetryInterval = TimeSpan.FromMilliseconds(2);
+        private readonly static TimeSpan RetryInterval = TimeSpan.FromMilliseconds(3);
 
         internal async Task<ResponseMessage> ProcessMessageAsync(
             RequestMessage request,
