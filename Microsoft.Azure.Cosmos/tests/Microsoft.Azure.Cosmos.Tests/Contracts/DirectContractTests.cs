244b01fc691acaf8debe8a2ecb43e8e35e04bee8
Jason Hunter <invino4@users.noreply.github.com>
2021-03-29 15:06:51 -0700

    Update DirectContractTests.cs
    Update extensions in test.

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Contracts/DirectContractTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Contracts/DirectContractTests.cs
@@ -105,7 +105,7 @@ namespace Microsoft.Azure.Cosmos.Contracts
                 { "System.Memory", new Version(4, 5, 4) },
                 { "System.Buffers", new Version(4, 5, 1) },
                 { "System.Runtime.CompilerServices.Unsafe", new Version(4, 5, 3) },
-                { "System.Threading.Tasks.Extensions", new Version(4, 5, 2) },
+                { "System.Threading.Tasks.Extensions", new Version(4, 5, 4) },
                 { "System.ValueTuple", new Version(4, 5, 0) },
                 { "Microsoft.Bcl.HashCode", new Version(1, 1, 0) },
                 { "Azure.Core", new Version(1, 3, 0) },
