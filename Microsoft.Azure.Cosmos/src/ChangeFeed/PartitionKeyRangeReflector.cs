f90ed40ecc57c6ce0c1811c7fd7f0b48bea0518d
Michael Koltachev <mkolt@microsoft.com>
2021-04-04 11:19:44 -0700

    old hanges

---
+++ Microsoft.Azure.Cosmos/src/ChangeFeed/PartitionKeyRangeReflector.cs
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.ChangeFeed
+{
+    using System.Collections.Generic;
+    using Microsoft.Azure.Cosmos.Query.Core.QueryClient;
+
+    internal class PartitionKeyRangeReflector
+    {
+        private readonly ContainerInternal container;
+        private readonly CosmosQueryClient queryClient;
+
+        public PartitionKeyRangeReflector(ContainerInternal container, CosmosQueryClient queryClient)
+        {
+            this.container = container;
+            this.queryClient = queryClient;
+        }
+
+        //public List<PartitionKeyRange> GetOverlappingRanges()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
