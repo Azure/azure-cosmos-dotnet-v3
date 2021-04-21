e4074b545a1ab8d4b79798e4a3ed1702f058178d
leminh98 <leminh.ams@gmail.com>
2021-04-21 08:31:45 -0700

    Query: Adds support for LIKE statement and INT system functions to c# query parser to avoid gateway query plan call when service interop is not available. (#2353)
    * modify the sql dom
    
    * Update the CST file
    
    * make some grammar change
    
    * fix a missing if statement
    
    * address code review
    
    * modify grammar file to fix bug
    
    * Regenerate the appropriate files for grammar
    
    * changing public to private in IsqlListener
    
    * clean up some build errors
    
    * add missing functions
    
    * add placeholder for scalar expression evaluator
    
    * Add parser test for like
    
    * address coder review 2
    
    * remove the bug rules
    
    * make changes to grammar file again
    
    Co-authored-by: Minh Le <leminh@microsoft.com>
    Co-authored-by: j82w <j82w@users.noreply.github.com>

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Query/Parser/IdentifierSqlParserBaselineTests.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Tests.Query.Parser
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public sealed class IdentifierSqlParserBaselineTests : SqlParserBaselineTests
+    {
+        [TestMethod]
+        public void Unicode()
+        {
+            List<SqlParserBaselineTestInput> inputs = new List<SqlParserBaselineTestInput>()
+            {
+                // Positive
+                CreateInput(description: "Basic", identifier: "a"),
+                CreateInput(description: "Basic Capitalized", identifier: "A"),
+                CreateInput(description: "Underscore In Front", identifier: "_a"),
+                CreateInput(description: "Number then Letter", identifier: "_12a"),
+                CreateInput(description: "Letter then Number", identifier: "ab12"),
+                CreateInput(description: "Number then Letter then Number", identifier: "_12ab34"),
+                CreateInput(description: "Letter then Number then Letter", identifier: "ab12cd"),
+
+                // Negative
+                CreateInput(description: "Number In Front", identifier: "12a"),
+                CreateInput(description: "Special Character", identifier: "ab-cd"),
+                CreateInput(description: "Special Character 2", identifier: "ab:cd"),
+                CreateInput(description: "Special Character 3", identifier: "ab{cd}"),
+                CreateInput(description: "Special Character 4", identifier: "ab(cd)"),
+            };
+
+            this.ExecuteTestSuite(inputs);
+        }
+
+        public static SqlParserBaselineTestInput CreateInput(string description, string identifier)
+        {
+            return new SqlParserBaselineTestInput(description, $"SELECT c.{identifier} FROM c");
+        }
+    }
+}
