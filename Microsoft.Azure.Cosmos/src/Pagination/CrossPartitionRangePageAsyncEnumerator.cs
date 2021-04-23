ecf90d65e189163a9c72d08c004d3909140e9ef8
Matias Quaranta <ealsur@users.noreply.github.com>
2021-04-23 10:37:48 -0700

    Change Feed: Fixes pull model request count for NotModified scenario (#2405)
    * Correctness of test
    
    * Adding NextRange
    
    * Consuming NextRange
    
    * Adding tests
    
    * Revert local test changes for debugging
    
    * Adding timeout
    
    * cleanup extra code
    
    * baselines
    
    * more baselines
    
    * Refactoring
    
    * TryPeekNext

--- Microsoft.Azure.Cosmos/src/Pagination/CrossPartitionRangePageAsyncEnumerator.cs
+++ Microsoft.Azure.Cosmos/src/Pagination/CrossPartitionRangePageAsyncEnumerator.cs
@@ -27,6 +27,7 @@ namespace Microsoft.Azure.Cosmos.Pagination
         private readonly CreatePartitionRangePageAsyncEnumerator<TPage, TState> createPartitionRangeEnumerator;
         private readonly AsyncLazy<IQueue<PartitionRangePageAsyncEnumerator<TPage, TState>>> lazyEnumerators;
         private CancellationToken cancellationToken;
+        private FeedRangeState<TState>? nextState;
 
         public CrossPartitionRangePageAsyncEnumerator(
             IFeedRangeProvider feedRangeProvider,
@@ -120,6 +121,7 @@ namespace Microsoft.Azure.Cosmos.Pagination
                 {
                     this.Current = default;
                     this.CurrentRange = default;
+                    this.nextState = default;
                     return false;
                 }
 
@@ -185,6 +187,7 @@ namespace Microsoft.Azure.Cosmos.Pagination
 
                     this.Current = TryCatch<CrossFeedRangePage<TPage, TState>>.FromException(currentPaginator.Current.Exception);
                     this.CurrentRange = currentPaginator.FeedRangeState.FeedRange;
+                    this.nextState = CrossPartitionRangePageAsyncEnumerator<TPage, TState>.GetNextRange(enumerators);
                     return true;
                 }
 
@@ -214,6 +217,7 @@ namespace Microsoft.Azure.Cosmos.Pagination
                 this.Current = TryCatch<CrossFeedRangePage<TPage, TState>>.FromResult(
                     new CrossFeedRangePage<TPage, TState>(currentPaginator.Current.Result, crossPartitionState));
                 this.CurrentRange = currentPaginator.FeedRangeState.FeedRange;
+                this.nextState = CrossPartitionRangePageAsyncEnumerator<TPage, TState>.GetNextRange(enumerators);
                 return true;
             }
         }
@@ -222,6 +226,18 @@ namespace Microsoft.Azure.Cosmos.Pagination
         {
             // Do Nothing.
             return default;
+        }
+
+        public bool TryPeekNext(out FeedRangeState<TState> nextState)
+        {
+            if (this.nextState.HasValue)
+            {
+                nextState = this.nextState.Value;
+                return true;
+            }
+
+            nextState = default;
+            return false;
         }
 
         public void SetCancellationToken(CancellationToken cancellationToken)
@@ -234,6 +250,17 @@ namespace Microsoft.Azure.Cosmos.Pagination
             return exeception is CosmosException cosmosException
                 && (cosmosException.StatusCode == HttpStatusCode.Gone)
                 && (cosmosException.SubStatusCode == (int)Documents.SubStatusCodes.PartitionKeyRangeGone);
+        }
+
+        private static FeedRangeState<TState>? GetNextRange(IQueue<PartitionRangePageAsyncEnumerator<TPage, TState>> enumerators)
+        {
+            if (enumerators == null 
+                || enumerators.Count == 0)
+            {
+                return default;
+            }
+
+            return enumerators.Peek()?.FeedRangeState;
         }
 
         private interface IQueue<T> : IEnumerable<T>
