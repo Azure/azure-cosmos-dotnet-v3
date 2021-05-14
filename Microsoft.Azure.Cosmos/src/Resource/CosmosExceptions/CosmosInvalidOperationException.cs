63445f529675c080dab3b9d9909aef517431a12a
Jake Willey <jawilley@microsoft.com>
2021-05-14 11:42:36 -0700

    Adding invalid operaiton and null reference exceptions

---
+++ Microsoft.Azure.Cosmos/src/Resource/CosmosExceptions/CosmosInvalidOperationException.cs
@@ -0,0 +1,83 @@
+//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos
+{
+    using System;
+    using System.Collections;
+    using Microsoft.Azure.Cosmos.Diagnostics;
+    using Microsoft.Azure.Cosmos.Tracing;
+
+    /// <summary>
+    /// The exception is a wrapper for InvalidOperationException. This wrapper
+    /// adds a way to access the CosmosDiagnostics and appends additional information
+    /// to the message for easier troubleshooting.
+    /// </summary>
+    internal class CosmosInvalidOperationException : InvalidOperationException
+    {
+        private readonly InvalidOperationException originalException;
+
+        /// <summary>
+        /// Create an instance of CosmosInvalidOperationException
+        /// </summary>
+        internal CosmosInvalidOperationException(
+            InvalidOperationException originalException,
+            ITrace trace)
+        {
+            this.originalException = originalException ?? throw new ArgumentNullException(nameof(originalException));
+            if (trace == null)
+            {
+                throw new ArgumentNullException(nameof(trace));
+            }
+
+            this.Diagnostics = new CosmosTraceDiagnostics(trace);
+
+            string diagnosticString;
+            try
+            {
+                diagnosticString = this.Diagnostics.ToString();
+            }
+            catch (Exception e)
+            {
+                diagnosticString = $"CosmosDiagnostics.ToString() failed: {e}";
+            }
+
+            this.Message = $"{originalException.Message} CosmosDiagnostics: {diagnosticString}";
+        }
+
+        /// <inheritdoc/>
+        public override string Source
+        {
+            get => this.originalException.Source;
+            set => this.originalException.Source = value;
+        }
+
+        /// <inheritdoc/>
+        public override string Message { get; }
+
+        /// <inheritdoc/>
+        public override string StackTrace => this.originalException.StackTrace;
+
+        /// <inheritdoc/>
+        public override IDictionary Data => this.originalException.Data;
+
+        /// <summary>
+        /// Gets the diagnostics for the request
+        /// </summary>
+        public CosmosDiagnostics Diagnostics { get; }
+
+        /// <inheritdoc/>
+        public override string HelpLink
+        {
+            get => this.originalException.HelpLink;
+            set => this.originalException.HelpLink = value;
+        }
+
+        /// <inheritdoc/>
+        public override Exception GetBaseException()
+        {
+            return this.originalException.GetBaseException();
+        }
+    }
+}
---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/CosmosExceptionTests.cs
@@ -0,0 +1,84 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CosmosUnexpectedExceptionTests : BaseCosmosClientHelper
+    {
+        private ContainerInternal Container = null;
+
+        [TestInitialize]
+        public async Task TestInitialize()
+        {
+            await base.TestInit();
+            string PartitionKey = "/pk";
+            ContainerResponse response = await this.database.CreateContainerAsync(
+                new ContainerProperties(id: Guid.NewGuid().ToString(), partitionKeyPath: PartitionKey),
+                cancellationToken: this.cancellationToken);
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Container);
+            Assert.IsNotNull(response.Resource);
+            this.Container = (ContainerInternal)response;
+        }
+
+        [TestCleanup]
+        public async Task Cleanup()
+        {
+            await base.TestCleanup();
+        }
+
+        [TestMethod]
+        public async Task CheckTracesIncludedWithAllExceptionsTestAsync()
+        {
+            RequestHandlerHelper requestHandlerHelper = new RequestHandlerHelper();
+            CosmosClient cosmosClient = TestCommon.CreateCosmosClient(
+                customizeClientBuilder: builder => builder.AddCustomHandlers(requestHandlerHelper));
+            Container containerWithFailure = cosmosClient.GetContainer(this.database.Id, this.Container.Id);
+
+            requestHandlerHelper.UpdateRequestMessage = (request) => throw new NullReferenceException("Mock NullReferenceException");
+            await this.CheckForTracesAsync<NullReferenceException>(containerWithFailure, messageContainsDiagnostics: true);
+
+            requestHandlerHelper.UpdateRequestMessage = (request) => throw new InvalidOperationException("Mock InvalidOperationException");
+            await this.CheckForTracesAsync<InvalidOperationException>(containerWithFailure, messageContainsDiagnostics: true);
+
+            requestHandlerHelper.UpdateRequestMessage = (request) => throw new ObjectDisposedException("Mock ObjectDisposedException");
+            await this.CheckForTracesAsync<ObjectDisposedException>(containerWithFailure, messageContainsDiagnostics: false);
+        }
+
+
+        private async Task CheckForTracesAsync<ExceptionType>(
+            Container container,
+            bool messageContainsDiagnostics) where ExceptionType : Exception
+        {
+            ToDoActivity toDoActivity = ToDoActivity.CreateRandomToDoActivity();
+
+            try
+            {
+                await container.CreateItemAsync<ToDoActivity>(
+                    toDoActivity,
+                    new Cosmos.PartitionKey(toDoActivity.pk));
+
+                Assert.Fail("Should have thrown");
+            }
+            catch (ExceptionType e)
+            {
+                if (messageContainsDiagnostics)
+                {
+                    Assert.IsTrue(e.Message.Contains("Client Configuration"));
+                }
+                else
+                {
+                    Assert.IsFalse(e.Message.Contains("CosmosDiagnostics"));
+                }
+
+                Assert.IsTrue(e.ToString().Contains("Client Configuration"));
+            }
+        }
+    }
+}
