ed5667de8beb6d65c780e2d9c494b04987488509
Jake Willey <jawilley@microsoft.com>
2021-05-24 06:18:27 -0700

    Add cosmos diagnostics to null ref exception

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/CosmosNullReferenceExceptionTests.cs
@@ -0,0 +1,68 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos.Diagnostics;
+    using Microsoft.Azure.Documents;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CosmosNullReferenceExceptionTests : BaseCosmosClientHelper
+    {
+        private ContainerInternal container = null;
+
+        [TestInitialize]
+        public async Task TestInitialize()
+        {
+            await base.TestInit();
+            string PartitionKey = "/pk";
+            ContainerResponse response = await this.database.CreateContainerAsync(
+                new ContainerProperties(id: Guid.NewGuid().ToString(), partitionKeyPath: PartitionKey),
+                cancellationToken: this.cancellationToken);
+            Assert.IsNotNull(response);
+            Assert.IsNotNull(response.Container);
+            Assert.IsNotNull(response.Resource);
+            this.container = (ContainerInternal)response;
+        }
+
+        [TestCleanup]
+        public async Task Cleanup()
+        {
+            await base.TestCleanup();
+        }
+
+        [TestMethod]
+        public async Task CosmosEndToEndNullReferenceExceptionTestAsync()
+        {
+            string errorMessage = Guid.NewGuid().ToString();
+            RequestHandlerHelper requestHandlerHelper = new RequestHandlerHelper
+            {
+                UpdateRequestMessage = (request) => throw new NullReferenceException(errorMessage)
+            };
+
+            using CosmosClient client = TestCommon.CreateCosmosClient(builder => builder.AddCustomHandlers(requestHandlerHelper));
+            Container containerWithNullRef = client.GetContainer(this.database.Id, this.container.Id);
+
+            try
+            {
+                ToDoActivity toDoActivity = ToDoActivity.CreateRandomToDoActivity();
+                await containerWithNullRef.CreateItemAsync(toDoActivity);
+                Assert.Fail("Create should throw a null reference exception");
+            }
+            catch(NullReferenceException nre)
+            {
+                Assert.AreEqual(typeof(CosmosNullReferenceException), nre.GetType());
+                Assert.IsTrue(nre.Message.Contains("CreateItemAsync"));
+                string cosmosToString = nre.ToString();
+                Assert.IsFalse(cosmosToString.Contains("Microsoft.Azure.Cosmos.CosmosNullReferenceException"), $"The internal wrapper exception should not be exposed to users. {cosmosToString}");
+                Assert.IsTrue(cosmosToString.Contains(errorMessage));
+                Assert.IsTrue(cosmosToString.Contains("CreateItemAsync"));
+            }
+        }
+    }
+}
---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/CosmosNullReferenceExceptionTests.cs
@@ -0,0 +1,52 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+namespace Microsoft.Azure.Cosmos.Tests
+{
+    using System;
+    using System.Threading;
+    using Microsoft.Azure.Cosmos.Tracing;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Tests for <see cref="CancellationToken"/>  scenarios.
+    /// </summary>
+    [TestClass]
+    public class CosmosNullReferenceExceptionTests
+    {
+        [TestMethod]
+        public void CosmosNullRefWrapingTest()
+        {
+            string message = "Test1234 NullReferenceException";
+            NullReferenceException nullReferenceException;
+            try
+            {
+                throw new NullReferenceException(message);
+            }
+            catch(NullReferenceException nre)
+            {
+                nullReferenceException = nre;
+            }
+
+            string rootTraceName = "TestRoot";
+            ITrace trace = Trace.GetRootTrace(rootTraceName);
+            using (trace.StartChild("startChild")) { }
+
+            CosmosNullReferenceException cosmosNullReferenceException = new CosmosNullReferenceException(
+                nullReferenceException,
+                trace);
+
+            Assert.AreEqual(nullReferenceException.StackTrace, cosmosNullReferenceException.StackTrace);
+            Assert.AreEqual(nullReferenceException.InnerException, cosmosNullReferenceException.InnerException);
+            Assert.AreEqual(nullReferenceException.Data, cosmosNullReferenceException.Data);
+
+            Assert.IsTrue(cosmosNullReferenceException.Message.Contains(message));
+            Assert.IsTrue(cosmosNullReferenceException.Message.Contains(rootTraceName));
+            Assert.AreNotEqual(nullReferenceException.Message, cosmosNullReferenceException.Message);
+            string cosmosToString = cosmosNullReferenceException.ToString();
+            Assert.IsFalse(cosmosToString.Contains("Microsoft.Azure.Cosmos.CosmosNullReferenceException"), $"The internal wrapper exception should not be exposed to users. {cosmosToString}");
+            Assert.IsTrue(cosmosToString.Contains(message));
+            Assert.IsTrue(cosmosToString.Contains(rootTraceName));
+        }
+    }
+}
