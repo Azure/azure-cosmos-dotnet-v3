Azure/azure-cosmos-dotnet-v3

commit 8686eaa1a7ac548e6f01fa5e9fa638658695d6be

Diagnostics: Adds Api for getting all regions contacted by a request (#2312)
* Api for getting all regions contacted by a request

* Updating public contract

* Updating tuple names

* Adding Emulator Test

* Renaming tuple

* optimizing tree search

Co-authored-by: j82w <j82w@users.noreply.github.com>

Microsoft.Azure.Cosmos/src/Tracing/TraceData/ClientSideRequestStatisticsTraceDatum.cs
@@ -29,7 +29,7 @@ namespace Microsoft.Azure.Cosmos.Tracing.TraceData
             this.ContactedReplicas = new List<Uri>();
             this.StoreResponseStatisticsList = new List<StoreResponseStatistics>();
             this.FailedReplicas = new HashSet<Uri>();
-            this.RegionsContacted = new HashSet<Uri>();
+            this.RegionsContactedWithName = new HashSet<(string, Uri)>();
             this.clientSideRequestStatisticsCreateTime = Stopwatch.GetTimestamp();
         }
 
@@ -48,6 +48,8 @@ namespace Microsoft.Azure.Cosmos.Tracing.TraceData
         public HashSet<Uri> FailedReplicas { get; }
 
         public HashSet<Uri> RegionsContacted { get; }
+
+        public HashSet<(string, Uri)> RegionsContactedWithName { get; }
 
         public TimeSpan RequestLatency
         {
@@ -119,6 +121,7 @@ namespace Microsoft.Azure.Cosmos.Tracing.TraceData
 
             DateTime responseTime = DateTime.UtcNow;
             Uri locationEndpoint = request.RequestContext.LocationEndpointToRoute;
+            string regionName = request.RequestContext.RegionName;
             StoreResponseStatistics responseStatistics = new StoreResponseStatistics(
                 startDateTime,
                 responseTime,
@@ -141,7 +144,7 @@ namespace Microsoft.Azure.Cosmos.Tracing.TraceData
 
                 if (locationEndpoint != null)
                 {
-                    this.RegionsContacted.Add(locationEndpoint);
+                    this.RegionsContactedWithName.Add((regionName, locationEndpoint));
                 }
 
                 this.StoreResponseStatisticsList.Add(responseStatistics);

Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/CosmosItemTests.cs
@@ -2433,6 +2433,18 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
             Assert.AreEqual(HttpStatusCode.Created, responseAstype.StatusCode);
         }
 
+        [TestMethod]
+        public async Task RegionsContactedTest()
+        {
+            ToDoActivity item = ToDoActivity.CreateRandomToDoActivity();
+            ItemResponse<ToDoActivity> response = await this.Container.CreateItemAsync<ToDoActivity>(item, new Cosmos.PartitionKey(item.pk));
+            Assert.IsNotNull(response.Diagnostics);
+            IReadOnlyList<(string region, Uri uri)> regionsContacted = response.Diagnostics.GetContactedRegions();
+            Assert.AreEqual(regionsContacted.Count, 1);
+            Assert.AreEqual(regionsContacted[0].region, Regions.SouthCentralUS);
+            Assert.IsNotNull(regionsContacted[0].uri);
+        }
+
 #if PREVIEW
         [TestMethod]
         public async Task VerifyDocumentCrudWithMultiHashKind()

Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Tracing/ContactedRegionsTests.cs
@@ -1,0 +1,67 @@
+﻿namespace Microsoft.Azure.Cosmos.Tests.Tracing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Microsoft.Azure.Cosmos.Diagnostics;
+    using Microsoft.Azure.Cosmos.Tracing;
+    using Microsoft.Azure.Cosmos.Tracing.TraceData;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ContactedRegionsTests
+    {
+        [TestMethod]
+        public void ContactedRegionsTest()
+        {
+            CosmosDiagnostics diagnostics = new CosmosTraceDiagnostics(this.CreateTestTraceTree());
+            IReadOnlyList<(string, Uri)> regionsContacted = diagnostics.GetContactedRegions();
+            Assert.IsNotNull(regionsContacted);
+            Assert.AreEqual(regionsContacted.Count, 4);
+        }
+
+        private ITrace CreateTestTraceTree()
+        {
+            ITrace trace;
+            using (trace  = Trace.GetRootTrace("Root Trace", TraceComponent.Unknown, TraceLevel.Info))
+            {
+                using (ITrace firstLevel = trace.StartChild("First level Node", TraceComponent.Unknown, TraceLevel.Info))
+                {
+                    using (ITrace secondLevel = trace.StartChild("Second level Node", TraceComponent.Unknown, TraceLevel.Info))
+                    {
+                        using (ITrace thirdLevel = trace.StartChild("Third level Node", TraceComponent.Unknown, TraceLevel.Info))
+                        {
+                            thirdLevel.AddDatum("Client Side Request Stats", this.GetDatumObject(Regions.CentralUS));
+                        }
+                    }
+
+                    using (ITrace secondLevel = trace.StartChild("Second level Node", TraceComponent.Unknown, TraceLevel.Info))
+                    {
+                        secondLevel.AddDatum("Client Side Request Stats", this.GetDatumObject(Regions.CentralIndia, Regions.EastUS2));
+                    }
+                }
+
+                using (ITrace firstLevel = trace.StartChild("First level Node", TraceComponent.Unknown, TraceLevel.Info))
+                {
+                    firstLevel.AddDatum("Client Side Request Stats", this.GetDatumObject(Regions.FranceCentral));
+                }
+            }
+
+            return trace;
+        }
+
+        private TraceDatum GetDatumObject(string regionName1, string regionName2 = null)
+        {
+            ClientSideRequestStatisticsTraceDatum datum = new ClientSideRequestStatisticsTraceDatum(DateTime.UtcNow);
+            Uri uri1 = new Uri("http://someUri1.com");
+            datum.RegionsContactedWithName.Add((regionName1, uri1));
+            if (regionName2 != null)
+            {
+                Uri uri2 = new Uri("http://someUri2.com");
+                datum.RegionsContactedWithName.Add((regionName2, uri2));
+            }
+
+            return datum;
+        }
+    }
+}

Microsoft.Azure.Cosmos/src/Diagnostics/CosmosDiagnostics.cs
@@ -4,6 +4,7 @@
 namespace Microsoft.Azure.Cosmos
 {
     using System;
+    using System.Collections.Generic;
 
     /// <summary>
     ///  Contains the cosmos diagnostic information for the current request to Azure Cosmos DB service.
@@ -27,5 +28,11 @@ namespace Microsoft.Azure.Cosmos
         /// </summary>
         /// <returns>The string field <see cref="CosmosDiagnostics"/> instance in the Azure CosmosDB database service.</returns>
         public abstract override string ToString();
+
+        /// <summary>
+        /// Gets the list of all regions that were contacted for a request
+        /// </summary>
+        /// <returns>The list of tuples containing the Region name and the URI</returns>
+        public abstract IReadOnlyList<(string regionName, Uri uri)> GetContactedRegions();
     }
 }

Microsoft.Azure.Cosmos/src/Diagnostics/CosmosTraceDiagnostics.cs
@@ -5,9 +5,12 @@
 namespace Microsoft.Azure.Cosmos.Diagnostics
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
     using Microsoft.Azure.Cosmos.Json;
     using Microsoft.Azure.Cosmos.Tracing;
+    using Microsoft.Azure.Cosmos.Tracing.TraceData;
 
     internal sealed class CosmosTraceDiagnostics : CosmosDiagnostics
     {
@@ -38,6 +41,31 @@ namespace Microsoft.Azure.Cosmos.Diagnostics
         public override TimeSpan GetClientElapsedTime()
         {
             return this.Value.Duration;
+        }
+
+        public override IReadOnlyList<(string regionName, Uri uri)> GetContactedRegions()
+        {
+            HashSet<(string, Uri)> regionsContacted = new HashSet<(string, Uri)>();
+            ITrace rootTrace = this.Value;
+            this.WalkTraceTreeForRegionsContated(rootTrace, regionsContacted);
+            return regionsContacted.ToList();
+        }
+
+        private void WalkTraceTreeForRegionsContated(ITrace currentTrace, HashSet<(string, Uri)> regionsContacted)
+        {
+            foreach (object datums in currentTrace.Data.Values)
+            {
+                if (datums is ClientSideRequestStatisticsTraceDatum clientSideRequestStatisticsTraceDatum)
+                {
+                    regionsContacted.UnionWith(clientSideRequestStatisticsTraceDatum.RegionsContactedWithName);
+                    return;
+                }
+            }
+
+            foreach (ITrace childTrace in currentTrace.Children)
+            {
+                this.WalkTraceTreeForRegionsContated(childTrace, regionsContacted);
+            }
         }
 
         private string ToJsonString()

Microsoft.Azure.Cosmos/src/Tracing/TraceWriter.TraceJsonWriter.cs
@@ -193,7 +193,7 @@ namespace Microsoft.Azure.Cosmos.Tracing
 
                 this.WriteJsonUriArrayWithDuplicatesCounted("ContactedReplicas", clientSideRequestStatisticsTraceDatum.ContactedReplicas);
 
-                this.WriteJsonUriArray("RegionsContacted", clientSideRequestStatisticsTraceDatum.RegionsContacted);
+                this.WriteRegionsContactedArray("RegionsContacted", clientSideRequestStatisticsTraceDatum.RegionsContactedWithName);
                 this.WriteJsonUriArray("FailedReplicas", clientSideRequestStatisticsTraceDatum.FailedReplicas);
 
                 this.jsonWriter.WriteFieldName("AddressResolutionStatistics");
@@ -315,6 +315,22 @@ namespace Microsoft.Azure.Cosmos.Tracing
                     foreach (Uri contactedReplica in uris)
                     {
                         this.WriteStringValueOrNull(contactedReplica?.ToString());
+                    }
+                }
+
+                this.jsonWriter.WriteArrayEnd();
+            }
+
+            private void WriteRegionsContactedArray(string propertyName, IEnumerable<(string, Uri)> uris)
+            {
+                this.jsonWriter.WriteFieldName(propertyName);
+                this.jsonWriter.WriteArrayStart();
+
+                if (uris != null)
+                {
+                    foreach ((string _, Uri uri) contactedRegion in uris)
+                    {
+                        this.WriteStringValueOrNull(contactedRegion.uri?.ToString());
                     }
                 }
 

Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/BaselineTest/TestBaseline/TraceWriterBaselineTests.TraceData.xml
@@ -222,8 +222,8 @@
             datum.FailedReplicas.Add(uri1);
             datum.FailedReplicas.Add(uri2);
 
-            datum.RegionsContacted.Add(uri1);
-            datum.RegionsContacted.Add(uri2);
+            datum.RegionsContactedWithName.Add(("local", uri1));
+            datum.RegionsContactedWithName.Add(("local", uri2));
 
             datum.RequestEndTimeUtc = DateTime.MaxValue;
 
@@ -377,7 +377,7 @@
 
             datum.FailedReplicas.Add(default);
 
-            datum.RegionsContacted.Add(default);
+            datum.RegionsContactedWithName.Add(default);
 
             datum.RequestEndTimeUtc = default;
 

Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Tracing/TraceWriterBaselineTests.cs
@@ -373,8 +373,8 @@ namespace Microsoft.Azure.Cosmos.Tests.Tracing
                         datum.FailedReplicas.Add(uri1);
                         datum.FailedReplicas.Add(uri2);
 
-                        datum.RegionsContacted.Add(uri1);
-                        datum.RegionsContacted.Add(uri2);
+                        datum.RegionsContactedWithName.Add(("local", uri1));
+                        datum.RegionsContactedWithName.Add(("local", uri2));
 
                         datum.RequestEndTimeUtc = DateTime.MaxValue;
 
@@ -426,7 +426,7 @@ namespace Microsoft.Azure.Cosmos.Tests.Tracing
 
                         datum.FailedReplicas.Add(default);
 
-                        datum.RegionsContacted.Add(default);
+                        datum.RegionsContactedWithName.Add(default);
 
                         datum.RequestEndTimeUtc = default;
 

commit e840be94361124ce0b3e2b6c2d743239581fe0f1

ClientSideRequestStatistics: Fixes NullReferenceException caused by RegionsContacted in IClientSideRequestStatistics (#2562)
Issue introduced in #2312

The original RegionsContacted hashset was never getting set so it is always null. There are certain service unavailable code paths that check if multiple regions were contacted. Since the hashset is never set it is null and causing the null reference exceptions.

Microsoft.Azure.Cosmos/src/Tracing/TraceData/ClientSideRequestStatisticsTraceDatum.cs
@@ -72,7 +72,18 @@ namespace Microsoft.Azure.Cosmos.Tracing.TraceData
 
         public HashSet<Uri> FailedReplicas { get; }
 
-        public HashSet<Uri> RegionsContacted { get; }
+        public HashSet<Uri> RegionsContacted
+        {
+            get
+            {
+                HashSet<Uri> regionContacted = new HashSet<Uri>();
+                foreach ((string _, Uri uriContacted) in this.RegionsContactedWithName)
+                {
+                    regionContacted.Add(uriContacted);
+                }
+                return regionContacted;
+            }
+        }
 
         public HashSet<(string, Uri)> RegionsContactedWithName { get; }
 

Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/CosmosItemTests.cs
@@ -32,6 +32,7 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
     using JsonSerializer = Json.JsonSerializer;
     using JsonWriter = Json.JsonWriter;
     using PartitionKey = Documents.PartitionKey;
+    using static Microsoft.Azure.Cosmos.SDK.EmulatorTests.TransportClientHelper;
 
     [TestClass]
     public class CosmosItemTests : BaseCosmosClientHelper
@@ -2730,6 +2731,40 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
             Assert.AreEqual(regionsContacted.Count, 1);
             Assert.AreEqual(regionsContacted[0].region, Regions.SouthCentralUS);
             Assert.IsNotNull(regionsContacted[0].uri);
+        }
+
+        [TestMethod]
+        public async Task HaLayerDoesNotThrowNullOnGoneExceptionTest()
+        {
+            CosmosClientOptions clientOptions = new CosmosClientOptions()
+            {
+                TransportClientHandlerFactory = (x) =>
+                    new TransportClientWrapper(client: x, interceptor: (uri, resource, dsr) =>
+                    {
+                        dsr.RequestContext.ClientRequestStatistics.GetType().GetProperty("IsCpuOverloaded").SetValue(
+                            dsr.RequestContext.ClientRequestStatistics, true);
+                        if (resource.operationType.IsReadOperation())
+                        {
+                            throw Documents.Rntbd.TransportExceptions.GetGoneException(
+                                uri,
+                                Guid.NewGuid());
+                        }
+                    })
+            };
+
+            CosmosClient cosmosClient = TestCommon.CreateCosmosClient(clientOptions);
+            Container container = cosmosClient.GetContainer(this.database.Id, this.Container.Id);
+            ToDoActivity testItem = ToDoActivity.CreateRandomToDoActivity();
+            await container.CreateItemAsync<ToDoActivity>(testItem);
+
+            try
+            {
+                await container.ReadItemAsync<ToDoActivity>(testItem.id, new Cosmos.PartitionKey(testItem.pk));
+            }
+            catch (CosmosException ex)
+            {
+                Assert.AreEqual(ex.StatusCode, HttpStatusCode.ServiceUnavailable);
+            }
         }
 
 #if PREVIEW

Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Tracing/ClientSideRequestStatisticsTraceDatumTests.cs
@@ -69,6 +69,15 @@
                 (clientSideRequestStatistics) => clientSideRequestStatistics.StoreResponseStatisticsList);
         }
 
+        [TestMethod]
+        public void VerifyIClientSideRequestStatisticsNullTests()
+        {
+            IClientSideRequestStatistics clientSideRequestStatistics = new ClientSideRequestStatisticsTraceDatum(DateTime.UtcNow);
+            Assert.IsNotNull(clientSideRequestStatistics.ContactedReplicas);
+            Assert.IsNotNull(clientSideRequestStatistics.FailedReplicas);
+            Assert.IsNotNull(clientSideRequestStatistics.RegionsContacted);
+        }
+
         private async Task ConcurrentUpdateTestHelper<T>(
             Action<ClientSideRequestStatisticsTraceDatum, CancellationToken> backgroundUpdater,
             Func<ClientSideRequestStatisticsTraceDatum, IEnumerable<T>> getList)

Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Tracing/ContactedRegionsTests.cs
@@ -6,18 +6,36 @@
     using Microsoft.Azure.Cosmos.Diagnostics;
     using Microsoft.Azure.Cosmos.Tracing;
     using Microsoft.Azure.Cosmos.Tracing.TraceData;
+    using Microsoft.Azure.Documents;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class ContactedRegionsTests
     {
         [TestMethod]
-        public void ContactedRegionsTest()
+        public void ContactedRegionsWithNameTest()
         {
             CosmosDiagnostics diagnostics = new CosmosTraceDiagnostics(this.CreateTestTraceTree());
             IReadOnlyList<(string, Uri)> regionsContacted = diagnostics.GetContactedRegions();
             Assert.IsNotNull(regionsContacted);
             Assert.AreEqual(regionsContacted.Count, 4);
+        }
+
+        [TestMethod]
+        public void ContactedRegionsWithoutNameTest()
+        {
+            ClientSideRequestStatisticsTraceDatum clientSideRequestStatistics = new ClientSideRequestStatisticsTraceDatum(DateTime.UtcNow);
+            Assert.AreEqual(clientSideRequestStatistics.RegionsContacted.Count, 0);
+
+            Uri uri1 = new Uri("http://someUri1.com");
+            clientSideRequestStatistics.RegionsContactedWithName.Add(("WestUs", uri1));
+            Assert.AreEqual(clientSideRequestStatistics.RegionsContacted.Count, 1);
+            Assert.IsTrue(clientSideRequestStatistics.RegionsContacted.Contains(uri1));
+
+            Uri uri2 = new Uri("http://someUri2.com");
+            clientSideRequestStatistics.RegionsContactedWithName.Add(("EastUs", uri2));
+            Assert.AreEqual(clientSideRequestStatistics.RegionsContacted.Count, 2);
+            Assert.IsTrue(clientSideRequestStatistics.RegionsContacted.Contains(uri2));
         }
 
         private ITrace CreateTestTraceTree()