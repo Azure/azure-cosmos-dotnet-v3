abdd394e2ec7d391b501adfac1c2fe6c0508a776
Jake Willey <jawilley@microsoft.com>
2021-06-28 11:17:29 -0700

    V2 query test

---
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/v2/QuerySinglePkStreamV2BenchmarkOperation.cs
@@ -0,0 +1,109 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace CosmosBenchmark
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Documents;
+    using Microsoft.Azure.Documents.Client;
+    using Microsoft.Azure.Documents.Linq;
+
+    internal class QuerySinglePkStreamV2BenchmarkOperation : IBenchmarkOperation
+    {
+        private readonly DocumentClient documentClient;
+        private readonly string partitionKeyPath;
+        private readonly Dictionary<string, object> sampleJObject;
+
+        private readonly string databsaeName;
+        private readonly string containerName;
+
+        private readonly string executionItemPartitionKey;
+        private readonly string executionItemId;
+        private readonly SqlQuerySpec queryDefinition;
+        private readonly FeedOptions queryRequestOptions;
+        private bool initialized = false;
+
+        public QuerySinglePkStreamV2BenchmarkOperation(
+            DocumentClient documentClient,
+            string dbName,
+            string containerName,
+            string partitionKeyPath,
+            string sampleJson)
+        {
+            this.databsaeName = dbName;
+            this.containerName = containerName;
+
+            this.documentClient = documentClient;
+            this.partitionKeyPath = partitionKeyPath.Replace("/", "");
+
+            this.sampleJObject = JsonHelper.Deserialize<Dictionary<string, object>>(sampleJson);
+            this.executionItemPartitionKey = Guid.NewGuid().ToString();
+            this.queryRequestOptions = new FeedOptions() { PartitionKey = new PartitionKey(this.executionItemPartitionKey) };
+            this.executionItemId = Guid.NewGuid().ToString();
+            this.sampleJObject["id"] = this.executionItemId;
+            this.sampleJObject[this.partitionKeyPath] = this.executionItemPartitionKey;
+
+            this.queryDefinition = new SqlQuerySpec("select * from T where T.id = @id")
+            {
+                Parameters = new SqlParameterCollection()
+                {
+                    new SqlParameter()
+                    {
+                        Name = "@id",
+                        Value = this.executionItemId
+                    }
+                }
+            };
+        }
+
+        public async Task<OperationResult> ExecuteOnceAsync()
+        {
+            Uri containerUri = UriFactory.CreateDocumentCollectionUri(this.databsaeName, this.containerName);
+            IDocumentQuery<dynamic> query = this.documentClient.CreateDocumentQuery(
+                containerUri,
+                this.queryDefinition,
+                this.queryRequestOptions).AsDocumentQuery();
+
+            double totalCharge = 0;
+            Func<string> lastDiagnostics = null;
+            while (query.HasMoreResults)
+            {
+                FeedResponse<dynamic> feedResponse = await query.ExecuteNextAsync();
+                totalCharge += feedResponse.RequestCharge;
+                lastDiagnostics = () => feedResponse.RequestDiagnosticsString;
+            }
+
+            return new OperationResult()
+            {
+                DatabseName = databsaeName,
+                ContainerName = containerName,
+                RuCharges = totalCharge,
+                LazyDiagnostics = lastDiagnostics,
+            };
+        }
+
+        public async Task PrepareAsync()
+        {
+            if (this.initialized)
+            {
+                return;
+            }
+
+            Uri containerUri = UriFactory.CreateDocumentCollectionUri(this.databsaeName, this.containerName);
+            ResourceResponse<Document> itemResponse = await this.documentClient.CreateDocumentAsync(
+                    containerUri,
+                    this.sampleJObject,
+                    new RequestOptions() { PartitionKey = new PartitionKey(this.executionItemPartitionKey) });
+            if (itemResponse.StatusCode != HttpStatusCode.Created)
+            {
+                throw new Exception($"Create failed with statuscode: {itemResponse.StatusCode}");
+            }
+
+            this.initialized = true;
+        }
+    }
+}
