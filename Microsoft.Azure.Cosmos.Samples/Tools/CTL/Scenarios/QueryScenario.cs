549079efa6a68bf26f395f48524d4ff1480c9d5a
Matias Quaranta <ealsur@hotmail.com>
2021-09-14 11:22:26 -0700

    Adding 3 queries

--- Microsoft.Azure.Cosmos.Samples/Tools/CTL/Scenarios/QueryScenario.cs
+++ Microsoft.Azure.Cosmos.Samples/Tools/CTL/Scenarios/QueryScenario.cs
@@ -13,7 +13,7 @@ namespace CosmosCTL
     using Microsoft.Azure.Cosmos;
     using Microsoft.Extensions.Logging;
     using System.Text;
-    using App.Metrics.Counter;
+    using App.Metrics.Gauge;
 
     internal class QueryScenario : ICTLScenario
     {
@@ -52,53 +52,104 @@ namespace CosmosCTL
             string loggingContextIdentifier,
             CancellationToken cancellationToken)
         {
+            await Task.WhenAll(
+                ExecuteQueryAndGatherResultsAsync(
+                    config, 
+                    cosmosClient, 
+                    logger, 
+                    metrics, 
+                    loggingContextIdentifier, 
+                    cancellationToken, 
+                    queryText: "select * from c", 
+                    queryName: "Star",
+                    expectedResults: config.PreCreatedDocuments > 0 ? this.initializationResult.InsertedDocuments: 0),
+                ExecuteQueryAndGatherResultsAsync(
+                    config, 
+                    cosmosClient, 
+                    logger, 
+                    metrics, 
+                    loggingContextIdentifier, 
+                    cancellationToken, 
+                    queryText: "select * from c order by c.id", 
+                    queryName: "OrderBy",
+                    expectedResults: config.PreCreatedDocuments > 0 ? this.initializationResult.InsertedDocuments : 0),
+                ExecuteQueryAndGatherResultsAsync(
+                    config, 
+                    cosmosClient, 
+                    logger, 
+                    metrics, 
+                    loggingContextIdentifier, 
+                    cancellationToken, 
+                    queryText: "select count(1) from c", 
+                    queryName: "Aggregates",
+                    expectedResults: 1)
+                );
+        }
+
+        private async static Task ExecuteQueryAndGatherResultsAsync(CTLConfig config,
+            CosmosClient cosmosClient,
+            ILogger logger,
+            IMetrics metrics,
+            string loggingContextIdentifier,
+            CancellationToken cancellationToken,
+            string queryText,
+            string queryName,
+            long expectedResults)
+        {
             Stopwatch stopWatch = Stopwatch.StartNew();
 
-            CounterOptions documentCounter = new CounterOptions { Name = "#Documents received", Context = loggingContextIdentifier };
-            while (stopWatch.Elapsed <= config.RunningTimeDurationAsTimespan)
+            GaugeOptions documentGauge = new GaugeOptions
+            {
+                Name = $"#{queryName} Query received documents",
+                MeasurementUnit = Unit.Items,
+                Context = loggingContextIdentifier
+            };
+
+            Container container = cosmosClient.GetContainer(config.Database, config.Collection);
+            while (stopWatch.Elapsed <= config.RunningTimeDurationAsTimespan
+                && !cancellationToken.IsCancellationRequested)
             {
                 // To really debug what happened on the query, having a list of all continuations would be useful.
                 List<string> allContinuations = new List<string>();
-                int documentTotal = 0;
+                long documentTotal = 0;
                 string continuation;
-                Container container = cosmosClient.GetContainer(config.Database, config.Collection);
-                FeedIterator<Dictionary<string, string>> query = container.GetItemQueryIterator<Dictionary<string, string>>("select * from c");
+                FeedIterator<Dictionary<string, string>> query = container.GetItemQueryIterator<Dictionary<string, string>>(queryText);
                 try
                 {
                     while (query.HasMoreResults)
                     {
                         FeedResponse<Dictionary<string, string>> response = await query.ReadNextAsync();
                         documentTotal += response.Count;
-                        metrics.Measure.Counter.Increment(documentCounter, response.Count);
                         continuation = response.ContinuationToken;
                         allContinuations.Add(continuation);
                         if (continuation != null)
                         {
                             // Use continuation to paginate on the query instead of draining just the initial query
                             // This validates that we can indeed move forward with the continuation
-                            query = container.GetItemQueryIterator<Dictionary<string, string>>("select * from c", continuation);
+                            query = container.GetItemQueryIterator<Dictionary<string, string>>(queryText, continuation);
                         }
                     }
 
-                    if (config.PreCreatedDocuments > 0)
-                    {
-                        if (this.initializationResult.InsertedDocuments != documentTotal)
-                        {
-                            StringBuilder errorDetail = new StringBuilder();
-                            errorDetail.AppendLine($"Expected to read {this.initializationResult.InsertedDocuments} but got {documentTotal}");
-                            foreach (string c in allContinuations)
-                            {
-                                errorDetail.AppendLine($"Continuation: {c}");
-                            }
+                    metrics.Measure.Gauge.SetValue(documentGauge, documentTotal);
 
-                            logger.LogError(errorDetail.ToString());
+                    if (expectedResults > 0 && expectedResults != documentTotal)
+                    {
+                        StringBuilder errorDetail = new StringBuilder();
+                        errorDetail.AppendLine($"{queryName} Query expected to read {expectedResults} but got {documentTotal}");
+                        foreach (string c in allContinuations)
+                        {
+                            errorDetail.AppendLine($"Continuation: {c}");
                         }
+
+                        logger.LogError(errorDetail.ToString());
                     }
                 }
                 catch (Exception ex)
                 {
+                    metrics.Measure.Gauge.SetValue(documentGauge, documentTotal);
+
                     StringBuilder errorDetail = new StringBuilder();
-                    errorDetail.AppendLine("Failure while looping through query.");
+                    errorDetail.AppendLine($"{queryName} Query failure while looping through query.");
                     foreach (string c in allContinuations)
                     {
                         errorDetail.AppendLine($"Continuation: {c}");
