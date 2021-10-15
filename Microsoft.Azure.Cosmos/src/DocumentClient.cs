20c475d128b32b2c5333e071bc33c26db711be80
SchintaMicrosoft <89664119+SchintaMicrosoft@users.noreply.github.com>
2021-10-15 14:50:38 -0700

    CosmosClient.ReadAccountAsync: Fixes it to throw CosmosException instead of DocumentClientException (#2792)
    I have tested the Readaccountasync method with invalid key. it is throwing document client exception instead of Cosmos Exception. So I try to catch that error in EnsureValidClientAsync method to convert it to Cosmos exception and I have added test method EnsureUnauthorized_ThrowsCosmosClientException_ReadAccountAsync in CosmosPermissionTests.cs .

--- Microsoft.Azure.Cosmos/src/DocumentClient.cs
+++ Microsoft.Azure.Cosmos/src/DocumentClient.cs
@@ -1442,6 +1442,12 @@ namespace Microsoft.Azure.Cosmos
                     this.isSuccessfullyInitialized = true;
                     return;
                 }
+                catch (DocumentClientException ex)
+                {
+                    throw Resource.CosmosExceptions.CosmosExceptionFactory.Create(
+                         dce: ex,
+                         trace: trace);
+                }
                 catch (Exception e)
                 {
                     DefaultTrace.TraceWarning("initializeTask failed {0}", e.ToString());
@@ -1459,8 +1465,18 @@ namespace Microsoft.Azure.Cosmos
                     initTask = this.initializeTask;
                 }
 
-                await initTask;
-                this.isSuccessfullyInitialized = true;
+                try
+                {
+                    await initTask;
+                    this.isSuccessfullyInitialized = true;
+                }
+                catch (DocumentClientException ex)
+                {
+                    throw Resource.CosmosExceptions.CosmosExceptionFactory.Create(
+                         dce: ex,
+                         trace: trace);
+                }
+                
             }
         }
 
