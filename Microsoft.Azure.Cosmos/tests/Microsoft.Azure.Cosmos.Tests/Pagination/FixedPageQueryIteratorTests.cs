100a110430e08bcef4ee1fe845292824b824aaa0
Neil Deshpande <ndeshpan@microsoft.com>
2021-10-28 14:05:59 -0700

    Add tests for hydrating the FixedPageIterator from continuation token

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Pagination/FixedPageQueryIteratorTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Pagination/FixedPageQueryIteratorTests.cs
@@ -12,6 +12,7 @@ namespace Microsoft.Azure.Cosmos.Tests.Pagination
     using System.Threading.Tasks;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
+    using Newtonsoft.Json;
 
     [TestClass]
     public class FixedPageQueryIteratorTests
@@ -19,72 +20,78 @@ namespace Microsoft.Azure.Cosmos.Tests.Pagination
         private readonly IReadOnlyList<FixedPageQueryIteratorTestCase> TestCases = new List<FixedPageQueryIteratorTestCase>
         {
             MakeTest(
-                new List<TestFeedResponse>
+                responses: new List<TestFeedResponse>
                 {
-                    new TestFeedResponse(Enumerable.Repeat(1, 10), "2", 200, "1"),
+                    new TestFeedResponse(Enumerable.Repeat(1, 10), "1", 200, "a"),
                 },
-                new List<(int FixedPageSize, IReadOnlyList<TestFeedResponse> ExpectedResponses)>
+                expected: new List<(int FixedPageSize, IReadOnlyList<TestFeedResponse> ExpectedResponses)>
                 {
-                    (5,
-                    new List<TestFeedResponse>
+                    (FixedPageSize: 5,
+                    ExpectedResponses: new List<TestFeedResponse>
                     { 
                         new TestFeedResponse(
                             Enumerable.Repeat(1, 5),
                             new FixedPageQueryIterator<int>.ContinuationState(token: null, skip: 5).ToString(),
                             200,
-                            "1\r\n")
+                            "a\r\n"),
+                        new TestFeedResponse(
+                            Enumerable.Repeat(1, 5),
+                            null,
+                            0,
+                            "a\r\n")
                     })
                 }),
             MakeTest(
-                new List<TestFeedResponse>
+                responses: new List<TestFeedResponse>
                 {
-                    new TestFeedResponse(Enumerable.Repeat(1, 5), "2", 200, "1"),
-                    new TestFeedResponse(Enumerable.Repeat(2, 5), null, 200, "2"),
+                    new TestFeedResponse(Enumerable.Repeat(1, 5), "1", 200, "a"),
+                    new TestFeedResponse(Enumerable.Repeat(2, 5), null, 200, "b"),
                 },
-                new List<(int FixedPageSize, IReadOnlyList<TestFeedResponse> ExpectedResponses)>
+                expected: new List<(int FixedPageSize, IReadOnlyList<TestFeedResponse> ExpectedResponses)>
                 {
-                    (10,
-                    new List<TestFeedResponse>
+                    (FixedPageSize: 10,
+                    ExpectedResponses: new List<TestFeedResponse>
                     {
                         new TestFeedResponse(
                             Enumerable.Repeat(1, 5).Concat(Enumerable.Repeat(2, 5)),
                             null,
                             400,
-                            "1\r\n2\r\n")
+                            "a\r\nb\r\n")
                     })
                 }),
             MakeTest(
-                new List<TestFeedResponse>
+                responses: new List<TestFeedResponse>
                 {
-                    new TestFeedResponse(Enumerable.Repeat(1, 5), "2", 200, "1"),
-                    new TestFeedResponse(Enumerable.Repeat(2, 3), "3", 100, "2"),
-                    new TestFeedResponse(Enumerable.Repeat(3, 3), null, 150, "3"),
+                    new TestFeedResponse(Enumerable.Repeat(1, 5), "1", 200, "a"),
+                    new TestFeedResponse(Enumerable.Repeat(2, 3), "2", 100, "b"),
+                    new TestFeedResponse(Enumerable.Repeat(3, 3), null, 150, "c"),
                 },
-                new List<(int FixedPageSize, IReadOnlyList<TestFeedResponse> ExpectedResponses)>
+                expected: new List<(int FixedPageSize, IReadOnlyList<TestFeedResponse> ExpectedResponses)>
                 {
-                    (10,
-                    new List<TestFeedResponse>
+                    (FixedPageSize: 10,
+                    ExpectedResponses: new List<TestFeedResponse>
                     {
                         new TestFeedResponse(
                             Enumerable
                                 .Repeat(1, 5)
                                 .Concat(Enumerable.Repeat(2, 3))
                                 .Concat(Enumerable.Repeat(3, 2)),
-                            new FixedPageQueryIterator<int>.ContinuationState(token: "3", skip: 2).ToString(),
+                            new FixedPageQueryIterator<int>.ContinuationState(token: "2", skip: 2).ToString(),
                             450,
-                            "1\r\n2\r\n3\r\n")
+                            "a\r\nb\r\nc\r\n"),
+                        new TestFeedResponse(Enumerable.Repeat(3, 1), null, 0, "c\r\n")
                     })
                 }),
             MakeTest(
-                new List<TestFeedResponse>
+                responses: new List<TestFeedResponse>
                 {
-                    new TestFeedResponse(Enumerable.Repeat(1, 5), "2", 200, "1"),
-                    new TestFeedResponse(Enumerable.Repeat(2, 3), null, 100, "2")
+                    new TestFeedResponse(Enumerable.Repeat(1, 5), "1", 200, "a"),
+                    new TestFeedResponse(Enumerable.Repeat(2, 3), null, 100, "b")
                 },
-                new List<(int FixedPageSize, IReadOnlyList<TestFeedResponse> ExpectedResponses)>
+                expected: new List<(int FixedPageSize, IReadOnlyList<TestFeedResponse> ExpectedResponses)>
                 {
-                    (10,
-                    new List<TestFeedResponse>
+                    (FixedPageSize: 10,
+                    ExpectedResponses: new List<TestFeedResponse>
                     {
                         new TestFeedResponse(
                             Enumerable
@@ -92,7 +99,7 @@ namespace Microsoft.Azure.Cosmos.Tests.Pagination
                                 .Concat(Enumerable.Repeat(2, 3)),
                             null,
                             300,
-                            "1\r\n2\r\n")
+                            "a\r\nb\r\n")
                     })
                 })
         };
@@ -111,34 +118,64 @@ namespace Microsoft.Azure.Cosmos.Tests.Pagination
             foreach (FixedPageQueryIteratorTestCase testCase in this.TestCases)
             {
                 TestFeedIterator testIterator = new TestFeedIterator(testCase.Responses);
-                Mock<Container> mockContainer = new Mock<Container>();
+                Mock<Container> mockContainer = new Mock<Container>(MockBehavior.Strict);
                 mockContainer.Setup(c => c
                     .GetItemQueryIterator<int>(queryDefinition, null, queryRequestOptions))
                     .Returns(testIterator);
 
                 foreach ((int fixedPageSize, IReadOnlyList<TestFeedResponse> expectedResponses) in testCase.Expected)
                 {
-                    FeedIterator<int> iterator = FixedPageQueryIterator<int>.Create(
+                    using FeedIterator<int> iterator = FixedPageQueryIterator<int>.Create(
                         mockContainer.Object,
                         queryDefinition,
                         queryRequestOptions,
                         null,
                         fixedPageSize);
 
+                    string continuationToken = null;
+                    string underlyingContinuationToken = null;
                     foreach (TestFeedResponse expectedResponse in expectedResponses)
                     {
                         Assert.IsTrue(iterator.HasMoreResults);
                         FeedResponse<int> response = await iterator.ReadNextAsync();
                         ValidateResponse(expected: expectedResponse, actual: response);
+
+                        {
+                            int underlyingPageIndex = 0;
+                            Assert.IsTrue(underlyingContinuationToken == null || int.TryParse(underlyingContinuationToken, out underlyingPageIndex));
+
+                            TestFeedIterator continuedIterator = new TestFeedIterator(testCase.Responses, underlyingPageIndex);
+                            Mock<Container> continuedContainer = new Mock<Container>(MockBehavior.Strict);
+                            continuedContainer.Setup(c => c
+                                .GetItemQueryIterator<int>(queryDefinition, underlyingContinuationToken, queryRequestOptions))
+                                .Returns(continuedIterator);
+
+                            using FeedIterator<int> continuedFixedPageIterator = FixedPageQueryIterator<int>.Create(
+                                continuedContainer.Object,
+                                queryDefinition,
+                                queryRequestOptions,
+                                continuationToken,
+                                fixedPageSize);
+
+                            Assert.IsTrue(continuedFixedPageIterator.HasMoreResults);
+                            FeedResponse<int> continuedResponse = await continuedFixedPageIterator.ReadNextAsync();
+                            ValidateResponse(expected: expectedResponse, actual: continuedResponse, chargeOverride: continuedIterator.RequestCharge);
+                        }
+
+                        continuationToken = response.ContinuationToken;
+                        underlyingContinuationToken = continuationToken != null ?
+                            JsonConvert
+                                .DeserializeObject<FixedPageQueryIterator<int>.ContinuationState>(continuationToken)
+                                .ContinuationToken :
+                            null;
                     }
 
-                    // TODO: fix up test cases above and uncomment this
-                    // Assert.IsFalse(iterator.HasMoreResults);
+                    Assert.IsFalse(iterator.HasMoreResults);
                 }
             }
         }
 
-        private static void ValidateResponse(FeedResponse<int> expected, FeedResponse<int> actual)
+        private static void ValidateResponse(FeedResponse<int> expected, FeedResponse<int> actual, double? chargeOverride = null)
         {
             Assert.AreEqual(expected.ActivityId, actual.ActivityId);
             Assert.AreEqual(expected.ContinuationToken, actual.ContinuationToken);
@@ -147,8 +184,16 @@ namespace Microsoft.Azure.Cosmos.Tests.Pagination
             Assert.AreEqual(expected.ETag, actual.ETag);
             Assert.AreEqual(expected.IndexMetrics, actual.IndexMetrics);
             Assert.AreEqual(expected.StatusCode, actual.StatusCode);
-            Assert.AreEqual(expected.RequestCharge, actual.RequestCharge);
             CollectionAssert.AreEqual(expected.Resource.ToList(), actual.Resource.ToList());
+
+            if (chargeOverride.HasValue)
+            {
+                Assert.AreEqual(chargeOverride, actual.RequestCharge);
+            }
+            else
+            {
+                Assert.AreEqual(expected.RequestCharge, actual.RequestCharge);
+            }
         }
 
         private static FixedPageQueryIteratorTestCase MakeTest(
@@ -217,15 +262,24 @@ namespace Microsoft.Azure.Cosmos.Tests.Pagination
         private class TestFeedIterator : FeedIterator<int>
         {
             public TestFeedIterator(IReadOnlyList<TestFeedResponse> responses)
+                : this(responses: responses, continuationToken: 0)
+            {
+            }
+
+            public TestFeedIterator(IReadOnlyList<TestFeedResponse> responses, int continuationToken)
             {
                 this.Responses = responses ?? throw new ArgumentNullException(nameof(responses));
+                this.Index = continuationToken;
+                this.RequestCharge = 0;
             }
 
             public IReadOnlyList<TestFeedResponse> Responses { get; }
 
-            public int Index { get; set; }
+            public int Index { get; private set; }
 
             public override bool HasMoreResults => this.Index < this.Responses.Count;
+
+            public double RequestCharge { get; private set; }
 
             public override Task<FeedResponse<int>> ReadNextAsync(CancellationToken cancellationToken = default)
             {
@@ -234,6 +288,7 @@ namespace Microsoft.Azure.Cosmos.Tests.Pagination
                     throw new InvalidOperationException("No more results");
                 }
 
+                this.RequestCharge += this.Responses[this.Index].RequestCharge;
                 return Task.FromResult<FeedResponse<int>>(this.Responses[this.Index++]);
             }
         }
