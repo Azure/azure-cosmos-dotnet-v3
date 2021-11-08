28435e2f58ac4396531a9885551dc85d00f03df1
Santosh Kulkarni <66682828+kr-santosh@users.noreply.github.com>
2021-11-08 14:06:22 +0530

    Updated file/folder names

---
+++ Microsoft.Azure.Cosmos.Samples/Usage/ReEncryption/ReEncryptionSupport/ReEncryptionItemResponseExtensions.cs
@@ -0,0 +1,54 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace Cosmos.Samples.ReEncryption
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos;
+
+    internal static class ReEncryptionItemResponseExtensions
+    {
+        internal static Task<ReEncryptionOperationResponse<T>> CaptureReEncryptionOperationResponseAsync<T>(
+            this Task<ItemResponse<T>> task,
+            T item)
+        {
+            return task.ContinueWith(itemResponse =>
+            {
+                if (itemResponse.IsCompleted && itemResponse.Exception == null)
+                {
+                    return new ReEncryptionOperationResponse<T>()
+                    {
+                        Item = item,
+                        IsSuccessful = true,
+                        RequestUnitsConsumed = task.Result.RequestCharge,
+                    };
+                }
+
+                AggregateException innerExceptions = itemResponse.Exception.Flatten();
+
+                if (innerExceptions
+                    .InnerExceptions
+                    .FirstOrDefault(innerEx => innerEx is CosmosException) is CosmosException cosmosException)
+                {
+                    return new ReEncryptionOperationResponse<T>()
+                    {
+                        Item = item,
+                        RequestUnitsConsumed = cosmosException.RequestCharge,
+                        IsSuccessful = false,
+                        CosmosException = cosmosException,
+                    };
+                }
+
+                return new ReEncryptionOperationResponse<T>()
+                {
+                    Item = item,
+                    IsSuccessful = false,
+                    CosmosException = innerExceptions.InnerExceptions.FirstOrDefault(),
+                };
+            });
+        }
+    }
+}
