1178f9c100c37c1e0f1488c55d16e5a43e241d36
deparash <71714415+deparash@users.noreply.github.com>
2021-11-09 17:35:54 +0530

    Update TransactionalBatchInternal.cs

--- Microsoft.Azure.Cosmos/src/Batch/TransactionalBatchInternal.cs
+++ Microsoft.Azure.Cosmos/src/Batch/TransactionalBatchInternal.cs
@@ -11,16 +11,5 @@ namespace Microsoft.Azure.Cosmos
 
     internal abstract class TransactionalBatchInternal : TransactionalBatch
     {
-        /// <summary>
-        /// Adds an operation to patch an item into the batch.
-        /// </summary>
-        /// <param name="id">The unique id of the item.</param>
-        /// <param name="patchOperations">Represents a list of operations to be sequentially applied to the referred Cosmos item.</param>
-        /// <param name="requestOptions">(Optional) The options for the item request.</param>
-        /// <returns>The transactional batch instance with the operation added.</returns>
-        public abstract TransactionalBatch PatchItem(
-                string id,
-                System.Collections.Generic.IReadOnlyList<PatchOperation> patchOperations,
-                TransactionalBatchPatchItemRequestOptions requestOptions = null);
     }
 }
