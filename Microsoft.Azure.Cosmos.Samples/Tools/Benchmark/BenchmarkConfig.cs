8eef0155896a22efa53e5867272215f3b0ed12be
Sourabh Jain <sourabh.jain.107@gmail.com>
2021-11-10 19:03:25 +0530

    add telemetry config

--- Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/BenchmarkConfig.cs
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/BenchmarkConfig.cs
@@ -98,6 +98,12 @@ namespace CosmosBenchmark
         [Option(Required = false, HelpText = "Enable Telemetry")]
         public bool EnableTelemetry { get; set; }
 
+        [Option(Required = false, HelpText = "Telemetry Schedule in Seconds")]
+        public string TelemetryScheduleInSec { get; set; }
+
+        [Option(Required = false, HelpText = "Telemetry Endpoint")]
+        public string TelemetryEndpoint { get; set; }
+
         [Option(Required = false, HelpText = "Endpoint to publish results to")]
         public string ResultsEndpoint { get; set; }
 
@@ -167,13 +173,17 @@ namespace CosmosBenchmark
                 ApplicationName = BenchmarkConfig.UserAgentSuffix,
                 MaxRetryAttemptsOnRateLimitedRequests = 0,
                 MaxRequestsPerTcpConnection = this.MaxRequestsPerTcpConnection,
-                MaxTcpConnectionsPerEndpoint = this.MaxTcpConnectionsPerEndpoint,
-                EnableClientTelemetry = this.EnableTelemetry
+                MaxTcpConnectionsPerEndpoint = this.MaxTcpConnectionsPerEndpoint
             };
 
-            if(this.EnableTelemetry)
+            if (this.EnableTelemetry)
             {
-                Environment.SetEnvironmentVariable(Microsoft.Azure.Cosmos.Telemetry.ClientTelemetryOptions.EnvPropsClientTelemetrySchedulingInSeconds, "1");
+                Environment.SetEnvironmentVariable(Microsoft.Azure.Cosmos.Telemetry.ClientTelemetryOptions.EnvPropsClientTelemetryEnabled, "true");
+                Environment.SetEnvironmentVariable(Microsoft.Azure.Cosmos.Telemetry.ClientTelemetryOptions.EnvPropsClientTelemetrySchedulingInSeconds, this.TelemetryScheduleInSec ?? "600");
+                if (!string.IsNullOrEmpty(this.TelemetryEndpoint))
+                {
+                    Environment.SetEnvironmentVariable(Microsoft.Azure.Cosmos.Telemetry.ClientTelemetryOptions.EnvPropsClientTelemetryEndpoint, this.TelemetryEndpoint);
+                }
             }
 
             if (!string.IsNullOrWhiteSpace(this.ConsistencyLevel))
