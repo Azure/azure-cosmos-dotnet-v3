62a6006bd60351e95b5b5291e7cd12c0f0868e43
Sourabh Jain <sourabh.jain.107@gmail.com>
2021-11-16 22:33:36 +0530

    Revert "Revert "add test""
    This reverts commit d8cdda25b2d5e73b1ed674340420473513ae370e.

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/GatewayClientSideRequestStatsTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/GatewayClientSideRequestStatsTests.cs
@@ -8,7 +8,6 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
     using System.Net.Http;
     using System.Threading;
     using System.Threading.Tasks;
-    using Antlr4.Runtime.Tree;
     using Microsoft.Azure.Cosmos.Diagnostics;
     using Microsoft.Azure.Cosmos.Tracing;
     using Microsoft.Azure.Cosmos.Tracing.TraceData;
@@ -43,6 +42,8 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
             ItemResponse<ToDoActivity> response = await this.Container.CreateItemAsync(item);
 
             ClientSideRequestStatisticsTraceDatum datum = this.GetClientSideRequestStatsFromTrace(((CosmosTraceDiagnostics)response.Diagnostics).Value, "Transport");
+            Assert.IsNotNull(datum.RegionsContacted);
+            Assert.IsTrue(datum.RegionsContacted.Count > 0);
             Assert.IsNotNull(datum.HttpResponseStatisticsList);
             Assert.AreEqual(datum.HttpResponseStatisticsList.Count, 1);
             Assert.IsNotNull(datum.HttpResponseStatisticsList[0].HttpResponseMessage);
@@ -73,6 +74,8 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
                 
                 ItemResponse<ToDoActivity> response = await container.ReadItemAsync<ToDoActivity>(item.id, new PartitionKey(item.pk));
                 ClientSideRequestStatisticsTraceDatum datum = this.GetClientSideRequestStatsFromTrace(((CosmosTraceDiagnostics)response.Diagnostics).Value, traceToFind);
+                Assert.IsNotNull(datum.RegionsContacted);
+                Assert.IsTrue(datum.RegionsContacted.Count > 0);
                 Assert.IsNotNull(datum.HttpResponseStatisticsList);
                 Assert.AreEqual(datum.HttpResponseStatisticsList.Count, expectedHttpCalls);
                 Assert.IsTrue(datum.HttpResponseStatisticsList[0].Exception is OperationCanceledException);
@@ -93,6 +96,8 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
                 Container container = cosmosClient.GetContainer(this.Database.Id, this.Container.Id);
                 ItemResponse<ToDoActivity> response = await container.ReadItemAsync<ToDoActivity>(item.id, new PartitionKey(item.pk));
                 ClientSideRequestStatisticsTraceDatum datum = this.GetClientSideRequestStatsFromTrace(((CosmosTraceDiagnostics)response.Diagnostics).Value, "Transport Request");
+                Assert.IsNotNull(datum.RegionsContacted);
+                Assert.IsTrue(datum.RegionsContacted.Count > 0);
                 Assert.IsNotNull(datum.HttpResponseStatisticsList);
                 // One call for collection cache, 2 calls for PK range cache and 1 call for Address Resolution
                 Assert.AreEqual(datum.HttpResponseStatisticsList.Count, 4);
