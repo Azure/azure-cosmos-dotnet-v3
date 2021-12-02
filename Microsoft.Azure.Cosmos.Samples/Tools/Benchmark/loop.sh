e7cd84e077d750abbeb7eb42e6fd50f3aa3d79f8
Jake Willey <jawilley@microsoft.com>
2021-12-02 06:59:26 -0800

    Adding loop script and updating the run log to include the new query operations

---
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/loop.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export OSSProjectRef=True
+export RESULTS_PK=test_runs
+export PL=18
+
+#These must be configured
+export ACCOUNT_ENDPOINT= 
+export ACCOUNT_KEY=
+export TELEMETRY_ENDPOINT=
+
+#Loop forever
+i=0
+while :
+do
+    #Kill any running processes
+    pkill -f run.sh
+    git pull origin --depth=1
+
+    # Query operations take a long time
+    # Only run it them once every 10 runs
+    if [ $(expr $i % 10) != "0" ]; then
+        export INCLUDE_QUERY = true
+    else
+        export INCLUDE_QUERY = false
+    fi
+    i++
+
+    ./run.sh
+
+    echo "====== Waiting for 10Sec ================="
+    sleep 10 #Wait for 10sec
+    
+done
+
