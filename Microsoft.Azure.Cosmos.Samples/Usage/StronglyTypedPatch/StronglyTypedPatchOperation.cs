60cef6262442e79a4fbe8cc9d3249ababe16ba9b
Avi Levin <avilevin@microsoft.com>
2021-12-05 14:05:23 +0200

    Add StronglyTypedPatch models

---
+++ Microsoft.Azure.Cosmos.Samples/Usage/StronglyTypedPatch/StronglyTypedPatchOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.Azure.Cosmos;
+
+namespace StronglyTypedPatch
+{
+
+    public abstract class StronglyTypedPatchOperation<TObject>
+    {
+
+    }
+
+    public class StronglyTypedPatchOperation<TObject, TValue> : StronglyTypedPatchOperation<TObject>
+    {
+        internal StronglyTypedPatchOperation(
+            TValue value,
+            PatchOperationType operationType,
+            Expression<Func<TObject, TValue>> path)
+        {
+            this.Value = value;
+            this.OperationType = operationType;
+            this.Path = path;
+        }
+
+        public TValue Value { get; }
+
+        public PatchOperationType OperationType { get; }
+
+        public Expression<Func<TObject, TValue>> Path { get; }
+    }
+}
---
+++ Microsoft.Azure.Cosmos.Samples/Usage/StronglyTypedPatch/StronglyTypedPatchOperationBuilder.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.Azure.Cosmos;
+
+namespace StronglyTypedPatch
+{
+    public class StronglyTypedPatchOperationBuilder<TObject>
+    {
+        private readonly List<StronglyTypedPatchOperation<TObject>> stronglyTypedPatchOperations = new();
+
+        public IReadOnlyList<StronglyTypedPatchOperation<TObject>> StronglyTypedPatchOperations => this.stronglyTypedPatchOperations;
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithAdd<TValue>(Expression<Func<TObject, TValue>> path, TValue value)
+        {
+            this.stronglyTypedPatchOperations.Add(new StronglyTypedPatchOperation<TObject, TValue>(value, PatchOperationType.Add, path));
+            return this;
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithRemove<TValue>(Expression<Func<TObject, TValue>> path)
+        {
+            this.stronglyTypedPatchOperations.Add(new StronglyTypedPatchOperation<TObject, TValue>(default, PatchOperationType.Remove, path));
+            return this;
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithReplace<TValue>(Expression<Func<TObject, TValue>> path, TValue value)
+        {
+            this.stronglyTypedPatchOperations.Add(new StronglyTypedPatchOperation<TObject, TValue>(value, PatchOperationType.Replace, path));
+            return this;
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithSet<TValue>(Expression<Func<TObject, TValue>> path, TValue value)
+        {
+            this.stronglyTypedPatchOperations.Add(new StronglyTypedPatchOperation<TObject, TValue>(value, PatchOperationType.Set, path));
+            return this;
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithIncrement(Expression<Func<TObject, byte>> path, byte value)
+        {
+            return this.WithIncrement<byte>(path, value);
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithIncrement(Expression<Func<TObject, short>> path, short value)
+        {
+            return this.WithIncrement<short>(path, value);
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithIncrement(Expression<Func<TObject, int>> path, int value)
+        {
+            return this.WithIncrement<int>(path, value);
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithIncrement(Expression<Func<TObject, long>> path, long value)
+        {
+            return this.WithIncrement<long>(path, value);
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithIncrement(Expression<Func<TObject, sbyte>> path, sbyte value)
+        {
+            return this.WithIncrement<sbyte>(path, value);
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithIncrement(Expression<Func<TObject, ushort>> path, ushort value)
+        {
+            return this.WithIncrement<ushort>(path, value);
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithIncrement(Expression<Func<TObject, uint>> path, uint value)
+        {
+            return this.WithIncrement<uint>(path, value);
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithIncrement(Expression<Func<TObject, ulong>> path, ulong value)
+        {
+            return this.WithIncrement<ulong>(path, value);
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithIncrement(Expression<Func<TObject, float>> path, float value)
+        {
+            return this.WithIncrement<float>(path, value);
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithIncrement(Expression<Func<TObject, double>> path, double value)
+        {
+            return this.WithIncrement<double>(path, value);
+        }
+
+        public StronglyTypedPatchOperationBuilder<TObject> WithIncrement(Expression<Func<TObject, decimal>> path, decimal value)
+        {
+            return this.WithIncrement<decimal>(path, value);
+        }
+
+        private StronglyTypedPatchOperationBuilder<TObject> WithIncrement<TValue>(Expression<Func<TObject, TValue>> path, TValue value)
+        {
+            this.stronglyTypedPatchOperations.Add(new StronglyTypedPatchOperation<TObject, TValue>(value, PatchOperationType.Increment, path));
+            return this;
+        }
+
+        public List<PatchOperation> ToUntyped(CosmosClient client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
