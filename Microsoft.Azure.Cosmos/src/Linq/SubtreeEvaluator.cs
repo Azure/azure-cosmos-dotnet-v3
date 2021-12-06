fbca6331a75eb95f9bbe1b3a94ffba02ecb776f8
Benjamin Pannell <bpannell@microsoft.com>
2021-12-06 22:49:07 +0000

    feat: Implement an optimized path for member access expressions within a LINQ-to-SQL expression
    This optimization handles the common case of accessing a member (variable,
    field, property) in the ambient scope, as shown below:
    
    ```
    var bob = "Bob";
    users.Where(u => u.Username == bob);
    ```
    
    In this scenario, we will attempt to avoid compiling a lambda expression to
    evaluate `bob` and instead rely on reflection member access to retrieve the
    constant value. This helps us avoid the global lock held by
    `System.Reflection.Emit.DynamicMethod` while constructing the
    dynamic method table, which alleviates a serious performance bottleneck
    on highly threaded systems.
    
    In particular, this helps avoid a pathological async-over-sync situation
    which can lead to thread pool exhaustion and hot-lock thrashing.

--- Microsoft.Azure.Cosmos/src/Linq/SubtreeEvaluator.cs
+++ Microsoft.Azure.Cosmos/src/Linq/SubtreeEvaluator.cs
@@ -6,12 +6,15 @@ namespace Microsoft.Azure.Cosmos.Linq
     using System;
     using System.Collections.Generic;
     using System.Linq.Expressions;
+    using System.Reflection;
 
     /// <summary> 
     /// Evaluates and replaces sub-trees when first candidate is reached (top-down) 
     /// </summary> 
     internal sealed class SubtreeEvaluator : ExpressionVisitor
     {
+        private static readonly Dictionary<(Type Type, string Member), MemberInfo> memberInfoCache = new Dictionary<(Type Type, string Member), MemberInfo>();
+
         private HashSet<Expression> candidates;
 
         public SubtreeEvaluator(HashSet<Expression> candidates)
@@ -44,12 +47,39 @@ namespace Microsoft.Azure.Cosmos.Linq
 
         private Expression EvaluateConstant(Expression expression)
         {
+            if (expression.CanReduce)
+            {
+                expression = expression.Reduce();
+            }
+
             if (expression.NodeType == ExpressionType.Constant)
             {
                 return expression;
             }
+
+            // This is an optimization which attempts to avoid the compilation of a delegate lambda for
+            // conversion of an expression to a constant by handling member access on a constant through
+            // reflection instead.
+            //
+            // This is done because the compilation of a delegate takes a global lock which causes highly
+            // threaded clients to exhibit async-over-sync thread exhaustion behaviour on this call path
+            // even when doing relatively straightforward queries.
+            if (expression is MemberExpression memberExpression && memberExpression.Expression is ConstantExpression targetConstant)
+            {
+                if (memberExpression.Member is FieldInfo fieldInfo)
+                {
+                    return Expression.Constant(fieldInfo.GetValue(targetConstant.Value));
+                }
+
+                if (memberExpression.Member is PropertyInfo propertyInfo)
+                {
+                    return Expression.Constant(propertyInfo.GetValue(targetConstant.Value));
+                }
+            }
+
             LambdaExpression lambda = Expression.Lambda(expression);
             Delegate function = lambda.Compile();
+
             return Expression.Constant(function.DynamicInvoke(null), expression.Type);
         }
     }
