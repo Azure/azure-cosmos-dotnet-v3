9ea5f1e7d105f2644bb4173e88ac70a47133b8a4
Benjamin Pannell <bpannell@microsoft.com>
2021-12-07 10:31:48 +0000

    test: Add some performance benchmarks to highlight the difference in reflection property access perf

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Linq/LinqToSqlBenchmark.cs
@@ -0,0 +1,41 @@
+﻿namespace Microsoft.Azure.Cosmos.Linq
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Text;
+    using BenchmarkDotNet.Attributes;
+
+    public class LinqToSqlBenchmark
+    {
+        private class BenchmarkDocument
+        {
+            public string Property { get; set; }
+        }
+
+        [Benchmark(Baseline = true)]
+        public void DelegatePropertyAccess()
+        {
+            var holder = new
+            {
+                Property = "test"
+            };
+
+            this.DoTranslate(doc => doc.Property == holder.Property);
+        }
+
+        [Benchmark]
+        public void ReflectionPropertyAccess()
+        {
+            string variable = "test";
+
+            this.DoTranslate(doc => doc.Property == variable);
+        }
+
+        private void DoTranslate<R>(Expression<Func<BenchmarkDocument, R>> expression)
+        {
+            SqlTranslator.TranslateExpression(expression.Body);
+        }
+    }
+}
