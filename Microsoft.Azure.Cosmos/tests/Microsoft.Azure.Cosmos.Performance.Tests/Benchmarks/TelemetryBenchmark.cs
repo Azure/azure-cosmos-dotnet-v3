962283c5e73c0a65bf21228bdc6991c3814ed641
Sourabh Jain <sourabhjain@microsoft.com>
2021-12-14 22:18:49 +0530

    add more collect scenario

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Benchmarks/TelemetryBenchmark.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Benchmarks/TelemetryBenchmark.cs
@@ -27,7 +27,7 @@ namespace Microsoft.Azure.Cosmos.Performance.Tests.Benchmarks
         }
 
         [Benchmark]
-        public void CollectTest()
+        public void CollectOriginalTest()
         {
             this.CollectOriginal(cosmosDiagnostics: this.diagnostics,
                 statusCode: HttpStatusCode.OK,
@@ -38,6 +38,81 @@ namespace Microsoft.Azure.Cosmos.Performance.Tests.Benchmarks
                 resourceType: ResourceType.Document,
                 consistencyLevel: "eventual",
                 requestCharge: 10d);
+        }
+
+        [Benchmark]
+        public void CollectRegionContactedTest()
+        {
+            this.CollectRegionContacted(cosmosDiagnostics: this.diagnostics);
+        }
+
+        [Benchmark]
+        public void CollectOperationAndHistogramTest()
+        {
+            this.CollectOperationAndHistogram(cosmosDiagnostics: this.diagnostics,
+                statusCode: HttpStatusCode.OK,
+                responseSizeInBytes: 1000,
+                containerId: "containerid",
+                databaseId: "databaseid",
+                operationType: OperationType.Read,
+                resourceType: ResourceType.Document,
+                consistencyLevel: "eventual",
+                requestCharge: 10d);
+        }
+
+
+        internal void CollectOperationAndHistogram(CosmosDiagnostics cosmosDiagnostics,
+                HttpStatusCode statusCode,
+                long responseSizeInBytes,
+                string containerId,
+                string databaseId,
+                OperationType operationType,
+                ResourceType resourceType,
+                string consistencyLevel,
+                double requestCharge)
+        {
+            // Recording Request Latency and Request Charge
+            OperationInfo payloadKey = new OperationInfo(regionsContacted: "Region",
+                                            responseSizeInBytes: responseSizeInBytes,
+                                            consistency: consistencyLevel,
+                                            databaseName: databaseId,
+                                            containerName: containerId,
+                                            operation: operationType,
+                                            resource: resourceType,
+                                            statusCode: (int)statusCode);
+
+            payloadKey = this.operationInfo.GetOrAdd(payloadKey.Key, payloadKey);
+
+            try
+            {
+                payloadKey.latency.RecordValue(cosmosDiagnostics.GetClientElapsedTime().Ticks);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Latency Recording Failed by Telemetry. Exception : {0}", ex.Message);
+            }
+
+            long requestChargeToRecord = (long)(requestCharge * ClientTelemetryOptions.HistogramPrecisionFactor);
+            try
+            {
+                payloadKey.requestcharge.RecordValue(requestChargeToRecord);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Request Charge Recording Failed by Telemetry. Request Charge Value : {0}  Exception : {1} ", requestChargeToRecord, ex.Message);
+            }
+
+        }
+
+        internal void CollectRegionContacted(CosmosDiagnostics cosmosDiagnostics)
+        {
+            if (cosmosDiagnostics == null)
+            {
+                throw new ArgumentNullException(nameof(cosmosDiagnostics));
+            }
+
+             ClientTelemetryHelper.GetContactedRegions(cosmosDiagnostics);
+
         }
 
         internal void CollectOriginal(CosmosDiagnostics cosmosDiagnostics,
