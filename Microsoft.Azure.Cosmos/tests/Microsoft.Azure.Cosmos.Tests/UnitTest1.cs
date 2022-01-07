a014d33abd52b4cd4679b0dcf4637cd76cdafa3a
Heet Jayant Detroja <hdetroja@microsoft.com>
2022-01-07 15:05:45 -0800

    binary vs text test

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Documents.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Azure.Cosmos;
+using System;
+
+namespace BinaryVsTextTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Microsoft.Azure.Cosmos.FeedOptions feedOptions = new FeedOptions()
+            {
+                ContentSerializationOptions = CosmosBinary
+            };
+            
+
+        }
+    }
+}
