9dca68b3dee490e1d15070ae042bc0da3972ca98
Jake Willey <jawilley@microsoft.com>
2022-01-18 13:20:18 -0800

    Samples and tests: Refactors to use .NET 6

---
+++ UpdateDotNetVersion.ps1
@@ -0,0 +1,11 @@
+﻿# Script to update all the csproj .NET target framework version
+$projFiles = Get-ChildItem . *.csproj -rec
+
+foreach ($file in $projFiles)
+{
+    $content = (Get-Content $file.PSPath)
+    if($content -match "<TargetFramework>netcoreapp3.1</TargetFramework>"){
+    
+        $content -replace "<TargetFramework>netcoreapp3.1</TargetFramework>", "<TargetFramework>net6.0</TargetFramework>" | Set-Content $file.PSPath
+    }
+}
