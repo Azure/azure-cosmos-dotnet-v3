ba3d1c59c785092fb6c0e4e46481bc1f4c23fe8f
Asket Agarwal <asket.agarwal96@gmail.com>
2022-01-27 16:20:22 +0530

    restore

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Tracing/TraceWriterBaselineTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Tracing/TraceWriterBaselineTests.cs
@@ -961,11 +961,6 @@ namespace Microsoft.Azure.Cosmos.Tests.Tracing
             {
                 //NoImplementation
             }
-
-            public void AddOrUpdateDatum(string key, object value)
-            {
-                this.data[key] = value;
-            }
         }
     }
 }
