d98c8b611d402a9b83b14523016a19a152e2bb05
Sourabh Jain <sourabhjain@microsoft.com>
2022-02-20 23:45:49 +0530

    add documentation

--- Microsoft.Azure.Cosmos/src/Telemetry/Diagnostics/ICosmosInstrumentation.cs
+++ Microsoft.Azure.Cosmos/src/Telemetry/Diagnostics/ICosmosInstrumentation.cs
@@ -8,6 +8,9 @@ namespace Microsoft.Azure.Cosmos.Telemetry.Diagnostics
     using System.Net;
     using Documents;
 
+    /// <summary>
+    /// Cosmos Instrumentation Interface
+    /// </summary>
 #if INTERNAL
     public
 #else
@@ -15,10 +18,26 @@ namespace Microsoft.Azure.Cosmos.Telemetry.Diagnostics
 #endif 
         interface ICosmosInstrumentation : IDisposable
         {
+            /// <summary>
+            /// Mark Failed
+            /// </summary>
+            /// <param name="ex"></param>
             public void MarkFailed(Exception ex);
 
+            /// <summary>
+            /// Add attributes to scope
+            /// </summary>
             public void AddAttributesToScope();
 
+            /// <summary>
+            /// Record values
+            /// </summary>
+            /// <param name="requestCharge"></param>
+            /// <param name="operationType"></param>
+            /// <param name="statusCode"></param>
+            /// <param name="databaseId"></param>
+            /// <param name="containerId"></param>
+            /// <param name="queryText"></param>
             public void Record(double requestCharge,
                 string operationType,
                 HttpStatusCode statusCode, 
@@ -26,8 +45,18 @@ namespace Microsoft.Azure.Cosmos.Telemetry.Diagnostics
                 string containerId = null,
                 string queryText = null);
 
+            /// <summary>
+            /// Record Values
+            /// </summary>
+            /// <param name="accountName"></param>
+            /// <param name="userAgent"></param>
+            /// <param name="connectionMode"></param>
             public void Record(Uri accountName, string userAgent, ConnectionMode connectionMode);
 
+            /// <summary>
+            /// Record Values
+            /// </summary>
+            /// <param name="diagnostics"></param>
             public void Record(CosmosDiagnostics diagnostics);
         }
 }
