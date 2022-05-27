a8a4e024abe6b82c5454031253aa0ad33fc7f73a
Christopher Currens <currens.chris@gmail.com>
2022-05-27 08:06:14 -0400

    Remove unused namespaces

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Linq/CosmosLinqJsonConverterTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Linq/CosmosLinqJsonConverterTests.cs
@@ -5,19 +5,11 @@
 namespace Microsoft.Azure.Cosmos.Linq
 {
     using System;
-    using System.Collections;
-    using System.Collections.Generic;
     using System.Globalization;
-    using System.Linq;
     using System.Linq.Expressions;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
-    using SqlObjects;
-    using Expression = System.Linq.Expressions.Expression;
 
     [TestClass]
     public class CosmosLinqJsonConverterTests
