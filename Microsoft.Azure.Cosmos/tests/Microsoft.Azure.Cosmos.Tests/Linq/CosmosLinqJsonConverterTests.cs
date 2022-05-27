4e6a4f76a697db7e9902c75092e9c1f4d345f22c
Christopher Currens <currens.chris@gmail.com>
2022-05-27 08:03:48 -0400

    Update test

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Linq/CosmosLinqJsonConverterTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Linq/CosmosLinqJsonConverterTests.cs
@@ -5,16 +5,19 @@
 namespace Microsoft.Azure.Cosmos.Linq
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Globalization;
     using System.Linq;
     using System.Linq.Expressions;
+    using System.Threading;
     using System.Threading.Tasks;
     using Castle.DynamicProxy.Generators.Emitters.SimpleAST;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
     using SqlObjects;
+    using Expression = System.Linq.Expressions.Expression;
 
     [TestClass]
     public class CosmosLinqJsonConverterTests
@@ -22,22 +25,17 @@ namespace Microsoft.Azure.Cosmos.Linq
         [TestMethod]
         public void DateTimeKindIsPreservedTest()
         {
-            // NOTE: This test does not actually use the emulator, just a convenient way to get access
-            // to a container to create a Linq queryable
-            const string account = "https://localhost:8081";
-            const string key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
-            using CosmosClient client = new CosmosClient(account, key);
-            Container container = client.GetContainer("NotEven", "MakingARequest");
-
             // Should work for UTC time
             DateTime utcDate = new DateTime(2022, 5, 26, 0, 0, 0, DateTimeKind.Utc);
-            IQueryable<TestDocument> query = container.GetItemLinqQueryable<TestDocument>().Where(a => a.StartDate <= utcDate);
-            Assert.IsTrue(query.ToString().Contains("2022-05-26"));
+            Expression<Func<TestDocument, bool>> expr = a => a.StartDate <= utcDate;
+            string sql = SqlTranslator.TranslateExpression(expr.Body);
+            Assert.AreEqual("(a[\"StartDate\"] <= \"2022-05-26\")", sql);
 
-            // Should work for local time
+            //// Should work for local time
             DateTime localDate = new DateTime(2022, 5, 26, 0, 0, 0, DateTimeKind.Local);
-            query = container.GetItemLinqQueryable<TestDocument>().Where(a => a.StartDate <= localDate);
-            Assert.IsTrue(query.ToString().Contains("2022-05-26"));
+            expr = a => a.StartDate <= localDate;
+            sql = SqlTranslator.TranslateExpression(expr.Body);
+            Assert.AreEqual("(a[\"StartDate\"] <= \"2022-05-26\")", sql);
         }
 
         class TestDocument
