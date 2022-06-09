01e5b29221be33e9873149a9dac16e8f45ae69d1
Santosh Kulkarni <66682828+kr-santosh@users.noreply.github.com>
2022-06-10 00:05:15 +0530

    Client Encryption: Adds fix to allow partition key path and id to be part of client encryption policy. (#3211)
    * Support PK and Id encryption. Bump up policy format version.
    
    * Update DotNetSDKAPI.json
    
    * Update CosmosContainerTests.cs
    
    * fixes as per review comments. Added float pk constructor.
    
    * Update ClientEncryptionPolicy.cs
    
    * fixed exception message.
    
    * Update ClientEncryptionPolicy.cs
    
    * fixes, policy format in client encryption policy definition.
    
    * get raw partition key values from partition key list.
    
    * Update CosmosContainerTests.cs
    
    * Fixes as per review request.
    
    * fixed contracts.
    
    * Fixes as per review comments.
    
    Co-authored-by: Matias Quaranta <ealsur@users.noreply.github.com>

--- Microsoft.Azure.Cosmos/src/Fluent/Settings/ClientEncryptionPolicyDefinition.cs
+++ Microsoft.Azure.Cosmos/src/Fluent/Settings/ClientEncryptionPolicyDefinition.cs
@@ -9,19 +9,55 @@ namespace Microsoft.Azure.Cosmos.Fluent
 
     /// <summary>
     /// <see cref="ClientEncryptionPolicy"/> fluent definition.
+    /// The <see cref="ClientEncryptionPolicy"/> should be initialized with
+    /// policyFormatVersion 2 and "Deterministic" encryption type, if "id" property or properties which are part of partition key need to be encrypted.
+    /// All partition key property values included as part of <see cref="ClientEncryptionIncludedPath"/> have to be JSON strings.
     /// </summary>
+    /// <example>
+    /// This example shows how to create a <see cref="ClientEncryptionPolicy"/> using <see cref="ClientEncryptionPolicyDefinition"/>.
+    /// <code language="c#">
+    /// <![CDATA[
+    /// ClientEncryptionIncludedPath path1 = new ClientEncryptionIncludedPath()
+    /// {
+    ///     Path = partitionKeyPath,
+    ///     ClientEncryptionKeyId = "key1",
+    ///     EncryptionAlgorithm = "AEAD_AES_256_CBC_HMAC_SHA256",
+    ///     EncryptionType = "Deterministic"
+    /// };
+    /// 
+    /// ClientEncryptionIncludedPath path2 = new ClientEncryptionIncludedPath()
+    /// {
+    ///     Path = "/id",
+    ///     ClientEncryptionKeyId = "key2",
+    ///     EncryptionAlgorithm = "AEAD_AES_256_CBC_HMAC_SHA256",
+    ///     EncryptionType = "Deterministic"
+    /// };
+    /// 
+    /// ContainerResponse containerResponse = await this.database.DefineContainer(containerName, partitionKeyPath)
+    ///    .WithClientEncryptionPolicy(policyFormatVersion:2)
+    ///    .WithIncludedPath(path1)
+    ///    .WithIncludedPath(path2)
+    ///    .Attach()
+    ///    .CreateAsync()
+    /// };
+    /// ]]>
+    /// </code>
+    /// </example>
     public sealed class ClientEncryptionPolicyDefinition
     {
         private readonly Collection<ClientEncryptionIncludedPath> clientEncryptionIncludedPaths = new Collection<ClientEncryptionIncludedPath>();
         private readonly ContainerBuilder parent;
         private readonly Action<ClientEncryptionPolicy> attachCallback;
+        private readonly int policyFormatVersion;
 
         internal ClientEncryptionPolicyDefinition(
             ContainerBuilder parent,
-            Action<ClientEncryptionPolicy> attachCallback)
+            Action<ClientEncryptionPolicy> attachCallback,
+            int policyFormatVersion = 1)
         {
             this.parent = parent;
             this.attachCallback = attachCallback;
+            this.policyFormatVersion = (policyFormatVersion > 2 || policyFormatVersion < 1) ? throw new ArgumentException($"Supported versions of client encryption policy are 1 and 2. ") : policyFormatVersion;
         }
 
         /// <summary>
@@ -41,7 +77,7 @@ namespace Microsoft.Azure.Cosmos.Fluent
         /// <returns>An instance of the parent.</returns>
         public ContainerBuilder Attach()
         {
-            this.attachCallback(new ClientEncryptionPolicy(this.clientEncryptionIncludedPaths));
+            this.attachCallback(new ClientEncryptionPolicy(this.clientEncryptionIncludedPaths, this.policyFormatVersion));
             return this.parent;
         }
     }
