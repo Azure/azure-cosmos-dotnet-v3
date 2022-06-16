86ea3463b3fe1adc45ae412f23c2ede7d2ca9db5
Santosh Kulkarni <66682828+kr-santosh@users.noreply.github.com>
2022-06-16 16:37:15 +0530

    Fixes

--- Microsoft.Azure.Cosmos.Encryption/tests/EmulatorTests/MdeEncryptionTests.cs
+++ Microsoft.Azure.Cosmos.Encryption/tests/EmulatorTests/MdeEncryptionTests.cs
@@ -7,7 +7,6 @@ namespace Microsoft.Azure.Cosmos.Encryption.EmulatorTests
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
-    using System.Globalization;
     using System.IO;
     using System.Linq;
     using System.Net;
