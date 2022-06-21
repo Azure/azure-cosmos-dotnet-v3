3f07d529c52b1ce33e413ea7c4d64c8ea6aaa639
Kiran Kumar Kolli <kirankk@microsoft.com>
2022-06-21 09:21:56 -0700

    Adding a perf benchmark

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Benchmarks/AzureCredentialBenchmark.cs
@@ -0,0 +1,39 @@
+﻿namespace Microsoft.Azure.Cosmos.Benchmarks
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using BenchmarkDotNet.Attributes;
+    using global::Azure;
+    using Microsoft.Azure.Cosmos.Authorization;
+
+    public class AzureCredentialBenchmark
+    {
+        private readonly AzureKeyCredentialAuthorizationTokenProvider akcp;
+        private readonly AuthorizationTokenProviderMasterKey atpMasterKey;
+
+        public AzureCredentialBenchmark()
+        {
+            string authKey = Convert.ToBase64String(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString()));
+
+            AzureKeyCredential keyCredential = new AzureKeyCredential(authKey);
+            this.akcp = new AzureKeyCredentialAuthorizationTokenProvider(keyCredential);
+            this.atpMasterKey = new AuthorizationTokenProviderMasterKey(authKey);
+        }
+
+        [Benchmark]
+        public async Task AzureCredential()
+        {
+            Documents.Collections.NameValueCollectionWrapper headers = new Documents.Collections.NameValueCollectionWrapper();
+            await this.akcp.GetUserAuthorizationTokenAsync(String.Empty, "docs", "GET", headers, Documents.AuthorizationTokenType.PrimaryMasterKey, Cosmos.Tracing.NoOpTrace.Singleton);
+        }
+
+        [Benchmark]
+        public async Task MasterKeyCredential()
+        {
+            Documents.Collections.NameValueCollectionWrapper headers = new Documents.Collections.NameValueCollectionWrapper();
+            await this.akcp.GetUserAuthorizationTokenAsync(String.Empty, "docs", "GET", headers, Documents.AuthorizationTokenType.PrimaryMasterKey, Cosmos.Tracing.NoOpTrace.Singleton);
+        }
+    }
+}
