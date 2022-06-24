90a548f8fe6c05c6aa480f285e30c2bf99b6f41d
Sourabh Jain <sourabhjain@microsoft.com>
2022-06-25 00:29:53 +0530

    remove opentelemetry test decorator

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Telemetry/OpenTelemetryTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Telemetry/OpenTelemetryTests.cs
@@ -6,12 +6,10 @@ namespace Microsoft.Azure.Cosmos.Tests
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-    [TestClass]
     public abstract class OpenTelemetryTests
     {
         private static ClientDiagnosticListener testListener;
 
-        [ClassInitialize]
         public static void ClassInitialize(TestContext context)
         {
             OpenTelemetryTests.testListener = new ClientDiagnosticListener("Azure.Cosmos");
