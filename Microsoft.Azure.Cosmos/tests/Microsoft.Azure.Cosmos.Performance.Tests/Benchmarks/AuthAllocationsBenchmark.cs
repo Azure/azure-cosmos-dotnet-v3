94b9f3822ae64bd5ef9ff0eaf7c6ceb4f736fb1e
Iman Malik <imanmalik@microsoft.com>
2022-06-27 08:52:39 -0400

    add benchmark

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Benchmarks/AuthAllocationsBenchmark.cs
@@ -0,0 +1,30 @@
+﻿namespace Microsoft.Azure.Cosmos.Benchmarks
+{
+    using System;
+    using BenchmarkDotNet.Attributes;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [MemoryDiagnoser]
+    public class AuthAllocationsBenchmark
+    {
+        public AuthAllocationsBenchmark() 
+        { 
+        }
+
+        [Benchmark]
+        public void DateTimeToString()
+        {
+            _ = DateTime.UtcNow.ToString("r");
+        }
+
+        [Benchmark]
+        public void Rfc1123DateTimeCacheUtcNow()
+        {
+            _ = Rfc1123DateTimeCache.UtcNow();
+        }
+
+    }
+}
