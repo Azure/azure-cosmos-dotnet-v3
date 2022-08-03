1990b0a49b7b4569c4fefaeda761180ab7658768
Neil Deshpande <ndeshpan@microsoft.com>
2022-08-02 18:03:39 -0700

    Small bug fix in GroupByQueryPipelineStage and more test coverage

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/Query/CosmosUndefinedQueryTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/Query/CosmosUndefinedQueryTests.cs
@@ -1,12 +1,17 @@
 namespace Microsoft.Azure.Cosmos.EmulatorTests.Query
 {
+    using System;
     using System.Collections;
     using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
     using System.Linq;
     using System.Runtime.CompilerServices;
     using System.Runtime.InteropServices;
+    using System.Text;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos.CosmosElements;
+    using Microsoft.Azure.Cosmos.CosmosElements.Numbers;
     using Microsoft.Azure.Cosmos.Json;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -22,35 +27,92 @@ namespace Microsoft.Azure.Cosmos.EmulatorTests.Query
 
         private const int MixedTypeCount = 5;
 
+        private static readonly IEnumerable<string> Documents = CreateDocuments(DocumentCount);
+
         [TestMethod]
         public async Task OrderByUndefinedProjectUndefined()
         {
-            IEnumerable<string> documents = CreateDocuments();
-
-            static async Task TypedImplementation(Container container, IReadOnlyList<CosmosObject> _)
+            UndefinedProjectionTestCase[] testCases = new[]
             {
-                string query = "SELECT c.AlwaysUndefinedField FROM c ORDER BY c.AlwaysUndefinedField";
+                MakeUndefinedProjectionTest(
+                    query: "SELECT c.AlwaysUndefinedField FROM c ORDER BY c.AlwaysUndefinedField",
+                    expectedCount: DocumentCount),
+                MakeUndefinedProjectionTest(
+                    query: "SELECT VALUE c.AlwaysUndefinedField FROM c ORDER BY c.AlwaysUndefinedField",
+                    expectedCount: 0)
+            };
 
-                List<UndefinedProjection> results = await QueryWithContinuationTokensAsync<UndefinedProjection>(container, query);
-                Assert.AreEqual(DocumentCount, results.Count);
-                Assert.IsTrue(results.All(x => x is UndefinedProjection));
+            static async Task TypedImplementation(Container container, IReadOnlyList<CosmosObject> _, UndefinedProjectionTestCase[] testCases)
+            {
+                foreach (UndefinedProjectionTestCase testCase in testCases)
+                {
+                    foreach (int pageSize in new[] { 5, 10, -1 })
+                    {
+                        List<UndefinedProjection> results = await RunQueryAsync<UndefinedProjection>(
+                            container,
+                            testCase.Query,
+                            new QueryRequestOptions { MaxItemCount = pageSize });
+    
+                        Assert.AreEqual(testCase.ExpectedResultCount, results.Count);
+                        Assert.IsTrue(results.All(x => x is UndefinedProjection));
+                    }
+                }
             }
 
-            await this.CreateIngestQueryDeleteAsync(
-                ConnectionModes.Direct,
-                CollectionTypes.MultiPartition,
-                documents,
-                TypedImplementation);
+            await this.CreateIngestQueryDeleteAsync<UndefinedProjectionTestCase[]>(
+                ConnectionModes.Direct | ConnectionModes.Gateway,
+                CollectionTypes.MultiPartition | CollectionTypes.SinglePartition | CollectionTypes.NonPartitioned,
+                Documents,
+                TypedImplementation,
+                testCases);
         }
 
-
-        private static IEnumerable<string> CreateDocuments()
+        [TestMethod]
+        public async Task GroupByUndefined()
         {
-            List<MixedTypeDocument> documents = new List<MixedTypeDocument>();
+            GroupByUndefinedTestCase[] testCases = new[]
+            {
+                MakeGroupByTest(
+                    query: "SELECT c.AlwaysUndefinedField as MixedTypeField, COUNT(1) as ExpectedCount FROM c GROUP BY c.AlwaysUndefinedField",
+                    groups: new List<GroupByProjection>()
+                    {
+                        MakeGrouping(
+                            key: null,
+                            value: DocumentCount)
+                    })
+            };
+
+            static async Task TypedImplementation(Container container, IReadOnlyList<CosmosObject> _, GroupByUndefinedTestCase[] testCases)
+            {
+                foreach (GroupByUndefinedTestCase testCase in testCases)
+                {
+                    foreach (int pageSize in new[] { 5, 10, -1 })
+                    {
+                        List<GroupByProjection> actual = await QueryWithoutContinuationTokensAsync<GroupByProjection>(
+                            container,
+                            testCase.Query,
+                            new QueryRequestOptions { MaxItemCount = pageSize, MaxConcurrency = 0 });
+
+                        CollectionAssert.AreEquivalent(testCase.ExpectedGroups, actual);
+                    }
+                }
+            }
+
+            await this.CreateIngestQueryDeleteAsync<GroupByUndefinedTestCase[]>(
+                ConnectionModes.Direct | ConnectionModes.Gateway,
+                CollectionTypes.MultiPartition | CollectionTypes.SinglePartition | CollectionTypes.NonPartitioned,
+                Documents,
+                TypedImplementation,
+                testCases);
+        }
+
+        private static IEnumerable<string> CreateDocuments(int count)
+        {
+            List<string> documents = new List<string>();
 
             int booleanCount = 0;
             int integerCount = 0;
-            for (int index = 0; index < DocumentCount; ++index)
+            for (int index = 0; index < count; ++index)
             {
                 CosmosElement mixedTypeElement;
                 switch (index % MixedTypeCount)
@@ -68,7 +130,7 @@ namespace Microsoft.Azure.Cosmos.EmulatorTests.Query
                         break;
 
                     case 3:
-                        mixedTypeElement = CosmosNumber.Parse((++integerCount).ToString());
+                        mixedTypeElement = CosmosNumber64.Create(++integerCount);
                         break;
 
                     case 4:
@@ -82,21 +144,130 @@ namespace Microsoft.Azure.Cosmos.EmulatorTests.Query
                 }
 
                 MixedTypeDocument document = new MixedTypeDocument(index, mixedTypeElement);
-                documents.Add(document);
+                documents.Add(document.ToString());
             }
 
-            return documents.Select(x => x.ToString());
+            return documents;
         }
 
-        private readonly struct UntypedTestCase
+        private readonly struct UndefinedProjectionTestCase
         {
+            public UndefinedProjectionTestCase(string query, int expectedResultCount)
+            {
+                this.Query = query;
+                this.ExpectedResultCount = expectedResultCount;
+            }
+
             public string Query { get; }
 
-            public int PageSize { get; }
+            public int ExpectedResultCount { get; }
         }
 
-        private class UndefinedProjection
+        private static UndefinedProjectionTestCase MakeUndefinedProjectionTest(string query, int expectedCount)
         {
+            return new UndefinedProjectionTestCase(query, expectedCount);
+        }
+
+        private readonly struct GroupByUndefinedTestCase
+        {
+            public GroupByUndefinedTestCase(string query, List<GroupByProjection> groups)
+            {
+                this.Query = query;
+                this.ExpectedGroups = groups;
+            }
+
+            public string Query { get; }
+
+            public List<GroupByProjection> ExpectedGroups { get; }
+        }
+
+        private static GroupByUndefinedTestCase MakeGroupByTest(string query, List<GroupByProjection> groups)
+        {
+            return new GroupByUndefinedTestCase(query, groups);
+        }
+
+        private class GroupByProjection : IEquatable<GroupByProjection>
+        {
+            public CosmosElement MixedTypeField { get; set; }
+
+            public int ExpectedCount { get; set; }
+
+            public static bool operator ==(GroupByProjection left, GroupByProjection right)
+            {
+                bool result;
+                if(left is null && right is null)
+                {
+                    result = true;
+                }
+                else if(left is null || right is null)
+                {
+                    result = false;
+                }
+                else
+                {
+                    result = left.Equals(right);
+                }
+
+                return result;
+            }
+
+            public static bool operator !=(GroupByProjection left, GroupByProjection right)
+            {
+                return !(left == right);
+            }
+
+            public bool Equals(GroupByProjection other)
+            {
+                bool result;
+                if(other is null)
+                {
+                    result = false;
+                }
+                else
+                {
+                    result = this.ExpectedCount == other.ExpectedCount;
+                    if (this.MixedTypeField == null && other.MixedTypeField == null)
+                    {
+                    }
+                    else if (this.MixedTypeField == null || other.MixedTypeField == null)
+                    {
+                        result = false;
+                    }
+                    else
+                    {
+                        result = result && this.MixedTypeField.Equals(other.MixedTypeField);
+                    }
+                }
+
+                return result;
+            }
+
+            public override bool Equals(object other)
+            {
+                return (other is not null) && (other is GroupByProjection projection)
+                    && this.Equals(projection);
+            }
+
+            public override int GetHashCode()
+            {
+                HashCode hash = new HashCode();
+                hash.Add(this.ExpectedCount);
+                hash.Add(this.MixedTypeField);
+                return hash.ToHashCode();
+            }
+        }
+
+        private static GroupByProjection MakeGrouping(CosmosElement key, int value)
+        {
+            return new GroupByProjection() { MixedTypeField = key, ExpectedCount = value };
+        }
+
+        private class UndefinedProjection : IEquatable<UndefinedProjection>
+        {
+            public bool Equals(UndefinedProjection other)
+            {
+                return true;
+            }
         }
 
         private class MixedTypeDocument
