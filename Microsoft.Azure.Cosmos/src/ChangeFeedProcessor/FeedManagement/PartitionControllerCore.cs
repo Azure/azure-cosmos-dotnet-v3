841b420d53534f7c4431526a92cec3f5c4ba778c
Matias Quaranta <ealsur@hotmail.com>
2022-08-22 14:45:09 -0700

    Removes leaks

--- Microsoft.Azure.Cosmos/src/ChangeFeedProcessor/FeedManagement/PartitionControllerCore.cs
+++ Microsoft.Azure.Cosmos/src/ChangeFeedProcessor/FeedManagement/PartitionControllerCore.cs
@@ -70,7 +70,18 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.FeedManagement
             catch (Exception ex)
             {
                 await this.RemoveLeaseAsync(lease: lease, wasAcquired: false).ConfigureAwait(false);
-                await this.monitor.NotifyErrorAsync(lease.CurrentLeaseToken, ex);
+                if (ex is LeaseLostException leaseLostException
+                    && leaseLostException.InnerException != null)
+                {
+                    // LeaseLostException by itself is not loggable, unless it contains a related inner exception
+                    // For cases when the lease or container has been deleted or the lease has been stolen
+                    await this.monitor.NotifyErrorAsync(lease.CurrentLeaseToken, leaseLostException.InnerException);
+                }
+                else
+                {
+                    await this.monitor.NotifyErrorAsync(lease.CurrentLeaseToken, ex);
+                }
+
                 throw;
             }
 
@@ -118,7 +129,7 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.FeedManagement
                     await this.monitor.NotifyLeaseReleaseAsync(lease.CurrentLeaseToken);
                 }
 
-                DefaultTrace.TraceVerbose("Lease with token {0}: taken by another host during release");
+                DefaultTrace.TraceVerbose("Lease with token {0}: taken by another host during release", lease.CurrentLeaseToken);
             }
             catch (Exception ex)
             {
