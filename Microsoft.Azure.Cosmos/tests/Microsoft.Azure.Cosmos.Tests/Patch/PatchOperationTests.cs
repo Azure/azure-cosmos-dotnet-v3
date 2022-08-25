1803c2c4332eca98e2193c18351ce08112ad658e
Ahmed Zaman <ahmedrzaman@gmail.com>
2022-08-25 10:11:15 +0100

    Removing namespace change

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Patch/PatchOperationTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Patch/PatchOperationTests.cs
@@ -2,7 +2,7 @@
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 //------------------------------------------------------------
 
-namespace Microsoft.Azure.Cosmos.Tests.Patch
+namespace Microsoft.Azure.Cosmos.Tests
 {
     using System;
     using System.IO;
