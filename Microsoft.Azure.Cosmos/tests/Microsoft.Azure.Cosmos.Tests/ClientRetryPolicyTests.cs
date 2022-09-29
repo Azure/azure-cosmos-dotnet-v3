f04b4d5e28284448f466587b20aef2d45add766e
Nalu Tripician <ntripician@microsoft.com>
2022-09-29 15:57:28 -0400

    Added Expected Behavior and Tests

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/ClientRetryPolicyTests.cs
@@ -0,0 +1,193 @@
+﻿namespace Microsoft.Azure.Cosmos.Client.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Microsoft.Azure.Cosmos.Routing;
+    using Moq;
+    using Microsoft.Azure.Documents;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Globalization;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos.Core.Trace;
+    using Microsoft.Azure.Documents.Collections;
+    using Microsoft.Azure.Documents.Routing;
+    using System.Net.WebSockets;
+    using System.Net.Http.Headers;
+    using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+    using System.Collections.Specialized;
+
+    /// <summary>
+    /// Tests for <see cref="ClientRetryPolicy"/>
+    /// </summary>
+    [TestClass]
+    public sealed class ClientRetryPolicyTests
+    {
+        private static Uri Location1Endpoint = new Uri("https://location1.documents.azure.com");
+        private static Uri Location2Endpoint = new Uri("https://location2.documents.azure.com");
+
+        private ReadOnlyCollection<string> preferredLocations;
+        private AccountProperties databaseAccount;
+        private GlobalPartitionEndpointManager partitionKeyRangeLocationCache;
+        private Mock<IDocumentClientInternal> mockedClient;
+
+        [TestMethod]
+        public void Http503SubStatusHandelingTests()
+        {
+
+            const bool enableEndpointDiscovery = true;
+            //Create GlobalEndpointManager
+            using GlobalEndpointManager endpointManager = this.Initialize(
+               useMultipleWriteLocations: false,
+               enableEndpointDiscovery: enableEndpointDiscovery,
+               isPreferredLocationsListEmpty: true);
+
+            //Create Retry Policy
+            ClientRetryPolicy retryPolicy = new ClientRetryPolicy(endpointManager, this.partitionKeyRangeLocationCache, enableEndpointDiscovery, new RetryOptions());
+            
+            CancellationToken cancellationToken = new CancellationToken();
+            Exception serviceUnavailableException = new Exception();
+            Mock<INameValueCollection> nameValueCollection = new Mock<INameValueCollection>();
+
+            //Test for Substatus 0 
+
+            HttpStatusCode serviceUnavailable = HttpStatusCode.ServiceUnavailable;
+            SubStatusCodes unknown = SubStatusCodes.Unknown;
+
+            DocumentClientException documentClientException1 = new DocumentClientException(
+               message: "Service Unavailable - Unknown",
+               innerException: serviceUnavailableException,
+               responseHeaders: nameValueCollection.Object,
+               statusCode: serviceUnavailable,
+               substatusCode: unknown,
+               requestUri: null
+               );
+
+            Task<ShouldRetryResult> retryStatus1 = retryPolicy.ShouldRetryAsync(documentClientException1, cancellationToken);
+
+            Assert.IsFalse(retryStatus1.Result.ShouldRetry);
+
+            //Test for SDK Generated Substatus Code
+            SubStatusCodes sdkGenerated = SubStatusCodes.TransportGenerated503;
+            
+            DocumentClientException documentClientException2 = new DocumentClientException(
+               message: "Service Unavailable - SDK Generated",
+               innerException: serviceUnavailableException,
+               responseHeaders: nameValueCollection.Object,
+               statusCode: serviceUnavailable,
+               substatusCode: sdkGenerated,
+               requestUri: null
+               );
+
+            Task<ShouldRetryResult> retryStatus2 = retryPolicy.ShouldRetryAsync(documentClientException2, cancellationToken);
+
+            Assert.IsFalse(retryStatus2.Result.ShouldRetry);
+        }
+
+        private static AccountProperties CreateDatabaseAccount(
+            bool useMultipleWriteLocations,
+            bool enforceSingleMasterSingleWriteLocation)
+        {
+            Collection<AccountRegion> writeLocations = new Collection<AccountRegion>()
+                {
+                    { new AccountRegion() { Name = "location1", Endpoint = ClientRetryPolicyTests.Location1Endpoint.ToString() } },
+                    { new AccountRegion() { Name = "location2", Endpoint = ClientRetryPolicyTests.Location2Endpoint.ToString() } },
+                };
+
+            if (!useMultipleWriteLocations
+                && enforceSingleMasterSingleWriteLocation)
+            {
+                // Some pre-existing tests depend on the account having multiple write locations even on single master setup
+                // Newer tests can correctly define a single master account (single write region) without breaking existing tests
+                writeLocations = new Collection<AccountRegion>()
+                {
+                    { new AccountRegion() { Name = "location1", Endpoint = ClientRetryPolicyTests.Location1Endpoint.ToString() } }
+                };
+            }
+
+            AccountProperties databaseAccount = new AccountProperties()
+            {
+                EnableMultipleWriteLocations = useMultipleWriteLocations,
+                ReadLocationsInternal = new Collection<AccountRegion>()
+                {
+                    { new AccountRegion() { Name = "location1", Endpoint = ClientRetryPolicyTests.Location1Endpoint.ToString() } },
+                    { new AccountRegion() { Name = "location2", Endpoint = ClientRetryPolicyTests.Location2Endpoint.ToString() } },
+                },
+                WriteLocationsInternal = writeLocations
+            };
+
+            return databaseAccount;
+        }
+
+        private GlobalEndpointManager Initialize(
+            bool useMultipleWriteLocations,
+            bool enableEndpointDiscovery,
+            bool isPreferredLocationsListEmpty,
+            bool enforceSingleMasterSingleWriteLocation = false, // Some tests depend on the Initialize to create an account with multiple write locations, even when not multi master
+            ReadOnlyCollection<string> preferedRegionListOverride = null,
+            bool enablePartitionLevelFailover = false,
+            bool multimasterMetadataWriteRetryTest = false)
+        {
+            this.databaseAccount = ClientRetryPolicyTests.CreateDatabaseAccount(
+                useMultipleWriteLocations,
+                enforceSingleMasterSingleWriteLocation);
+
+            if (isPreferredLocationsListEmpty)
+            {
+                this.preferredLocations = new List<string>().AsReadOnly();
+            }
+            else
+            {
+                // Allow for override at the test method level if needed
+                this.preferredLocations = preferedRegionListOverride != null ? preferedRegionListOverride : new List<string>()
+                {
+                    "location1",
+                    "location2"
+                }.AsReadOnly();
+            }
+
+            if (!multimasterMetadataWriteRetryTest)
+            {
+                this.mockedClient = new Mock<IDocumentClientInternal>();
+                mockedClient.Setup(owner => owner.ServiceEndpoint).Returns(ClientRetryPolicyTests.Location1Endpoint);
+                mockedClient.Setup(owner => owner.GetDatabaseAccountInternalAsync(It.IsAny<Uri>(), It.IsAny<CancellationToken>())).ReturnsAsync(this.databaseAccount);
+            }
+            else
+            {
+                this.mockedClient = new Mock<IDocumentClientInternal>();
+                mockedClient.Setup(owner => owner.ServiceEndpoint).Returns(ClientRetryPolicyTests.Location2Endpoint);
+                mockedClient.Setup(owner => owner.GetDatabaseAccountInternalAsync(It.IsAny<Uri>(), It.IsAny<CancellationToken>())).ReturnsAsync(this.databaseAccount);
+            }
+
+            ConnectionPolicy connectionPolicy = new ConnectionPolicy()
+            {
+                EnableEndpointDiscovery = enableEndpointDiscovery,
+                UseMultipleWriteLocations = useMultipleWriteLocations,
+            };
+
+            foreach (string preferredLocation in this.preferredLocations)
+            {
+                connectionPolicy.PreferredLocations.Add(preferredLocation);
+            }
+
+            GlobalEndpointManager endpointManager = new GlobalEndpointManager(this.mockedClient.Object, connectionPolicy);
+            endpointManager.InitializeAccountPropertiesAndStartBackgroundRefresh(this.databaseAccount);
+
+            if (enablePartitionLevelFailover)
+            {
+                this.partitionKeyRangeLocationCache = new GlobalPartitionEndpointManagerCore(endpointManager);
+            }
+            else
+            {
+                this.partitionKeyRangeLocationCache = GlobalPartitionEndpointManagerNoOp.Instance;
+            }
+
+            return endpointManager;
+        }
+
+    }
+}
