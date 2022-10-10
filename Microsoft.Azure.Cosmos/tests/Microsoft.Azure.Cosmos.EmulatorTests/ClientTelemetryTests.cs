c0b21c0c3aea64547bbb47f5bab1f7f8ef165547
Sourabh Jain <sourabhjain@microsoft.com>
2022-10-10 23:56:39 +0530

    sample tgit status

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/ClientTelemetryTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/ClientTelemetryTests.cs
@@ -182,7 +182,21 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
         {
             ClientTelemetryTests.ResetSystemUsageMonitor(false);
         }
-            
+
+        [TestMethod]
+        public async Task RandomTesting()
+        {
+            CosmosClientBuilder clientBuilder = new CosmosClientBuilder(accountEndpoint: "", authKeyOrResourceToken: "");
+
+            CosmosClient client = clientBuilder.Build();
+
+            AccountProperties accountProperty = await client.ReadAccountAsync();
+            Console.WriteLine("========================> " + accountProperty.Id);
+
+            string data = await client.ReadAccountClientConfigAsync();
+            Console.WriteLine("========================> " + data);
+        }
+
         [TestMethod]
         [DataRow(ConnectionMode.Direct, true)]
         [DataRow(ConnectionMode.Gateway, true)]
