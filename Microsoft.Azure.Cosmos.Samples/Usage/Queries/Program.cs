a0d4ef8adfa0048f27021c512e12ff054e17bc30
Abhijit P. Pai <abpai@microsoft.com>
2022-12-14 21:29:08 +0530

    Updates

--- Microsoft.Azure.Cosmos.Samples/Usage/Queries/Program.cs
+++ Microsoft.Azure.Cosmos.Samples/Usage/Queries/Program.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos;
     using Microsoft.Extensions.Configuration;
@@ -12,18 +14,22 @@
         private static string databaseName = "";
         private static string containerName = "";
 
-        public static async Task Main(string[] _)
+        private static int feedRangeStartIndex = 0;
+        private static int feedRangeCount = 0;
+
+        public static async Task Main(string[] args)
         {
             try
             {
                 IConfigurationRoot configuration = new ConfigurationBuilder()
                     .AddJsonFile("appSettings.json")
+                    .AddCommandLine(args)
                     .Build();
-                
+
                 databaseName = configuration["databaseName"];
                 containerName = configuration["containerName"];
-
-
+                feedRangeStartIndex = int.Parse(configuration["feedRangeStartIndex"]);
+                feedRangeCount = int.Parse(configuration["feedRangeCount"]);
                 string endpoint = configuration["EndPointUrl"];
                 if (string.IsNullOrEmpty(endpoint))
                 {
@@ -50,10 +56,23 @@
                 Exception baseException = e.GetBaseException();
                 Console.WriteLine("Error: {0}, Message: {1}", e.Message, baseException.Message);
             }
-            finally
+        }
+
+        private class IdAndTs
+        {
+            public string Id { get; set; }
+            public string Rid { get; set; }
+            public string ETag { get; set; }
+            public long TS { get; set; }
+
+            public override int GetHashCode()
             {
-                Console.WriteLine("End of demo, press any key to exit.");
-                Console.ReadKey();
+                return this.Id.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                return (obj as IdAndTs).Id == this.Id;
             }
         }
 
@@ -61,36 +80,106 @@
         {
             Container container = client.GetDatabase(databaseName).GetContainer(containerName);
 
-            QueryDefinition query = new QueryDefinition("SELECT * FROM c ORDER by ky asc");
-            string prevky = null;
-            HashSet<string> ids = new HashSet<string>();
-            
-            using (FeedIterator<JObject> resultSetIterator = container.GetItemQueryIterator<JObject>(query))
+            ContainerProperties containerProperties = await container.ReadContainerAsync();
+            string[] selfLinkSegments = containerProperties.SelfLink.Split('/');
+            string databaseRid = selfLinkSegments[1];
+            string containerRid = selfLinkSegments[3];
+            Container containerByRid = client.GetContainer(databaseRid, containerRid);
+
+            QueryDefinition query = new QueryDefinition("SELECT c.id, c.ky, c._ts, c._rid, c._etag FROM c ORDER by c.ky asc");
+            IReadOnlyList<FeedRange> ranges = await container.GetFeedRangesAsync();
+            List<Task> tasks = new List<Task>();
+            long tot = 0;
+            long dupe = 0;
+            DateTime startDateTime = DateTime.UtcNow;
+            foreach (FeedRange range1 in ranges.Skip(feedRangeStartIndex).Take(feedRangeCount))
             {
-                while (resultSetIterator.HasMoreResults)
+                FeedRange range = range1;
+                Console.WriteLine(range.ToJsonString());
+                tasks.Add(Task.Run(async () =>
                 {
-                    FeedResponse<JObject> response = await resultSetIterator.ReadNextAsync();
-                    foreach (JObject result in response)
+                    try
                     {
-                        string currentky = result["ky"].Value<string>();
-                        string currentid = result["id"].Value<string>();
+                        string prevky = null;
+                        HashSet<IdAndTs> idAndTss = new HashSet<IdAndTs>();
 
-                        if(prevky != currentky)
+                        using (FeedIterator<JObject> resultSetIterator = container.GetItemQueryIterator<JObject>
+                            (range, query))
                         {
-                            ids.Clear();
-                        }
+                            while (resultSetIterator.HasMoreResults)
+                            {
+                                FeedResponse<JObject> response = await resultSetIterator.ReadNextAsync();
+                                Interlocked.Add(ref tot, response.Count);
+                                foreach (JObject result in response)
+                                {
+                                    string currentky = result["ky"].Value<string>();
+                                    IdAndTs current = new IdAndTs();
+                                    current.Id = result["id"].Value<string>();
+                                    current.Rid = result["_rid"].Value<string>();
+                                    current.ETag = result["_etag"].Value<string>();
+                                    current.TS = result["_ts"].Value<long>();
 
-                        if (ids.Contains(currentid))
-                        {
-                            Console.WriteLine("ky: " + currentky + " id: " + currentid);
-                        }
+                                    if (prevky != currentky)
+                                    {
+                                        idAndTss.Clear();
+                                    }
 
-                        ids.Add(currentid);
-                        prevky = currentky;
+                                    if (idAndTss.TryGetValue(current, out IdAndTs old))
+                                    {
+                                        Interlocked.Increment(ref dupe);
+                                        if (old.TS < current.TS)
+                                        {
+                                            ResponseMessage r = await containerByRid.DeleteItemStreamAsync(
+                                                old.Rid,
+                                                new PartitionKey(currentky),
+                                                new ItemRequestOptions()
+                                                {
+                                                    IfMatchEtag = old.ETag
+                                                }
+                                            );
+
+                                            r.EnsureSuccessStatusCode();
+                                        }
+                                        else if (current.TS < old.TS)
+                                        {
+                                            ResponseMessage r = await containerByRid.DeleteItemStreamAsync(
+                                                current.Rid,
+                                                new PartitionKey(currentky),
+                                                new ItemRequestOptions()
+                                                {
+                                                    IfMatchEtag = current.ETag
+                                                }
+                                            );
+
+                                            r.EnsureSuccessStatusCode();
+                                        }
+                                        else
+                                        {
+                                            throw new Exception("Same ts" + currentky + " " + current.Id + " " + current.TS + " " + current.Rid + " " + old.Rid);
+                                        }
+                                    }
+
+                                    idAndTss.Add(current);
+                                    prevky = currentky;
+                                }
+                            }
+                        }
                     }
-                }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error " + range1.ToJsonString() + " " + ex);
+                    }
+                }));
             }
+
+            await Task.WhenAny(Task.WhenAll(tasks), Task.Run(async () =>
+            {
+                while (true)
+                {
+                    Console.WriteLine((DateTime.UtcNow - startDateTime).ToString("c") + " " + Interlocked.Read(ref dupe) + " " + Interlocked.Read(ref tot));
+                    await Task.Delay(5000);
+                }
+            }));
         }
     }
 }
-
