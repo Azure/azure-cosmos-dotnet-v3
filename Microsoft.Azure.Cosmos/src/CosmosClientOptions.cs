c29abfedcbd98edf5ac49c9e58c3befc21dd9c3e
aavasthy <113193425+aavasthy@users.noreply.github.com>
2023-01-24 11:52:53 -0500

    CosmosClientOptions: Adds ServerCertificateCustomValidationCallback for Http and TCP (#3636)
    * Adding ServerCertificateCustomValidationCallback in clientoptions
    
    * Adding Server callback for Http and fixing tests
    
    * Fixing failing E2Etests
    
    * Resolving merge conflicts
    
    * Running update contracts script
    
    * Running Update contracts script
    
    * Running Update contracts script
    
    * Reverting the v3 version change
    
    * Update based on review comments
    
    * Added unit tests
    
    * Added remarks for callback delegate
    
    * Ran update contracts script
    
    * Update based on review comments
    
    * Ran update contracts script
    
    * Updated unit tests
    
    * Making ssl validation function private
    
    * Updating test files
    
    * Update remarks for sslvalidation public contract
    
    * Added emulator tests for server validation
    
    Co-authored-by: Debdatta Kunda <87335885+kundadebdatta@users.noreply.github.com>

--- Microsoft.Azure.Cosmos/src/CosmosClientOptions.cs
+++ Microsoft.Azure.Cosmos/src/CosmosClientOptions.cs
@@ -12,6 +12,8 @@ namespace Microsoft.Azure.Cosmos
     using System.Net;
     using System.Net.Http;
     using System.Net.Http.Headers;
+    using System.Net.Security;
+    using System.Security.Cryptography.X509Certificates;
     using Microsoft.Azure.Cosmos.Fluent;
     using Microsoft.Azure.Documents;
     using Microsoft.Azure.Documents.Client;
@@ -634,6 +636,16 @@ namespace Microsoft.Azure.Cosmos
         internal Func<TransportClient, TransportClient> TransportClientHandlerFactory { get; set; }
 
         /// <summary>
+        /// A callback delegate to do custom certificate validation for both HTTP and TCP.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// Customizing SSL verification is not recommended in production environments.
+        /// </para>
+        /// </remarks>
+        public Func<X509Certificate2, X509Chain, SslPolicyErrors, bool> ServerCertificateCustomValidationCallback { get; set; }
+       
+        /// <summary>
         /// API type for the account
         /// </summary>
         internal ApiType ApiType { get; set; }
@@ -746,7 +758,8 @@ namespace Microsoft.Azure.Cosmos
                 EnablePartitionLevelFailover = this.EnablePartitionLevelFailover,
                 PortReuseMode = this.portReuseMode,
                 EnableTcpConnectionEndpointRediscovery = this.EnableTcpConnectionEndpointRediscovery,
-                HttpClientFactory = this.httpClientFactory
+                HttpClientFactory = this.httpClientFactory,
+                ServerCertificateCustomValidationCallback = this.ServerCertificateCustomValidationCallback
             };
 
             if (this.EnableClientTelemetry.HasValue)
