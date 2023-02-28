92288b71417d31a623806dc2fa9ef9a30a963274
neildsh <ndeshpan@microsoft.com>
2023-02-27 17:45:13 -0800

    Add emulator tests for the bypass query parsing scenario

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/Query/BypassQueryParsingTests.cs
@@ -0,0 +1,66 @@
+namespace Microsoft.Azure.Cosmos.EmulatorTests.Query
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos.CosmosElements;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    [TestCategory("Query")]
+    public sealed class BypassQueryParsingTests : QueryTestsBase
+    {
+        private const string PartitionKeyField = "key";
+
+        [TestMethod]
+        public async Task TestBypassQueryParsingWithNonePartitionKey()
+        {
+            int documentCount = 400;
+            QueryRequestOptions feedOptions = new QueryRequestOptions { PartitionKey = PartitionKey.None };
+            string query = "SELECT VALUE r.numberField FROM r";
+            IReadOnlyList<int> expected = Enumerable.Range(0, documentCount).ToList();
+
+            async Task ImplementationAsync(Container container, IReadOnlyList<CosmosObject> documents)
+            {
+                ContainerInternal containerCore = container as ContainerInlineCore;
+
+                MockCosmosQueryClient cosmosQueryClientCore = new MockCosmosQueryClient(
+                    containerCore.ClientContext,
+                    containerCore,
+                    forceQueryPlanGatewayElseServiceInterop: true);
+
+                ContainerInternal containerWithBypassParsing = new ContainerInlineCore(
+                    containerCore.ClientContext,
+                    (DatabaseCore)containerCore.Database,
+                    containerCore.Id,
+                    cosmosQueryClientCore);
+
+                List<CosmosElement> items = await RunQueryAsync(containerWithBypassParsing, query, feedOptions);
+                int[] actual = items.Cast<CosmosNumber>().Select(x => (int)Number64.ToLong(x.Value)).ToArray();
+
+                Assert.IsTrue(expected.SequenceEqual(actual));
+            }
+
+            IReadOnlyList<string> documents = CreateSinglePartitionDocuments(documentCount);
+
+            await this.CreateIngestQueryDeleteAsync(
+                ConnectionModes.Direct | ConnectionModes.Gateway,
+                CollectionTypes.SinglePartition | CollectionTypes.MultiPartition,
+                documents,
+                ImplementationAsync,
+                "/" + PartitionKeyField);
+        }
+
+        private static IReadOnlyList<string> CreateSinglePartitionDocuments(int documentCount)
+        {
+            List<string> documents = new List<string>(documentCount);
+            for (int i = 0; i < documentCount; ++i)
+            {
+                string document = $@"{{ {PartitionKeyField}: ""/value"", ""numberField"": {i}, ""nullField"": null }}";
+                documents.Add(document);
+            }
+
+            return documents;
+        }
+    }
+}
