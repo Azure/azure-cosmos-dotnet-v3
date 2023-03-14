cc79bb6ffa8d4366d92b721afaf0d4d5d696afba
Aditya Kotalwar <akotalwar@microsoft.com>
2023-03-14 15:36:11 -0700

    Added summary for each field in AdditionalRequestHeaders

--- Microsoft.Azure.Cosmos/src/RequestOptions/AdditionalRequestHeaders.cs
+++ Microsoft.Azure.Cosmos/src/RequestOptions/AdditionalRequestHeaders.cs
@@ -12,14 +12,18 @@ namespace Microsoft.Azure.Cosmos.Query.Core
     /// </summary>
     internal class AdditionalRequestHeaders
     {
+        // Boolean to let the backend know if a query is utilizing the OptimisticDirectExecute pipeline.
         internal bool OptimisticDirectExecute { get; }
 
+        // Boolean to let the backend know if continuations are expected for a certain query.
         internal bool IsContinuationExpected { get; }
 
         internal string ContentType { get; }
 
+        // Boolean to let the backend know if a given operation is a query or not.
         internal bool IsQuery { get; }
 
+        // GUID to enable backend to link multiple activityIds that belong to the same operation.
         internal Guid CorrelatedActivityId { get; }
 
         public AdditionalRequestHeaders(bool optimisticDirectExecute = false, bool isContinuationExpected = false, Guid correlatedActivityId = new Guid(), bool isQuery = true, string contentType = MediaTypes.QueryJson)
