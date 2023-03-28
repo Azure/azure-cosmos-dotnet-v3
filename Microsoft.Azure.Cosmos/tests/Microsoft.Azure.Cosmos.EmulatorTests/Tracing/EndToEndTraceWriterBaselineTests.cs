a0138188c9af4dc0b7ab49a8330392f60962c121
Sourabh Jain <sourabhjain@microsoft.com>
2023-03-28 16:47:56 +0530

    dispose listener

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/Tracing/EndToEndTraceWriterBaselineTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/Tracing/EndToEndTraceWriterBaselineTests.cs
@@ -98,6 +98,8 @@ namespace Microsoft.Azure.Cosmos.EmulatorTests.Tracing
 
             Util.DisposeOpenTelemetryAndCustomListeners();
 
+            testListener.Dispose();
+            
             EndToEndTraceWriterBaselineTests.client?.Dispose();
             EndToEndTraceWriterBaselineTests.bulkClient?.Dispose();
             EndToEndTraceWriterBaselineTests.miscCosmosClient?.Dispose();
