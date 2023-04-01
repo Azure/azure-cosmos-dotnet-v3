8679108526b419f002c891b47e74fb06b96ed955
Abhijit P. Pai <abpai@microsoft.com>
2023-04-01 14:15:00 +0530

    Utility to get EPK given a PK

---
+++ Microsoft.Azure.Cosmos.Samples/Usage/GetEPK/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Cosmos.Samples.Shared
+{
+    using System;
+    using System.Reflection;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Assembly direct = Assembly.Load("Microsoft.Azure.Cosmos.Direct");
+            Type pkiType = direct.GetType("Microsoft.Azure.Documents.Routing.PartitionKeyInternal");
+            MethodInfo fromJsonStringMethod = pkiType.GetMethod("FromJsonString");
+            object pki = fromJsonStringMethod.Invoke(null, new object[] { "[ \"" + args[0] + "\" ]" });
+
+            string methodName = "GetEffectivePartitionKeyForHashPartitioning";
+            if(args.Length > 1 && args[1].Contains("2"))
+            {
+                methodName += "V2";
+            }
+
+            MethodInfo getEpkMethod = pkiType.GetMethod(methodName, BindingFlags.Instance | BindingFlags.NonPublic);
+            string epk = (string)getEpkMethod.Invoke(pki, null);
+            Console.WriteLine(epk);
+        }
+    }
+}
