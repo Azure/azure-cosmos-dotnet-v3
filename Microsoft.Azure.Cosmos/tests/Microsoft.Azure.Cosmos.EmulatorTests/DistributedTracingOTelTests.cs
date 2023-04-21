c6528f4fe148997e34128dcc3af9153d3634f3e5
Arooshi Avasthy <aavasthy@microsoft.com>
2023-04-21 10:06:32 -0700

    Updated unit tests

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/DistributedTracingOTelTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/DistributedTracingOTelTests.cs
@@ -20,13 +20,20 @@
     using System.Runtime.InteropServices;
     using System.Globalization;
     using System.Threading;
+    using Newtonsoft.Json;
+    using Microsoft.Azure.Cosmos.Telemetry.Models;
+    using System.Net.Http;
+    using System.Net;
+    using Microsoft.Azure.Cosmos.Fluent;
 
     [VisualStudio.TestTools.UnitTesting.TestClass]
     public sealed class DistributedTracingOTelTests
     {
-        public static CosmosClient client;
-        public static Database database;
-        public static Container container;
+        private static CosmosClient client;
+        private static Database database;
+        private static readonly Container container;
+        private CosmosClientBuilder cosmosClientBuilder;
+        private List<string> preferredRegionList;
 
         [TestInitialize]
         public void TestInitialize()
@@ -91,11 +98,70 @@
                     });
 
                 ItemResponse<JToken> createResponse = await containerResponse.Container.CreateItemAsync(JToken.Parse(cosmosObject.ToString()));
+                
                 Assert.IsTrue(createResponse.RequestMessage.Headers.TryGetValue("traceparent", out string traceheader));
                 Assert.IsNotNull(traceheader);
-                Console.WriteLine(CustomOtelExporter.CollectedActivities);
+                
+                string diagnosticsCreateItem = createResponse.Diagnostics.ToString();
+                JObject objDiagnosticsCreate = JObject.Parse(diagnosticsCreateItem);
+                string distributedTraceId = (string)objDiagnosticsCreate["data"]["DistributedTraceId"];
+                Assert.IsTrue(!string.IsNullOrEmpty(distributedTraceId));
                 Assert.AreEqual(4, CustomOtelExporter.CollectedActivities.Count());
-                Assert.AreEqual($"{OpenTelemetryAttributeKeys.DiagnosticNamespace}.Operation", CustomOtelExporter.CollectedActivities.FirstOrDefault().Source.Name);
+            }
+        }
+
+        [TestMethod]
+        public async Task NoSourceEnabled_ResultsInNoActivity2()
+        {
+            AppContext.SetSwitch("Azure.Experimental.EnableActivitySource", true);
+            AzureCore.ActivityExtensions.ResetFeatureSwitch();
+
+            HttpClientHandlerHelper httpHandler = new HttpClientHandlerHelper();
+            HttpClient httpClient = new HttpClient(httpHandler);
+            string traceparentHeader = null;
+
+            httpHandler.RequestCallBack = (request, cancellation) =>
+            {
+                if (request.RequestUri.AbsoluteUri.Equals(ClientTelemetryOptions.GetClientTelemetryEndpoint().AbsoluteUri))
+                {
+                    traceparentHeader = request.Headers.GetValues("traceparent").FirstOrDefault();
+                    HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+
+                    string jsonObject = request.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    return Task.FromResult(result);
+                }
+                return null;
+            };
+
+            this.preferredRegionList = new List<string>
+            {
+                "region1",
+                "region2"
+            };
+            this.cosmosClientBuilder = TestCommon.GetDefaultConfiguration()
+                                        .WithApplicationPreferredRegions(this.preferredRegionList)
+                                        .WithHttpClientFactory(() => new HttpClient(httpHandler));
+
+            this.SetClient(mode == ConnectionMode.Gateway
+                ? this.cosmosClientBuilder.WithConnectionModeGateway().Build()
+                : this.cosmosClientBuilder.Build());
+
+            this.database = await this.GetClient().CreateDatabaseAsync(Guid.NewGuid().ToString());
+
+            using (TracerProvider provider = Sdk.CreateTracerProviderBuilder()
+                .AddCustomOtelExporter()
+                .AddSource($"{OpenTelemetryAttributeKeys.DiagnosticNamespace}.Operation")
+                .AddSource($"{OpenTelemetryAttributeKeys.DiagnosticNamespace}.Request")
+                .Build())
+            {
+                client = TestCommon.CreateCosmosClient(
+                    useGateway: false,
+                    enableDistributingTracing: true);
+
+                database = await client.CreateDatabaseAsync(
+                        Guid.NewGuid().ToString(),
+                        cancellationToken: default);
+                Assert.AreEqual(1, CustomOtelExporter.CollectedActivities.Count());
             }
         }
 
