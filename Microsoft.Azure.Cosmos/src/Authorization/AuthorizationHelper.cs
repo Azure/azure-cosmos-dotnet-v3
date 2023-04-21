8b9f48b7eb3e00fed0f1358f04d86cfd245b9724
Arooshi Avasthy <aavasthy@microsoft.com>
2023-04-21 15:40:32 -0700

    Update maxCapacity in Authorizationhelper

--- Microsoft.Azure.Cosmos/src/Authorization/AuthorizationHelper.cs
+++ Microsoft.Azure.Cosmos/src/Authorization/AuthorizationHelper.cs
@@ -25,6 +25,7 @@ namespace Microsoft.Azure.Cosmos
         public const int DefaultMasterTokenExpiryInSeconds = 900;
         private const int MaxAadAuthorizationHeaderSize = 16 * 1024;
         private const int MaxResourceTokenAuthorizationHeaderSize = 8 * 1024;
+        private const int MaxCapacity = 524288; //0.5 MB
         private static readonly string AuthorizationFormatPrefixUrlEncoded = HttpUtility.UrlEncode(string.Format(CultureInfo.InvariantCulture, Constants.Properties.AuthorizationFormat,
                 Constants.Properties.MasterToken,
                 Constants.Properties.TokenVersion,
@@ -781,7 +782,6 @@ namespace Microsoft.Azure.Cosmos
         {
             // Create a large enough buffer that URL encode can use it.
             // Increase the buffer by 3x so it can be used for the URL encoding
-            const int MaxCapacity = 524288; //0.5 MB
             int capacity = Math.Min(Base64.GetMaxEncodedToUtf8Length(hashPayLoad.Length) * 3, MaxCapacity);
             Span<byte> encodingBuffer = stackalloc byte[capacity];
             encodingBuffer.Clear();
