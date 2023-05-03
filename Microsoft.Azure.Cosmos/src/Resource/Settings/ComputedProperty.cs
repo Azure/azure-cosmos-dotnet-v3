fb79092e95480035c758050ac4ff402731fa51de
Aditya Sarpotdar <adityasa@microsoft.com>
2023-05-03 23:40:46 +0000

    Suite0 fixes.

--- Microsoft.Azure.Cosmos/src/Resource/Settings/ComputedProperty.cs
+++ Microsoft.Azure.Cosmos/src/Resource/Settings/ComputedProperty.cs
@@ -4,7 +4,9 @@
 
 namespace Microsoft.Azure.Cosmos
 {
+    using System.Collections.Generic;
     using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     /// <summary> 
     /// Represents a computed property definition in a Cosmos DB collection.
@@ -40,5 +42,12 @@ namespace Microsoft.Azure.Cosmos
         /// </remarks>
         [JsonProperty(PropertyName = "query")]
         public string Query { get; set; }
+
+        /// <summary>
+        /// This contains additional values for scenarios where the SDK is not aware of new fields. 
+        /// This ensures that if resource is read and updated none of the fields will be lost in the process.
+        /// </summary>
+        [JsonExtensionData]
+        internal IDictionary<string, JToken> AdditionalProperties { get; private set; }
     }
 }
