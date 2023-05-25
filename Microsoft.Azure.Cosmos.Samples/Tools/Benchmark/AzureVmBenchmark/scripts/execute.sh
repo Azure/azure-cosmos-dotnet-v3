d7fbff6699de843711378f244c96d81f902424dd
David Chaava <v-dchaava@microsoft.com>
2023-05-26 00:02:16 +0200

    modified:   Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureVmBenchmark/scripts/execute.sh

--- Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureVmBenchmark/scripts/execute.sh
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureVmBenchmark/scripts/execute.sh
@@ -8,4 +8,4 @@ echo "##########CUSTOM_SCRIPT_URL###########: $CUSTOM_SCRIPT_URL"
 # Running custom-script in background, arm template completion wont wait on this
 # stdout and stderr will be logged in <$HOME>/custom-script.out and <$HOME>/custom-script.err
 curl -o custom-script.sh $CUSTOM_SCRIPT_URL
-nohup bash custom-script.sh > "/home/${ADMIN_USER_NAME}/agent.out" 2> "/home/${ADMIN_USER_NAME}/agent.err" &
+bash custom-script.sh
