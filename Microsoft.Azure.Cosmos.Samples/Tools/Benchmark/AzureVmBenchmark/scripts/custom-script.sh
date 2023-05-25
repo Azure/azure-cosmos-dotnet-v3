5402b6a3dd8d2a64b67d75aef88ba3fea0ac910b
David Chaava <v-dchaava@microsoft.com>
2023-05-26 00:08:32 +0200

    modified:   Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureVmBenchmark/scripts/custom-script.sh

--- Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureVmBenchmark/scripts/custom-script.sh
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureVmBenchmark/scripts/custom-script.sh
@@ -11,6 +11,11 @@ echo "##########VM_COUNT###########: $VM_COUNT"
 echo "##########BENCHMARKING_TOOLS_BRANCH_NAME###########: $BENCHMARKING_TOOLS_BRANCH_NAME"
 echo "##########BENCHMARKING_TOOLS_URL###########: $BENCHMARKING_TOOLS_URL"
 
+export OSSProjectRef=True
+export DOTNET_CLI_HOME=/temp
+export RESULTS_PK="runs-summary" #For test runs use different one
+export PL=18
+
 #Cloning Test Bench Repo
 echo "########## Cloning Test Bench repository ##########"
 git clone https://github.com/Azure/azure-cosmos-dotnet-v3.git
