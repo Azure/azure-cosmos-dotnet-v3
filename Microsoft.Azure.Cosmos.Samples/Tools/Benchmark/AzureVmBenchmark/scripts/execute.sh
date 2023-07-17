c8853ac613f0470bbc64361dee43514e3d890ed4
David Chaava <v-dchaava@microsoft.com>
2023-07-17 22:09:31 +0200

    modified:   Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureVmBenchmark/scripts/execute.sh

--- Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureVmBenchmark/scripts/execute.sh
+++ Microsoft.Azure.Cosmos.Samples/Tools/Benchmark/AzureVmBenchmark/scripts/execute.sh
@@ -5,8 +5,5 @@
 export DOTNET_CLI_HOME=/temp
 
 cloud-init status --wait
-echo "##########CUSTOM_SCRIPT_URL###########: $CUSTOM_SCRIPT_URL"
-# Running custom-script in background, arm template completion wont wait on this
-# stdout and stderr will be logged in <$HOME>/custom-script.out and <$HOME>/custom-script.err
 curl -o custom-script.sh $CUSTOM_SCRIPT_URL
 bash -x custom-script.sh
