d6c01316ccee343b46bcad793c6cab59f8240298
Sourabh Jain <sourabhjain@microsoft.com>
2023-08-10 14:27:20 +0530

    added test file

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Contracts/ClientTelemetryServiceContractTest.cs
@@ -0,0 +1,31 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.Tests.Contracts
+{
+    using System;
+    using Microsoft.Azure.Cosmos.Telemetry.Models;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json;
+    using NJsonSchema;
+    using static Microsoft.Azure.Cosmos.PatchConstants;
+
+    [TestClass]
+    public class ClientTelemetryServiceContractTest
+    {
+        [TestMethod]
+        public void TraceApiContractTest()
+        {
+            JsonSchema schema = JsonSchema.FromType(typeof(Schemas));
+            Console.WriteLine(schema.ToJson());
+        }
+    }
+
+    [Serializable]
+    internal class Schemas
+    {
+        [JsonProperty(PropertyName = "clientTelemetryProperties")]
+        internal ClientTelemetryProperties ClientTelemetryProperties { get; set; }
+    }
+}
