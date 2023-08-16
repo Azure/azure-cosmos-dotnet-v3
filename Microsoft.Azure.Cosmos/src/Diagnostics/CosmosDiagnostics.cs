2ee91fd821fbcbb7a5171f04e2aaddf87edf2264
Rinat Minibaev <132935507+rinatmini@users.noreply.github.com>
2023-08-16 10:07:22 -0700

    Added small remark for GetContactedRegions method documentation

--- Microsoft.Azure.Cosmos/src/Diagnostics/CosmosDiagnostics.cs
+++ Microsoft.Azure.Cosmos/src/Diagnostics/CosmosDiagnostics.cs
@@ -79,7 +79,7 @@ namespace Microsoft.Azure.Cosmos
         /// <summary>
         /// Gets the list of all regions that were contacted for a request
         /// </summary>
-        /// <returns>The list of tuples containing the Region name and the URI</returns>
+        /// <returns>The list of tuples containing the Region name and the URI. The returned list contains unique regions and doesn't guarantee ordering of the regions contacted from the first to the last</returns>
         public abstract IReadOnlyList<(string regionName, Uri uri)> GetContactedRegions();
     }
 }
