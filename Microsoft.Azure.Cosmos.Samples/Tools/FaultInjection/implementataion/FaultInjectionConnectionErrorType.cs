eaab2370b0451afc91768ba18c96f26344be6ae9
Nalu Tripician <nalutrip@icloud.com>
2023-08-29 15:44:56 -0700

    move files arround

---
+++ Microsoft.Azure.Cosmos.Samples/Tools/FaultInjection/implementataion/FaultInjectionConnectionErrorType.cs
@@ -0,0 +1,21 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+namespace Microsoft.Azure.Cosmos.FaultInjection
+{
+    /// <summary>
+    /// Connection Error Type for Fault Injection
+    /// </summary>
+    public enum FaultInjectionConnectionErrorType
+    {
+        /// <summary>
+        /// Emulates a connection close because of a recieved stream close
+        /// </summary>
+        RECIEVED_STREAM_CLOSED,
+
+        /// <summary>
+        /// Emulates a connection close because of a failure to recieve a response
+        /// </summary>
+        RECIEVE_FAILED,
+    }
+}
