7e7e8a174b532f68f5247384860f270f95e3a420
Aditya Kotalwar <akotalwar@microsoft.com>
2023-09-26 12:12:24 -0700

    Added missing null reference coverage

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Query/OptimisticDirectExecutionQueryBaselineTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Query/OptimisticDirectExecutionQueryBaselineTests.cs
@@ -434,23 +434,26 @@
                     partitionKeyPath: @"/pk",
                     partitionKeyValue: "a");
 
-                try
+                foreach (bool isMultiPartition in new[] { true, false })
                 {
-                    int result = await this.GetPipelineAndDrainAsync(
-                                    input,
-                                    numItems: 100,
-                                    isMultiPartition: false,
-                                    expectedContinuationTokenCount: 0,
-                                    requiresDist: true,
-                                    useQueryPlan: false);
-                }
-                catch(Exception ex)
-                {
-                    Assert.IsTrue(ex.InnerException.Message.Contains(testCase.ExpectedMessage));
-                    continue;
-                }
+                    try
+                    {
+                        int result = await this.GetPipelineAndDrainAsync(
+                                        input,
+                                        numItems: 100,
+                                        isMultiPartition: isMultiPartition,
+                                        expectedContinuationTokenCount: 0,
+                                        requiresDist: true,
+                                        useQueryPlan: false);
+                    }
+                    catch (Exception ex)
+                    {
+                        Assert.IsTrue(ex.InnerException.Message.Contains(testCase.ExpectedMessage));
+                        continue;
+                    }
 
-                Assert.Fail();
+                    Assert.Fail();
+                }
             }
         }
 
