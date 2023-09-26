12b2514c45c0dafadd3fd60e183b04d156938c47
Sourabh Jain <sourabhjain@microsoft.com>
2023-09-26 14:43:18 +0530

    wip2

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Telemetry/OpenTelemetryRecorderTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Telemetry/OpenTelemetryRecorderTests.cs
@@ -12,6 +12,7 @@ namespace Microsoft.Azure.Cosmos.Tests.Telemetry
     using Microsoft.Azure.Cosmos.Diagnostics;
     using Microsoft.Azure.Cosmos.Tracing;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
 
     [TestClass]
     public class OpenTelemetryRecorderTests
@@ -49,6 +50,24 @@ namespace Microsoft.Azure.Cosmos.Tests.Telemetry
         {
             Assembly asm = OpenTelemetryRecorderTests.GetAssemblyLocally(DllName);
 
+            IReadOnlyList<string> excludedClasses = new List<string>()
+            {
+                "MediaResponse",
+                "CosmosQuotaResponse",
+                "OpenTelemetryResponse",
+                "ClientEncryptionKeyResponse",
+                "ContainerResponse",
+                "DatabaseResponse",
+                "PermissionResponse",
+                "ThroughputResponse",
+                "UserResponse",
+                "StoredProcedureResponse",
+                "TriggerResponse",
+                "UserDefinedFunctionResponse",
+                "ChangeFeedEstimatorFeedResponse",
+                "ChangeFeedEstimatorEmptyFeedResponse"
+            };
+
             // Get all types (including internal) defined in the assembly
             IEnumerable<Type> actualClasses = asm
                 .GetTypes()
@@ -56,9 +75,34 @@ namespace Microsoft.Azure.Cosmos.Tests.Telemetry
 
             foreach (Type className in actualClasses)
             {
-                Console.WriteLine(className.Name);
-                //Assert.IsTrue(OpenTelemetryCoreRecorder.OTelCompatibleExceptions.Keys.Contains(className), $"{className.Name} is not added in {typeof(OpenTelemetryCoreRecorder).Name} Class OTelCompatibleExceptions dictionary");
+                if (className.IsAbstract || className.IsInterface || excludedClasses.Contains(className.Name))
+                {
+                    continue;
+                }
+
+                Console.WriteLine("===================> " + className.FullName);
+                object instance = CreateInstance(className);
+                if (instance is TransactionalBatchResponse transactionInstance)
+                {
+                    OpenTelemetryResponse oTelResponse = new OpenTelemetryResponse(transactionInstance);
+                }
+                else if (instance is ResponseMessage responseMessageInstance)
+                {
+                    OpenTelemetryResponse oTelResponse = new OpenTelemetryResponse(responseMessageInstance);
+                }
+                else
+                {
+                    Console.WriteLine($"*********** Response type {className.Name} is not supported");
+                }
             }
+
+            excludedClasses = new List<string>()
+            {
+                "IStoredProcedureResponse`1",
+                "IDocumentFeedResponse`1",
+                "OpenTelemetryResponse`1",
+                "DocumentFeedResponse`1"
+            };
 
             actualClasses = asm
                .GetTypes()
@@ -66,10 +110,116 @@ namespace Microsoft.Azure.Cosmos.Tests.Telemetry
 
             foreach (Type className in actualClasses)
             {
-                Console.WriteLine(className.Name);
-                //Assert.IsTrue(OpenTelemetryCoreRecorder.OTelCompatibleExceptions.Keys.Contains(className), $"{className.Name} is not added in {typeof(OpenTelemetryCoreRecorder).Name} Class OTelCompatibleExceptions dictionary");
+                if (className.IsAbstract || className.IsInterface || excludedClasses.Contains(className.Name))
+                {
+                    continue;
+                }
+
+                Console.WriteLine("===================> " + className.FullName);
+                object instance = CreateInstance(className);
+                if (instance is FeedResponse<object> feedResponse)
+                {
+                    OpenTelemetryResponse<object> oTelResponse = new OpenTelemetryResponse<object>(feedResponse);
+                }
+                else if (instance is Response<object> responseMessageInstance)
+                {
+                    OpenTelemetryResponse<object> oTelResponse = new OpenTelemetryResponse<object>(responseMessageInstance);
+                }
+                else
+                {
+                    Console.WriteLine($"*********** Response type {className.Name} is not supported");
+                }
             }
 
+        }
+
+        private static object CreateInstance(Type type)
+        {
+            Console.WriteLine(type.Name);
+
+            if (type != null)
+            {
+                if (type.Name == "ITrace")
+                {
+                    Console.WriteLine("Itace type : " + type.Name);
+                    return NoOpTrace.Singleton;
+                }
+                if (type.Name == "CosmosSerializer")
+                {
+                    Console.WriteLine("CosmosSerializer  : " + type.Name);
+                    return new CosmosJsonDotNetSerializer();
+                }
+                if (type.Name == "CosmosSerializationFormatOptions")
+                {
+                    Console.WriteLine("CosmosSerializer  : " + type.Name);
+                    return null;
+                }
+
+                if (type.IsAbstract || type.IsInterface)
+                {
+                    Console.WriteLine("isabstract or interface");
+                    return null;
+                }
+
+                ConstructorInfo[] constructors = type.GetConstructors(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+                ConstructorInfo constructor = constructors.FirstOrDefault();
+
+                if (constructor != null)
+                {
+                    ParameterInfo[] parameters = constructor.GetParameters();
+
+                    Console.WriteLine("   Constructor parameters : " + parameters.Length);
+
+                    object[] parameterValues = new object[parameters.Length];
+                    for (int i = 0; i < parameters.Length; i++)
+                    {
+                        parameterValues[i] = GetDefault(parameters[i].ParameterType);
+                    }
+
+                    return constructor.Invoke(parameterValues);
+                    
+                }
+                else
+                {
+                    return Activator.CreateInstance(type);
+                }
+            }
+
+            return null;
+        }
+
+        private static object GetDefault(Type type)
+        {
+            if (type.IsValueType)
+            {
+                if(type.Name == "IntPtr")
+                {
+                    return null;
+                }
+
+                Console.WriteLine("Value Type : " + type.Name);
+                return Activator.CreateInstance(type);
+            }
+            else if (type.IsArray)
+            {
+                Console.WriteLine("Array Type : " + type.Name);
+                return Array.CreateInstance(type.GetElementType(), 0);
+            }
+            else if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IReadOnlyList<>))
+            {
+                Console.WriteLine("List Type : " + type.Name);
+                return (IReadOnlyList<object>)Activator.CreateInstance(typeof(List<>).MakeGenericType(type.GetGenericArguments()));
+            }
+            else if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IList<>))
+            {
+                Console.WriteLine("List Type : " + type.Name);
+                return Activator.CreateInstance(typeof(List<>).MakeGenericType(type.GetGenericArguments()));
+            }
+            else
+            {
+                return CreateInstance(type);
+            }
         }
 
         [TestMethod]
