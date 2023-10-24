5d244812ec24cd4c73ee937a4e676783c7a40ddf
Aditya Kotalwar <akotalwar@microsoft.com>
2023-10-23 18:08:08 -0700

    Removed all changes from QueryPartitionProviderTestInstance

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Utils/QueryPartitionProviderTestInstance.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Utils/QueryPartitionProviderTestInstance.cs
@@ -1,7 +1,3 @@
-//------------------------------------------------------------
-// Copyright (c) Microsoft Corporation.  All rights reserved.
-//------------------------------------------------------------
-
 namespace Microsoft.Azure.Cosmos.Tests
 {
     using System.Collections.Generic;
@@ -9,49 +5,30 @@ namespace Microsoft.Azure.Cosmos.Tests
 
     public static class QueryPartitionProviderTestInstance
     {
-        internal static QueryPartitionProvider Create(string key = null, object value = null)
+        public static readonly IReadOnlyDictionary<string, object> DefaultQueryEngineConfiguration = new Dictionary<string, object>()
         {
-            Dictionary<string, object> queryEngineConfiguration = new Dictionary<string, object>()
-            {
-                {"maxSqlQueryInputLength", 262144},
-                {"maxJoinsPerSqlQuery", 5},
-                {"maxLogicalAndPerSqlQuery", 2000},
-                {"maxLogicalOrPerSqlQuery", 2000},
-                {"maxUdfRefPerSqlQuery", 10},
-                {"maxInExpressionItemsCount", 16000},
-                {"queryMaxGroupByTableCellCount", 500000 },
-                {"queryMaxInMemorySortDocumentCount", 500},
-                {"maxQueryRequestTimeoutFraction", 0.90},
-                {"sqlAllowNonFiniteNumbers", false},
-                {"sqlAllowAggregateFunctions", true},
-                {"sqlAllowSubQuery", true},
-                {"sqlAllowScalarSubQuery", true},
-                {"allowNewKeywords", true},
-                {"sqlAllowLike", true},
-                {"sqlAllowGroupByClause", true},
-                {"maxSpatialQueryCells", 12},
-                {"spatialMaxGeometryPointCount", 256},
-                {"sqlDisableQueryILOptimization", false},
-                {"sqlDisableFilterPlanOptimization", false},
-                {"clientDisableOptimisticDirectExecution", false}
-            };
+            {"maxSqlQueryInputLength", 262144},
+            {"maxJoinsPerSqlQuery", 5},
+            {"maxLogicalAndPerSqlQuery", 2000},
+            {"maxLogicalOrPerSqlQuery", 2000},
+            {"maxUdfRefPerSqlQuery", 10},
+            {"maxInExpressionItemsCount", 16000},
+            {"queryMaxGroupByTableCellCount", 500000 },
+            {"queryMaxInMemorySortDocumentCount", 500},
+            {"maxQueryRequestTimeoutFraction", 0.90},
+            {"sqlAllowNonFiniteNumbers", false},
+            {"sqlAllowAggregateFunctions", true},
+            {"sqlAllowSubQuery", true},
+            {"sqlAllowScalarSubQuery", true},
+            {"allowNewKeywords", true},
+            {"sqlAllowLike", true},
+            {"sqlAllowGroupByClause", true},
+            {"maxSpatialQueryCells", 12},
+            {"spatialMaxGeometryPointCount", 256},
+            {"sqlDisableQueryILOptimization", false},
+            {"sqlDisableFilterPlanOptimization", false}
+        };
 
-            if (key != null && value != null)
-            {
-                if (queryEngineConfiguration.TryGetValue(key, out _))
-                {
-                    queryEngineConfiguration[key] = value;
-                }
-            }
-
-            return new QueryPartitionProvider(queryEngineConfiguration);
-        }
-
-        internal static readonly QueryPartitionProvider Object = Create();
-
-        internal static QueryPartitionProvider CreateModifiedQueryConfigPartitionProviderObject(string key = null, string value = null)
-        { 
-            return Create(key, value);
-        }
+        internal static readonly QueryPartitionProvider Object = new QueryPartitionProvider(DefaultQueryEngineConfiguration as IDictionary<string, object>);
     }
 }
