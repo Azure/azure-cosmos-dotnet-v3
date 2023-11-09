d1958010ef769f6cdca81fb083d16d7818fb71ce
Nalu Tripician <nalutrip@icloud.com>
2023-11-09 12:55:32 -0800

    assembly info changes

---
+++ Microsoft.Azure.Cosmos.Samples/Tools/FaultInjection/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Microsoft.Azure.Cosmos" + AssemblyKeys.ProductPublicKey)]
+[assembly: InternalsVisibleTo("Microsoft.Azure.Cosmos" + AssemblyKeys.TestPublicKey)]
