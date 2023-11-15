94eb0cb219f536c454840c900a31ecbb97f757cc
Nalu Tripician <nalutrip@icloud.com>
2023-11-15 14:40:52 -0800

    small changes

--- Microsoft.Azure.Cosmos.Samples/Tools/FaultInjection/AssemblyInfo.cs
+++ Microsoft.Azure.Cosmos.Samples/Tools/FaultInjection/AssemblyInfo.cs
@@ -4,5 +4,8 @@
 
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2" + AssemblyKeys.MoqPublicKey)]
 [assembly: InternalsVisibleTo("Microsoft.Azure.Cosmos" + AssemblyKeys.ProductPublicKey)]
 [assembly: InternalsVisibleTo("Microsoft.Azure.Cosmos" + AssemblyKeys.TestPublicKey)]
+[assembly: InternalsVisibleTo("Azure.Cosmos" + AssemblyKeys.ProductPublicKey)]
+[assembly: InternalsVisibleTo("Azure.Cosmos" + AssemblyKeys.TestPublicKey)]
