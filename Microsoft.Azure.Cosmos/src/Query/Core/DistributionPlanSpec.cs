190d16d860a9c8dc9a2f70d981bd5ea43295ee8c
Aditya Kotalwar <akotalwar@microsoft.com>
2023-12-04 04:53:13 -0800

    Removed binary sample

--- Microsoft.Azure.Cosmos/src/Query/Core/DistributionPlanSpec.cs
+++ Microsoft.Azure.Cosmos/src/Query/Core/DistributionPlanSpec.cs
@@ -4,10 +4,22 @@
 
 namespace Microsoft.Azure.Cosmos.Query.Core
 {
+    using System;
+
     internal sealed class DistributionPlanSpec
     {
         public DistributionPlanSpec(string backendDistributionPlan, string clientDistributionPlan)
         {
+            if (string.IsNullOrEmpty(backendDistributionPlan))
+            {
+                throw new ArgumentException("Backend distribution plan cannot be null or empty.");
+            }
+
+            if (string.IsNullOrEmpty(clientDistributionPlan))
+            {
+                throw new ArgumentException("Client distribution plan cannot be null or empty.");
+            }
+
             this.BackendDistributionPlan = backendDistributionPlan;
             this.ClientDistributionPlan = clientDistributionPlan;
         }
