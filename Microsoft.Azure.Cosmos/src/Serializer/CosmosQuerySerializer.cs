5593fd0557dc11e560269424c103e66896db5a24
Maya-Painter <mayapainter@microsoft.com>
2023-12-12 16:13:48 -0800

    API fix

--- Microsoft.Azure.Cosmos/src/Serializer/CosmosQuerySerializer.cs
+++ Microsoft.Azure.Cosmos/src/Serializer/CosmosQuerySerializer.cs
@@ -1,7 +1,7 @@
 ﻿//------------------------------------------------------------
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 //------------------------------------------------------------
-namespace Microsoft.Azure.Cosmos.Serializer
+namespace Microsoft.Azure.Cosmos
 {
     using System.Reflection;
 
