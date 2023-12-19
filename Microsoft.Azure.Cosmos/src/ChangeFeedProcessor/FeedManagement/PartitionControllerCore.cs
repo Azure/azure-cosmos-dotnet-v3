bddcd5f9ec2c1a9cc08821146fd209b018aa36bf
Matias Quaranta <ealsur@hotmail.com>
2023-12-19 09:52:24 -0800

    refactoring

--- Microsoft.Azure.Cosmos/src/ChangeFeedProcessor/FeedManagement/PartitionControllerCore.cs
+++ Microsoft.Azure.Cosmos/src/ChangeFeedProcessor/FeedManagement/PartitionControllerCore.cs
@@ -89,8 +89,7 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.FeedManagement
                 throw;
             }
 
-            PartitionSupervisor supervisor = this.partitionSupervisorFactory.Create(lease);
-            this.ProcessPartitionAsync(supervisor, lease).LogException();
+            this.ProcessPartitionAsync(lease).LogException();
         }
 
         public override async Task ShutdownAsync()
@@ -146,8 +145,10 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.FeedManagement
             }
         }
 
-        private async Task ProcessPartitionAsync(PartitionSupervisor partitionSupervisor, DocumentServiceLease lease)
+        private async Task ProcessPartitionAsync(DocumentServiceLease lease)
         {
+            using PartitionSupervisor partitionSupervisor = this.partitionSupervisorFactory.Create(lease);
+
             try
             {
                 await partitionSupervisor.RunAsync(this.shutdownCts.Token).ConfigureAwait(false);
@@ -167,8 +168,6 @@ namespace Microsoft.Azure.Cosmos.ChangeFeed.FeedManagement
             }
 
             await this.RemoveLeaseAsync(lease: lease, wasAcquired: true).ConfigureAwait(false);
-
-            partitionSupervisor.Dispose();
         }
 
         private async Task HandlePartitionGoneAsync(DocumentServiceLease lease, string lastContinuationToken)
