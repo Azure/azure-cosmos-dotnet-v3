7b877631ead5c3d0364f07a2f09fd7f8281345d4
Nalu Tripician <27316859+NaluTripician@users.noreply.github.com>
2023-12-28 13:19:01 -0500

    removed unneeded changes

--- Microsoft.Azure.Cosmos/src/Headers/CosmosMessageHeadersInternal.cs
+++ Microsoft.Azure.Cosmos/src/Headers/CosmosMessageHeadersInternal.cs
@@ -13,7 +13,7 @@ namespace Microsoft.Azure.Cosmos
     using Microsoft.Azure.Documents;
     using Microsoft.Azure.Documents.Collections;
 
-    internal abstract class CosmosMessageHeadersInternal 
+    internal abstract class CosmosMessageHeadersInternal
     {
         public virtual string Authorization
         {
