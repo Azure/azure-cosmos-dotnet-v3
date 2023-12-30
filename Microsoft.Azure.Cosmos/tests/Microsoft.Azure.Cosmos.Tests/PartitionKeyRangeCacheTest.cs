c5556853dbc8a63466d5bea5bc1f69ccdd0014e2
Debdatta Kunda <87335885+kundadebdatta@users.noreply.github.com>
2023-12-30 04:51:20 -0800

    [Internal] Per Partition Automatic Failover: Fixes Metadata Requests Retry Policy (#4205)
    * Code changes to retry on next preferred region for metadata reads on gateway timeouts.
    
    * Code changes to add retry for PK Ranges call.
    
    * Code changes to mark endpoint unavailable for read when cosmos exception occurs!
    
    * Code changes to fix unit tests. Added global endpoint manager in Pk Range Cache ctor.
    
    * Code changes to fix unit tests.
    
    * Code changes to fix build break.
    
    * Minor code clean-up.
    
    * Code changes to capture metadata location endpoint within on before send request.
    
    * Code changes to address review comments.
    
    * Code changes to fix build failure.
    
    * Code changes to refactor metadata timeout policy.
    
    * Code changes to add retry for request timeout. Fix emulator tests.
    
    * Code changes to add metadata retry policy unit tests.
    
    * Code changes to add more tests.
    
    * Code changes to refactor metadata retry policy logic to increment location index. Addressed review comments.
    
    * Code changes to address review comments.
    
    * Code changes to address review comments.
    
    * Code changes to add separate condition for pk range requests.

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/PartitionKeyRangeCacheTest.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/PartitionKeyRangeCacheTest.cs
@@ -21,6 +21,8 @@ namespace Microsoft.Azure.Cosmos.Tests
     using TraceLevel = Cosmos.Tracing.TraceLevel;
     using Newtonsoft.Json.Linq;
     using Newtonsoft.Json;
+    using System;
+    using Microsoft.Azure.Cosmos.Resource.CosmosExceptions;
 
     /// <summary>
     /// Unit Tests for <see cref="PartitionKeyRangeCache"/>.
@@ -77,6 +79,11 @@ namespace Microsoft.Azure.Cosmos.Tests
                     [HttpConstants.HttpHeaders.ETag] = eTag,
                 };
 
+                Mock<IDocumentClientInternal> mockDocumentClient = new Mock<IDocumentClientInternal>();
+                mockDocumentClient.Setup(client => client.ServiceEndpoint).Returns(new Uri("https://foo"));
+
+                using GlobalEndpointManager endpointManager = new (mockDocumentClient.Object, new ConnectionPolicy());
+
                 mockStoreModel.SetupSequence(x => x.ProcessMessageAsync(It.IsAny<DocumentServiceRequest>(), It.IsAny<CancellationToken>()))
                      .ReturnsAsync(new DocumentServiceResponse(new MemoryStream(singlePkCollectionCacheByte),
                             new StoreResponseNameValueCollection()
@@ -91,7 +98,7 @@ namespace Microsoft.Azure.Cosmos.Tests
                     .Returns(new ValueTask<string>(authToken));
 
                 // Act.
-                PartitionKeyRangeCache partitionKeyRangeCache = new(mockTokenProvider.Object, mockStoreModel.Object, mockCollectioNCache.Object);
+                PartitionKeyRangeCache partitionKeyRangeCache = new(mockTokenProvider.Object, mockStoreModel.Object, mockCollectioNCache.Object, endpointManager);
                 IReadOnlyList<PartitionKeyRange> partitionKeyRanges = await partitionKeyRangeCache.TryGetOverlappingRangesAsync(
                     containerRId,
                     FeedRangeEpk.FullRange.Range,
@@ -117,6 +124,138 @@ namespace Microsoft.Azure.Cosmos.Tests
                 Assert.IsTrue(!string.IsNullOrWhiteSpace(secondPkRangeValue));
                 Assert.IsTrue(string.IsNullOrEmpty(firstPkRangeValue));
                 Assert.AreEqual(eTag, secondPkRangeValue);
+            }
+        }
+
+        /// <summary>
+        /// Test to validate that when the gateway service is unavailable, the partition key range cache is able to retry
+        /// the Get PK Range calls to different regions, based on the preferred locations count. Therefore, the cache keep
+        /// retrying until the max attempt is exausted.
+        /// </summary>
+        [TestMethod]
+        [DataRow(1, false, DisplayName = "Validates when the preferred location count is just one, the cache retries only once and fails.")]
+        [DataRow(2, false, DisplayName = "Validates when the preferred location count is two, the cache retries twice and fails.")]
+        [DataRow(3, true, DisplayName = "Validates when the preferred location count is three, the cache retries thrice and succeeds on the last attempt.")]
+        public async Task TryGetOverlappingRangesAsync_WhenGatewayThrowsServiceUnavailable_ShouldMarkReadEndpointAsUnavailable2(int preferredLocationsCount, bool shouldSucceed)
+        {
+            // Arrange.
+            string eTag = "483";
+            string authToken = "token!";
+            string containerRId = "kjhsAA==";
+            string singlePkCollectionCache = "{\"_rid\":\"3FIlAOzjvyg=\",\"PartitionKeyRanges\":[{\"_rid\":\"3FIlAOzjvygCAAAAAAAAUA==\",\"id\":\"0\",\"_etag\":\"\\\"00005565-0000-0800-0000-621fd98a0000\\\"\",\"minInclusive\":\"\",\"maxExclusive\":\"FF\",\"ridPrefix\":0,\"_self\":\"dbs/3FIlAA==/colls/3FIlAOzjvyg=/pkranges/3FIlAOzjvygCAAAAAAAAUA==/\",\"throughputFraction\":1,\"status\":\"splitting\",\"parents\":[],\"_ts\":1646254474,\"_lsn\":44}],\"_count\":1}";
+            byte[] singlePkCollectionCacheByte = Encoding.UTF8.GetBytes(singlePkCollectionCache);
+            using (ITrace trace = Trace.GetRootTrace(this.TestContext.TestName, TraceComponent.Unknown, TraceLevel.Info))
+            {
+                Mock<IStoreModel> mockStoreModel = new();
+                Mock<CollectionCache> mockCollectioNCache = new();
+                Mock<ICosmosAuthorizationTokenProvider> mockTokenProvider = new();
+                NameValueCollectionWrapper headers = new()
+                {
+                    [HttpConstants.HttpHeaders.ETag] = eTag,
+                };
+
+                Uri serviceUri = new("https://foo");
+                Mock<IDocumentClientInternal> mockDocumentClient = new Mock<IDocumentClientInternal>();
+                mockDocumentClient.Setup(client => client.ServiceEndpoint).Returns(serviceUri);
+
+                Mock<IGlobalEndpointManager> mockedEndpointManager = new Mock<IGlobalEndpointManager>();
+                mockedEndpointManager
+                    .Setup(gem => gem.ResolveServiceEndpoint(It.IsAny<DocumentServiceRequest>()))
+                    .Returns(serviceUri);
+
+                mockedEndpointManager
+                    .Setup(gem => gem.PreferredLocationCount)
+                    .Returns(preferredLocationsCount);
+
+                mockStoreModel.SetupSequence(x => x.ProcessMessageAsync(It.IsAny<DocumentServiceRequest>(), It.IsAny<CancellationToken>()))
+                     .ThrowsAsync(CosmosExceptionFactory.CreateServiceUnavailableException(
+                                            message: "Service is Unavailable.",
+                                            headers: new Headers()
+                                            {
+                                                ActivityId = System.Diagnostics.Trace.CorrelationManager.ActivityId.ToString(),
+                                                SubStatusCode = SubStatusCodes.TransportGenerated503
+                                            },
+                                            trace: trace,
+                                            innerException: null))
+                     .ThrowsAsync(CosmosExceptionFactory.CreateServiceUnavailableException(
+                                            message: "Service is Unavailable.",
+                                            headers: new Headers()
+                                            {
+                                                ActivityId = System.Diagnostics.Trace.CorrelationManager.ActivityId.ToString(),
+                                                SubStatusCode = SubStatusCodes.TransportGenerated503
+                                            },
+                                            trace: trace,
+                                            innerException: null))
+                     .ThrowsAsync(CosmosExceptionFactory.CreateServiceUnavailableException(
+                                            message: "Service is Unavailable.",
+                                            headers: new Headers()
+                                            {
+                                                ActivityId = System.Diagnostics.Trace.CorrelationManager.ActivityId.ToString(),
+                                                SubStatusCode = SubStatusCodes.TransportGenerated503
+                                            },
+                                            trace: trace,
+                                            innerException: null))
+                     .ReturnsAsync(new DocumentServiceResponse(new MemoryStream(singlePkCollectionCacheByte),
+                            new StoreResponseNameValueCollection()
+                            {
+                                ETag = eTag,
+                            },
+                            HttpStatusCode.OK))
+                     .ReturnsAsync(new DocumentServiceResponse(null, headers, HttpStatusCode.NotModified, null))
+                     .ReturnsAsync(new DocumentServiceResponse(null, headers, HttpStatusCode.NotModified, null));
+
+                mockTokenProvider.Setup(x => x.GetUserAuthorizationTokenAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<INameValueCollection>(), It.IsAny<AuthorizationTokenType>(), It.IsAny<ITrace>()))
+                    .Returns(new ValueTask<string>(authToken));
+
+                PartitionKeyRangeCache partitionKeyRangeCache = new(mockTokenProvider.Object, mockStoreModel.Object, mockCollectioNCache.Object, mockedEndpointManager.Object);
+
+                if (shouldSucceed)
+                {
+                    // Act.
+                    IReadOnlyList<PartitionKeyRange> partitionKeyRanges = await partitionKeyRangeCache.TryGetOverlappingRangesAsync(
+                        containerRId,
+                        FeedRangeEpk.FullRange.Range,
+                        trace,
+                        forceRefresh: true);
+
+                    // Assert.
+                    string dataPath = "children[0].data", previousContinuationTokenKey = "['Previous Continuation Token']", diagnostics = new CosmosTraceDiagnostics(trace).ToString();
+                    JObject traceData = JsonConvert.DeserializeObject<JObject>(diagnostics);
+
+                    Assert.IsNotNull(traceData);
+
+                    string firstPkRangeCacheKeyName = ((JProperty)traceData.SelectToken(dataPath)?.First).Name;
+                    string secondPkRangeCacheKeyName = ((JProperty)traceData.SelectToken(dataPath)?.Last).Name;
+
+                    Assert.IsTrue(!string.IsNullOrWhiteSpace(firstPkRangeCacheKeyName));
+                    Assert.IsTrue(!string.IsNullOrWhiteSpace(secondPkRangeCacheKeyName));
+                    Assert.AreNotEqual(firstPkRangeCacheKeyName, secondPkRangeCacheKeyName);
+
+                    string firstPkRangeValue = ((JProperty)traceData.SelectToken(dataPath).First)?.Value?.SelectToken(previousContinuationTokenKey)?.ToString();
+                    string secondPkRangeValue = ((JProperty)traceData.SelectToken(dataPath).Last)?.Value?.SelectToken(previousContinuationTokenKey)?.ToString();
+
+                    Assert.IsTrue(!string.IsNullOrWhiteSpace(secondPkRangeValue));
+                    Assert.IsTrue(string.IsNullOrEmpty(firstPkRangeValue));
+                    Assert.AreEqual(eTag, secondPkRangeValue);
+                }
+                else
+                {
+                    // Act.
+                    CosmosException cosmosException = await Assert.ThrowsExceptionAsync<CosmosException>(() => partitionKeyRangeCache.TryGetOverlappingRangesAsync(
+                        containerRId,
+                        FeedRangeEpk.FullRange.Range,
+                        trace,
+                        forceRefresh: true));
+
+                    // Assert.
+                    string diagnostics = new CosmosTraceDiagnostics(trace).ToString();
+                    JObject traceData = JsonConvert.DeserializeObject<JObject>(diagnostics);
+
+                    Assert.IsNotNull(cosmosException);
+                    Assert.IsNotNull(traceData);
+                    Assert.AreEqual(HttpStatusCode.ServiceUnavailable, cosmosException.StatusCode);
+                    Assert.AreEqual(SubStatusCodes.TransportGenerated503, cosmosException.Headers.SubStatusCode);
+                }
             }
         }
     }
