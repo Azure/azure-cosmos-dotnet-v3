d6ed5c0399178a116e583bab8ea7afd75925a8ea
Nalu Tripician <27316859+NaluTripician@users.noreply.github.com>
2023-12-31 15:16:56 -0500

    [Internal] GlobalEndpointManager: Fixes Memory Leak (#4226)
    * memory leak fix
    
    * removed commented out code
    
    * changed to async so test would pass, with just static GetPropetiesHelper will get disposed before requests go through
    
    * added thread saftey
    
    * fixed to interlock pattern
    
    ---------
    
    Co-authored-by: Debdatta Kunda <87335885+kundadebdatta@users.noreply.github.com>

--- Microsoft.Azure.Cosmos/src/Routing/GlobalEndpointManager.cs
+++ Microsoft.Azure.Cosmos/src/Routing/GlobalEndpointManager.cs
@@ -114,25 +114,26 @@ namespace Microsoft.Azure.Cosmos.Routing
         /// The 2 additional tasks will go through all the preferred regions in parallel
         /// It will return the first success and stop the parallel tasks.
         /// </summary>
-        public static Task<AccountProperties> GetDatabaseAccountFromAnyLocationsAsync(
+        public static async Task<AccountProperties> GetDatabaseAccountFromAnyLocationsAsync(
             Uri defaultEndpoint,
             IList<string>? locations,
             Func<Uri, Task<AccountProperties>> getDatabaseAccountFn,
             CancellationToken cancellationToken)
         {
-            GetAccountPropertiesHelper threadSafeGetAccountHelper = new GetAccountPropertiesHelper(
+            using (GetAccountPropertiesHelper threadSafeGetAccountHelper = new GetAccountPropertiesHelper(
                defaultEndpoint,
                locations?.GetEnumerator(),
                getDatabaseAccountFn,
-               cancellationToken);
-
-            return threadSafeGetAccountHelper.GetAccountPropertiesAsync();
+               cancellationToken))
+            {
+                return await threadSafeGetAccountHelper.GetAccountPropertiesAsync();
+            }
         }
 
         /// <summary>
         /// This is a helper class to 
         /// </summary>
-        private class GetAccountPropertiesHelper
+        private class GetAccountPropertiesHelper : IDisposable
         {
             private readonly CancellationTokenSource CancellationTokenSource;
             private readonly Uri DefaultEndpoint;
@@ -141,6 +142,7 @@ namespace Microsoft.Azure.Cosmos.Routing
             private readonly List<Exception> TransientExceptions = new List<Exception>();
             private AccountProperties? AccountProperties = null;
             private Exception? NonRetriableException = null;
+            private int disposeCounter = 0;
 
             public GetAccountPropertiesHelper(
                 Uri defaultEndpoint,
@@ -345,6 +347,15 @@ namespace Microsoft.Azure.Cosmos.Routing
                 }
 
                 return false;
+            }
+
+            public void Dispose()
+            {
+                if (Interlocked.Increment(ref this.disposeCounter) == 1)
+                {
+                    this.CancellationTokenSource?.Cancel();
+                    this.CancellationTokenSource?.Dispose();
+                }
             }
         }
 
