17bbdab53b3760664c38f95cc5edace18da4639d
Maya Painter <130110800+Maya-Painter@users.noreply.github.com>
2024-01-05 13:30:29 -0800

    Query: Adds support for System.Text LINQ Custom Serializer (#4138)
    * initial commit
    
    * add more tests
    
    * cleanup
    
    * camel case fix
    
    * fix precidence and add test
    
    * cleanup
    
    * cleanup
    
    * Implement serializers
    
    * clenaup and bugfix
    
    * cleanup constructors
    
    * fix baseline
    
    * updates
    
    * cleanup
    
    * removed requirement to decorate enums
    
    * nit PR comments
    
    * bug fix and api updates
    
    * Pr comments
    
    * remove datacontract and newtonsoft serializer types
    
    * Pr comments and adding tests
    
    * updates
    
    * Aggregate tests and new serializer member
    
    * More tests and cleanup
    
    * PR comments
    
    * PR comments
    
    * PR comments - internal options class
    
    * Update Program.cs
    
    * comment fixes
    
    * PR comments
    
    * Add preview flag
    
    * API changes
    
    * new serialization interface
    
    * Update API and serializer updates
    
    * API fix
    
    * cleanup
    
    * PR comments
    
    * update interface
    
    * fix
    
    * API
    
    * Update Microsoft.Azure.Cosmos/src/Serializer/ICosmosLinqSerializer.cs
    
    Co-authored-by: Kiran Kumar Kolli <kirankk@microsoft.com>
    
    * Some PR comments
    
    * adding sample code
    
    * Enum rename and interfact to abstract class
    
    * PR comments
    
    * PR comments
    
    * Remove CosmosLinqSerializerType
    
    * last one (hopefully)
    
    * Update Microsoft.Azure.Cosmos/src/Linq/TranslationContext.cs
    
    Co-authored-by: Matias Quaranta <ealsur@users.noreply.github.com>
    
    * Update Microsoft.Azure.Cosmos/src/Linq/TranslationContext.cs
    
    Co-authored-by: Matias Quaranta <ealsur@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Kiran Kumar Kolli <kirankk@microsoft.com>
    Co-authored-by: Matias Quaranta <ealsur@users.noreply.github.com>

--- Microsoft.Azure.Cosmos/src/Linq/TypeSystem.cs
+++ Microsoft.Azure.Cosmos/src/Linq/TypeSystem.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Azure.Cosmos.Linq
 
         public static string GetMemberName(this MemberInfo memberInfo, TranslationContext context)
         {
-            return context.CosmosLinqSerializer.SerializeMemberName(memberInfo, context.LinqSerializerOptions);
+            return context.CosmosLinqSerializer.SerializeMemberName(memberInfo);
         }
 
         private static Type GetElementType(Type type, HashSet<Type> visitedSet)
