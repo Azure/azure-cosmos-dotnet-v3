c9ae732d41d21698367c605321840bf00163a3d7
philipthomas <philipthomas@microsoft.com>
2024-01-24 11:39:00 -0500

    tests for changefeedprocessor with allversionsanddeletes

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/ChangeFeed/GetChangeFeedProcessorBuilderWithAllVersionsAndDeletesTests.cs
@@ -0,0 +1,130 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests.ChangeFeed
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    [TestCategory("ChangeFeedProcessor with AllVersionsAndDeletes")]
+    public class GetChangeFeedProcessorBuilderWithAllVersionsAndDeletesTests : BaseChangeFeedClientHelper
+    {
+        private ContainerInternal Container;
+
+        [TestInitialize]
+        public async Task TestInitialize()
+        {
+            await base.ChangeFeedTestInit();
+
+            string PartitionKey = "/pk";
+            ContainerProperties properties = new ContainerProperties(id: Guid.NewGuid().ToString(), 
+                partitionKeyPath: PartitionKey);
+            properties.ChangeFeedPolicy.FullFidelityRetention = TimeSpan.FromMinutes(5);
+
+            ContainerResponse response = await this.database.CreateContainerAsync(properties,
+                throughput: 10000,
+                cancellationToken: this.cancellationToken);
+            this.Container = (ContainerInternal)response;
+        }
+
+        [TestCleanup]
+        public async Task Cleanup()
+        {
+            await base.TestCleanup();
+        }
+
+        [TestMethod]
+        [Owner("philipthomas-MSFT")]
+        [Description("Scenario: When a document is created, then updated, and finally deleted, there should be 3 changes that will appear for that " +
+            "document when using ChangeFeedProcessor with AllVersionsAndDeletes.")]
+        public async Task WhenADocumentIsCreatedThenUpdatedThenDeletedTestsAsync()
+        {
+            ManualResetEvent allDocsProcessed = new ManualResetEvent(false);
+            Exception exception = default;
+
+            ChangeFeedProcessor processor = this.Container
+                .GetChangeFeedProcessorBuilderWithAllVersionsAndDeletes(processorName: "processor", onChangesHandler: (ChangeFeedProcessorContext context, IReadOnlyCollection<ChangeFeedItemChange<dynamic>> docs, CancellationToken token) =>
+                {
+                    Assert.IsNotNull(context.LeaseToken);
+                    Assert.IsNotNull(context.Diagnostics);
+                    Assert.IsNotNull(context.Headers);
+                    Assert.IsNotNull(context.Headers.Session);
+                    Assert.IsTrue(context.Headers.RequestCharge > 0);
+                    Assert.IsTrue(context.Diagnostics.ToString().Contains("Change Feed Processor Read Next Async"));
+                    Assert.AreEqual(expected: 3, actual: docs.Count);
+
+                    ChangeFeedItemChange<dynamic> createChange = docs.ElementAt(0);
+                    Assert.IsNotNull(createChange.Current);
+                    Assert.AreEqual(expected: "1", actual: createChange.Current.id.ToString());
+                    Assert.AreEqual(expected: "1", actual: createChange.Current.pk.ToString());
+                    Assert.AreEqual(expected: "original test", actual: createChange.Current.description.ToString());
+                    Assert.AreEqual(expected: createChange.Metadata.OperationType, actual: ChangeFeedOperationType.Create);
+                    Assert.AreEqual(expected: createChange.Metadata.PreviousLsn, actual: 0);
+                    Assert.IsNull(createChange.Previous);
+
+                    ChangeFeedItemChange<dynamic> replaceChange = docs.ElementAt(1);
+                    Assert.IsNotNull(replaceChange.Current);
+                    Assert.AreEqual(expected: "1", actual: replaceChange.Current.id.ToString());
+                    Assert.AreEqual(expected: "1", actual: replaceChange.Current.pk.ToString());
+                    Assert.AreEqual(expected: "test after replace", actual: replaceChange.Current.description.ToString());
+                    Assert.AreEqual(expected: replaceChange.Metadata.OperationType, actual: ChangeFeedOperationType.Replace);
+                    Assert.AreEqual(expected: createChange.Metadata.Lsn, actual: replaceChange.Metadata.PreviousLsn);
+                    Assert.IsNull(replaceChange.Previous);
+
+                    ChangeFeedItemChange<dynamic> deleteChange = docs.ElementAt(2);
+                    Assert.IsNull(deleteChange.Current.id);
+                    Assert.AreEqual(expected: deleteChange.Metadata.OperationType, actual: ChangeFeedOperationType.Delete);
+                    Assert.AreEqual(expected: replaceChange.Metadata.Lsn, actual: deleteChange.Metadata.PreviousLsn);
+                    Assert.IsNotNull(deleteChange.Previous);
+                    Assert.AreEqual(expected: "1", actual: deleteChange.Previous.id.ToString());
+                    Assert.AreEqual(expected: "1", actual: deleteChange.Previous.pk.ToString());
+                    Assert.AreEqual(expected: "test after replace", actual: deleteChange.Previous.description.ToString());
+
+                    Assert.IsTrue(condition: createChange.Metadata.ConflictResolutionTimestamp < replaceChange.Metadata.ConflictResolutionTimestamp, message: "The create operation must happen before the replace operation.");
+                    Assert.IsTrue(condition: replaceChange.Metadata.ConflictResolutionTimestamp < deleteChange.Metadata.ConflictResolutionTimestamp, message: "The replace operation must happen before the delete operation.");
+                    Assert.IsTrue(condition: createChange.Metadata.Lsn < replaceChange.Metadata.Lsn, message: "The create operation must happen before the replace operation.");
+                    Assert.IsTrue(condition: createChange.Metadata.Lsn < replaceChange.Metadata.Lsn, message: "The replace operation must happen before the delete operation.");
+                    
+                    return Task.CompletedTask;
+                })
+                .WithInstanceName(Guid.NewGuid().ToString())
+                .WithLeaseContainer(this.LeaseContainer)
+                .WithErrorNotification((leaseToken, error) =>
+                {
+                    exception = error.InnerException;
+
+                    return Task.CompletedTask;
+                })
+                .Build();
+
+            // Start the processor, insert 1 document to generate a checkpoint, modify it, and then delete it.
+            // 1 second delay between operations to get different timestamps.
+
+            await processor.StartAsync();
+            await Task.Delay(BaseChangeFeedClientHelper.ChangeFeedSetupTime);
+
+            await this.Container.CreateItemAsync<dynamic>(new { id = "1", pk = "1", description = "original test" }, partitionKey: new PartitionKey("1"));
+            await Task.Delay(1000);
+            
+            await this.Container.UpsertItemAsync<dynamic>(new { id = "1", pk = "1", description = "test after replace" }, partitionKey: new PartitionKey("1"));
+            await Task.Delay(1000);
+
+            await this.Container.DeleteItemAsync<dynamic>(id: "1", partitionKey: new PartitionKey("1"));            
+
+            bool isStartOk = allDocsProcessed.WaitOne(10 * BaseChangeFeedClientHelper.ChangeFeedSetupTime);
+
+            await processor.StopAsync();
+
+            if (exception != default)
+            {
+                Assert.Fail(exception.ToString());
+            }
+        }
+    }
+}
