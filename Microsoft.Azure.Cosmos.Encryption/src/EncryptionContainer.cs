2a9f8b59ee17a1db5f257845a866a0b4a601e25a
philipthomas <philipthomas@microsoft.com>
2024-01-30 10:51:06 -0500

    once more

--- Microsoft.Azure.Cosmos.Encryption/src/EncryptionContainer.cs
+++ Microsoft.Azure.Cosmos.Encryption/src/EncryptionContainer.cs
@@ -592,7 +592,7 @@ namespace Microsoft.Azure.Cosmos.Encryption
                 });
         }
 
-#if SDKPROJECTREF || ENCRYPTIONPREVIEW || PREVIEW
+#if PREVIEW
         public override ChangeFeedProcessorBuilder GetChangeFeedProcessorBuilderWithAllVersionsAndDeletes<T>(
             string processorName,
             ChangeFeedHandler<ChangeFeedItemChange<T>> onChangesDelegate)
