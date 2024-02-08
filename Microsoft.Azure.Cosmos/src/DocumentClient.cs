a9599bd64854d406e6de1a34b35db5ccdf9f145d
Nalu Tripician <27316859+NaluTripician@users.noreply.github.com>
2024-02-08 11:58:07 -0800

    FaultInjection: Adds Fault Injection Library (#4281)
    * initial commit
    
    * add files
    
    * restructure + some cleanup
    
    * fixed await for chaos interceptor creation
    
    * added default operation/conntection type values
    
    * threshold to thresholdpercentage
    
    * more threshold changes
    
    * changed application to rule application for clarity
    
    * changed method names in application context
    
    * spelling fix
    
    * addressed comments
    
    * tests/csproj fixes
    
    * requested changes
    
    * requested changes
    
    * added comments arrount duration
    
    * added comments arrount duration
    
    * application context changes to avoid bounded staleness problem
    
    * requested changes and test fixes
    
    * removed unused code
    
    * update contracts
    
    * Revert "update contracts"
    
    This reverts commit 8df331713ff934c2c8964e901a72b0fd1092afad.

--- Microsoft.Azure.Cosmos/src/DocumentClient.cs
+++ Microsoft.Azure.Cosmos/src/DocumentClient.cs
@@ -117,7 +117,9 @@ namespace Microsoft.Azure.Cosmos
 
         //Fault Injection
         private readonly IChaosInterceptorFactory chaosInterceptorFactory;
-        private IChaosInterceptor chaosInterceptor;
+        private readonly IChaosInterceptor chaosInterceptor;
+
+        private bool isChaosInterceptorInititalized = false;
 
         //Auth
         internal readonly AuthorizationTokenProvider cosmosAuthorization;
@@ -490,6 +492,7 @@ namespace Microsoft.Azure.Cosmos
             this.IsLocalQuorumConsistency = isLocalQuorumConsistency;
             this.initTaskCache = new AsyncCacheNonBlocking<string, bool>(cancellationToken: this.cancellationTokenSource.Token);
             this.chaosInterceptorFactory = chaosInterceptorFactory;
+            this.chaosInterceptor = chaosInterceptorFactory?.CreateInterceptor(this);
 
             this.Initialize(
                 serviceEndpoint: serviceEndpoint,
@@ -1017,14 +1020,6 @@ namespace Microsoft.Azure.Cosmos
         // Always called from under the lock except when called from Intilialize method during construction.
         private async Task<bool> GetInitializationTaskAsync(IStoreClientFactory storeClientFactory)
         {
-            //Create the chaos interceptor if using fault injection
-            //Creating the chaos interceptor requires async calls, so we do it here instead of in the constructor
-            //This must also be done before creating the storeClientFactory for direct mode
-            if (this.chaosInterceptorFactory != null)
-            {
-                this.chaosInterceptor = await this.chaosInterceptorFactory.CreateInterceptorAsync(this);
-            }
-
             await this.InitializeGatewayConfigurationReaderAsync();
 
             if (this.desiredConsistencyLevel.HasValue)
@@ -1592,6 +1587,12 @@ namespace Microsoft.Azure.Cosmos
                     DefaultTrace.TraceWarning("EnsureValidClientAsync initializeTask failed {0}", e);
                     childTrace.AddDatum("initializeTask failed", e);
                     throw;
+                }
+
+                if (this.chaosInterceptorFactory != null && !this.isChaosInterceptorInititalized)
+                {
+                    this.isChaosInterceptorInititalized = true;
+                    await this.chaosInterceptorFactory.ConfigureChaosInterceptorAsync();
                 }
             }
         }
