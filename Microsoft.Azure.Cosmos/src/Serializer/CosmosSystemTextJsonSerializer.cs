bf78e7c21cafa5c9c1927f660c5045887ce5b84f
Debdatta Kunda <dkunda@microsoft.com>
2024-02-27 09:26:02 +0530

    Code changes to add default STJ serializer.

---
+++ Microsoft.Azure.Cosmos/src/Serializer/CosmosSystemTextJsonSerializer.cs
@@ -0,0 +1,83 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+    using global::Azure.Core.Serialization;
+
+    /// <summary>
+    /// This class provides a way to configure Linq Serialization Properties
+    /// </summary>
+    public class CosmosSystemTextJsonSerializer : CosmosLinqSerializer
+    {
+        private readonly JsonObjectSerializer systemTextJsonSerializer;
+
+        /// <summary>
+        /// Create an instance of <see cref="CosmosSystemTextJsonSerializer"/>
+        /// with the default values for the Cosmos SDK
+        /// </summary>
+        /// <param name="jsonSerializerOptions">options.</param>
+        public CosmosSystemTextJsonSerializer(JsonSerializerOptions jsonSerializerOptions)
+        {
+            this.systemTextJsonSerializer = new JsonObjectSerializer(jsonSerializerOptions);
+        }
+
+        /// <inheritdoc/>
+        public override T FromStream<T>(Stream stream)
+        {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
+            using (stream)
+            {
+                if (stream.CanSeek && stream.Length == 0)
+                {
+                    return default;
+                }
+
+                if (typeof(Stream).IsAssignableFrom(typeof(T)))
+                {
+                    return (T)(object)stream;
+                }
+
+                return (T)this.systemTextJsonSerializer.Deserialize(stream, typeof(T), default);
+            }
+        }
+
+        /// <inheritdoc/>
+        public override Stream ToStream<T>(T input)
+        {
+            MemoryStream streamPayload = new MemoryStream();
+            this.systemTextJsonSerializer.Serialize(streamPayload, input, input.GetType(), default);
+            streamPayload.Position = 0;
+            return streamPayload;
+        }
+
+        /// <inheritdoc/>
+        public override string SerializeMemberName(MemberInfo memberInfo)
+        {
+            System.Text.Json.Serialization.JsonExtensionDataAttribute jsonExtensionDataAttribute =
+                 memberInfo.GetCustomAttribute<System.Text.Json.Serialization.JsonExtensionDataAttribute>(true);
+            if (jsonExtensionDataAttribute != null)
+            {
+                return null;
+            }
+
+            JsonPropertyNameAttribute jsonPropertyNameAttribute = memberInfo.GetCustomAttribute<JsonPropertyNameAttribute>(true);
+
+            string memberName = !string.IsNullOrEmpty(jsonPropertyNameAttribute?.Name)
+                ? jsonPropertyNameAttribute.Name
+                : memberInfo.Name;
+
+            // Users must add handling for any additional attributes here
+
+            return memberName;
+        }
+    }
+}
