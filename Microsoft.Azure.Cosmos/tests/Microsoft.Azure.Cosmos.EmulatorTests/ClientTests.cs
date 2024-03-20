b6ae8a38604d4a7ef0a1258bdd2b1cda8dca0c1c
Nalu Tripician <nalutrip@icloud.com>
2024-03-20 14:23:54 -0700

    changed env location

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/ClientTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/ClientTests.cs
@@ -931,7 +931,6 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
         {
             string connectionString = TestCommon.GetMultiRegionConnectionString();
             Assert.IsNotNull(connectionString);
-            Assert.Fail(connectionString[..10]);
             using CosmosClient cosmosClient = new CosmosClient(connectionString);
             Assert.IsNotNull(cosmosClient);
             AccountProperties properties = await cosmosClient.ReadAccountAsync();
