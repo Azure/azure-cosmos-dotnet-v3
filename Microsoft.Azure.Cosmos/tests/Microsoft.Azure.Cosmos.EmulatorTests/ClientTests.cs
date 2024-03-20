501e18f67e6b637542b5b40a5381900c9a3350fc
Nalu Tripician <nalutrip@icloud.com>
2024-03-20 14:40:54 -0700

    test

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/ClientTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/ClientTests.cs
@@ -931,7 +931,7 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
         {
             string connectionString = TestCommon.GetMultiRegionConnectionString();
             Assert.IsNotNull("Connection String Null");
-            Assert.Fail(connectionString[..10]);
+            Assert.Fail(connectionString[..10]);//remove
             using CosmosClient cosmosClient = new CosmosClient(connectionString);
             Assert.IsNotNull(cosmosClient);
             AccountProperties properties = await cosmosClient.ReadAccountAsync();
