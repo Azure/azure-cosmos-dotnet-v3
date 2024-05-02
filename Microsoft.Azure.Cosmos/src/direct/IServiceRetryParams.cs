039be0c983ec945f341c48c8271a7d5f822db5be
Nalu Tripician (from Dev Box) <ntripician@microsoft.com>
2024-05-02 15:18:25 -0700

    update direct

---
+++ Microsoft.Azure.Cosmos/src/direct/IServiceRetryParams.cs
@@ -0,0 +1,15 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Documents
+{
+    /// <summary> 
+    /// Retry params that every service can configure
+    /// Add more api's that control retry as needed, currently only exposes overall timeout
+    /// </summary>
+    internal interface IServiceRetryParams
+    {
+        public bool TryGetRetryTimeoutInSeconds(out int retryTimeoutInSeconds);
+    }
+}
