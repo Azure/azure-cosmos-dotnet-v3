77127f0bc772988ad36f32083ffdee233429b72f
REDMOND\adityasa <adityasa@microsoft.com>
2024-05-25 13:28:01 -0700

    Addressed comments, updated test.

--- Microsoft.Azure.Cosmos/src/RequestOptions/QueryRequestOptions.cs
+++ Microsoft.Azure.Cosmos/src/RequestOptions/QueryRequestOptions.cs
@@ -238,6 +238,9 @@ namespace Microsoft.Azure.Cosmos
                 request.Headers.Add(HttpConstants.HttpHeaders.ResponseContinuationTokenLimitInKB, this.ResponseContinuationTokenLimitInKb.ToString());
             }
 
+            // All query APIs (GetItemQueryIterator, GetItemLinqQueryable and GetItemQueryStreamIterator) turn into ReadFeed operation if query text is null.
+            // In such a case, query pipelines are still involved (including QueryRequestOptions). In general backend only honors SupportedSerializationFormats
+            //  for OperationType Query but has a bug where it returns a binary response for ReadFeed API when partition key is also specified in the request.
             if (request.OperationType == OperationType.Query)
             {
                 request.Headers.CosmosMessageHeaders.SupportedSerializationFormats = this.SupportedSerializationFormats?.ToString() ?? DocumentQueryExecutionContextBase.DefaultSupportedSerializationFormats;
--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/Query/SupportedSerializationFormatsTest.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/Query/SupportedSerializationFormatsTest.cs
@@ -1,14 +1,13 @@
 ﻿namespace Microsoft.Azure.Cosmos.Query
 {
-    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos.CosmosElements;
     using Microsoft.Azure.Cosmos.EmulatorTests.Query;
-    using Microsoft.Azure.Cosmos.SDK.EmulatorTests.QueryOracle;
     using Microsoft.Azure.Documents;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Newtonsoft.Json.Linq;
 
     [TestClass]
     [TestCategory("Query")]
@@ -36,66 +35,89 @@
                 CollectionTypes.SinglePartition | CollectionTypes.MultiPartition,
                 inputDocuments,
                 this.TestSupportedSerializationFormatsHelper,
-                    "/id");
+                "/id");
         }
 
         private async Task TestSupportedSerializationFormatsHelper(Container container, IReadOnlyList<CosmosObject> documents)
         {
-            string[] expectedResults = new[] { "document_0", "document_1", "document_2", "document_3", "document_4", "document_5", "document_6", "document_7", "document_8", "document_9" };
-            string query = string.Format("SELECT c.name FROM c");
-            List<QueryRequestOptions> queryRequestOptionsList = new List<QueryRequestOptions>()
-            {
-                new QueryRequestOptions()
+            List<(Cosmos.PartitionKey?, string[]) > partitionKeyAndExpectedResults = new List<(Cosmos.PartitionKey? partitionKey, string[] documents)>
                 {
-                    SupportedSerializationFormats = SupportedSerializationFormats.CosmosBinary | SupportedSerializationFormats.HybridRow
-                },
-                new QueryRequestOptions()
-                {
-                    SupportedSerializationFormats = SupportedSerializationFormats.JsonText | SupportedSerializationFormats.CosmosBinary
-                },
-                new QueryRequestOptions()
-                {
-                    SupportedSerializationFormats = SupportedSerializationFormats.JsonText | SupportedSerializationFormats.HybridRow
-                },
-                new QueryRequestOptions()
-                {
-                    SupportedSerializationFormats = SupportedSerializationFormats.JsonText | SupportedSerializationFormats.CosmosBinary | SupportedSerializationFormats.HybridRow
-                },
-                new QueryRequestOptions()
-                {
-                    SupportedSerializationFormats = SupportedSerializationFormats.JsonText
-                },
-                new QueryRequestOptions()
-                {
-                    SupportedSerializationFormats = SupportedSerializationFormats.CosmosBinary
-                },
-                new QueryRequestOptions()
-                {
-                    SupportedSerializationFormats = SupportedSerializationFormats.CosmosBinary
-                },
-                new QueryRequestOptions()
-                {
-                    SupportedSerializationFormats = SupportedSerializationFormats.JsonText
-                }
-            };
+                    (
+                        partitionKey: null,
+                        documents: new[] { "document_0", "document_1", "document_2", "document_3", "document_4", "document_5", "document_6", "document_7", "document_8", "document_9" }
+                    ),
+                    (
+                        partitionKey: new Cosmos.PartitionKey("0"),
+                        documents: new[] { "document_0" }
+                    )
+                };
 
-            foreach (QueryRequestOptions requestOptions in queryRequestOptionsList)
-            {
-                List<CosmosElement> results = new List<CosmosElement>();
-                using (FeedIterator<CosmosElement> feedIterator = container.GetItemQueryIterator<CosmosElement>(new QueryDefinition(query), requestOptions: requestOptions))
+            foreach (string query in new string[]
                 {
-                    while (feedIterator.HasMoreResults)
+                    null,   // With null query, this turns into a ReadFeed API, for which SupportedSerializationFormats should be ignored.
+                    "SELECT * FROM c"
+                })
+            {
+                List<QueryRequestOptions> queryRequestOptionsList = new List<QueryRequestOptions>()
+                {
+                    new QueryRequestOptions()
                     {
-                        FeedResponse<CosmosElement> response = await feedIterator.ReadNextAsync();
-                        results.AddRange(response.ToList());
+                        SupportedSerializationFormats = SupportedSerializationFormats.CosmosBinary | SupportedSerializationFormats.HybridRow
+                    },
+                    new QueryRequestOptions()
+                    {
+                        SupportedSerializationFormats = SupportedSerializationFormats.JsonText | SupportedSerializationFormats.CosmosBinary
+                    },
+                    new QueryRequestOptions()
+                    {
+                        SupportedSerializationFormats = SupportedSerializationFormats.JsonText | SupportedSerializationFormats.HybridRow
+                    },
+                    new QueryRequestOptions()
+                    {
+                        SupportedSerializationFormats = SupportedSerializationFormats.JsonText | SupportedSerializationFormats.CosmosBinary | SupportedSerializationFormats.HybridRow
+                    },
+                    new QueryRequestOptions()
+                    {
+                        SupportedSerializationFormats = SupportedSerializationFormats.JsonText
+                    },
+                    new QueryRequestOptions()
+                    {
+                        SupportedSerializationFormats = SupportedSerializationFormats.CosmosBinary
+                    },
+                    new QueryRequestOptions()
+                    {
+                        SupportedSerializationFormats = SupportedSerializationFormats.CosmosBinary
+                    },
+                    new QueryRequestOptions()
+                    {
+                        SupportedSerializationFormats = SupportedSerializationFormats.JsonText
+                    }
+                };
+
+                foreach (QueryRequestOptions requestOptions in queryRequestOptionsList)
+                {
+                    QueryDefinition queryDefinition = query != null ? new QueryDefinition(query) : null;
+                    foreach ((Cosmos.PartitionKey? partitionKey, string[] expectedResults) in partitionKeyAndExpectedResults)
+                    {
+                        requestOptions.PartitionKey = partitionKey;
+
+                        List<JObject> queryResults = new List<JObject>();
+                        using (FeedIterator<JObject> feedIterator = container.GetItemQueryIterator<JObject>(queryDefinition, requestOptions: requestOptions))
+                        {
+                            while (feedIterator.HasMoreResults)
+                            {
+                                FeedResponse<JObject> response = await feedIterator.ReadNextAsync();
+                                queryResults.AddRange(response.ToList());
+                            }
+                        }
+
+                        string[] actualResults = queryResults
+                            .Select(doc => doc["name"].ToString())
+                            .ToArray();
+
+                        CollectionAssert.AreEquivalent(expectedResults, actualResults);
                     }
                 }
-
-                string[] actualResults = results
-                    .Select(doc => ((CosmosString)(doc as CosmosObject)["name"]).Value.ToString())
-                    .ToArray();
-
-                CollectionAssert.AreEquivalent(expectedResults, actualResults);
             }
         }
     }
