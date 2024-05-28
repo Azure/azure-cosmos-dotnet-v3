7c29b0158237e477aafdbbc6b6f8bb11b6cb807f
Matias Quaranta <ealsur@hotmail.com>
2024-05-28 10:40:02 -0700

    Revert

--- Microsoft.Azure.Cosmos/src/Query/v2Query/DocumentQueryExecutionContextBase.cs
+++ Microsoft.Azure.Cosmos/src/Query/v2Query/DocumentQueryExecutionContextBase.cs
@@ -32,9 +32,8 @@ namespace Microsoft.Azure.Cosmos.Query
 
     internal abstract class DocumentQueryExecutionContextBase : IDocumentQueryExecutionContext
     {
-        // ISSUE-TODO-adityasa-2024/5/27 - Re-enable binary format. Currently encryption codepath is not able to handle binary response.
-        public static readonly string DefaultSupportedSerializationFormats = SupportedSerializationFormats.JsonText.ToString();
-
+        public static readonly string DefaultSupportedSerializationFormats = string.Join(",", SupportedSerializationFormats.JsonText, SupportedSerializationFormats.CosmosBinary);
+        
         public readonly struct InitParams
         {
             public IDocumentQueryClient Client { get; }
