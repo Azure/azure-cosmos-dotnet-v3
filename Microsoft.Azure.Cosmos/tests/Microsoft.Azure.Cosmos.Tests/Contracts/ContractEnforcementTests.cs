a005c5608bc5daae00c7a0bdb4100423cda316e3
Kiran Kumar Kolli <kirankk@microsoft.com>
2024-07-02 16:56:27 -0700

    Unit test coverage to validate Cosmos .NET SDK versioning strategy

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Contracts/ContractEnforcementTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/Contracts/ContractEnforcementTests.cs
@@ -1,6 +1,9 @@
 ﻿namespace Microsoft.Azure.Cosmos.Tests.Contracts
 {
     using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Xml;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     
     [TestCategory("Windows")]
@@ -46,6 +49,31 @@
         public void UniqueKeyUnsealed()
         {
             Assert.IsFalse(typeof(UniqueKey).IsSealed);
+        }
+
+        [TestMethod]
+        public void ValdatePacakgeVersions()
+        {
+            const String VersionFile = "Directory.Build.props";
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(File.ReadAllText(VersionFile));
+
+            XmlNode propertyGroupNode = xmldoc.DocumentElement["PropertyGroup"];
+
+            string officialVersionText = propertyGroupNode?["ClientOfficialVersion"]?.InnerText;
+            string previewVersionText = propertyGroupNode?["ClientPreviewVersion"]?.InnerText;
+
+            Trace.TraceInformation($"Official Version: {officialVersionText}");
+            Trace.TraceInformation($"Preview Version: {previewVersionText}");
+
+            Version officialVersion = new Version(officialVersionText);
+            Version previewVersion = new Version(previewVersionText);
+
+            // Preview minor version should always be one ahead of the official version
+            Assert.AreEqual(officialVersion.Major, previewVersion.Major, $"{officialVersionText} {previewVersionText}");
+            Assert.AreEqual(officialVersion.Minor + 1, previewVersion.Minor, $"{officialVersionText} {previewVersionText}");
+            Assert.AreEqual(officialVersion.Build, previewVersion.Build, $"{officialVersionText} {previewVersionText}");
         }
     }
 }
