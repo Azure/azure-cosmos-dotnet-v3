64ba76440b9b18d3ba2f1cfd7320001349169f87
philipthomas <philipthomas@microsoft.com>
2024-08-14 13:46:29 -0400

    removed from Encryption

--- Microsoft.Azure.Cosmos.Encryption.Custom/src/EncryptionContainer.cs
+++ Microsoft.Azure.Cosmos.Encryption.Custom/src/EncryptionContainer.cs
@@ -1029,23 +1029,6 @@ namespace Microsoft.Azure.Cosmos.Encryption.Custom
         }
 #endif
 
-#if SDKPROJECTREF
-        public override Task<IReadOnlyList<Cosmos.FeedRange>> FindOverlappingRangesAsync(
-            Cosmos.PartitionKey partitionKey,
-            IReadOnlyList<Cosmos.FeedRange> feedRanges,
-            CancellationToken cancellationToken = default)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override IReadOnlyList<Cosmos.FeedRange> FindOverlappingRanges(
-            Cosmos.FeedRange feedRange,
-            IReadOnlyList<Cosmos.FeedRange> feedRanges)
-        {
-            throw new NotImplementedException();
-        }
-#endif
-
         private async Task<ResponseMessage> ReadManyItemsHelperAsync(
            IReadOnlyList<(string id, PartitionKey partitionKey)> items,
            ReadManyRequestOptions readManyRequestOptions = null,
