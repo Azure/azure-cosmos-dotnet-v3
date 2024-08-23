6d603f54bdfee42b2768e7407fdc4eb56fcc8271
Nalu Tripician <27316859+NaluTripician@users.noreply.github.com>
2024-08-23 08:19:02 -0700

    [Internal] Hedging: Refactors Tests  (#4641)
    * test improvements
    
    * fixed other multiregion test
    
    * requested changes
    
    * test fix
    
    * adds util function for multiregion setup
    
    * fixed util method/class
    
    * parallelize tasks

---
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/Utils/MultiRegionSetupHelpers.cs
@@ -0,0 +1,66 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
+{
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
+    using static Microsoft.Azure.Cosmos.SDK.EmulatorTests.CosmosAvailabilityStrategyTests;
+
+    public class MultiRegionSetupHelpers
+    {
+        private const string dbName = "availabilityStrategyTestDb";
+        private const string containerName = "availabilityStrategyTestContainer";
+        private const string changeFeedContainerName = "availabilityStrategyTestChangeFeedContainer";
+
+        public static async Task<(Database, Container, Container)> GetOrCreateMultiRegionDatabaseAndContainers(CosmosClient client)
+        {
+            Database database;
+            Container container;
+            Container changeFeedContainer;
+
+            DatabaseResponse db = await client.CreateDatabaseIfNotExistsAsync(
+                id: MultiRegionSetupHelpers.dbName,
+                throughput: 400);
+            database = db.Database;
+
+            if (db.StatusCode == HttpStatusCode.Created)
+            {
+                container = await database.CreateContainerIfNotExistsAsync(
+                    id: MultiRegionSetupHelpers.containerName,
+                    partitionKeyPath: "/pk",
+                    throughput: 400);
+                changeFeedContainer = await database.CreateContainerIfNotExistsAsync(
+                    id: MultiRegionSetupHelpers.changeFeedContainerName,
+                    partitionKeyPath: "/partitionKey",
+                    throughput: 400);
+
+                List<Task> tasks = new List<Task>()
+                {
+                    container.CreateItemAsync<AvailabilityStrategyTestObject>(
+                        new AvailabilityStrategyTestObject { Id = "testId", Pk = "pk" }),
+                    container.CreateItemAsync<AvailabilityStrategyTestObject>(
+                        new AvailabilityStrategyTestObject { Id = "testId2", Pk = "pk2" }),
+                    container.CreateItemAsync<AvailabilityStrategyTestObject>(
+                        new AvailabilityStrategyTestObject { Id = "testId3", Pk = "pk3" }),
+                    container.CreateItemAsync<AvailabilityStrategyTestObject>(
+                        new AvailabilityStrategyTestObject { Id = "testId4", Pk = "pk4" })
+                };
+
+                await Task.WhenAll(tasks);
+
+                //Must Ensure the data is replicated to all regions
+                await Task.Delay(60000);
+
+                return (database, container, changeFeedContainer);
+            }
+
+            container = database.GetContainer(MultiRegionSetupHelpers.containerName);
+            changeFeedContainer = database.GetContainer(MultiRegionSetupHelpers.changeFeedContainerName);
+
+            return (database, container, changeFeedContainer);
+        }
+    }
+}
