02ca4b771c8a2a03b9deb46ce40577b2ee4b68fe
Nalu Tripician <27316859+NaluTripician@users.noreply.github.com>
2024-09-04 13:17:32 -0700

    added test file

--- Microsoft.Azure.Cosmos/FaultInjection/tests/Utils/TestCommon.cs
+++ Microsoft.Azure.Cosmos/FaultInjection/tests/Utils/TestCommon.cs
@@ -2,10 +2,16 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Net;
+    using System.Text.Json.Serialization;
+    using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos.Fluent;
 
     internal static class TestCommon
     {
+        public const string FaultInjectionContainerName = "faultInjectionContainer";
+        public const string FaultInjectionDatabaseName = "faultInjectionDatabase";
+
         public const string EndpointMultiRegion = "";
         public const string AuthKeyMultiRegion = "";
         
@@ -66,6 +72,60 @@
             }
 
             return clientBuilder;
+        }
+
+        internal static async Task<(Database, Container)> GetOrCreateMultiRegionFIDatabaseAndContainers(CosmosClient client)
+        {
+            Database database;
+            Container container;
+
+            DatabaseResponse db = await client.CreateDatabaseIfNotExistsAsync(
+                id: TestCommon.FaultInjectionDatabaseName,
+                throughput: 400);
+            database = db.Database;
+
+            if (db.StatusCode == HttpStatusCode.Created)
+            {
+                container = await database.CreateContainerIfNotExistsAsync(
+                    id: TestCommon.FaultInjectionContainerName,
+                    partitionKeyPath: "/pk",
+                    throughput: 400);
+
+                List<Task> tasks = new List<Task>()
+                {
+                    container.CreateItemAsync<FaultInjectionTestObject>(
+                        new FaultInjectionTestObject { Id = "testId", Pk = "pk" }),
+                    container.CreateItemAsync<FaultInjectionTestObject>(
+                        new FaultInjectionTestObject { Id = "testId2", Pk = "pk2" }),
+                    container.CreateItemAsync<FaultInjectionTestObject>(
+                        new FaultInjectionTestObject { Id = "testId3", Pk = "pk3" }),
+                    container.CreateItemAsync<FaultInjectionTestObject>(
+                        new FaultInjectionTestObject { Id = "testId4", Pk = "pk4" })
+                };
+
+                await Task.WhenAll(tasks);
+
+                //Must Ensure the data is replicated to all regions
+                await Task.Delay(60000);
+
+                return (database, container);
+            }
+
+            container = database.GetContainer(TestCommon.FaultInjectionContainerName);
+
+            return (database, container);
+        }
+
+        internal class FaultInjectionTestObject
+        {
+            [JsonPropertyName("id")]
+            public string? Id { get; set; }
+
+            [JsonPropertyName("pk")]
+            public string? Pk { get; set; }
+
+            [JsonPropertyName("other")]
+            public string? Other { get; set; }
         }
     }
 }
