d6f89ea6f298e6b7459fa8246a22d8efe1aeb045
Juraj Blazek <jublazek@microsoft.com>
2024-09-12 13:41:09 +0200

    Add baseline benchmarks for `Microsoft.Azure.Cosmos.Encryption.Custom`

---
+++ Microsoft.Azure.Cosmos.Encryption.Custom/tests/Microsoft.Azure.Cosmos.Encryption.Custom.Performance.Tests/EncryptionBenchmark.cs
@@ -0,0 +1,102 @@
+﻿namespace Microsoft.Azure.Cosmos.Encryption.Custom.Performance.Tests
+{
+    using System.IO;
+    using BenchmarkDotNet.Attributes;
+    using Microsoft.Data.Encryption.Cryptography;
+    using Moq;
+
+    [RPlotExporter]
+    public partial class EncryptionBenchmark
+    {
+        private static readonly byte[] DekData = Enumerable.Repeat((byte)0, 32).ToArray();
+        private static readonly DataEncryptionKeyProperties DekProperties = new(
+                "id",
+                CosmosEncryptionAlgorithm.MdeAeadAes256CbcHmac256Randomized,
+                DekData,
+                new EncryptionKeyWrapMetadata("name", "value"), DateTime.UtcNow);
+        private static readonly Mock<EncryptionKeyStoreProvider> StoreProvider = new();
+
+        private TestDoc? testDoc;
+        private CosmosEncryptor? encryptor;
+        private Custom.DataEncryptionKey? dek;
+
+        private EncryptionOptions? encryptionOptions;
+        private byte[]? encryptedData;
+        private byte[]? plaintext;
+
+        [Params(1, 10, 100)]
+        public int DocumentSizeInKb { get; set; }
+
+        [GlobalSetup]
+        public async Task Setup()
+        {
+            StoreProvider
+                .Setup(x => x.UnwrapKey(It.IsAny<string>(), It.IsAny<KeyEncryptionKeyAlgorithm>(), It.IsAny<byte[]>()))
+                .Returns(DekData);
+
+            this.dek = this.CreateMdeDek();
+
+            Mock<DataEncryptionKeyProvider> keyProvider = new();
+            keyProvider
+                .Setup(x => x.FetchDataEncryptionKeyWithoutRawKeyAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => this.dek);
+
+            this.encryptor = new(keyProvider.Object);
+            this.testDoc = TestDoc.Create(approximateSize: this.DocumentSizeInKb * 1024);
+
+            this.encryptionOptions = CreateEncryptionOptions();
+
+            this.plaintext = EncryptionProcessor.BaseSerializer.ToStream(this.testDoc).ToArray();
+
+            Stream encryptedStream = await EncryptionProcessor.EncryptAsync(
+                 new MemoryStream(this.plaintext),
+                 this.encryptor,
+                 this.encryptionOptions,
+                 new CosmosDiagnosticsContext(),
+                 CancellationToken.None);
+
+            using MemoryStream memoryStream = new MemoryStream();
+            
+            encryptedStream.CopyTo(memoryStream);
+            this.encryptedData = memoryStream.ToArray();
+        }
+
+        [Benchmark]
+        public async Task Encrypt()
+        {
+            await EncryptionProcessor.EncryptAsync(
+                 new MemoryStream(this.plaintext!),
+                 this.encryptor,
+                 this.encryptionOptions,
+                 new CosmosDiagnosticsContext(),
+                 CancellationToken.None);
+        }
+
+        [Benchmark]
+        public async Task Decrypt()
+        {
+            await EncryptionProcessor.DecryptAsync(
+                new MemoryStream(this.encryptedData!),
+                this.encryptor,
+                new CosmosDiagnosticsContext(),
+                CancellationToken.None);
+        }
+
+        private Custom.DataEncryptionKey CreateMdeDek()
+        {
+            return new MdeEncryptionAlgorithm(DekProperties, EncryptionType.Deterministic, StoreProvider.Object, cacheTimeToLive: TimeSpan.MaxValue);
+        }
+
+        private static EncryptionOptions CreateEncryptionOptions()
+        {
+            EncryptionOptions options = new()
+            {
+                DataEncryptionKeyId = "dekId",
+                EncryptionAlgorithm = CosmosEncryptionAlgorithm.MdeAeadAes256CbcHmac256Randomized,
+                PathsToEncrypt = TestDoc.PathsToEncrypt
+            };
+
+            return options;
+        }
+    }
+}
---
+++ Microsoft.Azure.Cosmos.Encryption.Custom/tests/Microsoft.Azure.Cosmos.Encryption.Custom.Performance.Tests/Readme.md
@@ -0,0 +1,19 @@
+﻿``` ini
+
+BenchmarkDotNet=v0.13.3, OS=Windows 11 (10.0.22631.4169)
+11th Gen Intel Core i9-11950H 2.60GHz, 1 CPU, 16 logical and 8 physical cores
+.NET SDK=9.0.100-preview.7.24407.12
+  [Host] : .NET 6.0.33 (6.0.3324.36610), X64 RyuJIT AVX2
+
+Job=MediumRun  Toolchain=InProcessEmitToolchain  IterationCount=15  
+LaunchCount=2  WarmupCount=10  
+
+```
+|  Method | DocumentSizeInKb |        Mean |      Error |     StdDev |     Gen0 |     Gen1 |     Gen2 |  Allocated |
+|-------- |----------------- |------------:|-----------:|-----------:|---------:|---------:|---------:|-----------:|
+| **Encrypt** |                **1** |    **47.90 μs** |   **1.284 μs** |   **1.842 μs** |   **4.5776** |   **1.1597** |        **-** |   **56.22 KB** |
+| Decrypt |                1 |    59.67 μs |   1.041 μs |   1.558 μs |   5.2490 |   1.3428 |        - |   64.79 KB |
+| **Encrypt** |               **10** |   **154.57 μs** |   **2.728 μs** |   **3.998 μs** |  **20.7520** |   **4.1504** |        **-** |  **255.95 KB** |
+| Decrypt |               10 |   220.03 μs |   6.124 μs |   8.585 μs |  29.0527 |   5.8594 |        - |  357.41 KB |
+| **Encrypt** |              **100** | **2,761.51 μs** | **213.677 μs** | **319.822 μs** | **218.7500** | **173.8281** | **142.5781** | **2459.89 KB** |
+| Decrypt |              100 | 2,445.99 μs | 136.839 μs | 200.577 μs | 347.6563 | 300.7813 | 253.9063 | 3406.33 KB |
