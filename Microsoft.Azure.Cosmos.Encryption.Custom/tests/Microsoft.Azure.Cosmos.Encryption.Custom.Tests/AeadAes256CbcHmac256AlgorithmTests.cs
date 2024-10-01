611b3ac485adc698df5b3a7fe062cf8c4e42af0e
Jan Hyka <jahyka@microsoft.com>
2024-10-01 11:55:26 +0200

    + unit test

--- Microsoft.Azure.Cosmos.Encryption.Custom/tests/Microsoft.Azure.Cosmos.Encryption.Custom.Tests/AeadAes256CbcHmac256AlgorithmTests.cs
+++ Microsoft.Azure.Cosmos.Encryption.Custom/tests/Microsoft.Azure.Cosmos.Encryption.Custom.Tests/AeadAes256CbcHmac256AlgorithmTests.cs
@@ -6,6 +6,7 @@ namespace Microsoft.Azure.Cosmos.Encryption.Tests
 {
     using Microsoft.Azure.Cosmos.Encryption.Custom;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
     using System.Linq;
     using System.Text;
 
@@ -20,6 +21,8 @@ namespace Microsoft.Azure.Cosmos.Encryption.Tests
         [ClassInitialize]
         public static void ClassInitialize(TestContext testContext)
         {
+            _ = testContext;
+
             AeadAes256CbcHmac256AlgorithmTests.key = new AeadAes256CbcHmac256EncryptionKey(RootKey, "AEAes256CbcHmacSha256Randomized");
             AeadAes256CbcHmac256AlgorithmTests.algorithm = new AeadAes256CbcHmac256Algorithm(AeadAes256CbcHmac256AlgorithmTests.key, EncryptionType.Randomized, algorithmVersion: 1);
         }
@@ -38,6 +41,21 @@ namespace Microsoft.Azure.Cosmos.Encryption.Tests
             byte[] decrypted = algorithm.DecryptData(cipherTextBytes);
 
             Assert.IsTrue(plainTextBytes.SequenceEqual(decrypted));
+        }
+
+        [TestMethod]
+        public void DecryptUsingBufferDecryptsSuccessfully()
+        {
+            byte[] plainTextBytes = new byte[4] { 0, 1, 2, 3 };
+            byte[] encrypted = algorithm.EncryptData(plainTextBytes);
+
+            int plainTextMaxLength = algorithm.GetDecryptByteCount(encrypted.Length);
+            byte[] decrypted = new byte[plainTextMaxLength];
+
+            int decryptedBytes = algorithm.DecryptData(encrypted, 0, encrypted.Length, decrypted, 0);
+
+            Assert.AreEqual(plainTextBytes.Length, decryptedBytes);
+            Assert.IsTrue(plainTextBytes.SequenceEqual(decrypted.AsSpan(0, decryptedBytes).ToArray()));
         }
     }
 }
