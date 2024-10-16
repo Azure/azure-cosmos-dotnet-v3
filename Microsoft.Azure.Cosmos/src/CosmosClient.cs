b43f909b395830f811e2492e8d2325d2346d84b9
Sourabh Jain <sourabhjain@microsoft.com>
2024-10-16 10:55:22 +0530

    emit metrics

--- Microsoft.Azure.Cosmos/src/CosmosClient.cs
+++ Microsoft.Azure.Cosmos/src/CosmosClient.cs
@@ -1424,6 +1424,8 @@ namespace Microsoft.Azure.Cosmos
         {
             this.IncrementNumberOfActiveClients();
 
+            CosmosOperationMeter.AddActiveInstance(this.Endpoint);
+
             return Interlocked.Increment(ref numberOfClientsCreated);
         }
 
@@ -1437,6 +1439,8 @@ namespace Microsoft.Azure.Cosmos
             // In case dispose is called multiple times. Check if at least 1 active client is there
             if (NumberOfActiveClients > 0)
             {
+                CosmosOperationMeter.DisposeActiveInstance(this.Endpoint);
+
                 return Interlocked.Decrement(ref NumberOfActiveClients);
             }
 
