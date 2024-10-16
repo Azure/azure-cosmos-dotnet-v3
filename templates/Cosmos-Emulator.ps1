5179f887349e33df9bbb40d0d1605f0509fe18d4
Kiran Kumar Kolli <kirankk@microsoft.com>
2024-10-16 13:56:37 -0700

    Updating default arguments

--- templates/Cosmos-Emulator.ps1
+++ templates/Cosmos-Emulator.ps1
@@ -102,7 +102,7 @@ if ($Stage -eq "Launch")
       $argumentList += , $StartParameters
   } else {
     # Use the default params if none provided
-    $argumentList = "/noexplorer /noui /enablepreview /disableratelimiting /enableaadauthentication"
+    $argumentList = "/noexplorer /noui /enablepreview /EnableSqlComputeEndpoint /SqlComputePort=9999 /disableratelimiting /partitioncount=50 /consistency=Strong"
   }
 
   Write-Host "Starting emulator process: $Emulator $argumentList"
