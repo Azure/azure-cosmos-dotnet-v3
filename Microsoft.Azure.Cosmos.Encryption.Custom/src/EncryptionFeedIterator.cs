46b3ca421a4363d1133454ed177166c5f6908f9e
Jan Hyka <jahyka@microsoft.com>
2024-10-18 14:57:24 +0200

    + EncryptionFeedIterator

--- Microsoft.Azure.Cosmos.Encryption.Custom/src/EncryptionFeedIterator.cs
+++ Microsoft.Azure.Cosmos.Encryption.Custom/src/EncryptionFeedIterator.cs
@@ -4,12 +4,17 @@
 
 namespace Microsoft.Azure.Cosmos.Encryption.Custom
 {
-    using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Threading;
     using System.Threading.Tasks;
+#if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
+    using Microsoft.Azure.Cosmos.Encryption.Custom.StreamProcessing;
+    using Microsoft.Azure.Cosmos.Encryption.Custom.Transformation;
+#else
+    using System;
     using Newtonsoft.Json.Linq;
+#endif
 
     internal sealed class EncryptionFeedIterator : FeedIterator
     {
@@ -17,6 +22,25 @@ namespace Microsoft.Azure.Cosmos.Encryption.Custom
         private readonly Encryptor encryptor;
         private readonly CosmosSerializer cosmosSerializer;
 
+#if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
+        private readonly StreamManager streamManager;
+
+        private static readonly ArrayStreamSplitter StreamSplitter = new ArrayStreamSplitter();
+#endif
+
+#if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
+        public EncryptionFeedIterator(
+            FeedIterator feedIterator,
+            Encryptor encryptor,
+            CosmosSerializer cosmosSerializer,
+            StreamManager streamManager)
+        {
+            this.feedIterator = feedIterator;
+            this.encryptor = encryptor;
+            this.cosmosSerializer = cosmosSerializer;
+            this.streamManager = streamManager;
+        }
+#else
         public EncryptionFeedIterator(
             FeedIterator feedIterator,
             Encryptor encryptor,
@@ -26,6 +50,7 @@ namespace Microsoft.Azure.Cosmos.Encryption.Custom
             this.encryptor = encryptor;
             this.cosmosSerializer = cosmosSerializer;
         }
+#endif
 
         public override bool HasMoreResults => this.feedIterator.HasMoreResults;
 
@@ -38,10 +63,20 @@ namespace Microsoft.Azure.Cosmos.Encryption.Custom
 
                 if (responseMessage.IsSuccessStatusCode && responseMessage.Content != null)
                 {
+#if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
+                    Stream decryptedContent = this.streamManager.CreateStream();
+                    await EncryptionProcessor.DeserializeAndDecryptResponseAsync(
+                        responseMessage.Content,
+                        decryptedContent,
+                        this.encryptor,
+                        this.streamManager,
+                        cancellationToken);
+#else
                     Stream decryptedContent = await EncryptionProcessor.DeserializeAndDecryptResponseAsync(
                         responseMessage.Content,
                         this.encryptor,
                         cancellationToken);
+#endif
 
                     return new DecryptedResponseMessage(responseMessage, decryptedContent);
                 }
@@ -60,8 +95,14 @@ namespace Microsoft.Azure.Cosmos.Encryption.Custom
 
                 if (responseMessage.IsSuccessStatusCode && responseMessage.Content != null)
                 {
+#if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
+                    decryptableContent = await this.ConvertResponseToDecryptableItemsAsync<T>(
+                        responseMessage.Content,
+                        cancellationToken);
+#else
                     decryptableContent = this.ConvertResponseToDecryptableItems<T>(
                         responseMessage.Content);
+#endif
 
                     return (responseMessage, decryptableContent);
                 }
@@ -70,6 +111,28 @@ namespace Microsoft.Azure.Cosmos.Encryption.Custom
             }
         }
 
+#if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
+        private async Task<List<T>> ConvertResponseToDecryptableItemsAsync<T>(
+            Stream content,
+            CancellationToken token)
+        {
+            List<Stream> decryptableStreams = await StreamSplitter.SplitCollectionAsync(content, this.streamManager, token);
+            List<T> decryptableItems = new List<T>();
+
+            foreach (Stream item in decryptableStreams)
+            {
+                decryptableItems.Add(
+                    (T)(object)new DecryptableItemStream(
+                        item,
+                        this.encryptor,
+                        JsonProcessor.Stream,
+                        this.cosmosSerializer,
+                        this.streamManager));
+            }
+
+            return decryptableItems;
+        }
+#else
         private List<T> ConvertResponseToDecryptableItems<T>(
             Stream content)
         {
@@ -94,5 +157,6 @@ namespace Microsoft.Azure.Cosmos.Encryption.Custom
 
             return decryptableItems;
         }
+#endif
     }
 }
---
+++ Microsoft.Azure.Cosmos.Encryption.Custom/src/Transformation/ArrayStreamSplitter.cs
@@ -0,0 +1,180 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+#if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
+namespace Microsoft.Azure.Cosmos.Encryption.Custom.Transformation
+{
+    using System;
+    using System.Buffers;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Text.Json;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Cosmos.Encryption.Custom.RecyclableMemoryStreamMirror;
+
+    internal class ArrayStreamSplitter
+    {
+        internal int InitialBufferSize { get; set; } = 16384;
+
+        private static readonly JsonReaderOptions JsonReaderOptions = new () { AllowTrailingCommas = true, CommentHandling = JsonCommentHandling.Skip };
+
+        private static readonly ReadOnlyMemory<byte> DocumentsPropertyUtf8Bytes;
+
+        static ArrayStreamSplitter()
+        {
+            DocumentsPropertyUtf8Bytes = new Memory<byte>(Encoding.UTF8.GetBytes(Constants.DocumentsResourcePropertyName));
+        }
+
+        internal async Task<List<Stream>> SplitCollectionAsync(
+            Stream input,
+            StreamManager manager,
+            CancellationToken cancellationToken)
+        {
+            Stream readStream = input;
+            if (!input.CanSeek)
+            {
+                Stream temp = manager.CreateStream();
+                await input.CopyToAsync(temp, cancellationToken);
+                temp.Position = 0;
+                readStream = temp;
+            }
+
+            using ArrayPoolManager arrayPoolManager = new ();
+
+            byte[] buffer = arrayPoolManager.Rent(this.InitialBufferSize);
+
+            Utf8JsonWriter chunkWriter = null;
+
+            int leftOver = 0;
+            bool isFinalBlock = false;
+            bool isDocumentsArray = false;
+            RecyclableMemoryStream bufferWriter = null;
+            bool isDocumentsProperty = false;
+
+            RecyclableMemoryStreamManager recyclableMemoryStreamManager = new ();
+
+            JsonReaderState state = new (ArrayStreamSplitter.JsonReaderOptions);
+            List<Stream> outputList = new List<Stream>();
+
+            while (!isFinalBlock)
+            {
+                int dataLength = await readStream.ReadAsync(buffer.AsMemory(leftOver, buffer.Length - leftOver), cancellationToken);
+                int dataSize = dataLength + leftOver;
+                isFinalBlock = dataSize == 0;
+                long bytesConsumed = 0;
+
+                bytesConsumed = this.TransformBuffer(
+                    buffer.AsSpan(0, dataSize),
+                    outputList,
+                    isFinalBlock,
+                    ref bufferWriter,
+                    ref chunkWriter,
+                    ref state,
+                    ref isDocumentsProperty,
+                    ref isDocumentsArray,
+                    recyclableMemoryStreamManager);
+
+                leftOver = dataSize - (int)bytesConsumed;
+
+                // we need to scale out buffer
+                if (leftOver == dataSize)
+                {
+                    byte[] newBuffer = arrayPoolManager.Rent(buffer.Length * 2);
+                    buffer.AsSpan().CopyTo(newBuffer);
+                    buffer = newBuffer;
+                }
+                else if (leftOver != 0)
+                {
+                    buffer.AsSpan(dataSize - leftOver, leftOver).CopyTo(buffer);
+                }
+            }
+
+            await readStream.DisposeAsync();
+
+            return outputList;
+        }
+
+        private long TransformBuffer(Span<byte> buffer, List<Stream> outputList, bool isFinalBlock, ref RecyclableMemoryStream bufferWriter, ref Utf8JsonWriter chunkWriter, ref JsonReaderState state, ref bool isDocumentsProperty, ref bool isDocumentsArray, RecyclableMemoryStreamManager manager)
+        {
+            Utf8JsonReader reader = new Utf8JsonReader(buffer, isFinalBlock, state);
+
+            while (reader.Read())
+            {
+                Utf8JsonWriter currentWriter = chunkWriter;
+
+                JsonTokenType tokenType = reader.TokenType;
+
+                switch (tokenType)
+                {
+                    case JsonTokenType.None:
+                        break;
+                    case JsonTokenType.StartObject:
+                        if (isDocumentsArray && chunkWriter == null)
+                        {
+                            bufferWriter = new RecyclableMemoryStream(manager);
+                            chunkWriter = new Utf8JsonWriter((IBufferWriter<byte>)bufferWriter);
+                            chunkWriter?.WriteStartObject();
+                        }
+                        else
+                        {
+                            currentWriter?.WriteStartObject();
+                        }
+
+                        break;
+                    case JsonTokenType.EndObject:
+                        currentWriter?.WriteEndObject();
+                        if (reader.CurrentDepth == 2 && chunkWriter != null)
+                        {
+                            currentWriter.Flush();
+                            bufferWriter.Position = 0;
+                            outputList.Add(bufferWriter);
+
+                            bufferWriter = null;
+
+                            chunkWriter.Dispose();
+                            chunkWriter = null;
+                        }
+
+                        break;
+                    case JsonTokenType.StartArray:
+                        if (isDocumentsProperty && reader.CurrentDepth == 1)
+                        {
+                            isDocumentsArray = true;
+                        }
+
+                        currentWriter?.WriteStartArray();
+                        break;
+
+                    case JsonTokenType.EndArray:
+                        currentWriter?.WriteEndArray();
+                        if (isDocumentsArray && reader.CurrentDepth == 1)
+                        {
+                            isDocumentsArray = false;
+                            isDocumentsProperty = false;
+                        }
+
+                        break;
+
+                    case JsonTokenType.PropertyName:
+                        if (reader.ValueTextEquals(DocumentsPropertyUtf8Bytes.Span))
+                        {
+                            isDocumentsProperty = true;
+                        }
+
+                        currentWriter?.WritePropertyName(reader.ValueSpan);
+                        break;
+                    default:
+                        currentWriter?.WriteRawValue(reader.ValueSpan, true);
+                        break;
+                }
+            }
+
+            state = reader.CurrentState;
+            return reader.BytesConsumed;
+        }
+    }
+}
+#endif
