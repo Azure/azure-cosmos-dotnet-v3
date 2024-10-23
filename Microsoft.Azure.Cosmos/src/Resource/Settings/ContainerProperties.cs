89c79a21e7ba1f9e42f6f025fb7254f24843d8b4
Debdatta Kunda <87335885+kundadebdatta@users.noreply.github.com>
2024-10-22 17:49:46 -0700

    [Internal] ContainerProperties: Adds Full Text Search and Indexing Policy (#4837)
    # Pull Request Template
    
    ## Description
    
    This PR adds a new `FullTextPolicy ` in `ContainerProperties` and a new
    `FullTextIndexes` in the `IndexingPolicy` to enable Full Text Search in
    Cosmos DB.
    
    Below is a sample for the new `ContainerProperties` with Full Text
    Indexing Policy andFull Text Policy highlighted.
    
    ```
    {
    	"indexingPolicy": {
    		"automatic": true,
    		"indexingMode": "Consistent",
    		"additionalIndexPolicy": "indexpolicyvalue",
    		"includedPaths": [
    			{
    				"path": "/included/path",
    				"additionalIncludedPath": "includedPathValue",
    				"indexes": [
    
    				]
    			}
    		],
    		"excludedPaths": [
    			{
    				"path": "/excluded/path",
    				"additionalExcludedPath": "excludedPathValue"
    			}
    		],
    		"compositeIndexes": [
    			[
    				{
    					"path": "/composite/path",
    					"additionalCompositeIndex": "compositeIndexValue",
    					"order": "ascending"
    				}
    			]
    		],
    		"spatialIndexes": [
    			{
    				"path": "/spatial/path",
    				"additionalSpatialIndexes": "spatialIndexValue",
    				"types": [
    
    				]
    			}
    		],
    		"fullTextIndexes": [
    			{
    				"path": "/fullTextPath1",
    				"additionalFullTextIndex": "fullTextIndexValue1"
    			},
    			{
    				"path": "/fullTextPath2",
    				"additionalFullTextIndex": "fullTextIndexValue2"
    			},
    			{
    				"path": "/fullTextPath3"
    			}
    		]
    	},
    	"computedProperties": [
    		{
    			"name": "lowerName",
    			"query": "SELECT VALUE LOWER(c.name) FROM c"
    		},
    		{
    			"name": "estimatedTax",
    			"query": "SELECT VALUE c.salary * 0.2 FROM c"
    		}
    	],
    	"geospatialConfig": {
    		"type": "Geography",
    		"additionalGeospatialConfig": "geospatialConfigValue"
    	},
    	"uniqueKeyPolicy": {
    		"additionalUniqueKeyPolicy": "uniqueKeyPolicyValue",
    		"uniqueKeys": [
    			{
    				"paths": [
    					"/unique/key/path/1",
    					"/unique/key/path/2"
    				]
    			}
    		]
    	},
    	"conflictResolutionPolicy": {
    		"mode": "LastWriterWins",
    		"additionalConflictResolutionPolicy": "conflictResolutionValue"
    	},
    	"clientEncryptionPolicy": {
    		"includedPaths": [
    			{
    				"path": "/path",
    				"clientEncryptionKeyId": "clientEncryptionKeyId",
    				"encryptionType": "Randomized",
    				"additionalIncludedPath": "includedPathValue",
    				"encryptionAlgorithm": "AEAD_AES_256_CBC_HMAC_SHA256"
    			}
    		],
    		"policyFormatVersion": 1,
    		"additionalEncryptionPolicy": "clientEncryptionpolicyValue"
    	},
    	"id": "2a9f501b-6948-4795-8fd1-797defb5c466",
    	"partitionKey": {
    		"paths": [
    
    		],
    		"kind": "Hash"
    	},
    	"fullTextPolicy": {
    		"defaultLanguage": "en-US",
    		"fullTextPaths": [
    			{
    				"path": "/fullTextPath1",
    				"language": "en-US"
    			},
    			{
    				"path": "/fullTextPath2",
    				"language": "en-US"
    			},
    			{
    				"path": "/fullTextPath3",
    				"language": "en-US"
    			}
    		]
    	}
    }
    
    ```
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [x] New feature (non-breaking change which adds functionality)
    
    ## Closing issues
    
    To automatically close an issue: closes #4820

--- Microsoft.Azure.Cosmos/src/Resource/Settings/ContainerProperties.cs
+++ Microsoft.Azure.Cosmos/src/Resource/Settings/ContainerProperties.cs
@@ -84,6 +84,9 @@ namespace Microsoft.Azure.Cosmos
         [JsonProperty(PropertyName = "computedProperties", NullValueHandling = NullValueHandling.Ignore)]
         private Collection<ComputedProperty> computedProperties;
 
+        [JsonProperty(PropertyName = "fullTextPolicy", NullValueHandling = NullValueHandling.Ignore)]
+        private FullTextPolicy fullTextPolicyInternal;
+
         /// <summary>
         /// This contains additional values for scenarios where the SDK is not aware of new fields. 
         /// This ensures that if resource is read and updated none of the fields will be lost in the process.
@@ -358,6 +361,30 @@ namespace Microsoft.Azure.Cosmos
 
                 this.computedProperties = value;
             }
+        }
+
+        /// <summary>
+        /// Gets or sets the full text search paths for items in the container.
+        /// </summary>
+        /// <value>
+        /// It is an optional property.
+        /// By default, FullTextPolicy is set to null meaning the feature is turned off for the container.
+        /// </value>
+        /// <remarks>
+        /// <para>
+        /// The <see cref="Cosmos.FullTextPolicy"/> will be applied to all the items in the container.
+        /// </para>
+        /// </remarks>
+        [JsonIgnore]
+#if PREVIEW
+        public
+#else
+        internal
+#endif
+        FullTextPolicy FullTextPolicy
+        {
+            get => this.fullTextPolicyInternal;
+            set => this.fullTextPolicyInternal = value;
         }
 
         /// <summary>
