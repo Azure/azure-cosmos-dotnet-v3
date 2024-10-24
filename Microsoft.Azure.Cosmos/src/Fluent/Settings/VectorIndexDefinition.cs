94a9900e56a786f70dfd6ea998f0e6c49cb86818
Debdatta Kunda <dkunda@microsoft.com>
2024-10-23 17:41:13 -0700

    Code changes to fix vector index definition.

--- Microsoft.Azure.Cosmos/src/Fluent/Settings/VectorIndexDefinition.cs
+++ Microsoft.Azure.Cosmos/src/Fluent/Settings/VectorIndexDefinition.cs
@@ -58,7 +58,12 @@ namespace Microsoft.Azure.Cosmos.Fluent
         /// types DiskANN and quantizedFlat. Note that, the allowed range for this parameter is between 1 and 3.
         /// </param>
         /// <returns>An instance of the current <see cref="VectorIndexDefinition{T}"/>.</returns>
-        public VectorIndexDefinition<T> WithQuantizationByteSize(
+#if PREVIEW
+        public
+#else
+        internal
+#endif
+        VectorIndexDefinition<T> WithQuantizationByteSize(
             int quantizationByteSize)
         {
             this.vectorIndexPath.QuantizationByteSize = quantizationByteSize;
@@ -73,7 +78,12 @@ namespace Microsoft.Azure.Cosmos.Fluent
         /// This is an optional parameter and applies to index type DiskANN only. The allowed range for this parameter is between 25 and 500.
         /// </param>
         /// <returns>An instance of the current <see cref="VectorIndexDefinition{T}"/>.</returns>
-        public VectorIndexDefinition<T> WithIndexingSearchListSize(
+#if PREVIEW
+        public
+#else
+        internal
+#endif
+        VectorIndexDefinition<T> WithIndexingSearchListSize(
             int indexingSearchListSize)
         {
             this.vectorIndexPath.IndexingSearchListSize = indexingSearchListSize;
@@ -88,7 +98,12 @@ namespace Microsoft.Azure.Cosmos.Fluent
         /// applies to index types DiskANN and quantizedFlat.
         /// </param>
         /// <returns>An instance of the current <see cref="VectorIndexDefinition{T}"/>.</returns>
-        public VectorIndexDefinition<T> WithVectorIndexShardKey(
+#if PREVIEW
+        public
+#else
+        internal
+#endif        
+        VectorIndexDefinition<T> WithVectorIndexShardKey(
             string[] vectorIndexShardKey)
         {
             this.vectorIndexPath.VectorIndexShardKey = vectorIndexShardKey ?? throw new ArgumentNullException(nameof(vectorIndexShardKey));
