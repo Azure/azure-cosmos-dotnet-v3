8ce362205c636927889ab36f13fb95b9d3468dc6
Jan Hyka <jahyka@microsoft.com>
2024-10-24 08:05:20 +0200

    ~ fix the mess after merge

--- Microsoft.Azure.Cosmos.Encryption.Custom/src/EncryptionProcessor.cs
+++ Microsoft.Azure.Cosmos.Encryption.Custom/src/EncryptionProcessor.cs
@@ -29,7 +29,6 @@ namespace Microsoft.Azure.Cosmos.Encryption.Custom
         internal static readonly CosmosJsonDotNetSerializer BaseSerializer = new (JsonSerializerSettings);
 
 #if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
-        private static readonly StreamProcessor StreamProcessor = new ();
         private static readonly JsonWriterOptions JsonWriterOptions = new () { SkipValidation = true };
         private static readonly StreamProcessor StreamProcessor = new ();
 #endif
@@ -114,10 +113,6 @@ namespace Microsoft.Azure.Cosmos.Encryption.Custom
         }
 #endif
 
-            await EncryptionProcessor.StreamProcessor.EncryptStreamAsync(input, output, encryptor, encryptionOptions, cancellationToken);
-        }
-#endif
-
         /// <remarks>
         /// If there isn't any data that needs to be decrypted, input stream will be returned without any modification.
         /// Else input stream will be disposed, and a new stream is returned.
@@ -167,8 +162,6 @@ namespace Microsoft.Azure.Cosmos.Encryption.Custom
             {
                 JsonProcessor.Newtonsoft => await DecryptAsync(input, encryptor, diagnosticsContext, cancellationToken),
 #if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
-                JsonProcessor.Stream => await DecryptStreamAsync(input, encryptor, diagnosticsContext, cancellationToken),
-                JsonProcessor.SystemTextJson => await DecryptJsonNodeAsync(input, encryptor, diagnosticsContext, cancellationToken),
                 JsonProcessor.Stream => await DecryptStreamAsync(input, encryptor, diagnosticsContext, cancellationToken),
 #endif
                 _ => throw new InvalidOperationException("Unsupported Json Processor")
@@ -237,109 +230,6 @@ namespace Microsoft.Azure.Cosmos.Encryption.Custom
             await input.DisposeAsync();
             return context;
         }
-#endif
-
-#if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
-        public static async Task<(Stream, DecryptionContext)> DecryptJsonNodeAsync(
-            Stream input,
-            Stream output,
-            Encryptor encryptor,
-            CosmosDiagnosticsContext diagnosticsContext,
-            JsonProcessor jsonProcessor,
-            CancellationToken cancellationToken)
-        {
-            if (input == null)
-            {
-                return null;
-            }
-
-            if (jsonProcessor != JsonProcessor.Stream)
-            {
-                throw new NotSupportedException($"Streaming mode is only allowed for {nameof(JsonProcessor.Stream)}");
-            }
-
-            Debug.Assert(input.CanSeek);
-            Debug.Assert(output.CanWrite);
-            Debug.Assert(output.CanSeek);
-            Debug.Assert(encryptor != null);
-            Debug.Assert(diagnosticsContext != null);
-            input.Position = 0;
-
-            EncryptionPropertiesWrapper properties = await System.Text.Json.JsonSerializer.DeserializeAsync<EncryptionPropertiesWrapper>(input, cancellationToken: cancellationToken);
-            input.Position = 0;
-            if (properties?.EncryptionProperties == null)
-            {
-                await input.CopyToAsync(output, cancellationToken: cancellationToken);
-                return null;
-            }
-
-            DecryptionContext context;
-#pragma warning disable CS0618 // Type or member is obsolete
-            if (properties.EncryptionProperties.EncryptionAlgorithm == CosmosEncryptionAlgorithm.MdeAeadAes256CbcHmac256Randomized)
-            {
-                context = await StreamProcessor.DecryptStreamAsync(input, output, encryptor, properties.EncryptionProperties, diagnosticsContext, cancellationToken);
-            }
-            else if (properties.EncryptionProperties.EncryptionAlgorithm == CosmosEncryptionAlgorithm.AEAes256CbcHmacSha256Randomized)
-            {
-                (Stream stream, context) = await DecryptAsync(input, encryptor, diagnosticsContext, cancellationToken);
-                await stream.CopyToAsync(output, cancellationToken);
-                output.Position = 0;
-            }
-            else
-            {
-                input.Position = 0;
-                throw new NotSupportedException($"Encryption Algorithm: {properties.EncryptionProperties.EncryptionAlgorithm} is not supported.");
-            }
-#pragma warning restore CS0618 // Type or member is obsolete
-
-            if (context == null)
-            {
-                input.Position = 0;
-                return null;
-            }
-
-            await input.DisposeAsync();
-            return context;
-        }
-#endif
-
-#if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
-        public static async Task<(Stream, DecryptionContext)> DecryptStreamAsync(
-            Stream input,
-            Encryptor encryptor,
-            CosmosDiagnosticsContext diagnosticsContext,
-            CancellationToken cancellationToken)
-        {
-            if (input == null)
-            {
-                return (input, null);
-            }
-
-            Debug.Assert(input.CanSeek);
-            Debug.Assert(encryptor != null);
-            Debug.Assert(diagnosticsContext != null);
-            input.Position = 0;
-
-            EncryptionPropertiesWrapper properties = await System.Text.Json.JsonSerializer.DeserializeAsync<EncryptionPropertiesWrapper>(input, cancellationToken: cancellationToken);
-            input.Position = 0;
-            if (properties?.EncryptionProperties == null)
-            {
-                return (input, null);
-            }
-
-            MemoryStream ms = new ();
-
-            DecryptionContext context = await StreamProcessor.DecryptStreamAsync(input, ms, encryptor, properties.EncryptionProperties, diagnosticsContext, cancellationToken);
-            if (context == null)
-            {
-                input.Position = 0;
-                return (input, null);
-            }
-
-            await input.DisposeAsync();
-            return (ms, context);
-        }
-
 #endif
 
 #if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
