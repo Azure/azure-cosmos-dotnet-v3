283148ff4481b096225352c3cf05a585252e53fd
Dikshi Bahl <DikshiBahl@microsoft.com>
2024-11-11 10:27:21 -0600

    CrossRegionHedging: Fixes - add additional unit test

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/AvailabilityStrategyUnitTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Tests/AvailabilityStrategyUnitTests.cs
@@ -1,15 +1,16 @@
 ﻿namespace Microsoft.Azure.Cosmos.Tests
 {
     using System;
-    using System.Collections.Concurrent;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.IO;
     using System.Net.Http;
+    using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos;
     using Microsoft.Azure.Documents;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+    using System.Net;
     /// <summary>
     /// Tests for <see cref="AvailabilityStrategy"/>
     /// </summary>
@@ -58,5 +59,43 @@
                 Assert.AreEqual(httpRequest.DatabaseId, clone.DatabaseId);
             }
         }
+
+        [TestMethod]
+        public async Task CancellationTokenThrowsExceptionTest()
+        {
+            //Arrange
+            CrossRegionHedgingAvailabilityStrategy availabilityStrategy = new CrossRegionHedgingAvailabilityStrategy(
+                                                                   threshold: TimeSpan.FromMilliseconds(100),
+                                                                   thresholdStep: TimeSpan.FromMilliseconds(50));
+
+            RequestMessage request = new RequestMessage
+            {
+                ResourceType = ResourceType.Document,
+                OperationType = OperationType.Read
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            cts.Cancel();
+            
+            Collection<AccountRegion> readableLocations = new Collection<AccountRegion>();
+            AccountProperties databaseAccount = new AccountProperties()
+            {
+                ReadLocationsInternal = new Collection<AccountRegion>()
+                {
+                    { new AccountRegion() { Name = "US East", Endpoint = new Uri("https://location1.documents.azure.com").ToString() } },
+                    { new AccountRegion() { Name = "US West", Endpoint = new Uri("https://location2.documents.azure.com").ToString() } },
+                    
+                }
+            };
+            using CosmosClient mockCosmosClient = MockCosmosUtil.CreateMockCosmosClient();
+            mockCosmosClient.DocumentClient.GlobalEndpointManager.InitializeAccountPropertiesAndStartBackgroundRefresh(databaseAccount);
+
+            Func<RequestMessage, CancellationToken, Task<ResponseMessage>> sender = (request, token) => throw new OperationCanceledException("operation cancellation requested");
+
+            CosmosOperationCanceledException cancelledException = await Assert.ThrowsExceptionAsync<CosmosOperationCanceledException>(() =>
+                       availabilityStrategy.ExecuteAvailabilityStrategyAsync(sender, mockCosmosClient, request, cts.Token));
+        }
+
+       
     }
 }
