48bc50447ad705a22ad4fffd35a36bafcf4dc735
Achint-Agrawal <45819170+Achint-Agrawal@users.noreply.github.com>
2025-01-14 04:37:03 +0530

    ThroughputBucketing: Fixes ThroughputBucket to always be internal (#4960)
    # Pull Request Template
    
    ## Description
    Throughput bucketing is not currently released publicly. This PR makes
    ThroughputBucket internal. Will expose it in preview SDK once throughput
    bucketing is deployed in prod.
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [x] Bug fix (non-breaking change which fixes an issue)

--- Microsoft.Azure.Cosmos/src/RequestOptions/RequestOptions.cs
+++ Microsoft.Azure.Cosmos/src/RequestOptions/RequestOptions.cs
@@ -124,12 +124,7 @@ namespace Microsoft.Azure.Cosmos
         /// <see cref="RequestOptions.ThroughputBucket"/> cannot be set in RequestOptions.
         /// </remarks>
         /// <seealso href="https://aka.ms/cosmsodb-bucketing"/>
-#if PREVIEW
-        public
-#else
-        internal
-#endif
-        int? ThroughputBucket { get; set; }
+        internal int? ThroughputBucket { get; set; }
 
         /// <summary>
         /// Fill the CosmosRequestMessage headers with the set properties
