c1cd8270e094af9aab7622d463bd3c0a1349b145
Nalu Tripician <27316859+NaluTripician@users.noreply.github.com>
2025-03-17 17:16:38 -0700

    Update CosmosMultiHashTest.cs
    Added additional test coverage

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/CosmosMultiHashTest.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/CosmosMultiHashTest.cs
@@ -533,5 +533,27 @@
 
             Assert.AreEqual(1, feedResponse.Count());
         }
+
+        [TestMethod]
+        public async Task ReadManyAllNullPkValueTest()
+        {
+            Document doc = new Document { Id = "readMany" };
+
+            await this.container.CreateItemAsync<Document>(doc);
+
+            Cosmos.PartitionKey pk = new PartitionKeyBuilder()
+                .AddNoneType()
+                .AddNoneType()
+                .Build();
+
+            ItemResponse<Document> ir = await this.container.ReadItemAsync<Document>("readMany", pk);
+            Assert.IsNotNull(ir.Resource);
+            Assert.AreEqual(ir.StatusCode, HttpStatusCode.OK);
+
+            FeedResponse<Document> feedResponse = await this.container.ReadManyItemsAsync<Document>(
+                new List<(string, Cosmos.PartitionKey)> { ("readMany", pk) });
+            
+            Assert.AreEqual(1, feedResponse.Count());
+        }
     }
 }
