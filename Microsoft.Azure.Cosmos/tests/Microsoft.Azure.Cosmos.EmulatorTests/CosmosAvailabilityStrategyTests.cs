f1ba29e5d7fec38765dcad1a89628bfb1b210ac6
Nalu Tripician <27316859+NaluTripician@users.noreply.github.com>
2025-04-24 13:03:08 -0700

    Update CosmosAvailabilityStrategyTests.cs

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/CosmosAvailabilityStrategyTests.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.EmulatorTests/CosmosAvailabilityStrategyTests.cs
@@ -801,7 +801,7 @@ namespace Microsoft.Azure.Cosmos.SDK.EmulatorTests
                             Assert.IsNotNull(traceDiagnostic);
                             traceDiagnostic.Value.Data.TryGetValue("Hedge Context", out hedgeContext);
                             Assert.IsNotNull(hedgeContext);
-                            Assert.IsTrue(((List<string>)hedgeContext).Contains(region3));
+                            Assert.IsTrue(((IReadOnlyCollection<string>)hedgeContext).Contains(region3));
                         }
 
                         break;
