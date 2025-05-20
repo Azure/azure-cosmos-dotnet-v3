211580764c07c703db15174a09047b25b19d17fb
Kiran Kumar Kolli <kirankk@microsoft.com>
2025-05-20 12:36:13 -0700

    Fixing the issue

--- AOTTestSample/Program.cs
+++ AOTTestSample/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // dotnet publish -c Release -r linux-x64
+using Microsoft.Azure.Cosmos;
+
 Console.WriteLine("Hello, World!");
+
+CosmosClient cosmosClient = new CosmosClient("", "");
+Console.WriteLine($"Using endpoint: {cosmosClient.Endpoint}");
