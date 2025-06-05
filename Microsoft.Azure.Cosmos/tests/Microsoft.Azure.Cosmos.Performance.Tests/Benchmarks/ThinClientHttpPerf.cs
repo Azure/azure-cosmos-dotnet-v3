bbdfd5842befc0f55d5e6e696d47e610f4fee844
Arooshi Avasthy (from Dev Box) <aavasthy@microsoft.com>
2025-06-05 13:24:34 -0700

    Add new perf test thinclient.

--- Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Benchmarks/ThinClientHttpPerf.cs
+++ Microsoft.Azure.Cosmos/tests/Microsoft.Azure.Cosmos.Performance.Tests/Benchmarks/ThinClientHttpPerf.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.Azure.Cosmos
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+namespace Microsoft.Azure.Cosmos.Benchmarks
 {
     using System;
     using System.Collections.Generic;
