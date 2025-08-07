8d44827990abc38189b4965f0b2f2fee66f0b3f9
Kiran Kumar Kolli <kirankk@microsoft.com>
2025-08-07 13:18:56 -0700

    Fixing Error type

--- Microsoft.Azure.Cosmos/src/direct/Error.cs
+++ Microsoft.Azure.Cosmos/src/direct/Error.cs
@@ -3,7 +3,7 @@
 //------------------------------------------------------------
 namespace Microsoft.Azure.Documents
 {
-    using Newtonsoft.Json;
+    using System.Text.Json.Serialization;
 
     /// <summary> 
     /// Encapsulates error related details in the Azure Cosmos DB service.
@@ -13,7 +13,7 @@ namespace Microsoft.Azure.Documents
 #else
     public
 #endif
-    class Error : Resource
+    class Error : PlainResource
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="T:Microsoft.Azure.Documents.Error"/> class for the Azure Cosmos DB service.
@@ -27,60 +27,20 @@ namespace Microsoft.Azure.Documents
         /// Gets or sets the textual description of error code in the Azure Cosmos DB service.
         /// </summary>
         /// <value>The textual description of error code.</value>
-        [JsonProperty(PropertyName = Constants.Properties.Code)]
-        public string Code
-        {
-            get
-            {
-                return base.GetValue<string>(Constants.Properties.Code);
-            }
-            set
-            {
-                base.SetValue(Constants.Properties.Code, value);
-            }
-        }
+        [JsonPropertyName(Constants.Properties.Code)]
+        public string Code { get; set; }
 
         /// <summary>
         /// Gets or sets the error message in the Azure Cosmos DB service.
         /// </summary>
         /// <value>The error message.</value>
-        [JsonProperty(PropertyName = Constants.Properties.Message)]
-        public string Message
-        {
-            get
-            {
-                return base.GetValue<string>(Constants.Properties.Message);
-            }
-            set
-            {
-                base.SetValue(Constants.Properties.Message, value);
-            }
-        }
+        [JsonPropertyName(Constants.Properties.Message)]
+        public string Message { get; set; }
 
-        [JsonProperty(PropertyName = Constants.Properties.ErrorDetails)]
-        internal string ErrorDetails //Debug Only.
-        {
-            get
-            {
-                return base.GetValue<string>(Constants.Properties.ErrorDetails);
-            }
-            set
-            {
-                base.SetValue(Constants.Properties.ErrorDetails, value);
-            }
-        }
+        [JsonPropertyName(Constants.Properties.ErrorDetails)]
+        internal string ErrorDetails { get; set; }
 
-        [JsonProperty(PropertyName = Constants.Properties.AdditionalErrorInfo)]
-        internal string AdditionalErrorInfo
-        {
-            get
-            {
-                return base.GetValue<string>(Constants.Properties.AdditionalErrorInfo);
-            }
-            set
-            {
-                base.SetValue(Constants.Properties.AdditionalErrorInfo, value);
-            }
-        }
+        [JsonPropertyName(Constants.Properties.AdditionalErrorInfo)]
+        internal string AdditionalErrorInfo { get; set; }
     }
 }
