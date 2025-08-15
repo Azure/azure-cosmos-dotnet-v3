e2f7a881aa7f70dae6a46999b8a5bb4fc7348eec
Adam Novak <125261085+adamnova@users.noreply.github.com>
2025-08-15 10:29:43 +0200

    Cleanup

--- Microsoft.Azure.Cosmos.Encryption/tests/Microsoft.Azure.Cosmos.Encryption.Tests/EncryptionProcessorTests.cs
+++ Microsoft.Azure.Cosmos.Encryption/tests/Microsoft.Azure.Cosmos.Encryption.Tests/EncryptionProcessorTests.cs
@@ -26,27 +26,28 @@ namespace Microsoft.Azure.Cosmos.Encryption.Tests
         #region Shared Test Utilities
 
     // Thin wrappers so existing partial classes can call these helpers.
-    protected static MemoryStream ToStream(string json) => StreamTestHelpers.ToStream(json);
-    protected static string ReadToEnd(Stream s) => StreamTestHelpers.ReadToEnd(s);
+    protected static MemoryStream ToStream(string json)
+    {
+        return StreamTestHelpers.ToStream(json);
+    }
+    protected static string ReadToEnd(Stream s)
+    {
+        return StreamTestHelpers.ReadToEnd(s);
+    }
 
         private static EncryptionSettings CreateSettingsWithNoProperties()
         {
-            // Create an EncryptionSettings instance without invoking its private constructor
-            // and set PropertiesToEncrypt to an empty enumerable so no work is performed.
-            object settings = FormatterServices.GetUninitializedObject(typeof(EncryptionSettings));
-
-            var prop = typeof(EncryptionSettings).GetField("<PropertiesToEncrypt>k__BackingField", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-            prop.SetValue(settings, Array.Empty<string>());
-
-            return (EncryptionSettings)settings;
+            // Use the internal constructor normally; leaving the mapping empty results in
+            // PropertiesToEncrypt being an empty enumeration (no encryption work performed).
+            return new EncryptionSettings("rid", new List<string> { "/id" });
         }
 
         private static EncryptionSettings CreateSettingsForId()
         {
-            var settings = new EncryptionSettings("rid", new List<string> { "/id" });
+            EncryptionSettings settings = new EncryptionSettings("rid", new List<string> { "/id" });
             // Use an uninitialized container; it won't be used in the failure paths these tests exercise.
-            var container = (EncryptionContainer)FormatterServices.GetUninitializedObject(typeof(EncryptionContainer));
-            var forProperty = new EncryptionSettingForProperty(
+            EncryptionContainer container = (EncryptionContainer)FormatterServices.GetUninitializedObject(typeof(EncryptionContainer));
+            EncryptionSettingForProperty forProperty = new EncryptionSettingForProperty(
                 clientEncryptionKeyId: "cek1",
                 encryptionType: Mde.EncryptionType.Deterministic,
                 encryptionContainer: container,
@@ -56,13 +57,15 @@ namespace Microsoft.Azure.Cosmos.Encryption.Tests
         }
 
         private static Mde.AeadAes256CbcHmac256EncryptionAlgorithm CreateDeterministicAlgorithm()
-            => TestCryptoHelpers.CreateAlgorithm(Mde.EncryptionType.Deterministic);
+        {
+            return TestCryptoHelpers.CreateAlgorithm(Mde.EncryptionType.Deterministic);
+        }
 
         private static EncryptionSettings CreateSettingsWithInjected(string propertyName, Mde.AeadAes256CbcHmac256EncryptionAlgorithm algorithm)
         {
-            var settings = new EncryptionSettings("rid", new List<string> { "/id" });
-            var container = (EncryptionContainer)FormatterServices.GetUninitializedObject(typeof(EncryptionContainer));
-            var forProperty = new EncryptionSettingForProperty(
+            EncryptionSettings settings = new EncryptionSettings("rid", new List<string> { "/id" });
+            EncryptionContainer container = (EncryptionContainer)FormatterServices.GetUninitializedObject(typeof(EncryptionContainer));
+            EncryptionSettingForProperty forProperty = new EncryptionSettingForProperty(
                 clientEncryptionKeyId: "cek1",
                 encryptionType: Mde.EncryptionType.Deterministic,
                 encryptionContainer: container,
@@ -74,13 +77,16 @@ namespace Microsoft.Azure.Cosmos.Encryption.Tests
 
         private static EncryptionSettings CreateSettings(string prop, Mde.AeadAes256CbcHmac256EncryptionAlgorithm algo)
         {
-            var settings = new EncryptionSettings("rid", new List<string> { "/id" });
-            var container = (EncryptionContainer)FormatterServices.GetUninitializedObject(typeof(EncryptionContainer));
+            EncryptionSettings settings = new EncryptionSettings("rid", new List<string> { "/id" });
+            EncryptionContainer container = (EncryptionContainer)FormatterServices.GetUninitializedObject(typeof(EncryptionContainer));
             settings.SetEncryptionSettingForProperty(prop, new EncryptionSettingForProperty("cek1", Mde.EncryptionType.Deterministic, container, "dbRid", algo));
             return settings;
         }
 
-        private static Mde.AeadAes256CbcHmac256EncryptionAlgorithm Algo() => TestCryptoHelpers.CreateAlgorithm(Mde.EncryptionType.Deterministic);
+        private static Mde.AeadAes256CbcHmac256EncryptionAlgorithm Algo()
+        {
+            return TestCryptoHelpers.CreateAlgorithm(Mde.EncryptionType.Deterministic);
+        }
 
     // (Removed local KEK shim; rely on TestHelpers.TestCryptoHelpers instead.)
 
