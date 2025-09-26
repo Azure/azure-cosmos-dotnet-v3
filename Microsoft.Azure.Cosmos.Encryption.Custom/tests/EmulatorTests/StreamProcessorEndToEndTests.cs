db67e7c5d6962c712ff2483b008f433052b43289
Adam Novak <125261085+adamnova@users.noreply.github.com>
2025-09-26 12:05:11 +0200

    Cleanup

--- Microsoft.Azure.Cosmos.Encryption.Custom/tests/EmulatorTests/StreamProcessorEndToEndTests.cs
+++
@@ -1,4 +0,0 @@
-#if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
-// Placeholder removed content; tests merged into MdeCustomEncryptionTests.
-namespace Microsoft.Azure.Cosmos.Encryption.Custom.EmulatorTests { }
-#endif
