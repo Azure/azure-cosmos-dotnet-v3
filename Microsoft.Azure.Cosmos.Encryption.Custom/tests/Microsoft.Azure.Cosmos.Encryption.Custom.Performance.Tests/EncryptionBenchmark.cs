d752e0a358b7b419f11c3e98811704134f21bb8d
adamnova <125261085+adamnova@users.noreply.github.com>
2025-10-15 12:19:52 +0200

    [Client encryption]: Removes Encryption.Custom compression (#5399)
    # Pull Request Template
    
    ## Description
    
    Eliminated all code, tests, and API surface related to payload
    compression, including BrotliCompressor, CompressionOptions, and
    associated logic and metadata. The encryption format version and
    properties no longer reference compression, simplifying the SDK and its
    usage.
    
    #4678
    
    ---------
    
    Co-authored-by: Kiran Kumar Kolli <kirankk@microsoft.com>

--- Microsoft.Azure.Cosmos.Encryption.Custom/tests/Microsoft.Azure.Cosmos.Encryption.Custom.Performance.Tests/EncryptionBenchmark.cs
+++ Microsoft.Azure.Cosmos.Encryption.Custom/tests/Microsoft.Azure.Cosmos.Encryption.Custom.Performance.Tests/EncryptionBenchmark.cs
@@ -32,10 +32,7 @@
         [Params(1, 10, 100)]
         public int DocumentSizeInKb { get; set; }
 
-        [Params(CompressionOptions.CompressionAlgorithm.None, CompressionOptions.CompressionAlgorithm.Brotli)]
-        public CompressionOptions.CompressionAlgorithm CompressionAlgorithm { get; set; }
-
-#if NET8_0_OR_GREATER
+#if ENCRYPTION_CUSTOM_PREVIEW && NET8_0_OR_GREATER
         [Params(JsonProcessor.Newtonsoft, JsonProcessor.Stream)]
 #else
         [Params(JsonProcessor.Newtonsoft)]
@@ -130,10 +127,6 @@
                 DataEncryptionKeyId = "dekId",
                 EncryptionAlgorithm = CosmosEncryptionAlgorithm.MdeAeadAes256CbcHmac256Randomized,
                 PathsToEncrypt = TestDoc.PathsToEncrypt,
-                CompressionOptions = new()
-                {
-                    Algorithm = this.CompressionAlgorithm
-                },
                 JsonProcessor = this.JsonProcessor,
             };
 
