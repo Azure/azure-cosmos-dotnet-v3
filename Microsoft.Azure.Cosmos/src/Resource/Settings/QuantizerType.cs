8a2c3c63d9e1e080994bf66f8cc35bf6aff9875c
Arooshi Avasthy <aavasthy@microsoft.com>
2025-11-17 21:24:02 -0800

    Fix comments

--- Microsoft.Azure.Cosmos/src/Resource/Settings/QuantizerType.cs
+++ Microsoft.Azure.Cosmos/src/Resource/Settings/QuantizerType.cs
@@ -4,7 +4,13 @@
 namespace Microsoft.Azure.Cosmos
 {
     using System.Runtime.Serialization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
 
+    /// <summary>
+    /// Defines the quantizer type of a vector index path specification in the Azure Cosmos DB service.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
 #if PREVIEW
     public
 #else
@@ -12,9 +18,15 @@ namespace Microsoft.Azure.Cosmos
 #endif
     enum QuantizerType
     {
+        /// <summary>
+        /// Represents a product quantizer type.
+        /// </summary>
         [EnumMember(Value = "product")]
         Product,
 
+        /// <summary>
+        /// Represents a spherical quantizer type.
+        /// </summary>
         [EnumMember(Value = "spherical")]
         Spherical
     }
