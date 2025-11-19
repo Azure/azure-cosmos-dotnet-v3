287073a5e2a3a00de2f85d5b8cb8595de281ad92
Nalu Tripician <27316859+NaluTripician@users.noreply.github.com>
2025-11-19 15:02:44 -0800

    Update EncryptionContainer.cs

--- Microsoft.Azure.Cosmos.Encryption/src/EncryptionContainer.cs
+++ Microsoft.Azure.Cosmos.Encryption/src/EncryptionContainer.cs
@@ -732,14 +732,17 @@ namespace Microsoft.Azure.Cosmos.Encryption
         }
 
 #if ENCRYPTIONPREVIEW
-
         public override Task<SemanticRerankResult> SemanticRerankAsync(
             string rerankContext,
             IEnumerable<string> documents,
             IDictionary<string, object> options = null,
             CancellationToken cancellationToken = default)
         {
-            throw NotImplementedException("Semantic Reranking is not supported in the Encryption package);
+            return this.Container.SemanticRerankAsync(
+                rerankContext,
+                documents,
+                options,
+                cancellationToken);
         }
 
         public override async Task<ResponseMessage> DeleteAllItemsByPartitionKeyStreamAsync(
