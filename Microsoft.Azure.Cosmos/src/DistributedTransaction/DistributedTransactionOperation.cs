d6a69c8ee8e14a7290f3a89d768057ffa80552ed
Meghana Palaparthi <palaparthimeghana@gmail.com>
2026-01-27 00:36:21 -0600

    Users/meghana palaparthi/test (#5573)
    # Pull Request Template
    
    ## Description
    
    Please include a summary of the change and which issue is fixed. Include
    samples if adding new API, and include relevant motivation and context.
    List any dependencies that are required for this change.
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [] Bug fix (non-breaking change which fixes an issue)
    - [] New feature (non-breaking change which adds functionality)
    - [] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [] This change requires a documentation update
    
    ## Closing issues
    
    To automatically close an issue: closes #IssueNumber

---
+++ Microsoft.Azure.Cosmos/src/DistributedTransaction/DistributedTransactionOperation.cs
@@ -0,0 +1,74 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// ------------------------------------------------------------
+
+namespace Microsoft.Azure.Cosmos
+{
+    using System;
+    using System.IO;
+
+    //using Microsoft.Azure.Documents;
+
+    /// <summary>
+    /// Represents an operation on a document whichwill be executed as a part of a distributed transaction.
+    /// </summary>
+    internal class DistributedTransactionOperation
+    {
+        public DistributedTransactionOperation(
+            Documents.OperationType operationType,
+            int operationIndex,
+            string database,
+            string container,
+            PartitionKey partitionKey,
+            string id = null)
+        {
+            this.OperationType = operationType;
+            this.OperationIndex = operationIndex;
+            this.PartitionKey = partitionKey;
+            this.Database = database;
+            this.Container = container;
+            this.Id = id;
+        }
+
+        public PartitionKey PartitionKey { get; internal set; }
+
+        public string Database { get; internal set; }
+
+        public string Container { get; internal set; }
+
+        public Documents.OperationType OperationType { get; internal set; }
+
+        public int OperationIndex { get; internal set; } 
+
+        public string Id { get; internal set; }
+    }
+
+    internal class DistributedTransactionOperation<T> : DistributedTransactionOperation
+    {
+        public DistributedTransactionOperation(
+            Documents.OperationType operationType,
+            int operationIndex,
+            string database,
+            string container,
+            PartitionKey partitionKey,
+            T resource)
+            : base(operationType, operationIndex, database, container, partitionKey)
+        {
+            this.Resource = resource;
+        }
+
+        public DistributedTransactionOperation(
+            Documents.OperationType operationType,
+            int operationIndex,
+            string database,
+            string container,
+            PartitionKey partitionKey,
+            string id,
+            T resource)
+            : base(operationType, operationIndex, database, container, partitionKey, id)
+        {
+            this.Resource = resource;
+        }
+        public T Resource { get; internal set; }
+    }
+}
