f3b5ebffb59f77bddb427739400d5ad19a582ff8
Arooshi Avasthy <113193425+aavasthy@users.noreply.github.com>
2026-02-12 16:05:05 -0800

    [Direct package version update to 3.42.0]: Adds direct package version 3.42.0 and fixes EnableNRegionSynchronousCommit flag (#5610)
    # Pull Request Template
    
    ## Description
    
    Direct package changes in version 3.42.0:
    
    - for QueryPlan support
    https://msdata.visualstudio.com/CosmosDB/_git/CosmosDB/pullrequest/1909154
    - Json patch for semantics
    https://msdata.visualstudio.com/CosmosDB/_git/CosmosDB/pullrequest/1898070
    - N-Region Synchronous
    https://msdata.visualstudio.com/CosmosDB/_git/CosmosDB/pullrequest/1913007
    - QuorumReader/ConsistencyWriter not yielding early enough on repeated
    429s
    https://msdata.visualstudio.com/CosmosDB/_git/CosmosDB/pullrequest/1667829
    
    This PR also fixes the breaking changes for StoreCLient and
    StoreClientFactory creation as the AccountConfigurationProperties flag
    got removed.
    Refactors N-Region Synchronous Commit feature to use
    IServiceConfigurationReaderVNext interface instead of
    AccountConfigurationProperties, enabling Routing Gateway and Compute
    Gateway to properly consume this account-level feature.
    
    - Moved EnableNRegionSynchronousCommit from
    AccountConfigurationProperties to IServiceConfigurationReader.
    - Refactored V3 Code to use the boolean flag from
    IServiceConfigurationReader.
    - Removed dependency from AccountConfigurationProperties
    
    Test results for end to end test with msdata/direct branch against
    n-region flag enabled account.
    <img width="543" height="118" alt="image"
    src="https://github.com/user-attachments/assets/6bdb0e49-dcd4-4919-b052-0c30ae807e99"
    />
    <img width="353" height="141" alt="image"
    src="https://github.com/user-attachments/assets/9babd8d1-2751-40f9-8476-b24688758f6e"
    />
    
    ## Type of change
    
    Please delete options that are not relevant.
    
    - [X] New feature (non-breaking change which adds functionality)
    
    ## Closing issues
    
    To automatically close an issue: closes #5450

--- Microsoft.Azure.Cosmos/src/DocumentClient.cs
+++ Microsoft.Azure.Cosmos/src/DocumentClient.cs
@@ -6792,8 +6792,6 @@ namespace Microsoft.Azure.Cosmos
 
         private void CreateStoreModel(bool subscribeRntbdStatus)
         {
-            AccountConfigurationProperties accountConfigurationProperties = new (EnableNRegionSynchronousCommit: this.accountServiceConfiguration.AccountProperties.EnableNRegionSynchronousCommit);
-
             //EnableReadRequestsFallback, if not explicity set on the connection policy,
             //is false if the account's consistency is bounded staleness,
             //and true otherwise.
@@ -6808,8 +6806,7 @@ namespace Microsoft.Azure.Cosmos
                 this.UseMultipleWriteLocations && (this.accountServiceConfiguration.DefaultConsistencyLevel != Documents.ConsistencyLevel.Strong),
                 true,
                 enableReplicaValidation: this.isReplicaAddressValidationEnabled,
-                sessionRetryOptions: this.ConnectionPolicy.SessionRetryOptions,
-                accountConfigurationProperties: accountConfigurationProperties);
+                sessionRetryOptions: this.ConnectionPolicy.SessionRetryOptions);
 
             if (subscribeRntbdStatus)
             {
