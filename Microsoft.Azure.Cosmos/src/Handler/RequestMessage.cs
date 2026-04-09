f7b64a54e0889ae674e8d1723893e3f0c549bfe0
Kiran Kumar Kolli <kirankk@microsoft.com>
2026-04-09 15:21:30 -0700

    Exceptionless: Adds enabling exception less 403 (#5757)
    Excptionless: Adds enabling exception less 403

--- Microsoft.Azure.Cosmos/src/Handler/RequestMessage.cs
+++ Microsoft.Azure.Cosmos/src/Handler/RequestMessage.cs
@@ -296,6 +296,7 @@ namespace Microsoft.Azure.Cosmos
 
                 serviceRequest.UseStatusCodeForFailures = true;
                 serviceRequest.UseStatusCodeFor429 = true;
+                serviceRequest.UseStatusCodeFor403 = true;
                 serviceRequest.UseStatusCodeForBadRequest = true;
                 serviceRequest.Properties = this.Properties;
                 this.DocumentServiceRequest = serviceRequest;
