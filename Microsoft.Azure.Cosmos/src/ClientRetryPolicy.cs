b8897f9cbe384f7554be28bb66a79c0f85023d1c
Arooshi Avasthy <aavasthy@microsoft.com>
2026-07-10 11:33:30 -0700

    Add AAD feature flag

--- Microsoft.Azure.Cosmos/src/ClientRetryPolicy.cs
+++ Microsoft.Azure.Cosmos/src/ClientRetryPolicy.cs
@@ -539,7 +539,8 @@ namespace Microsoft.Azure.Cosmos
                 return this.ShouldRetryOnUnavailableEndpointStatusCodes();
             }
 
-            if (statusCode == HttpStatusCode.Unauthorized
+            if (ConfigurationManager.IsAadTokenRevocationEnabled()
+                && statusCode == HttpStatusCode.Unauthorized
                 && (subStatusCode == SubStatusCodes.AadTokenRevoked
                     || !string.IsNullOrEmpty(wwwAuthenticateHeaderValue)))
             {
